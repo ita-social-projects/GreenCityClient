3c3f545a66ccb9317d7f9a49570c6378603949a5
Nazar Stasyuk <nazar.stasyuk@gmail.com>
2019-08-25 11:35:13 +0300

    [issue - 55] Finish work with sign-in

--- src/app/component/user/auth/sign-up/sign-up.component.spec.ts
+++ src/app/component/user/auth/sign-up/sign-up.component.spec.ts
@@ -1,6 +1,6 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { SignUpComponent } from './sign-up.component';
+import {SignUpComponent} from './sign-up.component';
 
 describe('SignUpComponent', () => {
   let component: SignUpComponent;
@@ -8,9 +8,9 @@ describe('SignUpComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SignUpComponent ]
+      declarations: [SignUpComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
