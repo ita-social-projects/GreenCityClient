1f083ca9367c879e5d53d92febcebe8adb3e67f6
RostyslavKhasanov <rostyk.khasanov@gmail.com>
2019-08-30 12:12:08 +0300

    [issue - 62] lllllllllll

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,10 +1,17 @@
 import {Injectable} from '@angular/core';
 import {BaseService} from './base.service';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {UserForListDtoModel} from '../model/UserForListDto.model';
 import {PageableDtoModel} from '../model/PageableDto.model';
 import {UserPageableDtoModel} from '../model/UserPageableDto.model';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-type': 'application/json',
+    Authorization: 'klmlk'
+  })
+};
 
 @Injectable({
   providedIn: 'root'
@@ -16,7 +23,7 @@ export class UserService extends BaseService {
   }
 
   getAllUsers(paginationSettings: string): Observable<UserPageableDtoModel> {
-    return this.http.get<UserPageableDtoModel>(`${this.apiUrl}` + paginationSettings);
+    return this.http.get<UserPageableDtoModel>(`${this.apiUrl}` + paginationSettings, httpOptions);
   }
 
   updateUserStatus(id: number, userStatus: string) {
