72d43c2ed416157ec215265a07e204accdacfed3
MarianMilian <milanmarian@mail.ru>
2019-09-03 13:19:00 +0300

    [Issue-100] merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,25 +11,12 @@ import {SignInComponent} from './component/user/auth/sign-in/sign-in.component';
 import {SubmitEmailComponent} from './component/user/auth/submit-email/submit-email.component';
 import {RouterModule} from '@angular/router';
 import {router} from './router';
-import {HttpClientModule} from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
 import {AgmCoreModule} from '@agm/core';
 import {MapComponent} from './component/user/map/map.component';
 import {Ng2SearchPipeModule} from 'ng2-search-filter';
-import {AppComponent} from "./app.component";
-import {AdminComponent} from "./component/admin/admin.component";
-import {UserComponent} from "./component/user/user.component";
-import {GeneralComponent} from "./component/general/general.component";
-import {SignUpComponent} from "./component/user/auth/sign-up/sign-up.component";
-import {NavBarComponent} from "./component/user/nav-bar/nav-bar.component";
-import {AuthComponent} from "./component/user/auth/auth.component";
-import {SignInComponent} from "./component/user/auth/sign-in/sign-in.component";
-import {SubmitEmailComponent} from "./component/user/auth/submit-email/submit-email.component";
-import {RouterModule} from "@angular/router";
-import {router} from "./router";
-import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
-import {FormsModule} from "@angular/forms";
-import {InterceptorService} from "./service/interceptor.service";
+import {InterceptorService} from './service/interceptor.service';
 
 
 @NgModule({
