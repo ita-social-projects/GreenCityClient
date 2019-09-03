6c1d18b8fc646a1cf50ed6da05873c438f8ebce0
Nazar Stasyuk <nazar.stasyuk@gmail.com>
2019-09-03 14:23:56 +0300

    [issue - 70] Add func that redirect user to auth page if refresh token is expired

--- src/app/links.ts
+++ src/app/links.ts
@@ -1,5 +1,9 @@
+//back-end
 export const mailLink = "http://localhost:8080/";
 export const userOwnSecurityLink = mailLink + "ownSecurity/";
 export const userOwnSignUpLink = userOwnSecurityLink + "signUp/";
 export const userOwnSignInLink = userOwnSecurityLink + "signIn/";
 export const updateAccessTokenLink = userOwnSecurityLink + "updateAccessToken/";
+//front-end
+export const frontMailLink = "http://localhost:4200/";
+export const frontAuthLink = frontMailLink + "auth/";
