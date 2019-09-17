d5b58089f0471dcdeabe7d0272976d522bd9f529
studroma <studroma@gmail.com>
2019-09-17 19:20:23 +0300

    Added FilterService to store filtering params

--- src/app/component/filter/filter.component.ts
+++ src/app/component/filter/filter.component.ts
@@ -5,6 +5,7 @@ import {MapComponent} from '../user/map/map.component';
 import {FilterPlaceDtoModel} from '../../model/filtering/filter-place-dto.model';
 import {FilterDiscountDtoModel} from '../../model/filtering/filter-discount-dto.model';
 import {PlaceStatus} from '../../model/placeStatus.model';
+import {FilterPlaceService} from '../../service/filtering/filter-place.service';
 
 @Component({
   selector: 'app-filter',
@@ -12,9 +13,7 @@ import {PlaceStatus} from '../../model/placeStatus.model';
   styleUrls: ['./filter.component.css']
 })
 export class FilterComponent {
-  filter: FilterPlaceDtoModel;
-  discountMin = 0;
-  discountMax = 100;
+  filterDto: FilterPlaceDtoModel;
   options: Options = {
     floor: 0,
     ceil: 100,
@@ -22,13 +21,19 @@ export class FilterComponent {
     noSwitching: true
   };
 
-  constructor(private placeService: PlaceService, private mapComponent: MapComponent) {
+  constructor(private placeService: PlaceService,
+              private mapComponent: MapComponent,
+              private filterService: FilterPlaceService) {
   }
 
   applyFilters() {
     const discount = new FilterDiscountDtoModel(
-      this.mapComponent.category, this.mapComponent.specification, this.discountMin, this.discountMax);
-    this.filter = new FilterPlaceDtoModel(PlaceStatus.APPROVED, this.mapComponent.mapBounds, discount);
-    this.placeService.getFilteredPlaces(this.filter).subscribe((res) => this.mapComponent.place = res);
+      this.mapComponent.category,
+      this.mapComponent.specification,
+      this.filterService.discountMin,
+      this.filterService.discountMax);
+    console.log(this.options.floor);
+    this.filterDto = new FilterPlaceDtoModel(PlaceStatus.APPROVED, this.mapComponent.mapBounds, discount);
+    this.placeService.getFilteredPlaces(this.filterDto).subscribe((res) => this.mapComponent.place = res);
   }
 }
