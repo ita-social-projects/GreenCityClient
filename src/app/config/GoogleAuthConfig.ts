4f6e59081220fdd003238612e340c7a20a868b20
Nazar Stasyuk <nazar.stasyuk@gmail.com>
2019-09-20 15:34:38 +0300

    [issue - 156] add google key

--- src/app/config/GoogleAuthConfig.ts
+++ src/app/config/GoogleAuthConfig.ts
@@ -3,7 +3,7 @@ import {AuthServiceConfig, GoogleLoginProvider} from 'angularx-social-login';
 const config = new AuthServiceConfig([
   {
     id: GoogleLoginProvider.PROVIDER_ID,
-    provider: new GoogleLoginProvider('17831249917-e96sv9o6q5tp4shpem2dc5fbpr1b7p33.apps.googleusercontent.com')
+    provider: new GoogleLoginProvider('129513550972-ffqpdq6e5basbn9pcdvroqf20ffcg09f.apps.googleusercontent.com')
   }
 ]);
 
