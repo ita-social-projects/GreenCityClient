621584f199950c50b208aa4403b0c919c4d1dd82
ZakharSkaletskyi <sklzakhar@gmail.com>
2019-09-25 19:31:09 +0300

    sorting users for admin save for all session

--- src/app/component/admin/users/users.component.ts
+++ src/app/component/admin/users/users.component.ts
@@ -31,24 +31,26 @@ export class UsersComponent implements OnInit {
   flag = true;
   selectedColumnToSort: string;
   selectedDirectionToSort: string;
+  sortColumn = 'email';
+  sortDirection = 'asc';
 
-  sortParam = '&sort=' + this.adminService.staticSortColumn + ',' + this.adminService.staticSortDirection;
+  sortParam = '&sort=' + this.sortColumn + ',' + this.sortDirection;
   userEmail = this.userService.getUserEmail();
   displayedColumns: string[] = ['email', 'firstName', 'lastName', 'dateOfRegistration', 'role', 'block', 'deactivate'];
 
   constructor(
-    private userService: UserService, private titleService: Title, private ngFlashMessageService: NgFlashMessageService,
-    private adminService: AdminService) {
+    private userService: UserService, private titleService: Title, private ngFlashMessageService: NgFlashMessageService) {
   }
 
   @ViewChild(MatSort, {static: true}) sort: MatSort;
 
   ngOnInit() {
+    this.getFromLocalStorage();
     this.getRoles();
-    console.log('1=' + this.adminService.staticSortColumn + ' 2=' + this.adminService.staticSortDirection);
-    console.log('1=' + AdminService.sortColumn + ' 2=' + AdminService.sortDirection);
+    console.log('1=' + this.sortColumn + ' 2=' + this.sortDirection);
+    console.log('1=' + this.sortColumn + ' 2=' + this.sortDirection);
 
-    this.sortData(this.adminService.staticSortColumn, this.adminService.staticSortDirection);
+    this.sortData(this.sortColumn, this.sortDirection);
     this.titleService.setTitle('Admin - Users');
   }
 
@@ -84,7 +86,7 @@ export class UsersComponent implements OnInit {
       this.successfulAction('Role for ' + email + ' is updated to ' + role.substr(5));
     }, (error: HttpErrorResponse) => {
       this.errorMessage(error.error.message);
-      this.sortData(this.adminService.staticSortColumn, this.adminService.staticSortDirection);
+      this.sortData(this.sortColumn, this.sortDirection);
     });
   }
 
@@ -119,8 +121,8 @@ export class UsersComponent implements OnInit {
 
   sortData(columnToSort: string, direction: string) {
     this.sortParam = '&sort=' + columnToSort + ',' + direction;
-    this.selectedColumnToSort = columnToSort;
-    this.selectedDirectionToSort = direction;
+    this.sortColumn = columnToSort;
+    this.sortDirection = direction;
     this.filterByRegex(this.sortParam);
   }
 
@@ -134,12 +136,12 @@ export class UsersComponent implements OnInit {
       this.page = res.currentPage;
       this.totalItems = res.totalElements;
       this.dataSource.data = this.users;
-      AdminService.staticSortColumn = this.selectedColumnToSort;
-      AdminService.staticSortDirection = this.selectedDirectionToSort;
-      AdminService.sortDirection = this.selectedDirectionToSort;
-      AdminService.sortColumn = this.selectedColumnToSort;
+      // AdminService.staticSortColumn = this.selectedColumnToSort;
+      // AdminService.staticSortDirection = this.selectedDirectionToSort;
+      // AdminService.sortDirection = this.selectedDirectionToSort;
+      // AdminService.sortColumn = this.selectedColumnToSort;
       console.log('sortc&d=' + AdminService.sortColumn + ' ' + AdminService.sortDirection);
-
+      this.saveToLocalStorage();
     });
   }
 
@@ -152,6 +154,22 @@ export class UsersComponent implements OnInit {
     } else {
       this.flag = true;
       this.sortData(this.selectedColumnToSort, this.selectedDirectionToSort);
+    }
+  }
+
+  saveToLocalStorage() {
+    window.localStorage.setItem('sortColumn', this.sortColumn);
+    window.localStorage.setItem('sortDirection', this.sortDirection);
+    window.localStorage.setItem('page', String(this.page));
+    window.localStorage.setItem('totalItems', String(this.totalItems));
+  }
+
+  getFromLocalStorage() {
+    if (window.localStorage.getItem('sortColumn') !== null) {
+      this.sortColumn = window.localStorage.getItem('sortColumn');
+      this.sortDirection = window.localStorage.getItem('sortDirection');
+      this.page = Number(window.localStorage.getItem('page'));
+      this.totalItems = Number(window.localStorage.getItem('totalItems'));
     }
   }
 }
