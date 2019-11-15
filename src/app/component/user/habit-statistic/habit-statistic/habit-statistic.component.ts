7bef42c71f51affdc6da8cb39bae4c7af3fd955c
Oleh Kopylchak <oleg12311999@gmail.com>
2019-11-15 13:47:59 +0200

    Added localization for habit-statistic component

--- src/app/component/user/habit-statistic/habit-statistic/habit-statistic.component.ts
+++ src/app/component/user/habit-statistic/habit-statistic/habit-statistic.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Photo} from '../../../../model/photo/photo';
 import {HabitStatisticDto} from '../../../../model/habit/HabitStatisticDto';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-habit-statistic',
@@ -8,17 +9,20 @@ import {HabitStatisticDto} from '../../../../model/habit/HabitStatisticDto';
   styleUrls: ['./habit-statistic.component.css']
 })
 export class HabitStatisticComponent implements OnInit {
-
   @Input()
+  habitMessageObservable: Observable<string>;
   habitMessage: string;
   @Input()
-  advice: string;
+  adviceObservable: Observable<string>;
   @Input()
   itemIcon: Photo;
   @Input()
   habitStatisticDto: HabitStatisticDto;
+  advice: string;
 
   ngOnInit() {
+    this.adviceObservable.subscribe(advice => this.advice = advice);
+    this.habitMessageObservable.subscribe(habitMessage => this.habitMessage = habitMessage);
   }
 
 }
--- src/app/component/user/habit-statistic/tracked-habit-statistic.component.ts
+++ src/app/component/user/habit-statistic/tracked-habit-statistic.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {HabitStatisticService} from '../../../service/habit-statistic/habit-statistic.service';
 import {HabitStatisticDto} from '../../../model/habit/HabitStatisticDto';
 import {DayEstimation} from '../../../model/habit/DayEstimation';
+import {TranslateService} from '@ngx-translate/core';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-tracked-habit-statistic',
@@ -13,16 +15,26 @@ export class TrackedHabitStatisticComponent implements OnInit {
   activeCoffeeIcon = {name: 'assets/img/icon/coffee.png'};
 
   packageStatisticDto: HabitStatisticDto;
-  coffeeStatisticDto: HabitStatisticDto;
+  cupStatisticDto: HabitStatisticDto;
 
-  constructor(private service: HabitStatisticService) {
+  packageAdvice: Observable<string>;
+  cupAdvice: Observable<string>;
+
+  packageMessage: Observable<string>;
+  cupMessage: Observable<string>;
+
+  constructor(private service: HabitStatisticService, private translate: TranslateService) {
   }
 
   ngOnInit() {
     this.service.getTrackedHabits().subscribe(data => {
       this.packageStatisticDto = data[0];
-      this.coffeeStatisticDto = data[1];
+      this.cupStatisticDto = data[1];
     });
-  }
 
+    this.packageAdvice = this.translate.get('advice.package-advice');
+    this.cupAdvice = this.translate.get('advice.cup-advice');
+    this.packageMessage = this.translate.get('habit-message.package');
+    this.cupMessage = this.translate.get('habit-message.cup');
+  }
 }
