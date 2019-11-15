0313ea4bfc324cb9295db564b1039b386e2587e2
Oleh Kopylchak <oleg12311999@gmail.com>
2019-11-15 23:36:27 +0200

    Localization for pagination

--- src/app/component/admin/users/users.component.ts
+++ src/app/component/admin/users/users.component.ts
@@ -8,6 +8,7 @@ import {MatIconRegistry, MatSort, MatTableDataSource} from '@angular/material';
 import {HttpErrorResponse} from '@angular/common/http';
 import {AdminService} from '../../../service/admin/admin.service';
 import {PaginationComponent} from 'ngx-bootstrap';
+import {TranslateService} from '@ngx-translate/core';
 
 
 export interface Role {
@@ -38,12 +39,14 @@ export class UsersComponent implements OnInit {
   displayedColumns: string[] = ['email', 'firstName', 'lastName', 'dateOfRegistration', 'role', 'block', 'deactivate'];
   maxSizePagination = 6;
   sortArrow: string;
+  roleForTranslation: string;
+  isUpdatedToTranslation: string;
   @ViewChild('paginationElement', {static: false}) paginationComponent: PaginationComponent;
   @ViewChild(MatSort, {static: true}) sort: MatSort;
 
   constructor(
     private userService: UserService, private titleService: Title, private ngFlashMessageService: NgFlashMessageService,
-    iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    private translation: TranslateService, iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
     iconRegistry.addSvgIcon(
       'arrow-up',
       sanitizer.bypassSecurityTrustResourceUrl('assets/img/icon/arrows/arrow-up-bold.svg'));
@@ -59,6 +62,13 @@ export class UsersComponent implements OnInit {
     this.sortParam = '&sort=' + this.sortColumn + ',' + this.sortDirection;
     this.sortData(this.sortColumn, this.sortDirection);
     this.titleService.setTitle('Admin - Users');
+
+    this.translation.get('users.role-for').subscribe(translation => {
+      this.roleForTranslation = translation;
+    });
+    this.translation.get('users.is-updated-to').subscribe(translation => {
+      this.isUpdatedToTranslation = translation;
+    });
   }
 
   getCurrentPaginationSettings(sort: string): string {
@@ -90,7 +100,7 @@ export class UsersComponent implements OnInit {
 
   changeRole(id: number, role: string, email: string) {
     this.userService.updateUserRole(id, role).subscribe((data) => {
-      this.successfulAction('Role for ' + email + ' is updated to ' + role.substr(5));
+      this.successfulAction(' ' + this.roleForTranslation + ' ' + email + ' ' + this.isUpdatedToTranslation + ' ' + role.substr(5));
     }, (error: HttpErrorResponse) => {
       this.errorMessage(error.error.message);
       this.sortData(this.sortColumn, this.sortDirection);
@@ -182,6 +192,7 @@ export class UsersComponent implements OnInit {
       }
     }
   }
+
   setPaginationPageButtonsToCurrent() {
     this.paginationComponent.selectPage(this.page + 1);
   }
