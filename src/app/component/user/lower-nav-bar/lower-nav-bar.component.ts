1fc5ad17c593e7268c8bab4cc259fa8c0ebebbf4
hrufon <33417744+hrufon@users.noreply.github.com>
2019-12-25 12:48:59 +0200

    Add files via upload

---
+++ src/app/component/user/lower-nav-bar/lower-nav-bar.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { LocalStorageService } from 'src/app/service/localstorage/local-storage.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-lower-nav-bar',
+  templateUrl: './lower-nav-bar.component.html',
+  styleUrls: ['./lower-nav-bar.component.css']
+})
+export class LowerNavBarComponent implements OnInit {
+  readonly logo = 'assets/img/logo.png';
+
+  userId: number;
+
+  constructor(private localStorageService: LocalStorageService) { }
+
+  ngOnInit() { this.localStorageService.userIdBehaviourSubject.subscribe(userId => this.userId = userId); }
+
+  scrollTo(elementId) {
+    const element = document.getElementById(elementId);
+    element.scrollIntoView({ block: 'center', behavior: 'smooth' });
+  }
+}
