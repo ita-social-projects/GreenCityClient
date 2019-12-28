e85b1e1d32c52e0d3a7606f207bef7722dd6d8e3
nomadness <vitalii.skolozdra@gmail.com>
2019-12-28 15:55:53 +0200

    Logic for subscription

--- src/app/component/general/homepage/subscribe/subscribe.component.ts
+++ src/app/component/general/homepage/subscribe/subscribe.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SubscriptionService } from 'src/app/service/subscription/subscription.service';
 
 @Component({
   selector: 'app-subscribe',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubscribeComponent implements OnInit {
 
+  private readonly emailRegex = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
   readonly qrCode = 'assets/img/qr-code2.png';
 
-  constructor() { }
+  emailTouched: boolean;
+  emailValid: boolean;
 
-  ngOnInit() { }
+  email: string;
+
+  constructor(private subscriptionService: SubscriptionService) { }
+
+  ngOnInit() {
+    this.emailTouched = false;
+  }
+
+  validateEmail() {
+    this.emailTouched = true;
+    if (this.emailRegex.test(this.email) && this.email.length > 0) {
+      this.emailValid = true;
+    } else {
+      this.emailValid = false;
+    }
+  }
+
+  subscribe() {
+    if (this.emailValid) {
+      this.subscriptionService.subscribeToNewsletter(this.email);
+    }
+  }
 }
