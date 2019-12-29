cad5e07eff8d743a8816f68527030c4143714936
nomadness <vitalii.skolozdra@gmail.com>
2019-12-29 17:59:13 +0200

    Handle undefined state while loading

--- src/app/component/general/homepage/eco-events/eco-events.component.ts
+++ src/app/component/general/homepage/eco-events/eco-events.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NewsService } from 'src/app/service/news/news.service';
 import { NewsDto } from 'src/app/service/news/NewsDto';
 import { of } from 'rxjs';
@@ -10,10 +10,10 @@ import { catchError } from 'rxjs/operators';
   templateUrl: './eco-events.component.html',
   styleUrls: ['./eco-events.component.css']
 })
-export class EcoEventsComponent implements OnInit {
+export class EcoEventsComponent implements OnInit, OnDestroy {
   readonly eventImg = 'assets/img/main-event-placeholder.png';
   readonly arrow = 'assets/img/icon/arrow.png';
-  latestNews: NewsDto[];
+  latestNews: NewsDto[] = [];
 
   constructor(private newsService: NewsService, private languageService: LanguageService) { }
 
@@ -22,15 +22,19 @@ export class EcoEventsComponent implements OnInit {
     this.newsService.latestNewsSubject.pipe(
       catchError(() => of([]))
     ).subscribe(
-      data => {
-        data.forEach(el => el.creationDate = this.convertDate(el.creationDate));
-        this.latestNews = data;
+      (newsItems: NewsDto[]) => {
+        newsItems.forEach(el => el.creationDate = this.convertDate(el.creationDate));
+        this.latestNews = newsItems;
       },
       error => { throw error; }
     );
   }
 
-  convertDate(date: string): string {
+  ngOnDestroy() {
+    this.newsService.latestNewsSubject.unsubscribe();
+  }
+
+  private convertDate(date: string): string {
     const localizedMonth = this.languageService.getLocalizedMonth(new Date(date).getMonth());
     const formattedDate = new Date(date).getDay()
       + ' '
--- src/app/service/news/NewsDto.ts
+++ src/app/service/news/NewsDto.ts
@@ -3,4 +3,10 @@ export class NewsDto {
   title: string;
   text: string;
   creationDate: string;
+  constructor(id: number, title: string, text: string, creationDate: string) {
+    this.id = id;
+    this.title = title;
+    this.text = text;
+    this.creationDate = creationDate;
+  }
 }
