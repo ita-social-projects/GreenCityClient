85fe220733f5c5a76278affda016a251801684bc
shevtsiv <rostykshevtsiv@gmail.com>
2020-01-09 11:36:09 -0800

    Implement homepage habit items statistics representation
    By now only habit item names and according amount
    of not taken items are queried from the server, but in the
    future releases this behavior should be changed for
    full server-side pollution of habit statistics, including
    all the fields in `homePageHabitStats` array, localized
    according to the user's localization.

--- src/app/component/general/homepage/homepage/homepage.component.ts
+++ src/app/component/general/homepage/homepage/homepage.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { LocalStorageService } from 'src/app/service/localstorage/local-storage.service';
+import {UserService} from "../../../../service/user/user.service";
 
 @Component({
   selector: 'app-homepage',
@@ -9,17 +10,25 @@ import { LocalStorageService } from 'src/app/service/localstorage/local-storage.
 })
 export class HomepageComponent implements OnInit {
 
+  usersAmount: number;
   readonly guyImage = 'assets/img/guy.png';
   readonly path2 = 'assets/img/path-2.svg';
   readonly path4 = 'assets/img/path-4_3.png';
   readonly path5 = 'assets/img/path-5.png';
 
-  constructor(private router: Router, private localStorageService: LocalStorageService) { }
+  constructor(
+    private router: Router,
+    private localStorageService: LocalStorageService,
+    private userService: UserService
+  ) { }
 
   userId: number;
 
   ngOnInit() {
     this.localStorageService.userIdBehaviourSubject.subscribe(userId => this.userId = userId);
+    this.userService.countActivatedUsers().subscribe(num => {
+      this.usersAmount = num;
+    });
   }
 
   startHabit() {
