5fc2073a2b0879b0dec5354ccd1c3af2e1eea154
Oleh Kopylchak <oleg12311999@gmail.com>
2020-01-13 16:22:30 +0200

    Added request param for getting localized eco news.

--- src/app/service/news/news.service.ts
+++ src/app/service/news/news.service.ts
@@ -1,22 +1,24 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { NewsDto } from './NewsDto';
-import { catchError } from 'rxjs/operators';
-import { of, BehaviorSubject } from 'rxjs';
-import { latestNewsLink } from 'src/app/links';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {NewsDto} from './NewsDto';
+import {catchError} from 'rxjs/operators';
+import {of, BehaviorSubject} from 'rxjs';
+import {latestNewsLink} from 'src/app/links';
+import {LanguageService} from '../../i18n/language.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewsService {
-  private dataStore: { latestNews: NewsDto[] } = { latestNews: [] };
+  private dataStore: { latestNews: NewsDto[] } = {latestNews: []};
   private latestNewsSubject = new BehaviorSubject<NewsDto[]>([]);
   readonly latestNews = this.latestNewsSubject.asObservable();
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private languageService: LanguageService) {
+  }
 
   loadLatestNews() {
-    const $http = this.http.get<NewsDto[]>(latestNewsLink);
+    const $http = this.http.get<NewsDto[]>(`${latestNewsLink}?language=${this.languageService.getCurrentLanguage()}`);
     $http.pipe(
       catchError(() => of([]))
     ).subscribe(
@@ -24,7 +26,9 @@ export class NewsService {
         this.dataStore.latestNews = data;
         this.latestNewsSubject.next(Object.assign({}, this.dataStore).latestNews);
       },
-      error => { throw error; }
+      error => {
+        throw error;
+      }
     );
   }
 }
