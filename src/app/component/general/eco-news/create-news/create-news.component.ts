a452092fdfcfed3fdea4d9bea239a3acaff51478
RomanDeveloperAcc <roma04dan@gmail.com>
2020-02-24 14:59:37 +0200

    changed app.module and router

---
+++ src/app/component/general/eco-news/create-news/create-news.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-news',
+  templateUrl: './create-news.component.html',
+  styleUrls: ['./create-news.component.css']
+})
+export class CreateNewsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/component/general/eco-news/eco-news.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-eco-news',
+  templateUrl: './eco-news.component.html',
+  styleUrls: ['./eco-news.component.css']
+})
+export class EcoNewsComponent implements OnInit {
+
+  constructor(private titleService: Title) { }
+
+  ngOnInit() {
+    this.titleService.setTitle("Eco News");
+  }
+
+}
