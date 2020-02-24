a9367070b60a9e945e0d1296eefb939a9052379e
RomanDeveloperAcc <roma04dan@gmail.com>
2020-02-24 14:55:46 +0200

    transfered data from previous branch

---
+++ src/app/component/general/eco-news/news-list/news-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-news-list',
+  templateUrl: './news-list.component.html',
+  styleUrls: ['./news-list.component.css']
+})
+export class NewsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
