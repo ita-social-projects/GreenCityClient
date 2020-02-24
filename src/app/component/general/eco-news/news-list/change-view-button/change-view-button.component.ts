a540fd25d6c14cfaf89c4d278c6f78f7cadeff68
Ruslan Hrybchuk <58165964+RuslanHrybchuk@users.noreply.github.com>
2020-02-24 15:49:41 +0200

    мок

---
+++ src/app/component/general/eco-news/news-list/change-view-button/change-view-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-change-view-button',
+  templateUrl: './change-view-button.component.html',
+  styleUrls: ['./change-view-button.component.css']
+})
+export class ChangeViewButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
