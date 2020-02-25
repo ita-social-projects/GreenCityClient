5c30f92684086a432656681c69d3af9c84a9b93f
Ruslan Hrybchuk <58165964+RuslanHrybchuk@users.noreply.github.com>
2020-02-25 11:48:37 +0200

    News filter + view buttons

---
+++ src/app/component/general/eco-news/filter-news/filter-news.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-news',
+  templateUrl: './filter-news.component.html',
+  styleUrls: ['./filter-news.component.css']
+})
+export class FilterNewsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
