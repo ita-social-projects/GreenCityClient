9e84ed25a7e3bd4629926fb5b1204eff69606d1e
Ruslan Hrybchuk <58165964+RuslanHrybchuk@users.noreply.github.com>
2020-02-26 16:32:52 +0200

    visuals for filter buttons (done)

--- src/app/component/general/eco-news/filter-news/filter-news.component.ts
+++ src/app/component/general/eco-news/filter-news/filter-news.component.ts
@@ -1,15 +1,78 @@
+
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-filter-news',
   templateUrl: './filter-news.component.html',
   styleUrls: ['./filter-news.component.css']
+  
 })
+
 export class FilterNewsComponent implements OnInit {
+
+  styleGrid = {
+    news: false,
+    events: false,
+    courses: false,
+    initiatives: false,
+    ads: false
+  }
+
+  newsClick() {
+    this.styleGrid.news = true;
+  }
+
+  newsClickClose(event){
+    this.styleGrid.news = false;
+    event.stopPropagation();
+  }
+
+  eventsClick() {
+    this.styleGrid.events = true;
+  }
+
+  eventsClickClose(event){
+    this.styleGrid.events = false;
+    event.stopPropagation();
+  }
+
+  coursesClick() {
+    this.styleGrid.courses = true;
+  }
+
+  coursesClickClose(event){
+    this.styleGrid.courses = false;
+    event.stopPropagation();
+  }
+
+  initiativesClick() {
+    this.styleGrid.initiatives = true;
+  }
+
+  initiativesClickClose(event){
+    this.styleGrid.initiatives = false;
+    event.stopPropagation();
+  }
+
+  adsClick() {
+    this.styleGrid.ads = true;
+  }
+
+  adsClickClose(event){
+    this.styleGrid.ads = false;
+    event.stopPropagation();
+  }
+    // element.classList.remove('ul-eco-buttons-li');
+    // element.className  = "clicked-filter-button";
+
+    // //element.selector = "close-animation";
+    // let x = true;
 
   constructor() { }
 
   ngOnInit() {
+    
   }
 
 }
