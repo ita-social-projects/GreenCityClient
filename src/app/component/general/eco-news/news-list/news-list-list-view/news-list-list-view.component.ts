676adf498d2082139f92a079ae8d75a464997a7b
KimAT25 <kimat60@gmail.com>
2020-02-28 10:34:36 +0200

    added view length for title and text in econews page

--- src/app/component/general/eco-news/news-list/news-list-list-view/news-list-list-view.component.ts
+++ src/app/component/general/eco-news/news-list/news-list-list-view/news-list-list-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-news-list-list-view',
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./news-list-list-view.component.css']
 })
 export class NewsListListViewComponent implements OnInit {
-
-  constructor() { }
+  changedTitleLength: string;
+  changedTextLength: string;
+  @Input() id: string;
+  @Input() imagePath: string;
+  @Input() title: string;
+  @Input() text: string;
+  @Input() name: string;
+  @Input() tag: string;
+  @Input() creationDate: number;
+  constructor() {}
 
   ngOnInit() {
+    this.changeLength(this.title, this.text);
   }
-
+  changeLength(title: string, text: string) {
+    if (title.length > 70) {
+      this.changedTitleLength = title.slice(0, 70);
+      console.log(this.changedTitleLength.length);
+    }
+    if (text.length > 200) {
+      this.changedTextLength = text.slice(0, 200);
+      console.log(this.changedTextLength.length);
+    }
+  }
 }
