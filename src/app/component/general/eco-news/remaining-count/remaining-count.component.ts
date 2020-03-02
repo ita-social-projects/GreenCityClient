7028a26972ef6a68e993b2ccd0eaab024d81b1d3
Volodymyr Nakutnyi <volodymyrnakutnyi@gmail.com>
2020-03-02 16:31:26 +0200

    fixed filter and add component remainCount

--- src/app/component/general/eco-news/remaining-count/remaining-count.component.ts
+++ src/app/component/general/eco-news/remaining-count/remaining-count.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-remaining-count',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./remaining-count.component.css']
 })
 export class RemainingCountComponent implements OnInit {
-
+@Input() remainsCount: number;
   constructor() { }
 
   ngOnInit() {
--- src/app/pipe/filter-ecoNews-pipe/filter-eco-news.pipe.ts
+++ src/app/pipe/filter-ecoNews-pipe/filter-eco-news.pipe.ts
@@ -6,8 +6,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class FilterEcoNewsPipe implements PipeTransform {
 
   transform(ecoNewsArray: any, newsFilter:Array<string>): any { 
+    if(newsFilter.length!==0){
+      return ecoNewsArray.filter(elem=>newsFilter.includes(elem.tag));
+    }else{
+      return ecoNewsArray;
+    }
     
-    return ecoNewsArray.filter(elem=>elem.tag.includes(newsFilter))
+   
     
   }
 
