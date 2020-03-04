b0e6d030880e662df6f3602bc929981cf5e35753
Ruslan Hrybchuk <58165964+ruslanhrybchuk@users.noreply.github.com>
2020-03-04 16:11:25 +0200

    filter-news refactoring

--- src/app/component/general/eco-news/filter-news/filter-news.component.ts
+++ src/app/component/general/eco-news/filter-news/filter-news.component.ts
@@ -4,90 +4,21 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
   selector: 'app-filter-news',
   templateUrl: './filter-news.component.html',
   styleUrls: ['./filter-news.component.css']
-
 })
 
 export class FilterNewsComponent implements OnInit {
 
-  @Output() gridOutput = new EventEmitter<Array<string>>();
-
-  private emitter():void {
-    this.gridOutput.emit(this.emitTrueFilterValues());
-  }
-
-  private emitTrueFilterValues() :Array<string> {
-      let trueFilterValuesArray = [];
-
-      for(let i in this.styleGrid){
-        if (this.styleGrid[i] === true){
-          trueFilterValuesArray.push(i);
-        }
-      }
-    return trueFilterValuesArray;
-  }
+  private filters = ['news', 'events', 'courses', 'initiatives', 'ads'];
 
   styleGrid = {
-    news: false,
+    news: true,
     events: false,
     courses: false,
     initiatives: false,
     ads: false
-  }
+  };
 
-  newsClick() {
-    this.styleGrid.news = true;
-    this.emitter();
-  }
-
-  newsClickClose(event){
-    this.styleGrid.news = false;
-    event.stopPropagation();
-    this.emitter();
-  }
-
-  eventsClick() {
-    this.styleGrid.events = true;
-    this.emitter();
-  }
-
-  eventsClickClose(event){
-    this.styleGrid.events = false;
-    event.stopPropagation();
-    this.emitter();
-  }
-
-  coursesClick() {
-    this.styleGrid.courses = true;
-    this.emitter();
-  }
-
-  coursesClickClose(event){
-    this.styleGrid.courses = false;
-    event.stopPropagation();
-    this.emitter();
-  }
-
-  initiativesClick() {
-    this.styleGrid.initiatives = true;
-    this.emitter();
-  }
-
-  initiativesClickClose(event){
-    this.styleGrid.initiatives = false;
-    event.stopPropagation();
-    this.emitter();
-  }
-
-  adsClick() {
-    this.styleGrid.ads = true;
-    this.emitter();
-  }
-
-  adsClickClose(event){
-    this.styleGrid.ads = false;
-    event.stopPropagation();
-    this.emitter();
-  }
+  @Output() gridOutput = new EventEmitter<Array<string>>();
 
   constructor() { }
 
@@ -95,4 +26,29 @@ export class FilterNewsComponent implements OnInit {
     this.emitter();
   }
 
+  private emitter(): void {
+    this.gridOutput.emit(this.emitTrueFilterValues());
+  }
+
+  private emitTrueFilterValues(): Array<string> {
+      const trueFilterValuesArray = [];
+
+      for (const i in this.styleGrid) {
+        if (this.styleGrid[i] === true) {
+          trueFilterValuesArray.push(i);
+        }
+      }
+      return trueFilterValuesArray;
+  }
+
+  private selectFilter(currentFilter: string): void {
+    this.styleGrid[currentFilter] = true;
+    this.emitter();
+  }
+
+  private deselectFilter(unselectFilter: string, event): void {
+    this.styleGrid[unselectFilter] = false;
+    event.stopPropagation();
+    this.emitter();
+  }
 }
