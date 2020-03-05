912aba2406ddf18ff8aa67c0798294d91c7b2ac1
Volodymyr Nakutnyi <volodymyrnakutnyi@gmail.com>
2020-03-05 16:38:51 +0200

    add little formating

--- src/app/component/general/eco-news/news-list/news-list.component.ts
+++ src/app/component/general/eco-news/news-list/news-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, EventEmitter, Input, OnDestroy } from '@angular/core';
 import { EcoNewsService } from 'src/app/service/eco-news/eco-news.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-news-list',
@@ -15,6 +16,7 @@ export class NewsListComponent implements OnInit, OnDestroy {
   @Input() num: number;
   @Output() quantity = new EventEmitter<number>();
   @Output() current = new EventEmitter<number>();
+  private ecoNewsSubscription: Subscription;
   public allEcoNews = [];
   private temp = -1;
   elements = [];
@@ -23,26 +25,29 @@ export class NewsListComponent implements OnInit, OnDestroy {
   constructor(private ecoNewsService: EcoNewsService) { }
 
   ngOnInit() {
-    this.ecoNewsService
-      .getAllEcoNews()
-      .subscribe(data => {
-        this.allEcoNews = data;
-        console.log(this.allEcoNews.length)
-        this.quantity.emit(this.allEcoNews.length);
-        this.elements = this.allEcoNews.splice(0, 12);
-        this.remaining =this.allEcoNews.length-this.elements.length;
-        this.toggler = true;
-      });
+    this.fetchAllEcoNews();
+  }
+  private fetchAllEcoNews(): void {
+    this.ecoNewsSubscription = this.ecoNewsService
+    .getAllEcoNews()
+    .subscribe(data => {
+      this.allEcoNews = data;
+      console.log(this.allEcoNews.length)
+      this.quantity.emit(this.allEcoNews.length);
+      this.elements = this.allEcoNews.splice(0, 12);
+      this.remaining = this.allEcoNews.length - this.elements.length;
+      this.toggler = true;
+    });
   }
 
 
   ngOnChanges() {
-    if (this.toggler && this.temp<this.num) {
+    if (this.toggler && this.temp < this.num) {
       this.temp = this.num;
       this.addElements();
     }
     this.current.emit(this.elements.length);
-    this.remaining =this.allEcoNews.length-this.elements.length;
+    this.remaining = this.allEcoNews.length - this.elements.length;
   }
 
   addElements() {
@@ -50,8 +55,7 @@ export class NewsListComponent implements OnInit, OnDestroy {
     let k;
     if (this.allEcoNews.length - this.elements.length > 11) {
       k = 11;
-    }
-    else {
+    } else {
       k = this.allEcoNews.length - this.elements.length;
     }
     for (let i = 0; i < k; i++) {
@@ -63,7 +67,7 @@ export class NewsListComponent implements OnInit, OnDestroy {
   chageView(event: boolean) {
     this.view = event;
   }
-  ngOnDestroy(){
-    
+  ngOnDestroy() {
+    this.ecoNewsSubscription.unsubscribe();
   }
 }
