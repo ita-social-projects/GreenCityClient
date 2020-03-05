34151a3a93b6aab98046b75e179138e202733c16
KimAT25 <kimat60@gmail.com>
2020-03-05 16:12:32 +0200

    changed inputs from single input to interface input for gallery and list component, added separeted file for econews profile icons

--- src/app/component/general/eco-news/news-list/news-list-list-view/news-list-list-view.component.ts
+++ src/app/component/general/eco-news/news-list/news-list-list-view/news-list-list-view.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { EcoNewsModel } from 'src/app/model/eco-news/eco-news-model';
+import { ecoNewsIcons } from 'src/assets/img/icon/econews/profile-icons';
 
 @Component({
   selector: 'app-news-list-list-view',
@@ -6,18 +8,11 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./news-list-list-view.component.css']
 })
 export class NewsListListViewComponent implements OnInit {
-  @Input() id: string;
-  @Input() imagePath: string;
-  @Input() title: string;
-  @Input() text: string;
-  @Input() name: string;
-  @Input() tag: string;
-  @Input() creationDate: number;
+  @Input() ecoNewsModel: EcoNewsModel;
+  profileIcons = ecoNewsIcons;
+
   constructor() { }
 
-  readonly profileIcon = 'assets/img/icon/econews/profile-icon.png';
-  readonly calendarIcon = 'assets/img/icon/econews/calendar-icon.png';
-  readonly shareIcon = 'assets/img/icon/econews/share-icon.png';
 
   ngOnInit() { }
 }
--- src/app/model/eco-news/eco-news-model.ts
+++ src/app/model/eco-news/eco-news-model.ts
@@ -1,11 +1,11 @@
-import{EcoNews} from './eco-news.enum'
+import { EcoNews } from './eco-news.enum';
 
 export interface EcoNewsModel {
- id:number,
- imagePath:string,
- title:string,
- text:string,
- name:string,
- tag:EcoNews,
- creationDate:string,
+    id: number;
+    imagePath: string;
+    title: string;
+    text: string;
+    name: string;
+    tag: EcoNews;
+    creationDate: string;
 }
