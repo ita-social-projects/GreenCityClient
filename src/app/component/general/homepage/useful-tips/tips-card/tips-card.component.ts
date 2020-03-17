55966e86135e23dec7b6990de0f93e5fbdd14959
Roman Mykytyn <roman.mykytyn89@gmail.com>
2020-03-17 02:09:40 +0200

    add rus/eng language for tip component

--- src/app/component/general/homepage/useful-tips/tips-card/tips-card.component.ts
+++ src/app/component/general/homepage/useful-tips/tips-card/tips-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { LanguageService } from '../../../../../i18n/language.service';
 
 @Component({
   selector: 'app-tips-card',
@@ -6,9 +7,10 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./tips-card.component.css']
 })
 export class TipsCardComponent implements OnInit {
-  @Input() tip: { imageUrl, text };
+  language: string = this.languageService.getCurrentLanguage();
+  @Input() tip: { imageUrl, textUk, textRu, textEn };
 
-  constructor() { }
+  constructor(private languageService: LanguageService) { }
 
   ngOnInit() {
   }
