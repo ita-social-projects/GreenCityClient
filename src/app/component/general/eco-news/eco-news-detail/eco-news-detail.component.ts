06bfe70cf6262300c8867c842e8c71b754d42029
RomanDeveloperAcc <roma04dan@gmail.com>
2020-03-18 21:44:14 +0200

    added selector and fixed part of css

--- src/app/component/general/eco-news/eco-news-detail/eco-news-detail.component.ts
+++ src/app/component/general/eco-news/eco-news-detail/eco-news-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  selector: 'app-eco-news-detail',
   templateUrl: './eco-news-detail.component.html',
   styleUrls: ['./eco-news-detail.component.css']
 })
