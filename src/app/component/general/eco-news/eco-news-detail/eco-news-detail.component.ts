99ed7c9a88bc4aa81b8a32a54ddcb4b242840f18
RomanDeveloperAcc <roma04dan@gmail.com>
2020-03-19 18:38:49 +0200

    returned to dynamic data

--- src/app/component/general/eco-news/eco-news-detail/eco-news-detail.component.ts
+++ src/app/component/general/eco-news/eco-news-detail/eco-news-detail.component.ts
@@ -1,12 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { EcoNewsService } from '../../../../service/eco-news/eco-news.service';
+import { EcoNewsModel } from '../../../../model/eco-news/eco-news-model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-eco-news-detail',
   templateUrl: './eco-news-detail.component.html',
   styleUrls: ['./eco-news-detail.component.css']
 })
-export class EcoNewsDetailComponent implements OnInit {
-  constructor() { }
+export class EcoNewsDetailComponent implements OnInit, OnDestroy {
+  private newsItemSubscription: Subscription;
+  private newsId: number;
+  private newsItem: EcoNewsModel;
+  constructor(private route: ActivatedRoute,
+              private ecoNewsService: EcoNewsService) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.newsId = this.route.snapshot.params.id;
+    this.fetchNewsItem();
+  }
+
+  private fetchNewsItem(): void {
+    this.newsItemSubscription = this.ecoNewsService
+      .getEcoNewsById(this.newsId)
+      .subscribe(this.setNewsItem.bind(this));
+  }
+
+  private setNewsItem(item: EcoNewsModel): void {
+    this.newsItem = item;
+  }
+
+  ngOnDestroy() {
+    this.newsItemSubscription.unsubscribe();
+  }
 }
