dab35ec4054415521e3323d2be64ccc2e1cc6319
RomanDeveloperAcc <roma04dan@gmail.com>
2020-03-19 19:13:12 +0200

    added whitespace

--- src/app/component/general/eco-news/eco-news-detail/eco-news-detail.component.ts
+++ src/app/component/general/eco-news/eco-news-detail/eco-news-detail.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { EcoNewsService } from '../../../../service/eco-news/eco-news.service';
 import { EcoNewsModel } from '../../../../model/eco-news/eco-news-model';
