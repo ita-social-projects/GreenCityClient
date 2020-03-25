25a2afea8536423f54a0aee3df602e36885714f4
Ruslan Hrybchuk <58165964+ruslanhrybchuk@users.noreply.github.com>
2020-03-25 13:26:53 +0200

    fix conflict

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -127,6 +127,7 @@ import {InfiniteScrollModule} from 'ngx-infinite-scroll';
 import { EcoNewsDetailComponent } from './component/general/eco-news/eco-news-detail/eco-news-detail.component';
 import { CoreModule } from './component/core/core.module';
 import { PostNewsLoaderComponent } from './component/post-news-loader/post-news-loader.component';
+import { EcoNewsWidgetComponent } from './component/general/eco-news/eco-news-detail/eco-news-widget/eco-news-widget.component';
 
 const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
   direction: 'horizontal',
@@ -211,6 +212,7 @@ const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
     FilterEcoNewsPipe,
     EcoNewsDetailComponent,
     PostNewsLoaderComponent,
+    EcoNewsWidgetComponent
   ],
   imports: [
     CoreModule,
