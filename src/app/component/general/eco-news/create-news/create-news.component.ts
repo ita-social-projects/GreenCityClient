c4e0a8f7986f72a4f52da48e616123f4cfbdbc6c
Gusvarov <omelchenko.nazarii@gmail.com>
2020-03-30 15:22:46 +0300

    changes according to review

--- src/app/component/general/eco-news/create-news/create-news.component.ts
+++ src/app/component/general/eco-news/create-news/create-news.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, DoCheck } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { preparedImageForCreateEcoNews } from '../../../../links';
 import { Router } from '@angular/router';
 import { FormBuilder, Validators } from '@angular/forms';
@@ -10,19 +10,15 @@ import { FilterModel, LanguageModel } from './create-news-interface';
   templateUrl: './create-news.component.html',
   styleUrls: ['./create-news.component.css']
 })
-export class CreateNewsComponent implements OnInit, DoCheck {
-
-  constructor(private router: Router, 
-              private fb: FormBuilder, 
-              private createEcoNewsService: CreateEcoNewsService) {}
+export class CreateNewsComponent implements OnInit {
 
   public createNewsForm = this.fb.group({
-    title: ['', [Validators.maxLength(170)]],
+    title: ['', [Validators.required, Validators.maxLength(170)]],
     source: [''],
-    content: ['', [Validators.minLength(20)]],
+    content: ['', [Validators.required, Validators.minLength(20)]],
     tags: this.fb.array([])
   });
-
+  
   public filters: Array<FilterModel> = [
     {name: 'News', isActive: false},
     {name: 'Events', isActive: false}, 
@@ -31,51 +27,38 @@ export class CreateNewsComponent implements OnInit, DoCheck {
     {name: 'Ads', isActive: false}
   ];
 
-  public activeLanguage: string = 'en';
-  public link: string;
-  private preparedLink: string = preparedImageForCreateEcoNews;
-  private date: Date = new Date();
-  public formValid: boolean;
-
   public languages: Array<LanguageModel> = [
     {name: 'Ukrainian', lang: 'ua'},
     {name: 'English', lang: 'en'},
     {name: 'Russian', lang: 'ru'},
   ];
 
+  public activeLanguage: string = 'en';
+  public link: string;
+  private preparedLink: string = preparedImageForCreateEcoNews;
+  private date: Date = new Date();
+
+  constructor(private router: Router, 
+              private fb: FormBuilder, 
+              private createEcoNewsService: CreateEcoNewsService) {}  
+
   ngOnInit() {
     this.link = this.preparedLink;
     this.onSourceChange();
   }
 
-  ngDoCheck() {
-    if ( !this.createNewsForm.value.title || !this.createNewsForm.value.content ) {
-      this.formValid = true;
-    } else {
-      this.formValid = false;
-    }
-  }
-  
   private navigateByUrl(): void {
     this.router.navigateByUrl('/news');
   }
 
   public onSourceChange(): void {
     this.createNewsForm.get('source').valueChanges.subscribe(source => {
-      if ( !source ) {
-        this.link = this.preparedLink;
-      } else {
-        this.link = source;
-      }
+      this.link = !source ? this.preparedLink : source;
     });
   }
 
   public onSubmit(): void {
-    this.createEcoNewsService.sendFormData(this.createNewsForm, this.activeLanguage).subscribe(
-      (data: object) => {
-        console.log(data);
-      }
-    );
+    this.createEcoNewsService.sendFormData(this.createNewsForm, this.activeLanguage).subscribe();
   }
 
   public addFilters(filter: FilterModel): void { 
@@ -110,8 +93,10 @@ export class CreateNewsComponent implements OnInit, DoCheck {
     const formData = {
       text: this.createNewsForm.value['content'],
       title: this.createNewsForm.value['title']
-    }  
+    }; 
+
     this.createEcoNewsService.setTranslationByLang(this.activeLanguage, formData);
+    this.createNewsForm.reset();
     this.activeLanguage = language.lang;
     this.bindFormValue();
   }
