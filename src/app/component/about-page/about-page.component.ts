5b83acfd58bd8eca518f6739e4ff0fb24a9803c3
Ruslan Hrybchuk <58165964+ruslanhrybchuk@users.noreply.github.com>
2020-03-31 14:25:38 +0300

    finishing touches

--- src/app/component/about-page/about-page.component.ts
+++ src/app/component/about-page/about-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { LocalStorageService } from 'src/app/service/localstorage/local-storage.service';
+import { UserService } from '../../service/user/user.service';
 
 @Component({
   selector: 'app-about-page',
@@ -9,10 +12,23 @@ import { Title } from '@angular/platform-browser';
 
 export class AboutPageComponent implements OnInit {
   public actualYear = new Date().getFullYear();
+  private userId: number;
+  private usersAmount: number;
 
-  constructor(private titleService: Title) { }
+  constructor(private titleService: Title,
+              private router: Router,
+              private localStorageService: LocalStorageService,
+              private userService: UserService) { }
 
   ngOnInit() {
     this.titleService.setTitle('About');
+    this.localStorageService.userIdBehaviourSubject.subscribe(userId => this.userId = userId);
+    this.userService.countActivatedUsers().subscribe(num => {
+      this.usersAmount = num;
+    });
+  }
+
+  startHabit() {
+    this.router.navigate([this.userId, 'habits']);
   }
 }
