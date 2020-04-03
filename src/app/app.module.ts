851877644b34106595bfb73f719e6678ddc8e4e0
RomanDeveloperAcc <roma04dan@gmail.com>
2020-04-03 13:28:08 +0300

    created new auth module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -116,6 +116,7 @@ import { InputcolorDirective } from './directives/inputcolor.directive';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 import { CoreModule } from './component/core/core.module';
 import { EcoNewsModule } from './component/eco-news/eco-news.module';
+import { NewAuthModule } from './new-auth/new-auth.module';
 
 
 const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
@@ -192,6 +193,7 @@ const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
     InputcolorDirective
   ],
   imports: [
+    NewAuthModule,
     CoreModule,
     EcoNewsModule,
     InfiniteScrollModule,
---
+++ src/app/new-auth/new-auth.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  declarations: [],
+  imports: [],
+  providers: []
+})
+
+export class NewAuthModule { }
