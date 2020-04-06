96ab4a0645c9bb412ee66108e27ef97d378623ac
Volodymyr Nakutnyi <volodymyrnakutnyi@gmail.com>
2020-04-06 23:16:02 +0300

    simple

--- src/app/component/auth/auth.module.ts
+++ src/app/component/auth/auth.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { NewSignUpComponent } from './new-sign-up/new-sign-up.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 @NgModule({
   declarations: [
-    NewSignUpComponent
+    NewSignUpComponent,
+    ForgotPasswordComponent
   ],
   imports: [],
+  entryComponents: [
+    ForgotPasswordComponent
+  ],
   providers: []
 })
 
