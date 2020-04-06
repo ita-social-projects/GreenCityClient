77ac5c92b6286b0f060a22648bff26dc22d204b4
Ruslan Hrybchuk <58165964+ruslanhrybchuk@users.noreply.github.com>
2020-04-06 20:32:25 +0300

    Fixed issues with responsive layout

--- src/app/component/about-page/about-page.component.ts
+++ src/app/component/about-page/about-page.component.ts
@@ -1,34 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import { Title } from '@angular/platform-browser';
 import { Router } from '@angular/router';
 import { LocalStorageService } from 'src/app/service/localstorage/local-storage.service';
-import { UserService } from '../../service/user/user.service';
 
 @Component({
   selector: 'app-about-page',
   templateUrl: './about-page.component.html',
-  styleUrls: ['./about-page.component.css']
+  styleUrls: ['./about-page.component.scss']
 })
 
 export class AboutPageComponent implements OnInit {
   public actualYear = new Date().getFullYear();
   private userId: number;
-  private usersAmount: number;
 
-  constructor(private titleService: Title,
-              private router: Router,
-              private localStorageService: LocalStorageService,
-              private userService: UserService) { }
+  constructor(private router: Router,
+              private localStorageService: LocalStorageService) { }
 
   ngOnInit() {
-    this.titleService.setTitle('About');
     this.localStorageService.userIdBehaviourSubject.subscribe(userId => this.userId = userId);
-    this.userService.countActivatedUsers().subscribe(num => {
-      this.usersAmount = num;
-    });
   }
 
-  startHabit() {
+  private navigateToHabit(): void {
     this.router.navigate([this.userId, 'habits']);
   }
 }
