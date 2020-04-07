c04a06c2c2b57725511fb1bf8325d5c70216163e
Gusvarov <omelchenko.nazarii@gmail.com>
2020-04-07 19:17:47 +0300

    minor fix

--- src/app/component/auth/auth.module.ts
+++ src/app/component/auth/auth.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { NewSignUpComponent } from './new-sign-up/new-sign-up.component';
-import {SignInNewComponent} from './sign-in-new/sign-in-new.component';
-import {CoreModule} from '../core/core.module';
+import { SignInNewComponent } from './sign-in-new/sign-in-new.component';
+import { CoreModule } from '../core/core.module';
 
 @NgModule({
   declarations: [
