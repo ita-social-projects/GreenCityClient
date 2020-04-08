019032f254c3dc315140383fcd5fe4326e9d3806
KimAT25 <kimat60@gmail.com>
2020-04-08 18:24:57 +0300

    added B-logic to sign-in component, small correction in lang json files

--- src/app/component/auth/sign-in-new/sign-in-new.component.ts
+++ src/app/component/auth/sign-in-new/sign-in-new.component.ts
@@ -7,6 +7,8 @@ import { UserOwnSignInService } from '../../../service/auth/user-own-sign-in.ser
 import { Router } from '@angular/router';
 import { SignInIcons } from 'src/assets/img/icon/sign-in/sign-in-icons';
 import { MatDialogRef } from '@angular/material';
+import { UserOwnSignIn } from '../../../model/user-own-sign-in';
+import {LocalStorageService} from '../../../service/localstorage/local-storage.service';
 
 @Component({
   selector: 'app-sign-in-new',
@@ -19,20 +21,101 @@ export class SignInNewComponent implements OnInit {
   private googleImage = SignInIcons;
   private hidePassword = '../../../../assets/img/icon/eye.png';
   private showPassword = '../../../../assets/img/icon/eye-show.png';
+  public currentLang: string;
+
+  public userOwnSignIn: UserOwnSignIn;
+  public loadingAnim: boolean;
+  public emailErrorMessageBackEnd: string;
+  public passwordErrorMessageBackEnd: string;
+  public backEndError: string;
 
   constructor(
     private matDialogRef: MatDialogRef<SignInNewComponent>,
     private userOwnSignInService: UserOwnSignInService,
     private router: Router,
     private authService: AuthService,
-    private googleService: GoogleSignInService
+    private googleService: GoogleSignInService,
+    private localStorageService: LocalStorageService,
   ) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.userOwnSignIn = new UserOwnSignIn();
+    this.loadingAnim = false;
+    this.emailErrorMessageBackEnd = null;
+    this.passwordErrorMessageBackEnd = null;
+    this.backEndError = null;
+    this.localStorageService.userIdBehaviourSubject.subscribe(userId => {
+      if (userId) {
+        this.matDialogRef.close();
+      }
+    });
+    // this.getCurrentLanguage();
+  }
 
-  private closeSignInWindow(): void {
+  public signIn(userOwnSignIn: UserOwnSignIn) {
+    this.loadingAnim = true;
+    this.userOwnSignInService.signIn(userOwnSignIn).subscribe(
+      (data: UserSuccessSignIn) => {
+        this.loadingAnim = false;
+        this.userOwnSignInService.saveUserToLocalStorage(data);
+        this.localStorageService.setFirstName(data.firstName);
+        this.localStorageService.setFirstSignIn();
+        this.router.navigate(['/welcome'])
+          .then(success => console.log('redirect has succeeded ' + success))
+          .catch(fail => console.log('redirect has failed ' + fail));
+      },
+      (errors: HttpErrorResponse) => {
+        try {
+          errors.error.forEach(error => {
+            if (error.name === 'email') {
+              this.emailErrorMessageBackEnd = error.message;
+            } else if (error.name === 'password') {
+              this.passwordErrorMessageBackEnd = error.message;
+            }
+          });
+        } catch (e) {
+          this.backEndError = errors.error.message;
+        }
+        this.loadingAnim = false;
+      }
+    );
+  }
+
+  private CloseSignInWindow(): void {
     this.matDialogRef.close();
   }
+
+  signInWithGoogle() {
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID).then(data => {
+      this.googleService.signIn(data.idToken).subscribe(
+        (data1: UserSuccessSignIn) => {
+          this.userOwnSignInService.saveUserToLocalStorage(data1);
+          this.router.navigate(['/']);
+        },
+        (errors: HttpErrorResponse) => {
+          try {
+            errors.error.forEach(error => {
+              if (error.name === 'email') {
+                this.emailErrorMessageBackEnd = error.message;
+              } else if (error.name === 'password') {
+                this.passwordErrorMessageBackEnd = error.message;
+              }
+            });
+          } catch (e) {
+            this.backEndError = errors.error.message;
+          }
+        }
+      );
+    });
+  }
+
+  // getCurrentLanguage() {
+  //   this.localStorageService.selectedLanguageBehaviourSubject
+  //     .subscribe(currentLanguage => {
+  //       this.currentLang = currentLanguage;
+  //       console.log(this.currentLang);
+  //     });
+  // }
 
   private showHidePassword(input: HTMLInputElement, src: HTMLImageElement): void {
     input.type = input.type === 'password' ? 'text' : 'password';
