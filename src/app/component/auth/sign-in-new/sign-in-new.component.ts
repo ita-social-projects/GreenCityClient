7dbfcabe05c924d18767b2c9d597681b0b511465
KimAT25 <kimat60@gmail.com>
2020-04-09 22:51:48 +0300

    made refactoring of b-logic

--- src/app/component/auth/sign-in-new/sign-in-new.component.ts
+++ src/app/component/auth/sign-in-new/sign-in-new.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AuthService, GoogleLoginProvider } from 'angularx-social-login';
 import { GoogleSignInService } from '../../../service/auth/google-sign-in.service';
 import { UserSuccessSignIn } from '../../../model/user-success-sign-in';
@@ -6,28 +6,31 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { UserOwnSignInService } from '../../../service/auth/user-own-sign-in.service';
 import { Router } from '@angular/router';
 import { SignInIcons } from 'src/assets/img/icon/sign-in/sign-in-icons';
-import { MatDialogRef } from '@angular/material';
+import {MatDialog, MatDialogRef} from '@angular/material';
 import { UserOwnSignIn } from '../../../model/user-own-sign-in';
 import {LocalStorageService} from '../../../service/localstorage/local-storage.service';
+import {NewSignUpComponent} from '../new-sign-up/new-sign-up.component';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-sign-in-new',
   templateUrl: './sign-in-new.component.html',
   styleUrls: ['./sign-in-new.component.scss']
 })
-export class SignInNewComponent implements OnInit {
+export class SignInNewComponent implements OnInit, OnDestroy {
   private closeBtn = SignInIcons;
   private mainSignInImage = SignInIcons;
   private googleImage = SignInIcons;
   private hideShowPasswordImage = SignInIcons;
-
   public userOwnSignIn: UserOwnSignIn;
+  public userIdSubscription: Subscription;
   public loadingAnim: boolean;
   public emailErrorMessageBackEnd: string;
   public passwordErrorMessageBackEnd: string;
   public backEndError: string;
 
   constructor(
+    public dialog: MatDialog,
     private matDialogRef: MatDialogRef<SignInNewComponent>,
     private userOwnSignInService: UserOwnSignInService,
     private router: Router,
@@ -38,76 +41,101 @@ export class SignInNewComponent implements OnInit {
 
   ngOnInit() {
     this.userOwnSignIn = new UserOwnSignIn();
-    this.loadingAnim = false;
-    this.emailErrorMessageBackEnd = null;
-    this.passwordErrorMessageBackEnd = null;
-    this.backEndError = null;
-    this.localStorageService.userIdBehaviourSubject.subscribe(userId => {
+    this.defaultErrorMessage();
+    this.checkIfUserId();
+
+  }
+
+  private checkIfUserId(): void {
+    this.userIdSubscription = this.localStorageService.userIdBehaviourSubject
+      .subscribe(userId => {
       if (userId) {
         this.matDialogRef.close();
       }
     });
   }
 
-  public signIn(userOwnSignIn: UserOwnSignIn) {
+  public defaultErrorMessage(): void {
+    this.emailErrorMessageBackEnd = null;
+    this.passwordErrorMessageBackEnd = null;
+    this.backEndError = null;
+  }
+
+  private signIn(userOwnSignIn: UserOwnSignIn): void {
     this.loadingAnim = true;
     this.userOwnSignInService.signIn(userOwnSignIn).subscribe(
       (data: UserSuccessSignIn) => {
-        this.loadingAnim = false;
-        this.userOwnSignInService.saveUserToLocalStorage(data);
-        this.localStorageService.setFirstName(data.firstName);
-        this.localStorageService.setFirstSignIn();
-        this.router.navigate(['/welcome'])
-          .then(success => console.log('redirect has succeeded ' + success))
-          .catch(fail => console.log('redirect has failed ' + fail));
+        this.getUserSuccessSignInData(data);
       },
       (errors: HttpErrorResponse) => {
-        try {
-          errors.error.forEach(error => {
-            if (error.name === 'email') {
-              this.emailErrorMessageBackEnd = error.message;
-            } else if (error.name === 'password') {
-              this.passwordErrorMessageBackEnd = error.message;
-            }
-          });
-        } catch (e) {
-          this.backEndError = errors.error.message;
-        }
+        this.handError(errors);
         this.loadingAnim = false;
-      }
-    );
+      });
+  }
+
+  private signInWithGoogle(): void {
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID).then(data => {
+      this.googleService.signIn(data.idToken).subscribe(
+        (signInData: UserSuccessSignIn) => {
+          this.userSuccessGoogleSignIn(signInData);
+        },
+        (errors: HttpErrorResponse) => {
+          this.handError(errors);
+        });
+    });
+  }
+
+  private getUserSuccessSignInData(data: UserSuccessSignIn): void {
+    this.loadingAnim = false;
+    this.userOwnSignInService.saveUserToLocalStorage(data);
+    this.localStorageService.setFirstName(data.firstName);
+    this.localStorageService.setFirstSignIn();
+    this.router.navigate(['/welcome'])
+      .then(success => console.log('redirect has succeeded ' + success))
+      .catch(fail => console.log('redirect has failed ' + fail));
+  }
+
+  private handError(errors: HttpErrorResponse): void {
+    try {
+      errors.error.forEach(error => {
+        if (error.name === 'email') {
+          this.emailErrorMessageBackEnd = error.message;
+        } else if (error.name === 'password') {
+          this.passwordErrorMessageBackEnd = error.message;
+        }
+      });
+    } catch (e) {
+      this.backEndError = errors.error.message;
+    }
+  }
+
+  private userSuccessGoogleSignIn(data: UserSuccessSignIn): void {
+    this.userOwnSignInService.saveUserToLocalStorage(data);
+    this.router.navigate(['/welcome'])
+      .then(success => console.log('redirect has succeeded ' + success))
+      .catch(fail => console.log('redirect has failed ' + fail));
+  }
+
+  private showHidePassword(input: HTMLInputElement, src: HTMLImageElement): void {
+    input.type = input.type === 'password' ? 'text' : 'password';
+    src.src = input.type === 'password' ?
+      this.hideShowPasswordImage.hidePassword : this.hideShowPasswordImage.showPassword;
   }
 
   private CloseSignInWindow(): void {
     this.matDialogRef.close();
   }
 
-  signInWithGoogle() {
-    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID).then(data => {
-      this.googleService.signIn(data.idToken).subscribe(
-        (data1: UserSuccessSignIn) => {
-          this.userOwnSignInService.saveUserToLocalStorage(data1);
-          this.router.navigate(['/']);
-        },
-        (errors: HttpErrorResponse) => {
-          try {
-            errors.error.forEach(error => {
-              if (error.name === 'email') {
-                this.emailErrorMessageBackEnd = error.message;
-              } else if (error.name === 'password') {
-                this.passwordErrorMessageBackEnd = error.message;
-              }
-            });
-          } catch (e) {
-            this.backEndError = errors.error.message;
-          }
-        }
-      );
+  private signUpOpenWindow(): void {
+    this.matDialogRef.close();
+    this.dialog.open(NewSignUpComponent, {
+      hasBackdrop: true,
+      closeOnNavigation: true,
+      panelClass: 'custom-dialog-container',
     });
   }
 
-  private showHidePassword(input: HTMLInputElement, src: HTMLImageElement): void {
-    input.type = input.type === 'password' ? 'text' : 'password';
-    src.src = input.type === 'password' ? this.hideShowPasswordImage.hidePassword : this.hideShowPasswordImage.showPassword;
+  ngOnDestroy() {
+    this.userIdSubscription.unsubscribe();
   }
 }
