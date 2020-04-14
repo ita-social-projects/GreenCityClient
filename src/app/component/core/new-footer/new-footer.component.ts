8f94750a847b50d582672db1bfe85e49eeddeb4f
KimAT25 <kimat60@gmail.com>
2020-04-14 21:15:27 +0300

    created new footer with full responsive

---
+++ src/app/component/core/new-footer/new-footer.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import {footerIcons} from '../../../../assets/img/icon/footer/footer-icons';
+
+@Component({
+  selector: 'app-new-footer',
+  templateUrl: './new-footer.component.html',
+  styleUrls: ['./new-footer.component.scss']
+})
+export class NewFooterComponent implements OnInit {
+  public actualYear = new Date().getFullYear();
+  private fbIcon = footerIcons;
+  private linkedInIcon = footerIcons;
+  private twitter = footerIcons;
+  private logo = footerIcons;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/assets/img/icon/footer/footer-icons.ts
@@ -0,0 +1,6 @@
+export const footerIcons = {
+  facebookIcon: 'assets/img/icon/footer/facebook-icon.svg',
+  twitterIcon: 'assets/img/icon/footer/twitter-icon.svg',
+  linkedInIcon: 'assets/img/icon/footer/linkedin-icon.svg',
+  greenCityLogo: 'assets/img/icon/footer/greencity-logo.png',
+};
