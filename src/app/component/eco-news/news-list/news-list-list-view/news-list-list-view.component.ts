99c7bc41c8622dfe5c7e615a9f7644affb65d653
KimAT25 <kimat60@gmail.com>
2020-05-07 14:11:52 +0300

    added responsive dynamic tittle and text row amount

--- src/app/component/eco-news/news-list/news-list-list-view/news-list-list-view.component.ts
+++ src/app/component/eco-news/news-list/news-list-list-view/news-list-list-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {Component, OnInit, Input, ViewChild, ElementRef, Renderer2, AfterViewChecked } from '@angular/core';
 import { EcoNewsModel } from 'src/app/model/eco-news/eco-news-model';
 import { ecoNewsIcons } from 'src/assets/img/icon/econews/profile-icons';
 
@@ -8,13 +8,18 @@ import { ecoNewsIcons } from 'src/assets/img/icon/econews/profile-icons';
   styleUrls: ['./news-list-list-view.component.css'],
   changeDetection: 0
 })
-export class NewsListListViewComponent implements OnInit {
+export class NewsListListViewComponent implements OnInit, AfterViewChecked {
   @Input() ecoNewsModel: EcoNewsModel;
+  @ViewChild('titleHeight', { static: true }) titleHeight: ElementRef;
+  @ViewChild('textHeight', { static: true }) textHeight: ElementRef;
+
   profileIcons = ecoNewsIcons;
   private newsText: string;
   private newsImage: string;
+  private titleHeightOfElement: number;
+  private textHeightOfElement: number;
 
-  constructor() { }
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit() {
     this.textValidationOfMinCharacters();
@@ -28,5 +33,29 @@ export class NewsListListViewComponent implements OnInit {
   private checkNewsImage(): string {
     return this.newsImage = (this.ecoNewsModel.imagePath && this.ecoNewsModel.imagePath !== ' ') ?
       this.ecoNewsModel.imagePath : this.profileIcons.newsDefaultPictureList;
+  }
+
+  private checkHeightOfTittle(): void {
+    this.titleHeightOfElement = this.titleHeight.nativeElement.offsetHeight;
+    switch (true) {
+      case (this.titleHeightOfElement < 32):
+        this.textHeightOfElement = 96;
+        break;
+      case (this.titleHeightOfElement > 32 && this.titleHeightOfElement <= 64):
+        this.textHeightOfElement = 52;
+        break;
+      case (this.titleHeightOfElement > 64):
+        this.textHeightOfElement = 28;
+        break;
+    }
+    this.renderer.setStyle(this.textHeight.nativeElement, 'height', this.textHeightOfElement + 'px' );
+  }
+
+  private onResize(): void {
+    this.checkHeightOfTittle();
+  }
+
+  ngAfterViewChecked() {
+    this.checkHeightOfTittle();
   }
 }
