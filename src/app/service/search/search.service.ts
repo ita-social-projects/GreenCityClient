c2b89d779fed32466e05c39714e761a660d50d77
M.Naberezhnyi <naber201725@gmail.com>
2020-05-17 18:45:32 +0300

    added 'switchMap' to the searchService

--- src/app/service/search/search.service.ts
+++ src/app/service/search/search.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
-import { Observable, Subject } from 'rxjs';
+import { Observable, of, Subject } from 'rxjs';
 import { SearchModel } from '../../model/search/search.model';
+import { switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,9 @@ export class SearchService {
   public SearchEmitter = new Subject<boolean>();
 
   public getSearch(searchQuery: string): Observable<SearchModel> {
-    return this.http.get<SearchModel>(`${this.backEndLink}search?searchQuery=${searchQuery}`);
+    return this.http.get<SearchModel>(`${this.backEndLink}search?searchQuery=${searchQuery}`).pipe(
+      switchMap(res => of(res))
+    );
   }
 
   public openSearchSignal() {
