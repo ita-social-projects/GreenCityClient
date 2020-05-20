964a2515e3a6fef5a430e62a845c05a511adc364
vasyldzyuba <vasyl.dzuba1@gmail.com>
2020-05-20 21:38:20 +0300

    created three cards at the right side in the profile page

--- src/app/component/user/profile/profile.component.ts
+++ src/app/component/user/profile/profile.component.ts
@@ -9,6 +9,24 @@ export class ProfileComponent implements OnInit {
 
   constructor() { }
 
+  cardsData: any[] = [
+    {
+      title: 'Fact of the Day',
+      description: 'Adipisicing magna occaecat aliquip magna cillum cupidatat est laborum aliqua do consectetur.',
+      backgroundColor: '#13AA57'
+    },
+    {
+      title: 'Dear Friend',
+      description: 'Adipisicing magna occaecat aliquip magna cillum cupidatat est laborum aliqua do consectetur.',
+      backgroundColor: '#5200FF'
+    },
+    {
+      title: 'Dear Friend',
+      description: 'Adipisicing magna occaecat aliquip magna cillum cupidatat est laborum aliqua do consectetur.',
+      backgroundColor: '#FFC100'
+    }
+  ];
+
   ngOnInit() {
   }
 
