1c953f6c974e4798921e9cab35168ceb44722fff
Volodymyr Nakutnyi <volodymyrnakutnyi@gmail.com>
2020-05-21 22:42:23 +0300

    styled for Roman Danyliv)

--- src/app/component/auth/auth.module.ts
+++ src/app/component/auth/auth.module.ts
@@ -3,16 +3,16 @@ import { NewSignUpComponent } from './new-sign-up/new-sign-up.component';
 import { SignInNewComponent } from './sign-in-new/sign-in-new.component';
 import { RestorePasswordComponent } from './restore-password/restore-password.component';
 import { CoreModule } from '../core/core.module';
-import {SignInComponent} from '../user/auth/sign-in/sign-in.component';
-import {InputcolorDirective} from '../../directives/inputcolor.directive';
-import {SignUpComponent} from '../user/auth/sign-up/sign-up.component';
-import {AuthComponent} from '../user/auth/auth.component';
-import {SubmitEmailComponent} from '../user/auth/submit-email/submit-email.component';
-import {ProfileComponent} from '../user/profile/profile.component';
-import {RestoreComponent} from '../user/restore/restore.component';
-import {RestoreFormComponent} from '../user/restore-form/restore-form.component';
-import {ProfileMiddleMenuComponent} from '../user/profile/profile-middle/profile-middle-menu/profile-middle-menu.component';
-import {OneHabitComponent} from '../user/profile/profile-middle/profile-middle-menu/one-habit/one-habit.component';
+import { SignInComponent } from '../user/auth/sign-in/sign-in.component';
+import { InputcolorDirective } from '../../directives/inputcolor.directive';
+import { SignUpComponent } from '../user/auth/sign-up/sign-up.component';
+import { AuthComponent } from '../user/auth/auth.component';
+import { SubmitEmailComponent } from '../user/auth/submit-email/submit-email.component';
+import { ProfileComponent } from '../user/profile/profile.component';
+import { RestoreComponent } from '../user/restore/restore.component';
+import { RestoreFormComponent } from '../user/restore-form/restore-form.component';
+import { ProfileMiddleMenuComponent } from '../user/profile/profile-middle/profile-middle-menu/profile-middle-menu.component';
+import { OneHabitComponent } from '../user/profile/profile-middle/profile-middle-menu/one-habit/one-habit.component';
 
 @NgModule({
   declarations: [
