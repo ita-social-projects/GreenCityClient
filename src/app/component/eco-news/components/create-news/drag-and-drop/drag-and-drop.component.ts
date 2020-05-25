c4fc0fbc73e2bb419e9ad1a3b0e6017f6c231b84
Gusvarov <omelchenko.nazarii@gmail.com>
2020-05-25 11:49:43 +0300

    fixed cropper cancel button

--- src/app/component/eco-news/components/create-news/drag-and-drop/drag-and-drop.component.ts
+++ src/app/component/eco-news/components/create-news/drag-and-drop/drag-and-drop.component.ts
@@ -14,11 +14,14 @@ export class DragAndDropComponent implements OnInit {
 
   private imageChangedEvent: FileHandle[];
   private isCropper: boolean = true;
-  private extentionWarning: boolean = false;
   public files: FileHandle[] = [];
   public isWarning: boolean = false;
+  private croppedImage: string;
 
   private stopCropping(): void {
+    this.files.forEach(item => {
+      item.url = this.croppedImage;
+    }) 
     this.isCropper = false;
   }
 
@@ -27,14 +30,13 @@ export class DragAndDropComponent implements OnInit {
   }
 
   private imageCropped(event: ImageCroppedEvent): void {
-    this.files.forEach(item => {
-      item.url = event.base64;
-    })  
+    this.croppedImage = event.base64;
   }
 
   public filesDropped(files: FileHandle[]): void {
     this.files = files;
     this.createEcoNewsService.files = files;
+    this.isCropper = true;
     this.showWarning();
     this.createEcoNewsService.isImageValid = this.isWarning;
   }
@@ -42,22 +44,10 @@ export class DragAndDropComponent implements OnInit {
   public showWarning(): void {
     this.files.forEach(item => {
       if (item &&
-        item.file.size < 10485760 &&
         (item.file.type === 'image/jpeg' || item.file.type === 'image/png')) {
         this.isWarning = false;
       } else {
         this.isWarning = true;
-      }
-    })
-  }
-
-  public showExtentionWarning(): void {
-    this.files.forEach(item => {
-      if (item && 
-        item.file.type === 'image/jpeg' || item.file.type === 'image/png') {
-        this.extentionWarning = false;
-      } else {
-        this.extentionWarning = true;
       }
     })
   }
