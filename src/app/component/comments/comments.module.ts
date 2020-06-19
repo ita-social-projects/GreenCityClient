043b6f5290ce27050d55a19b65b92797a3fd0d0a
Gusvarov <omelchenko.nazarii@gmail.com>
2020-06-19 19:38:58 +0300

    deleted comments routing module

--- src/app/component/comments/comments.module.ts
+++ src/app/component/comments/comments.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommentsComponent } from './comments/comments.component';
 import { SharedModule } from '../shared/shared.module';
 import { CommonModule } from '@angular/common';
-import { CommentsRoutingModule } from './comments-routing.module';
 import { AddCommentComponent } from '../comments/comments/add-comment/add-comment.component';
 
 @NgModule({
@@ -11,7 +10,6 @@ import { AddCommentComponent } from '../comments/comments/add-comment/add-commen
     AddCommentComponent
   ],
   imports: [
-    CommentsRoutingModule,
     SharedModule,
     CommonModule,
   ],
