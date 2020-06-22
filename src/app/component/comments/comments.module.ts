5d71587e2fda410663ab328de43360720b876fcc
vasyldzyuba <vasyl.dzuba1@gmail.com>
2020-06-22 17:28:09 +0300

    added basic layout to comment text

--- src/app/component/comments/comments.module.ts
+++ src/app/component/comments/comments.module.ts
@@ -3,11 +3,13 @@ import {CommentsComponent} from './comments/comments.component';
 import {SharedModule} from '../shared/shared.module';
 import {CommonModule} from '@angular/common';
 import {CommentsRoutingModule} from './comments-routing.module';
+import { CommentBodyComponent } from './comments/comment-body/comment-body.component';
 
 
 @NgModule({
   declarations: [
-    CommentsComponent
+    CommentsComponent,
+    CommentBodyComponent
   ],
   imports: [
     CommentsRoutingModule,
