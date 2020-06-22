199dd7286013146f7c44e62baed18e87d1f71af8
vasyldzyuba <vasyl.dzuba1@gmail.com>
2020-06-22 18:54:38 +0300

    final comment-text layout

--- src/app/component/comments/comments.module.ts
+++ src/app/component/comments/comments.module.ts
@@ -4,12 +4,14 @@ import { CommentsComponent } from './components/comments.component';
 import { SharedModule } from '../shared/shared.module';
 import { AddCommentComponent } from '../comments/components/add-comment/add-comment.component';
 import { CommentLikeReplyComponent } from './components/comment-like-reply/comment-like-reply.component';
+import {CommentBodyComponent} from "./components/comment-body/comment-body.component";
 
 @NgModule({
   declarations: [
     CommentsComponent,
     AddCommentComponent,
-    CommentLikeReplyComponent
+    CommentLikeReplyComponent,
+    CommentBodyComponent,
   ],
   imports: [
     SharedModule,
