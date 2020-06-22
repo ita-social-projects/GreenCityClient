f980465d492786b22e9dad1e2ec0c183c58abdb2
vasyldzyuba <vasyl.dzuba1@gmail.com>
2020-06-22 22:44:11 +0300

    fixed PR comments requirements

--- src/app/component/comments/comments.module.ts
+++ src/app/component/comments/comments.module.ts
@@ -4,7 +4,7 @@ import { CommentsComponent } from './components/comments.component';
 import { SharedModule } from '../shared/shared.module';
 import { AddCommentComponent } from '../comments/components/add-comment/add-comment.component';
 import { CommentLikeReplyComponent } from './components/comment-like-reply/comment-like-reply.component';
-import {CommentBodyComponent} from './components/comment-body/comment-body.component';
+import { CommentBodyComponent } from './components/comment-body/comment-body.component';
 
 @NgModule({
   declarations: [
