77fac4a93d5d128fbe3c85f9c8315545d335c2eb
Gusvarov <omelchenko.nazarii@gmail.com>
2020-06-23 12:42:37 +0300

    made delete comment UI

--- src/app/component/comments/comments.module.ts
+++ src/app/component/comments/comments.module.ts
@@ -5,13 +5,15 @@ import { SharedModule } from '../shared/shared.module';
 import { AddCommentComponent } from '../comments/components/add-comment/add-comment.component';
 import { CommentLikeReplyComponent } from './components/comment-like-reply/comment-like-reply.component';
 import { ViewRepliesComponent } from './components/view-replies/view-replies.component';
+import { DeleteCommentComponent } from './components/delete-comment/delete-comment.component';
 
 @NgModule({
   declarations: [
     CommentsComponent,
     AddCommentComponent,
     CommentLikeReplyComponent,
-    ViewRepliesComponent
+    ViewRepliesComponent,
+    DeleteCommentComponent
   ],
   imports: [
     SharedModule,
@@ -20,7 +22,8 @@ import { ViewRepliesComponent } from './components/view-replies/view-replies.com
   exports: [
     AddCommentComponent,
     CommentLikeReplyComponent,
-    ViewRepliesComponent
+    ViewRepliesComponent,
+    DeleteCommentComponent
   ],
   providers: []
 })
