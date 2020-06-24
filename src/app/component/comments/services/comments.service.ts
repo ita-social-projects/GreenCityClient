c599672fc67a2e1ccbe9fd36857586ab401866a9
Gusvarov <omelchenko.nazarii@gmail.com>
2020-06-24 15:30:18 +0300

    fixes according to PR

--- src/app/component/comments/services/comments.service.ts
+++ src/app/component/comments/services/comments.service.ts
@@ -2,7 +2,7 @@ import { Injectable, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
 import { Observable, Subscription } from 'rxjs';
-import { environment } from '../../../../environments/environment';
+import { environment } from '@environment/environment';
 
 @Injectable({
   providedIn: 'root'
