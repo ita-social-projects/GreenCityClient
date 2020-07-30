9b0995ac81187323d168cd3bdff53b32510c0cb7
RomanDeveloperAcc <roma04dan@gmail.com>
2020-07-30 13:01:59 +0300

    added websockets

--- src/app/component/comments/components/like-comment/like-comment.component.ts
+++ src/app/component/comments/components/like-comment/like-comment.component.ts
@@ -1,12 +1,15 @@
-import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { CommentsService } from '../../services/comments.service';
+import * as Stomp from 'stompjs';
+import * as SockJS from 'sockjs-client';
+import { environment } from "@environment/environment";
 
 @Component({
   selector: 'app-like-comment',
   templateUrl: './like-comment.component.html',
   styleUrls: ['./like-comment.component.scss']
 })
-export class LikeCommentComponent implements OnInit {
+export class LikeCommentComponent implements OnInit, OnDestroy {
   @Input() commentId: number;
   @Input() likeState: boolean;
   @ViewChild('like', {static: true})
@@ -17,11 +20,15 @@ export class LikeCommentComponent implements OnInit {
     like: 'assets/img/comments/like.png',
     liked: 'assets/img/comments/liked.png'
   };
+  private connectionUrl = `${environment.backendLink}socket`;
+  private topic = '/topic/comment';
+  private stompClient: any;
 
   constructor(private commentsService: CommentsService) { }
 
   ngOnInit() {
     this.setStartingElements(this.likeState);
+    this.initializeWebSocketConnection();
   }
 
   private setStartingElements(state: boolean) {
@@ -31,7 +38,19 @@ export class LikeCommentComponent implements OnInit {
     this.like.nativeElement.srcset = this.commentsImages[imgName];
   }
 
+  private initializeWebSocketConnection(){
+    let ws = new SockJS(this.connectionUrl);
+    this.stompClient = Stomp.over(ws);
+    const that = this;
+    this.stompClient.connect({}, function (frame) {
+      that.stompClient.subscribe(that.topic, (message) => {
+        alert(1);
+      });
+    });
+  }
+
   public pressLike(): void {
+    this.stompClient.send('/app/likeAndCount' , {}, this.commentId);
     this.commentsService.postLike(this.commentId)
       .subscribe(() => {
         this.changeLkeBtn();
@@ -44,5 +63,11 @@ export class LikeCommentComponent implements OnInit {
 
     [this.span.nativeElement.innerText, imgName] = cond ? ['Liked', 'liked'] : ['Like', 'like'];
     this.like.nativeElement.srcset = this.commentsImages[imgName];
+  }
+
+  ngOnDestroy() {
+    if (this.stompClient != null) {
+      this.stompClient.disconnect();
+    }
   }
 }
