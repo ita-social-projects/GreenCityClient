a54ca5fdcd3a3ed56a5bc6662235f0a390ffa6a2
yulkats <tsyulka3007@gmail.com>
2020-08-13 16:06:14 +0300

    added profile statistics

--- src/app/component/user/components/profile/profile-service/profile.service.ts
+++ src/app/component/user/components/profile/profile-service/profile.service.ts
@@ -4,6 +4,7 @@ import { CardModel } from '@user-models/card.model';
 import { environment } from '@environment/environment';
 import { HttpClient } from '@angular/common/http';
 import { LocalStorageService } from '@global-service/localstorage/local-storage.service';
+import { ProfileStatistics } from '@user-models/profile-statistiscs';
 
 @Injectable({
   providedIn: 'root'
@@ -35,4 +36,7 @@ export class ProfileService {
     return this.http.get<object>(`${this.backEnd}user/isOnline/${this.userId}/`);
   }
 
+  public getUserProfileStatistics(): Observable<ProfileStatistics> {
+    return this.http.get<ProfileStatistics>(`${this.backEnd}user/${this.userId}/profileStatistics/`);
+  }
 }
