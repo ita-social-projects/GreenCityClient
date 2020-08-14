9398d79ce998d204d2ebcd1cfaf95bc503f9e05b
Volodymyr Nakutnyi <volodymyrnakutnyi@gmail.com>
2020-08-14 21:43:29 +0300

    revertSearchModel

--- src/app/model/search/search.model.ts
+++ src/app/model/search/search.model.ts
@@ -2,15 +2,7 @@ import { NewsSearchModel } from './newsSearch.model';
 import { TipsSearchModel } from './tipsSearch.model';
 
 export interface SearchModel {
-  id: number;
-  title: string;
-  tags: Array<string>;
   countOfResults: number;
   ecoNews: Array<NewsSearchModel>;
   tipsAndTricks: Array<TipsSearchModel>;
-  creationDate: string;
-  author: {
-    id: number;
-    name: string;
-  };
 }
