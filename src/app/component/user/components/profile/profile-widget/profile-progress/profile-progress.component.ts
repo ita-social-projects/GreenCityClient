551e760e518e98a3d491d9d4b7b920514cc54d91
Grishko <Agressor000000@gmail.com>
2020-08-19 15:16:53 +0300

    localization profile dashboard

--- src/app/component/user/components/profile/profile-widget/profile-progress/profile-progress.component.ts
+++ src/app/component/user/components/profile/profile-widget/profile-progress/profile-progress.component.ts
@@ -20,11 +20,9 @@ export class ProfileProgressComponent implements OnInit, OnDestroy {
   }
 
   public checkUserActivities(): void {
-    this.progressSubscription = this.profileService.getUserProfileStatistics()
-      .subscribe((statistics: ProfileStatistics) => {
-        console.log(statistics);
-        this.setUserProgress(statistics);
-      });
+    this.progressSubscription = this.profileService.getUserProfileStatistics().subscribe((statistics: ProfileStatistics) => {
+      this.setUserProgress(statistics);
+    });
   }
 
   public setUserProgress(item: ProfileStatistics) {
