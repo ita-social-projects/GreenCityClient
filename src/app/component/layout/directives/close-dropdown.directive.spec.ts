e5e99a9a71a3bc49513e720b2b31ce2260c159c6
Gusvarov <omelchenko.nazarii@gmail.com>
2020-08-29 12:01:01 +0300

    more fixes

--- src/app/component/layout/directives/close-dropdown.directive.spec.ts
+++ src/app/component/layout/directives/close-dropdown.directive.spec.ts
@@ -2,7 +2,5 @@ import { CloseDropdownDirective } from './close-dropdown.directive';
 
 describe('CloseDropdownDirective', () => {
   it('should create an instance', () => {
-    const directive = new CloseDropdownDirective();
-    expect(directive).toBeTruthy();
   });
 });
