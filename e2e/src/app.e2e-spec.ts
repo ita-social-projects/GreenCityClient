f98998c9f6dbf9a8229185bd1aeeaaa31e918478
Anton Hryshko <45083515+HryshkoAnton@users.noreply.github.com>
2020-09-03 23:06:50 +0300

    remove error from main page (#544)
    * remove error from main page
    Co-authored-by: mplesha <mykhaylo.plesha@gmail.com>

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -8,12 +8,7 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to GreenCityClient!');
-  });
-
-  afterEach(async () => {
+  it('Check if no errors emitted from the browser', async () => {
     // Assert that there are no errors emitted from the browser
     const logs = await browser.manage().logs().get(logging.Type.BROWSER);
     expect(logs).not.toContain(jasmine.objectContaining({
