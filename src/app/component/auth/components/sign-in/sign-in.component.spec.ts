9b78d4974ba59baa0e7fdc8a5eca46710537424e
mplesha <mykhaylo.plesha@gmail.com>
2020-09-04 08:10:34 +0300

    Update sign-in.component.spec.ts

--- src/app/component/auth/components/sign-in/sign-in.component.spec.ts
+++ src/app/component/auth/components/sign-in/sign-in.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SignInComponent } from './sign-in-new.component';
+import { SignInComponent } from './sign-in.component';
 
 describe('SignInNewComponent', () => {
   let component: SignInComponent;
