75fb9330eca7a238adbf6dbc40848617708e3278
yulkats <tsyulka3007@gmail.com>
2020-09-10 15:04:57 +0300

    fixed personal-photo.component.spec.ts

--- src/app/component/user/components/profile/edit-profile/personal-photo/personal-photo.component.spec.ts
+++ src/app/component/user/components/profile/edit-profile/personal-photo/personal-photo.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PersonalPhotoComponent } from './personal-photo.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatDialogModule } from '@angular/material/dialog';
+import { TranslateModule } from '@ngx-translate/core';
 
 describe('PersonalPhotoComponent', () => {
   let component: PersonalPhotoComponent;
@@ -8,7 +11,12 @@ describe('PersonalPhotoComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PersonalPhotoComponent ]
+      declarations: [ PersonalPhotoComponent ],
+      imports: [
+        MatDialogModule,
+        HttpClientTestingModule,
+        TranslateModule.forRoot(),
+      ]
     })
     .compileComponents();
   }));
