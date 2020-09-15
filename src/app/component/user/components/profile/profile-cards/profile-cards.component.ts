525c7e091e7564415056307a2c4159cc69d69ba1
Grishko <Agressor000000@gmail.com>
2020-09-15 17:39:32 +0300

    remove static height of fact card, add logic to profile-cards component

--- src/app/component/user/components/profile/profile-cards/profile-cards.component.ts
+++ src/app/component/user/components/profile/profile-cards/profile-cards.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { catchError } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
+import { CardModel } from '@user-models/card.model';
 import { ProfileService } from '../profile-service/profile.service';
 
 @Component({
@@ -6,18 +9,31 @@ import { ProfileService } from '../profile-service/profile.service';
   templateUrl: './profile-cards.component.html',
   styleUrls: ['./profile-cards.component.scss']
 })
-export class ProfileCardsComponent implements OnInit {
+export class ProfileCardsComponent implements OnInit, OnDestroy {
+  public profileSubscription: Subscription;
+  public factOfTheDay: CardModel;
+  public error;
 
-   constructor(private profileService: ProfileService) { }
-
-   public profileSubscription;
-   public facts = [];
+  constructor(private profileService: ProfileService) { }
 
   ngOnInit() {
-    this.profileSubscription = this.profileService.getFactsOfTheDay().subscribe(
-      (success) => {
-        this.facts = [success, ...this.facts];
-      }
-    );
+    this.getFactOfTheDay();
+  }
+
+  getFactOfTheDay(): void {
+    this.profileSubscription = this.profileService.getFactsOfTheDay()
+      .pipe(
+        catchError((error) => {
+          this.error = error;
+          return error;
+        })
+      )
+      .subscribe((success: CardModel) => {
+        this.factOfTheDay = success;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.profileSubscription.unsubscribe();
   }
 }
