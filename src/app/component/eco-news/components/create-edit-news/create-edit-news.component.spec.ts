0da4c6db1a9ee4c3ca9fa827f89fd9d8e7e43583
Grishko <Agressor000000@gmail.com>
2020-10-28 11:41:09 +0200

    tests

--- src/app/component/eco-news/components/create-edit-news/create-edit-news.component.spec.ts
+++ src/app/component/eco-news/components/create-edit-news/create-edit-news.component.spec.ts
@@ -1,6 +1,15 @@
+import { DragAndDropComponent } from './../../../shared/components/drag-and-drop/drag-and-drop.component';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { PostNewsLoaderComponent } from '../post-news-loader/post-news-loader.component';
 import { CreateEditNewsComponent } from './create-edit-news.component';
+import { TranslateModule } from '@ngx-translate/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ImageCropperModule } from 'ngx-image-cropper';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { MAT_DIALOG_DATA, MatDialogRef, MatDialogModule } from '@angular/material/dialog';
+import { ACTION_CONFIG, ACTION_TOKEN } from './action.constants';
+
 
 describe('CreateEditNewsComponent', () => {
   let component: CreateEditNewsComponent;
@@ -8,9 +17,23 @@ describe('CreateEditNewsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CreateEditNewsComponent ]
+      declarations: [CreateEditNewsComponent, PostNewsLoaderComponent, DragAndDropComponent],
+      imports: [
+        TranslateModule.forRoot(),
+        FormsModule,
+        ReactiveFormsModule,
+        ImageCropperModule,
+        RouterTestingModule,
+        HttpClientModule,
+        MatDialogModule
+      ],
+      providers: [
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+        { provide: MatDialogRef, useValue: {} },
+        { provide: ACTION_TOKEN, useValue: ACTION_CONFIG}
+      ],
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
