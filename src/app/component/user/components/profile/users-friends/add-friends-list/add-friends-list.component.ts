9e05c776b0a1632ff54cc634a0677ca87167f94a
skrebetsnataliia <skrebetsnatasha98@gmail.com>
2020-12-04 19:30:47 +0200

    fixed style issue

--- src/app/component/user/components/profile/users-friends/add-friends-list/add-friends-list.component.ts
+++ src/app/component/user/components/profile/users-friends/add-friends-list/add-friends-list.component.ts
@@ -9,7 +9,7 @@ import { UserFriendsService } from '@global-user/services/user-friends.service';
 })
 export class AddFriendsListComponent implements OnInit {
 
-  private items: FriendModel[];
+  public items: FriendModel[];
   public userId: number;
   public error: any;
 
@@ -19,8 +19,8 @@ export class AddFriendsListComponent implements OnInit {
 
   ngOnInit() {
     this.initUser();
-    this.userFriendsService.getRecommendedFriends(this.userId).subscribe(data => this.items=data,
-      error => {this.error = error.message; console.log(error);});
+    this.userFriendsService.getRecommendedFriends(this.userId).subscribe(data => this.items = data,
+      error => {this.error = error.message; console.log(error); });
     console.log(this.items);
   }
 
