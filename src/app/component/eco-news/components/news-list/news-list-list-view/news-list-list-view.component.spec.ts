e5819ca2345c73dcb216b9974fb344129f81b878
JimHolman1199 <jimholman1199@gmail.com>
2020-12-13 14:56:24 +0200

    [TASK] write tests to eco-news components, change layout of list component, fix all habit tests

--- src/app/component/eco-news/components/news-list/news-list-list-view/news-list-list-view.component.spec.ts
+++ src/app/component/eco-news/components/news-list/news-list-list-view/news-list-list-view.component.spec.ts
@@ -1,10 +1,23 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { EcoNewsModel } from '@eco-news-models/eco-news-model';
 
 import { NewsListListViewComponent } from './news-list-list-view.component';
 
 describe('NewsListListViewComponent', () => {
   let component: NewsListListViewComponent;
   let fixture: ComponentFixture<NewsListListViewComponent>;
+  const ecoNewsMock: EcoNewsModel = {
+    id: 1,
+    imagePath: 'string',
+    title: 'string',
+    text: 'string',
+    author: {
+        id: 1,
+        name: 'string'
+    },
+    tags: ['test'],
+    creationDate: '11',
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,10 +29,19 @@ describe('NewsListListViewComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(NewsListListViewComponent);
     component = fixture.componentInstance;
+    component.ecoNewsModel = ecoNewsMock;
+    component.profileIcons.newsDefaultPictureList = 'defaultImagePath';
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should get default image', () => {
+    ecoNewsMock.imagePath = ' ';
+    component.ecoNewsModel = ecoNewsMock;
+    component.checkNewsImage();
+    expect(component.newsImage).toBe('defaultImagePath');
   });
 });
--- src/app/component/eco-news/components/news-list/news-list-list-view/news-list-list-view.component.ts
+++ src/app/component/eco-news/components/news-list/news-list-list-view/news-list-list-view.component.ts
@@ -8,55 +8,16 @@ import { ecoNewsIcons } from 'src/app/image-pathes/profile-icons';
   styleUrls: ['./news-list-list-view.component.scss'],
   changeDetection: 0
 })
-export class NewsListListViewComponent implements AfterViewChecked {
+export class NewsListListViewComponent {
   @Input() ecoNewsModel: EcoNewsModel;
-  @ViewChild('titleHeight', {static: true}) titleHeight: ElementRef;
-  @ViewChild('textHeight', {static: true}) textHeight: ElementRef;
 
   public profileIcons = ecoNewsIcons;
   public newsImage: string;
-  public titleHeightOfElement: number;
-  public textHeightOfElement: number;
-  private quantityOfLines = {
-    hiddenSize: 0,
-    sSize: 28,
-    smSize: 32,
-    msSize: 52,
-    mSize: 64,
-    lSize: 96,
-  };
 
-  constructor(private renderer: Renderer2) {
-  }
-
-  ngAfterViewChecked() {
-    this.checkHeightOfTittle();
-  }
-
-  public onResize(): void {
-    this.checkHeightOfTittle();
-  }
+  constructor() {}
 
   public checkNewsImage(): string {
     return this.newsImage = (this.ecoNewsModel.imagePath && this.ecoNewsModel.imagePath !== ' ') ?
                               this.ecoNewsModel.imagePath : this.profileIcons.newsDefaultPictureList;
-  }
-
-  public checkHeightOfTittle(): void {
-    this.titleHeightOfElement = this.titleHeight.nativeElement.offsetHeight;
-    this.textHeightOfElement = this.calculateElementHeight();
-    this.renderer.setStyle(this.textHeight.nativeElement, 'height', `${this.textHeightOfElement}px`);
-  }
-
-  public calculateElementHeight(): number {
-    const titleHeight = this.titleHeightOfElement;
-    const linesQuantity = this.quantityOfLines;
-
-    const firstCheck = (titleHeight <= linesQuantity.lSize) ?
-                          linesQuantity.sSize : linesQuantity.hiddenSize;
-    const secondCheck = (titleHeight <= linesQuantity.mSize) ?
-                          linesQuantity.msSize : firstCheck;
-    return (titleHeight <= linesQuantity.smSize) ?
-                          linesQuantity.lSize : secondCheck;
   }
 }
