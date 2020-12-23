7ffdc6221d91c1798138fe22d0431d3124e0eb83
skrebetsnataliia <skrebetsnatasha98@gmail.com>
2020-12-23 12:53:15 +0200

    written unit tests for friend-item component

---
+++ src/app/component/user/components/profile/users-friends/friend-dashboard/friend-item/friend-item.component.spec.ts
@@ -0,0 +1,54 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { TranslateModule } from '@ngx-translate/core';
+
+import { FriendItemComponent } from './friend-item.component';
+
+describe('FriendItemComponent', () => {
+  let component: FriendItemComponent;
+  let fixture: ComponentFixture<FriendItemComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ FriendItemComponent ],
+      imports: [
+        TranslateModule.forRoot(),
+        HttpClientTestingModule,
+        RouterTestingModule.withRoutes([])
+      ],
+      schemas: [
+        CUSTOM_ELEMENTS_SCHEMA,
+        NO_ERRORS_SCHEMA]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FriendItemComponent);
+    component = fixture.componentInstance;
+    component.friend = { id: 1,
+      name: "Name",
+      profilePicture: "./",
+      added: true
+    }
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it( '', () => {
+    spyOn(component.addFriendEvent, "emit");
+    component.addFriend(component.friend.id);
+    expect(component.addFriendEvent.emit).toHaveBeenCalledWith(1);
+  })
+
+  it( '', () => {
+    spyOn(component.deleteFriendEvent, "emit");
+    component.deleteFriend(component.friend.id);
+    expect(component.deleteFriendEvent.emit).toHaveBeenCalledWith(1);
+  })
+});
