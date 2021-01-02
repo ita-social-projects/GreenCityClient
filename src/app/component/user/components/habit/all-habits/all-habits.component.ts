e3cff88683308768a057062e8d846b574f090b12
luchward <eduard.luchuk@gmail.com>
2021-01-02 21:44:17 +0200

    Back button has been added

--- src/app/component/user/components/habit/all-habits/all-habits.component.ts
+++ src/app/component/user/components/habit/all-habits/all-habits.component.ts
@@ -6,6 +6,9 @@ import { LocalStorageService } from '@global-service/localstorage/local-storage.
 import { HabitService } from '@global-service/habit/habit.service';
 import { map } from 'rxjs/operators';
 import { HabitInterface, HabitListInterface } from '../../../../../interface/habit/habit.interface';
+import { singleNewsImages } from '../../../../../image-pathes/single-news-images';
+import {HabitAssignService} from '@global-service/habit-assign/habit-assign.service';
+import {ProfileService} from '@global-user/components/profile/profile-service/profile.service';
 
 @Component({
   selector: 'app-all-habits',
@@ -29,10 +32,12 @@ export class AllHabitsComponent implements OnInit, OnDestroy {
   private habitsList: HabitInterface[] = [];
   private lang: string;
   private batchSize = 6;
+  public images = singleNewsImages;
 
   constructor( private habitService: HabitService,
                private localStorageService: LocalStorageService,
-               private translate: TranslateService ) { }
+               private translate: TranslateService,
+               public profileService: ProfileService ) { }
 
   ngOnInit() {
     this.onResize();
