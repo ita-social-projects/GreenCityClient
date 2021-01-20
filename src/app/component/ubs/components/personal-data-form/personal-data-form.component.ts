f061853f4c9fb1dbda096de3871fac1711cd906f
Volodymyr Lukashevych <volodia.lukashewwich@gmail.com>
2021-01-20 11:01:17 +0200

    did personal-data form

--- src/app/component/ubs/components/personal-data-form/personal-data-form.component.ts
+++ src/app/component/ubs/components/personal-data-form/personal-data-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-personal-data-form',
@@ -12,7 +12,59 @@ export class PersonalDataFormComponent implements OnInit {
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
-    this.personalDataForm = this.fb.group({});
+    this.personalDataForm = this.fb.group({
+      personalData: this.fb.group({
+        name: [null, [
+          Validators.required,
+          Validators.minLength(1),
+          Validators.maxLength(30),
+          Validators.pattern(/^[A-Za-zА-Яа-яїієё\.\'\-\\]+$/)
+        ]],
+        surname: [null, [
+          Validators.required,
+          Validators.minLength(1),
+          Validators.maxLength(30),
+          Validators.pattern(/^[A-Za-zА-Яа-яїієё\.\'\-\\]+$/)
+        ]],
+        email: [null, [
+          Validators.required,
+          Validators.email
+        ]],
+        phone: [null, [
+          Validators.required,
+          Validators.minLength(18)
+        ]]
+      }),
+      adress: this.fb.group({
+        city: [null, Validators.required],
+        district: [null, Validators.required],
+        street: [null, [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(40),
+          Validators.pattern(/^[A-Za-zА-Яа-яїієё0-9\'\,\-\ \\]+$/)
+        ]],
+        building: [null, [
+          Validators.required,
+          Validators.minLength(1),
+          Validators.maxLength(4),
+          Validators.pattern(/^[A-Za-zА-Яа-яїієё0-9]+$/)
+        ]],
+        block: [null, [
+          Validators.maxLength(2),
+          Validators.pattern(/^[A-Za-zА-Яа-яїієё0-9]+$/)
+        ]],
+        entrance: [null, [
+          Validators.maxLength(2),
+          Validators.pattern(/^-?(0|[1-9]\d*)?$/)
+        ]],
+        comment: [null, Validators.maxLength(170)]
+      })
+    });
+  }
+
+  submit(): void {
+    console.log(this.personalDataForm.value);
   }
 
 }
