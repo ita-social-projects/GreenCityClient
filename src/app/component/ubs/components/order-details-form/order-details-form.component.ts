d6c6bd1533e1641333cd9e3879b1c4a7cdcb5839
Kyrylo <kabramov.rc@gmail.com>
2021-01-21 18:00:14 +0200

    order-form first step

--- src/app/component/ubs/components/order-details-form/order-details-form.component.ts
+++ src/app/component/ubs/components/order-details-form/order-details-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-order-details-form',
@@ -8,11 +8,40 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class OrderDetailsFormComponent implements OnInit {
   orderDetailsForm: FormGroup;
+  total: number;
+  displayMes: boolean = false;
+  subBtn: boolean = true;
 
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
-    this.orderDetailsForm = this.fb.group({});
+    this.orderDetailsForm = this.fb.group({
+      bagServiceUbs: ['Пакет УБС Кур’єр'],
+      bagNumUbs: [0, Validators.required],
+      bagSizeUbs: ['120 л (250 грн)'],
+      bagPriceUbs: [''],
+      bagServiceClothesXL: ['Пакет Безнадійний одяг'],
+      bagNumClothesXL: [0, Validators.required],
+      bagSizeClothesXL: ['120 л (300 грн)'],
+      bagPriceClothesXL: [''],
+      bagServiceClothesM: ['Пакет Безнадійний одяг'],
+      bagNumClothesM: [0],
+      bagSizeClothesM: ['35 л (150 грн)'],
+      bagPriceClothesM: ['']
+    });
   }
 
+  calc() {
+    this.orderDetailsForm.patchValue({
+      bagPriceUbs: this.orderDetailsForm.value.bagNumUbs * 250,
+      bagPriceClothesXL: this.orderDetailsForm.value.bagNumClothesXL * 300,
+      bagPriceClothesM: this.orderDetailsForm.value.bagNumClothesM * 150
+    });
+    this.total = this.orderDetailsForm.value.bagNumUbs * 250 + this.orderDetailsForm.value.bagNumClothesXL * 300 +
+      this.orderDetailsForm.value.bagNumClothesM * 150;
+    if (this.total < 500) {
+      this.displayMes = true;
+      this.subBtn = true;
+    } else {this.displayMes = false; this.subBtn = false;}
+  }
 }
