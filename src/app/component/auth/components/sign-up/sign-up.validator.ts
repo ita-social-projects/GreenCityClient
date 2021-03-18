51b4c748bd6b7a32137d8fe173d580b92df07c13
Antonenko Olexiy <antonenkoolexiy.com>
2021-03-18 08:58:03 +0200

    Closes #1795 An improper message on entering to short password

--- src/app/component/auth/components/sign-up/sign-up.validator.ts
+++ src/app/component/auth/components/sign-up/sign-up.validator.ts
@@ -25,8 +25,12 @@ export function ValidatorRegExp(controlName: string) {
     if (control.value === '') {
       control.setErrors({ required: true });
     } else {
-      if (!control.value.match(regexp)) {
+      if (controlName === 'password' && control.value.length < 9 ) {
+        control.setErrors({ minlength: true });
+
+      } else if (!control.value.match(regexp)) {
         control.setErrors({ symbolInvalid: true });
+
       } else {
         control.setErrors(null);
       }
