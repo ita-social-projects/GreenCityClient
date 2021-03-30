20784bbae0532facd139617a97ba06728f8cf6af
Vitaly.Borysenko <vitaly.borysenko@gmail.com>
2021-03-30 17:27:55 +0300

    closes fixed ubs navigate while no user (#785)
    * added signin popup when no user
    
    * fixed ubs navigate while no user

--- src/app/component/ubs/ubs-routing.module.ts
+++ src/app/component/ubs/ubs-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthPageGuardService } from '@global-service/route-guards/auth-page-guard.service';
 import { UBSOrderFormComponent } from './components/ubs-order-form/ubs-order-form.component';
 import { UbsComponent } from './ubs.component';
 
-const ubsRoutes: Routes = [{
-  path: '', component: UbsComponent, children: [
-    {path: '', component: UBSOrderFormComponent}
-  ]
-}];
+const ubsRoutes: Routes = [
+  { path: '',
+    component: UbsComponent,
+    canActivate: [ AuthPageGuardService ],
+    children: [
+      { path: '', component: UBSOrderFormComponent }
+    ]
+  }];
 
 @NgModule({
   imports: [RouterModule.forChild(ubsRoutes)],
