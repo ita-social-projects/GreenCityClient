62b642fd4d5857b9e81d7409a67014bf3584297e
idzhersh <50026335+idzhersh@users.noreply.github.com>
2021-04-03 15:56:39 +0300

    Update habit-edit-shopping-list.component.ts

--- src/app/component/user/components/habit/add-new-habit/habit-edit-shopping-list/habit-edit-shopping-list.component.ts
+++ src/app/component/user/components/habit/add-new-habit/habit-edit-shopping-list/habit-edit-shopping-list.component.ts
@@ -1,13 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-habit-edit-shopping-list',
   templateUrl: './habit-edit-shopping-list.component.html',
   styleUrls: ['./habit-edit-shopping-list.component.scss']
 })
-export class HabitEditShoppingListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {}
-}
+export class HabitEditShoppingListComponent {}
