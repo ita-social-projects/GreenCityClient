05d7f73870f968a9dcc96e76b3848d5795a55f39
Kyrylo <kabramov.rc@gmail.com>
2021-04-06 13:15:06 +0300

    removed log

--- src/app/component/shared/components/drag-and-drop/drag-and-drop.component.ts
+++ src/app/component/shared/components/drag-and-drop/drag-and-drop.component.ts
@@ -41,7 +41,6 @@ export class DragAndDropComponent implements OnInit {
 
   public patchImage(): void {
     if (this.createEcoNewsService.isBackToEditing && this.formData.value.image) {
-      console.log('works');
       this.isCropper = false;
       this.files = [{file: name, url: this.formData.value.image}];
     }
