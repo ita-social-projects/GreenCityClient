ff514b6c9ab5245aafe1c2bc1c3ca09a09b7ba59
Kyrylo <kabramov.rc@gmail.com>
2021-04-09 00:25:24 +0300

    header navigation accessability

--- src/app/component/layout/components/header/header.component.ts
+++ src/app/component/layout/components/header/header.component.ts
@@ -34,9 +34,9 @@ export class HeaderComponent implements OnInit, OnDestroy {
   public isAllSearchOpen = false;
   public toggleBurgerMenu = false;
   public arrayLang: Array<LanguageModel> = [
-    { lang: 'Ua' },
-    { lang: 'En' },
-    { lang: 'Ru' }];
+    { lang: 'Ua', langName: 'ukrainian' },
+    { lang: 'En', langName: 'english' },
+    { lang: 'Ru', langName: 'russian' }];
   public isSearchClicked = false;
   private adminRoleValue = 'ROLE_ADMIN';
   private userRole: string;
@@ -124,7 +124,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
 
   setLangArr(): void {
     const language = this.languageService.getCurrentLanguage();
-    const currentLangObj = { lang: language.charAt(0).toUpperCase() + language.slice(1) };
+    const currentLangObj = { lang: language.charAt(0).toUpperCase() + language.slice(1), langName: language };
     const currentLangIndex = this.arrayLang.findIndex(lang => lang.lang === currentLangObj.lang);
     this.arrayLang = [
       currentLangObj,
--- src/app/component/layout/components/models/languageModel.ts
+++ src/app/component/layout/components/models/languageModel.ts
@@ -1,3 +1,4 @@
 export interface LanguageModel {
   lang: string;
+  langName: string;
 }
