59923c35b1e675b1c1916f74f18c99e5f1bc358f
Yurii Lisunov <lisykgerard50@gmail.com>
2021-04-16 19:10:58 +0300

    wrote tests for profile-service

---
+++ src/app/component/user/components/profile/profile-service/profile.service.spec.ts
@@ -0,0 +1,153 @@
+import { Language } from '@language-service/Language';
+import { LanguageService } from '@language-service/language.service';
+import { BehaviorSubject } from 'rxjs';
+import { LocalStorageService } from '@global-service/localstorage/local-storage.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import { ProfileService } from './profile.service';
+
+
+describe('ProfileService', () => {
+
+  let profileService: ProfileService;
+  let httpMock: HttpTestingController;
+
+  let localStorageServiceMock: LocalStorageService;
+  localStorageServiceMock = jasmine.createSpyObj('LocalStorageService', ['userIdBehaviourSubject']);
+  localStorageServiceMock.userIdBehaviourSubject = new BehaviorSubject(1111);
+
+  let languageServiceMock: LanguageService;
+  languageServiceMock = jasmine.createSpyObj('LanguageService', ['getCurrentLanguage']);
+  languageServiceMock.getCurrentLanguage = () => 'en' as Language;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ],
+      providers: [
+        ProfileService,
+        { provide: LocalStorageService, useValue : localStorageServiceMock },
+        { provide: LanguageService, useValue : languageServiceMock }
+      ]
+    });
+    profileService = TestBed.get(ProfileService);
+    httpMock = TestBed.get(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should be created', () => {
+    const service: ProfileService = TestBed.get(ProfileService);
+    expect(service).toBeTruthy();
+  });
+
+  it('should set user id', () => {
+    let userId = null;
+    // @ts-ignore
+    profileService.localStorageService.userIdBehaviourSubject
+    .subscribe(id => userId = id);
+    expect(userId).toBe(1111);
+  });
+
+  describe('getFactsOfTheDay', () => {
+    it('should return fact of the day', () => {
+      const fact = { id: 1, content: 'Great day!'};
+      profileService.getFactsOfTheDay().subscribe(info => {
+        expect(info.content).toBe('Great day!');
+      });
+
+      const req = httpMock.expectOne(`https://greencity.azurewebsites.net/factoftheday/?lang=en`);
+      expect(req.request.method).toBe('GET');
+      req.flush(fact);
+    });
+  });
+
+  describe('getUserInfo', () => {
+    it('should return user info', () => {
+      const userInfo = {
+        city: 'Lviv',
+        firstName: 'Anton',
+        userCredo: 'Eco',
+        profilePicturePath: 'somepath',
+        rating: 1999,
+        showEcoPlace: true,
+        showLocation: false,
+        showShoppingList: true,
+        socialNetworks: []
+      };
+
+      profileService.getUserInfo().subscribe(info => {
+        expect(info.rating).toBe(1999);
+      });
+
+      const req = httpMock.expectOne(`https://greencity-user.azurewebsites.net/user/1111/profile/`);
+      expect(req.request.method).toBe('GET');
+      req.flush(userInfo);
+    });
+  });
+
+  describe('getUserProfileStatistics', () => {
+    it('should return user profile statistics', () => {
+      const stat = {
+        amountHabitsInProgress: 2,
+        amountHabitsAcquired: 1,
+        amountWrittenTipsAndTrick: 0,
+        amountPublishedNews: 7
+    };
+
+      profileService.getUserProfileStatistics().subscribe(info => {
+        expect(info.amountHabitsAcquired).toBe(1);
+      });
+
+      const req = httpMock.expectOne(`https://greencity-user.azurewebsites.net/user/1111/profileStatistics/`);
+      expect(req.request.method).toBe('GET');
+      req.flush(stat);
+    });
+  });
+
+  describe('getEcoPlaces', () => {
+    it('should return eco-places', () => {
+      const places = [
+        {
+          placeId: 1,
+          name: 'string'
+        },
+        {
+        placeId: 2,
+        name: 'string2'
+        }
+      ];
+
+      profileService.getEcoPlaces().subscribe(info => {
+        expect(info.length).toBe(2);
+      });
+
+      const req = httpMock.expectOne(`https://greencity.azurewebsites.net/favorite_place/`);
+      expect(req.request.method).toBe('GET');
+      req.flush(places);
+    });
+  });
+
+  describe('getUserFriends', () => {
+    it('should six user friends', () => {
+      const userFriends = {
+        amountOfFriends: 30,
+        pagedFriends: {
+          currentPage: 1,
+          page: [],
+          totalElements: 6,
+          totalPages: 1
+        }
+      };
+
+      profileService.getUserFriends().subscribe(info => {
+        expect(info.pagedFriends.totalElements).toBe(6);
+      });
+
+      const req = httpMock.expectOne(`https://greencity-user.azurewebsites.net/user/1111/sixUserFriends/`);
+      expect(req.request.method).toBe('GET');
+      req.flush(userFriends);
+    });
+  });
+});
