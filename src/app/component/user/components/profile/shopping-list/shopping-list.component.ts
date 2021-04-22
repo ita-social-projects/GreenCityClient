ba4ea8943a0d7904ee16b4793548b652620bdd5d
Edw1x <erqwrewre12@gmail.com>
2021-04-22 14:52:33 +0300

    load bug fixed, slight design change, language support added

--- src/app/component/user/components/profile/shopping-list/shopping-list.component.ts
+++ src/app/component/user/components/profile/shopping-list/shopping-list.component.ts
@@ -1,42 +1,47 @@
-import { takeUntil } from 'rxjs/operators';
-import { ProfileService } from './../profile-service/profile.service';
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ShoppingList } from '@global-user/models/shoppinglist.model';
-import { Subscription, Subject } from 'rxjs';
+import { finalize, takeUntil } from "rxjs/operators";
+import { ProfileService } from "./../profile-service/profile.service";
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { ShoppingList } from "@global-user/models/shoppinglist.model";
+import { Subscription, Subject } from "rxjs";
 
 @Component({
-  selector: 'app-shopping-list',
-  templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.scss']
+  selector: "app-shopping-list",
+  templateUrl: "./shopping-list.component.html",
+  styleUrls: ["./shopping-list.component.scss"],
 })
 export class ShoppingListComponent implements OnInit, OnDestroy {
   public shoppingList: ShoppingList[];
   public profileSubscription: Subscription;
   private destroy$ = new Subject<void>();
-  constructor(private profileService: ProfileService) { }
+  constructor(private profileService: ProfileService) {}
 
   ngOnInit() {
     this.getShoppingList();
   }
 
   public getShoppingList(): void {
-    this.profileSubscription = this.profileService.getShoppingList()
-    .subscribe(
+    this.profileSubscription = this.profileService.getShoppingList().pipe(
+      takeUntil(this.destroy$),
+      finalize(() => {
+        if (!this.shoppingList) this.shoppingList = [];
+      })
+    ).subscribe(
       (shoppingListArr: ShoppingList[]) => this.shoppingList = shoppingListArr,
-      error => this.shoppingList = []
+      (error) => this.shoppingList = [],
     );
   }
 
   public toggleDone(item): void {
-    this.profileService.toggleStatusOfShoppingItem(item)
-    .pipe(takeUntil(this.destroy$))
-    .subscribe((success) => this.updateDataOnUi(item));
+    this.profileService
+      .toggleStatusOfShoppingItem(item)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((success) => this.updateDataOnUi(item));
   }
 
   private updateDataOnUi(item): any {
     const { status: prevItemStatus } = item;
-    const newItemStatus = prevItemStatus === 'ACTIVE' ? 'DONE' : 'ACTIVE';
-    return item.status = newItemStatus;
+    const newItemStatus = prevItemStatus === "ACTIVE" ? "DONE" : "ACTIVE";
+    return (item.status = newItemStatus);
   }
 
   ngOnDestroy() {
