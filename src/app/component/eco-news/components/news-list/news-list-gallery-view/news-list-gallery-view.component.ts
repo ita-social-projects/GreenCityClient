de4ca622d2dd593e11e516178bf17ecd8ee73c60
Roman Paslavskyi <46484914+paslavskyi9roman@users.noreply.github.com>
2021-05-08 15:25:05 +0300

    Bugfix/#1896 fixing the shading of the text (#861)
    * removing unnecessary div
    
    * work on displaying news body
    
    * Fixing the shading
    
    * fixing the slicing of the title
    
    * adding triple dot at the end of the visible text
    
    * removing unused code
    
    * fixing the linter check
    
    * adding responsive rule

--- src/app/component/eco-news/components/news-list/news-list-gallery-view/news-list-gallery-view.component.ts
+++ src/app/component/eco-news/components/news-list/news-list-gallery-view/news-list-gallery-view.component.ts
@@ -6,7 +6,7 @@ import { ecoNewsIcons } from 'src/app/image-pathes/profile-icons';
   selector: 'app-news-list-gallery-view',
   templateUrl: './news-list-gallery-view.component.html',
   styleUrls: ['./news-list-gallery-view.component.scss'],
-  changeDetection: 0,
+  changeDetection: 0
 })
 export class NewsListGalleryViewComponent {
   @Input() ecoNewsModel: EcoNewsModel;
