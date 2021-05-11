32fc56555421f28a45eecfafd76eb63e1e27e17e
Edw1x <erqwrewre12@gmail.com>
2021-05-11 18:31:04 +0300

    Data moved to separated file

--- src/app/component/places/places.component.ts
+++ src/app/component/places/places.component.ts
@@ -4,6 +4,10 @@ import { LocalStorageService } from '@global-service/localstorage/local-storage.
 import { HttpClient } from '@angular/common/http';
 import { MatDrawer } from '@angular/material';
 import { AgmMarker } from '@agm/core';
+
+import {markers} from './Data.js'
+import {cards} from './Data.js'
+
 @Component({
   selector: 'app-places',
   templateUrl: './places.component.html',
@@ -48,86 +52,8 @@ export class PlacesComponent implements OnInit, DoCheck {
       cardStars: []
     };
 
-    this.cardsCollection = [
-      {
-        id: 0,
-        cardName: 'Hotel Lviv',
-        cardAddress: 'Hotel address, №123',
-        cardText: 'this is Hotel Lviv',
-        cardImgUrl: 'assets/img/places/place_lviv-hotel.png',
-        cardRating: 0.3,
-        favorite: false
-      },
-      {
-        id: 1,
-        cardName: 'Magnus',
-        cardAddress: 'magnus address',
-        cardText: 'this is Magnus',
-        cardImgUrl: 'assets/img/places/place_magnus.png',
-        cardRating: 0.5,
-        favorite: false
-      },
-      {
-        id: 2,
-        cardName: 'McDonalds',
-        cardAddress: 'mac address, №123',
-        cardText: 'this is McDonalds',
-        cardImgUrl: 'assets/img/places/place_McDonalds.png',
-        cardRating: 0,
-        favorite: false
-      },
-      {
-        id: 3,
-        cardName: 'KFC',
-        cardAddress: 'KFC address, №123',
-        cardText: 'this is KFC',
-        cardImgUrl: 'assets/img/places/place_KFC.png',
-        cardRating: 0.9,
-        favorite: false
-      },
-      {
-        id: 4,
-        cardName: 'Staff',
-        cardAddress: 'Staff address, №123',
-        cardText: 'this is Staff',
-        cardImgUrl: 'assets/img/places/place_staff.png',
-        cardRating: 1,
-        favorite: false
-      }
-    ];
-
-    this.markerList = [
-      {
-        lat: 49.84579876141749,
-        lng: 24.025125288060412,
-        iconUrl: this.redIcon,
-        card: this.cardsCollection[0]
-      },
-      {
-        lat: 49.84423766639821,
-        lng: 24.02405838813841,
-        iconUrl: this.redIcon,
-        card: this.cardsCollection[1]
-      },
-      {
-        lat: 49.842959399097296,
-        lng: 24.026151152038356,
-        iconUrl: this.redIcon,
-        card: this.cardsCollection[2]
-      },
-      {
-        lat: 49.841649695791766,
-        lng: 24.026889537979542,
-        iconUrl: this.redIcon,
-        card: this.cardsCollection[3]
-      },
-      {
-        lat: 49.83929345961723,
-        lng: 24.025845314617513,
-        iconUrl: this.redIcon,
-        card: this.cardsCollection[4]
-      }
-    ];
+    this.cardsCollection = cards;
+    this.markerList = markers;
 
     this.markerListCopy = this.markerList.slice();
 
