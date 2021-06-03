8f7c5047e9d99d5f7ded66a6dfe490ff3b0e384c
Vlad Sevostianov <flypsoff@gmail.com>
2021-06-03 18:27:08 +0300

    components for employee

---
+++ src/app/ubs-admin/components/ubs-admin-employee/ubs-admin-employee.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ubs-admin-employee',
+  templateUrl: './ubs-admin-employee.component.html',
+  styleUrls: ['./ubs-admin-employee.component.scss']
+})
+export class UbsAdminEmployeeComponent {
+  fakeData = [
+    {
+      name: 'Богдан-Ігор',
+      surname: 'Антонович-Барський',
+      phoneNumber: '+380931123344',
+      email: 'avety_longname_andsurname@example.com',
+      role: ['Менеджер послуги', 'Менеджер обдзвону'],
+      location: ['Саперно-Слобідська', 'Петрівка'],
+      image: 'assets/img/profile/achievements/achievement-1.png'
+    },
+    {
+      name: 'Jake',
+      surname: 'Jhonson',
+      phoneNumber: '+380931123344',
+      email: 'avety_longname_andsurname@example.com',
+      role: ['Philologist', 'very long be here'],
+      location: ['Ivano-Frankivsk', 'Petrivka city']
+    },
+    {
+      name: 'Kohn',
+      surname: 'Deeer',
+      phoneNumber: '+380931123344',
+      email: 'vasiDeeertefanyk@example.com',
+      role: ['Philologist', 'very must be here'],
+      location: ['Ivano-Frankivsk', 'Petrivka city']
+    }
+  ];
+
+  constructor() {}
+}
