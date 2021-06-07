dc9ea73ec2a8526e843ef26f9fa7d00be43d85dc
Vlad Sevostianov <flypsoff@gmail.com>
2021-06-07 16:12:38 +0300

    some changes with styles

--- src/app/ubs-admin/components/ubs-admin-employee/ubs-admin-employee.component.ts
+++ src/app/ubs-admin/components/ubs-admin-employee/ubs-admin-employee.component.ts
@@ -31,6 +31,15 @@ export class UbsAdminEmployeeComponent {
       email: 'vasiDeeertefanyk@example.com',
       role: ['Philologist', 'very must be here'],
       location: ['Ivano-Frankivsk', 'Petrivka city']
+    },
+    {
+      name: 'Богдан-Ігор',
+      surname: 'Антонович-Барський-Антонович',
+      phoneNumber: '+380931123344',
+      email: 'avety_longname_andsurname@example.com',
+      role: ['Менеджер послуги', 'Менеджер обдзвону всіх присутніх людей'],
+      location: ['Саперно-Слобідська', 'Петрівка'],
+      image: 'assets/img/profile/achievements/achievement-1.png'
     }
   ];
 
