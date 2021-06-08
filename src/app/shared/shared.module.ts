2a47c0ad576afbf2e60f776e334d4876f664c00d
Yurii <Yurii_Pryshliak@external.mckinsey.com>
2021-06-08 18:37:37 +0300

    Created Localized currency pipe

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../material.module';
 import { SpinnerComponent } from './spinner/spinner.component';
+import { LocalizedCurrencyPipe } from './localized-currency-pipe/localized-currency.pipe';
 
 @NgModule({
-  exports: [SpinnerComponent],
-  declarations: [SpinnerComponent],
+  exports: [SpinnerComponent, LocalizedCurrencyPipe],
+  declarations: [SpinnerComponent, LocalizedCurrencyPipe],
   imports: [CommonModule, MaterialModule]
 })
 export class SharedModule {}
