0f6be3743b3f01f81e785b92bb2af2f89fed4fd3
YuriShevchuk <yurii.shev4uk@gmail.com>
2021-06-17 10:25:05 +0300

    add route

--- src/app/ubs-admin/ubs-admin-routing.module.ts
+++ src/app/ubs-admin/ubs-admin-routing.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UbsAdminTableComponent } from './components/ubs-admin-table/ubs-admin-table.component';
 import { UbsAdminComponent } from './ubs-admin.component';
+import { UbsAdminOrderComponent } from './components/ubs-admin-order/ubs-admin-order.component';
 
 const ubsAdminRoutes: Routes = [
   {
     path: '',
     component: UbsAdminComponent,
     children: [{ path: '', component: UbsAdminTableComponent }]
-  }
+  },
+  {
+    path: 'order',
+    component: UbsAdminOrderComponent
+  },
 ];
 
 @NgModule({
