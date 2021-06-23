dbdece38ff470cfcf19e7c0445228c1a308bb26b
YuriShevchuk <yurii.shev4uk@gmail.com>
2021-06-23 11:21:48 +0300

    delete empty method & lines

--- src/app/ubs-admin/components/ubs-admin-order-status/ubs-admin-order-status.component.ts
+++ src/app/ubs-admin/components/ubs-admin-order-status/ubs-admin-order-status.component.ts
@@ -1,12 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-ubs-admin-order-status',
   templateUrl: './ubs-admin-order-status.component.html',
   styleUrls: ['./ubs-admin-order-status.component.scss']
 })
-export class UbsAdminOrderStatusComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit() {}
-}
+export class UbsAdminOrderStatusComponent {}
