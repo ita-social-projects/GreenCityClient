926d6d249bec4f3cd8c738d217cc56a6f5b2069d
Edw1x <43093994+Edw1x@users.noreply.github.com>
2021-06-28 17:38:10 +0300

    Deleted '_'

--- src/app/main/component/ubs/components/ubs-personal-information/ubs-personal-information.component.ts
+++ src/app/main/component/ubs/components/ubs-personal-information/ubs-personal-information.component.ts
@@ -65,7 +65,7 @@ export class UBSPersonalInformationComponent extends FormBaseComponent implement
       .findAllAddresses()
       .pipe(takeUntil(this.destroy))
       .subscribe((list) => {
-        this.addresses = this._getLastAddresses(list.addressList);
+        this.addresses = this.getLastAddresses(list.addressList);
         this.personalDataForm.patchValue({
           address: this.addresses
         });
@@ -76,7 +76,7 @@ export class UBSPersonalInformationComponent extends FormBaseComponent implement
       });
   }
 
-  private _getLastAddresses(addressList: Address[]) {
+  private getLastAddresses(addressList: Address[]) {
     const lastAddresses = -4;
     return addressList.slice(lastAddresses);
   }
