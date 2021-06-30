f281edddf23ba53d10ae97b15b90db4817eba60b
Edw1x <43093994+Edw1x@users.noreply.github.com>
2021-06-30 09:26:19 +0300

    [Bugfix] Fondy and additional bugs fixed #2921 (#952)
    * Fondy and additional bugs fixed
    
    * Changed location from new window to current window

--- src/app/main/component/ubs/components/ubs-order-form/ubs-order-form.component.ts
+++ src/app/main/component/ubs/components/ubs-order-form/ubs-order-form.component.ts
@@ -1,22 +1,26 @@
-import { AfterViewInit, Component, ChangeDetectorRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ChangeDetectorRef, ViewChild, DoCheck } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { UBSSubmitOrderComponent } from '../ubs-submit-order/ubs-submit-order.component';
 import { UBSPersonalInformationComponent } from '../ubs-personal-information/ubs-personal-information.component';
 import { UBSOrderDetailsComponent } from '../ubs-order-details/ubs-order-details.component';
+import { MatHorizontalStepper } from '@angular/material/stepper';
 
 @Component({
   selector: 'app-ubs-order-form',
   templateUrl: './ubs-order-form.component.html',
-  styleUrls: ['./ubs-order-form.component.scss'],
+  styleUrls: ['./ubs-order-form.component.scss']
 })
-export class UBSOrderFormComponent implements AfterViewInit {
+export class UBSOrderFormComponent implements AfterViewInit, DoCheck {
   firstStepForm: FormGroup;
   secondStepForm: FormGroup;
   thirdStepForm: FormGroup;
 
+  completed = false;
+
   @ViewChild('firstStep', { static: false }) stepOneComponent: UBSOrderDetailsComponent;
   @ViewChild('secondStep', { static: false }) stepTwoComponent: UBSPersonalInformationComponent;
   @ViewChild('thirdStep', { static: false }) stepThreeComponent: UBSSubmitOrderComponent;
+  @ViewChild(MatHorizontalStepper, { static: false }) stepper: MatHorizontalStepper;
 
   constructor(private cdr: ChangeDetectorRef) {}
 
@@ -25,5 +29,11 @@ export class UBSOrderFormComponent implements AfterViewInit {
     this.secondStepForm = this.stepTwoComponent.personalDataForm;
     this.thirdStepForm = this.stepThreeComponent.paymentForm;
     this.cdr.detectChanges();
+  }
+
+  ngDoCheck(): void {
+    if (this.stepper && this.stepper.selected.state === 'finalStep') {
+      this.completed = true;
+    }
   }
 }
