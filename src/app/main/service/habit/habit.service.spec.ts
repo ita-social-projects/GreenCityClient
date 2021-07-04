eedd9341903ce24b5637b4c8e14c7f5dcb562a0b
Yulian <uliarx@gmail.com>
2021-07-04 23:18:35 +0300

    Move mocks to top

--- src/app/main/service/habit/habit.service.spec.ts
+++ src/app/main/service/habit/habit.service.spec.ts
@@ -8,6 +8,96 @@ describe('HabitService', () => {
   const habitLink = 'https://greencity.azurewebsites.net/habit';
   let habitService: HabitService;
   let httpMock: HttpTestingController;
+  let langMock = null;
+  const habitsMock = {
+    page: [
+      {
+        defaultDuration: 14,
+        habitTranslation: {
+          description: 'Description',
+          habitItem: 'Item',
+          languageCode: 'en',
+          name: 'Use a reusable water bottle'
+        },
+        id: 2,
+        image: './assets/img/habit-circle-bg-shape.png',
+        complexity: 1,
+        tags: [],
+        shoppingListItems: null
+      }
+    ],
+    totalElements: 31,
+    currentPage: 1,
+    totalPages: 31
+  };
+  const habitMock = {
+    defaultDuration: 14,
+    habitTranslation: {
+      description: 'Description',
+      habitItem: 'Item',
+      languageCode: 'en',
+      name: 'Use a towel instead of paper towels and napkins'
+    },
+    id: 1,
+    image: './assets/img/habit-circle-bg-shape.png',
+    complexity: 1,
+    tags: [],
+    shoppingListItems: [
+      {
+        id: 1,
+        text: 'Swedish cellulose dish cloths',
+        status: 'ACTIVE'
+      },
+      {
+        id: 2,
+        text: 'Reusable bamboo paper towels',
+        status: 'ACTIVE'
+      },
+      {
+        id: 3,
+        text: 'Wowables reusable & biodegradable paper towel',
+        status: 'ACTIVE'
+      }
+    ]
+  };
+  const habitList = [
+    {
+      id: 11,
+      text: 'Reusable stainless steel water bottle',
+      status: 'ACTIVE'
+    },
+    {
+      id: 12,
+      text: 'Reusable glass water bottle',
+      status: 'ACTIVE'
+    },
+    {
+      id: 13,
+      text: 'Collapsible Silicone Water Bottle',
+      status: 'ACTIVE'
+    }
+  ];
+  const tagsMock = ['clothes', 'eco', 'green', 'natural'];
+  const habitListMock = {
+    currentPage: 1,
+    page: [
+      {
+        defaultDuration: 12,
+        habitTranslation: {
+          description: 'habit, which will be useful for environment',
+          habitItem: 'bags',
+          languageCode: 'en',
+          name: 'habit for buying eco bags'
+        },
+        id: 1,
+        image: './assets/img/habit-circle-bg-shape.png',
+        tags: ['test'],
+        isAssigned: true
+      }
+    ],
+    totalElements: 1,
+    totalPages: 1
+  };
 
   let localStorageServiceMock: LocalStorageService;
   localStorageServiceMock = jasmine.createSpyObj('localStorageService', ['languageBehaviourSubject']);
@@ -32,159 +122,37 @@ describe('HabitService', () => {
   });
 
   it('should get language', () => {
-    let mockLang = null;
     localStorageServiceMock.languageBehaviourSubject.subscribe((lang) => {
-      mockLang = lang;
+      langMock = lang;
     });
-    expect(mockLang).toBe('en');
+    expect(langMock).toBe('en');
   });
 
   it('should return all habits', () => {
-    const habits = {
-      page: [
-        {
-          defaultDuration: 14,
-          habitTranslation: {
-            description: 'Description',
-            habitItem: 'Item',
-            languageCode: 'en',
-            name: 'Use a reusable water bottle'
-          },
-          id: 2,
-          image: './assets/img/habit-circle-bg-shape.png',
-          complexity: 1,
-          tags: [],
-          shoppingListItems: null
-        }
-      ],
-      totalElements: 31,
-      currentPage: 1,
-      totalPages: 31
-    };
-
     habitService.getAllHabits(1, 1).subscribe((data) => {
-      expect(data).toBe(habits);
+      expect(data).toBe(habitsMock);
     });
 
     const req = httpMock.expectOne(`${habitLink}?lang=en&page=1&size=1`);
     expect(req.cancelled).toBeFalsy();
     expect(req.request.responseType).toEqual('json');
     expect(req.request.method).toBe('GET');
-    req.flush(habits);
+    req.flush(habitsMock);
   });
 
   it('should return habit by id', () => {
-    const habit = {
-      defaultDuration: 14,
-      habitTranslation: {
-        description: 'Description',
-        habitItem: 'Item',
-        languageCode: 'en',
-        name: 'Use a towel instead of paper towels and napkins'
-      },
-      id: 1,
-      image: './assets/img/habit-circle-bg-shape.png',
-      complexity: 1,
-      tags: [],
-      shoppingListItems: [
-        {
-          id: 1,
-          text: 'Swedish cellulose dish cloths',
-          status: 'ACTIVE'
-        },
-        {
-          id: 2,
-          text: 'Reusable bamboo paper towels',
-          status: 'ACTIVE'
-        },
-        {
-          id: 3,
-          text: 'Wowables reusable & biodegradable paper towel',
-          status: 'ACTIVE'
-        },
-        {
-          id: 4,
-          text: 'Microfiber cloths',
-          status: 'ACTIVE'
-        },
-        {
-          id: 5,
-          text: 'Bamboo kitchen dish cloths',
-          status: 'ACTIVE'
-        },
-        {
-          id: 6,
-          text: 'Cotton napkins',
-          status: 'ACTIVE'
-        },
-        {
-          id: 7,
-          text: 'Reusable beeswax wrap',
-          status: 'ACTIVE'
-        },
-        {
-          id: 8,
-          text: 'Linen cocktail napkins',
-          status: 'ACTIVE'
-        },
-        {
-          id: 9,
-          text: 'Everyday cotton napkins',
-          status: 'ACTIVE'
-        },
-        {
-          id: 10,
-          text: 'Organic Cotton Dish Towels',
-          status: 'ACTIVE'
-        }
-      ]
-    };
-
     habitService.getHabitById(1).subscribe((data) => {
-      expect(data.habitTranslation.name).toBe('Use a towel instead of paper towels and napkins');
+      expect(data).toBe(habitMock);
     });
 
     const req = httpMock.expectOne(`${habitLink}/1?lang=en`);
     expect(req.cancelled).toBeFalsy();
     expect(req.request.responseType).toEqual('json');
     expect(req.request.method).toBe('GET');
-    req.flush(habit);
+    req.flush(habitMock);
   });
 
   it('should return habit shopping list', () => {
-    const habitList = [
-      {
-        id: 11,
-        text: 'Reusable stainless steel water bottle',
-        status: 'ACTIVE'
-      },
-      {
-        id: 12,
-        text: 'Reusable glass water bottle',
-        status: 'ACTIVE'
-      },
-      {
-        id: 13,
-        text: 'Collapsible Silicone Water Bottle',
-        status: 'ACTIVE'
-      },
-      {
-        id: 14,
-        text: 'Water Filter Bottle',
-        status: 'ACTIVE'
-      },
-      {
-        id: 15,
-        text: 'Flat reusable bottle',
-        status: 'ACTIVE'
-      },
-      {
-        id: 16,
-        text: 'Watter bottle with a straw',
-        status: 'ACTIVE'
-      }
-    ];
-
     habitService.getHabitShoppingList(2).subscribe((data) => {
       expect(data).toBe(habitList);
     });
@@ -197,46 +165,24 @@ describe('HabitService', () => {
   });
 
   it('should return habit tags', () => {
-    const tags = ['clothes', 'eco', 'green', 'natural'];
     habitService.getHabitsTags().subscribe((data) => {
       expect(data).not.toBeNull();
-      expect(data).toEqual(tags);
+      expect(data).toEqual(tagsMock);
     });
     const req = httpMock.expectOne(`${habitLink}/tags?lang=en`);
     expect(req.request.method).toBe('GET');
-    req.flush(tags);
+    req.flush(tagsMock);
   });
 
   it('should return habits by tag and lang', () => {
-    const habitList = {
-      currentPage: 1,
-      page: [
-        {
-          defaultDuration: 12,
-          habitTranslation: {
-            description: 'habit, which will be useful for environment',
-            habitItem: 'bags',
-            languageCode: 'en',
-            name: 'habit for buying eco bags'
-          },
-          id: 1,
-          image: './assets/img/habit-circle-bg-shape.png',
-          tags: ['test'],
-          isAssigned: true
-        }
-      ],
-      totalElements: 1,
-      totalPages: 1
-    };
-
     habitService.getHabitsByTagAndLang(1, 1, ['test']).subscribe((data) => {
       expect(data).not.toBeNull();
-      expect(data).toEqual(habitList);
+      expect(data).toEqual(habitListMock);
     });
 
     const req = httpMock.expectOne(`${habitLink}/tags/search?lang=en
                                               &page=1&size=1&sort=asc&tags=test`);
     expect(req.request.method).toBe('GET');
-    req.flush(habitList);
+    req.flush(habitListMock);
   });
 });
