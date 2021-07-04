3771c0fef29e09063afcc013a197c7e85bdaa8a0
Rostik Doroshenko <69986847+rostikdoroshenko@users.noreply.github.com>
2021-07-04 22:42:50 +0300

    feature/#2868-client-profile-page (#935)
    * created ubs-client-profile
    
    * merged
    
    * fixed
    
    * fixed
    
    * Update ubs-client-profile-page.component.scss
    
    * Update ubs-client-profile-page.component.scss
    
    * Delete ubs-routing.module.ts
    
    * fixed
    
    * Update ubs-submit-order.component.html
    
    * Update ubs-client-profile-page.component.scss
    
    * Update ubs-client-profile-page.component.html
    
    * Update ubs-client-profile-page.component.ts
    
    * Update ubs-client-profile-page.component.html
    
    * Update ubs-client-profile-page.component.scss

--- src/app/ubs-admin/ubs-admin-routing.module.ts
+++ src/app/ubs-admin/ubs-admin-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UbsClientProfilePageComponent } from './components/ubs-client-profile-page/ubs-client-profile-page.component';
 import { UbsAdminTableComponent } from './components/ubs-admin-table/ubs-admin-table.component';
 import { UbsAdminComponent } from './ubs-admin.component';
 
@@ -7,7 +8,10 @@ const ubsAdminRoutes: Routes = [
   {
     path: '',
     component: UbsAdminComponent,
-    children: [{ path: '', component: UbsAdminTableComponent }]
+    children: [
+      { path: '', component: UbsAdminTableComponent },
+      { path: 'profile', component: UbsClientProfilePageComponent }
+    ]
   }
 ];
 
