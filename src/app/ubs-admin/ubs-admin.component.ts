65dbfc0ba53d39c5012b721f93a1a34ce2f47bf7
Rostik Doroshenko <69986847+rostikdoroshenko@users.noreply.github.com>
2021-07-07 09:34:05 +0300

    Update ubs-admin.component.ts

--- src/app/ubs-admin/ubs-admin.component.ts
+++ src/app/ubs-admin/ubs-admin.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { LocalStorageService } from '@global-service/localstorage/local-storage.service';
 import { TranslateService } from '@ngx-translate/core';
-import { ReplaySubject } from 'rxjs';
+import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 
 @Component({
@@ -9,7 +9,7 @@ import { takeUntil } from 'rxjs/operators';
   templateUrl: './ubs-admin.component.html'
 })
 export class UbsAdminComponent implements OnInit, OnDestroy {
-  private destroy: ReplaySubject<any> = new ReplaySubject<any>(1);
+  private destroy: Subject<boolean> = new Subject<boolean>();
 
   constructor(private translate: TranslateService, private localStorageService: LocalStorageService) {}
 
