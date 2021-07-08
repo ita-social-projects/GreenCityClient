b4e12f69dce5c21d07c96ee4f59d2a1afed2770d
Yurii Shevchuk <34710231+YuriShevchuk@users.noreply.github.com>
2021-07-08 09:48:36 +0300

    Feature/#2795 ubs admin order (#938)
    * add colors & fonts in typography
    
    * add ubs-admin order page layout
    
    * add route
    
    * fix lint errors
    
    * fix style errors
    
    * fix style errors
    
    * skip tests
    
    * fix styles
    
    * add colors
    
    * fix styles on Order page
    
    * fix style errors
    
    * fix style errors
    
    * fix styles & delete constructor
    
    * fix error
    
    * skip tests
    
    * delete empty method & lines
    
    * add style for inputs for canceled order
    
    * fix styles
    
    * delete 'px' in '0px' error
    
    * delete useless method
    
    * add order-details form
    
    * add client-info form
    
    * remove line break
    
    * delete duplicate header
    
    * Add access modifiers & Table description
    
    Co-authored-by: oliachipko <olyachipco@gmail.com>

--- src/app/ubs-admin/ubs-admin.module.ts
+++ src/app/ubs-admin/ubs-admin.module.ts
@@ -2,19 +2,42 @@ import { AdminTableService } from './services/admin-table.service';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { UbsAdminTableComponent } from './components/ubs-admin-table/ubs-admin-table.component';
-import { UbsSidebarComponent } from './components/ubs-sidebar/ubs-sidebar.component';
+import { FormsModule } from '@angular/forms';
 import { MaterialModule } from '../material.module';
 import { SharedModule } from '../shared/shared.module';
+import { UBSAdminRoutingModule } from './ubs-admin-routing.module';
+import { UbsAdminTableComponent } from './components/ubs-admin-table/ubs-admin-table.component';
+import { UbsSidebarComponent } from './components/ubs-sidebar/ubs-sidebar.component';
 import { UbsHeaderComponent } from './components/ubs-header/ubs-header.component';
 import { UbsAdminComponent } from './ubs-admin.component';
-import { UBSAdminRoutingModule } from './ubs-admin-routing.module';
 import { UbsClientProfilePageComponent } from './components/ubs-client-profile-page/ubs-client-profile-page.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { UbsAdminResponsiblePersonsComponent } from './components/ubs-admin-responsible-persons/ubs-admin-responsible-persons.component';
+import { UbsAdminExportDetailsComponent } from './components/ubs-admin-export-details/ubs-admin-export-details.component';
+import { UbsAdminOrderPaymentComponent } from './components/ubs-admin-order-payment/ubs-admin-order-payment.component';
+import { UbsAdminOrderClientInfoComponent } from './components/ubs-admin-order-client-info/ubs-admin-order-client-info.component';
+import { UbsAdminOrderDetailsFormComponent } from './components/ubs-admin-order-details-form/ubs-admin-order-details-form.component';
+import { UbsAdminOrderStatusComponent } from './components/ubs-admin-order-status/ubs-admin-order-status.component';
+import { UbsAdminOrderComponent } from './components/ubs-admin-order/ubs-admin-order.component';
+import { UbsAdminAddressDetailsComponent } from './components/ubs-admin-address-details/ubs-admin-address-details.component';
 
 @NgModule({
-  declarations: [UbsAdminTableComponent, UbsSidebarComponent, UbsHeaderComponent, UbsAdminComponent, UbsClientProfilePageComponent],
-  imports: [CommonModule, MaterialModule, SharedModule, RouterModule, UBSAdminRoutingModule, ReactiveFormsModule],
+  declarations: [
+    UbsAdminTableComponent,
+    UbsSidebarComponent,
+    UbsHeaderComponent,
+    UbsAdminComponent,
+    UbsClientProfilePageComponent,
+    UbsAdminOrderComponent,
+    UbsAdminAddressDetailsComponent,
+    UbsAdminOrderStatusComponent,
+    UbsAdminResponsiblePersonsComponent,
+    UbsAdminExportDetailsComponent,
+    UbsAdminOrderPaymentComponent,
+    UbsAdminOrderClientInfoComponent,
+    UbsAdminOrderDetailsFormComponent
+  ],
+  imports: [CommonModule, MaterialModule, SharedModule, RouterModule, UBSAdminRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [AdminTableService],
   entryComponents: [UbsAdminTableComponent]
 })
