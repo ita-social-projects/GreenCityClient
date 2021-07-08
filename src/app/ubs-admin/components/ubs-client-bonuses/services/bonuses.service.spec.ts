7ca2054640bfdceb594198618609101aa3f1ec24
Khrystyna Tkachuk <vasyl.tkachuk3030@gmail.com>
2021-07-08 11:02:26 +0300

    fix test for bonuse service

--- src/app/ubs-admin/components/ubs-client-bonuses/services/bonuses.service.spec.ts
+++ src/app/ubs-admin/components/ubs-client-bonuses/services/bonuses.service.spec.ts
@@ -3,14 +3,12 @@ import { TestBed } from '@angular/core/testing';
 import { BonusesService } from './bonuses.service';
 
 describe('BonusesService', () => {
-  let service: BonusesService;
-
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(BonusesService);
   });
 
   it('should be created', () => {
+    const service: BonusesService = TestBed.get(BonusesService);
     expect(service).toBeTruthy();
   });
 });
