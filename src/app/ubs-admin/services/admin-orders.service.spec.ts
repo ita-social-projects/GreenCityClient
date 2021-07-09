226890d76bd5086cf4a62a126a756c3b26896b5a
AlekOK <51174530+AlekOK@users.noreply.github.com>
2021-07-09 04:14:02 +0300

    Update admin-orders.service.spec.ts

--- src/app/ubs-admin/services/admin-orders.service.spec.ts
+++ src/app/ubs-admin/services/admin-orders.service.spec.ts
@@ -1,9 +1,12 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { AdminOrdersService } from './admin-orders.service';
 
 describe('AdminOrdersService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule]
+  })
+);
 
   it('should be created', () => {
     const service: AdminOrdersService = TestBed.get(AdminOrdersService);
