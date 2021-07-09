00c50b7a62cfb96acb3c52143dfb2c55bd2fe0b6
AlekOK <51174530+AlekOK@users.noreply.github.com>
2021-07-09 14:50:28 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -17,7 +17,7 @@ export const environment = {
     measurementId: 'G-CCHRKQ4R0S'
   },
   ubsAdmin: {
-    backendEmployeesLink: 'https://greencity-ubs.azurewebsites.net/admin/ubs-employee/getAll-employees'
+    backendEmployeesLink: 'https://greencity-ubs.azurewebsites.net/admin/ubs-employee/getAll-employees',
     backendUbsAdminLink: 'https://greencity-ubs.azurewebsites.net/ubs'
   },
   googleClientId: '129513550972-eu9ej46rviv1ac8q14at62t2k5qon1pu.apps.googleusercontent.com',
