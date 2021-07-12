2c3ead378771b1ce47b03888a45a6e22f101ffaf
AlekOK <51174530+AlekOK@users.noreply.github.com>
2021-07-12 09:39:18 +0300

    Bugfix/#2848 admin employee (#948)
    * unit test for component
    
    * unit test for drag-add-drop directive
    
    * was changed to ts
    
    * some changes
    
    * deleted unused constant
    
    * new component for header
    
    * added ubs-header component to declarations
    
    * deleted header and style for it
    
    * added UbsHeaderComponent to declaration
    
    * add spinner when sort table and reduce table width
    
    * deleted tests
    
    * old tests
    
    * align list items in dropdown
    
    * refactor
    
    * added margin between icons and removew button
    
    * fixed width of component
    
    * child route for base ubs-admin component
    
    * added ng-content
    
    * container
    
    * changes
    
    * deleted empty scss file
    
    * added matMenuModule
    
    * added new route employee as child of ubs-admin
    
    * added some new components for employee
    
    * components for employee
    
    * some view changes
    
    * added styles
    
    * some changes with styles
    
    * fix
    
    * added NgxPaginationModule
    
    * created custom pagination
    
    * created shared pagination
    
    * some changes
    
    * added page
    
    * add service
    
    * Close bugfix/#2848-admin-employee
    
    * Fix tests
    
    * Fixed linter
    
    * Fixed ; lalalala
    
    * Update create-edit-news.component.ts
    
    * fix style
    
    * Update ubs-admin-employee.component.scss
    
    * Update ubs-admin-employee-card.component.scss
    
    * Update ubs-admin-employee-card.component.scss
    
    * Update ubs-admin-employee-card.component.scss
    
    * Fix style
    
    * Fix test Route
    
    * fix remarks
    
    * fix calendar test
    
    * fixed getEmployees metod
    
    * Update ubs-admin.interface.ts
    
    * Update ubs-admin-routing.module.ts
    
    * Update environment.ts
    
    * Update ubs-admin.module.ts
    
    * Create ubs-admin.module.ts
    
    * Update environment.prod.ts
    
    Co-authored-by: Vlad Sevostianov <flypsoff@gmail.com>
    Co-authored-by: oliachipko <olyachipco@gmail.com>
    Co-authored-by: Roksolana Dutchak <dytchakro@gmail.com>
    Co-authored-by: Antonenko Olexiy <antonenkoolexiy.com>

--- src/app/ubs-admin/models/ubs-admin.interface.ts
+++ src/app/ubs-admin/models/ubs-admin.interface.ts
@@ -1,7 +1,41 @@
+export interface Employees {
+  currentPage: number;
+  first: boolean;
+  hasNext: boolean;
+  hasPrevious: boolean;
+  last: boolean;
+  number: number;
+  page: Page[];
+  totalElements: number;
+  totalPages: number;
+}
+
+export interface Page {
+  email: string;
+  employeePositions: EmployeePositions[];
+  firstName: string;
+  id: number;
+  image: string;
+  lastName: string;
+  phoneNumber: string;
+  receivingStations: ReceivingStations[];
+}
+
+export interface EmployeePositions {
+  id: number;
+  name: string;
+}
+
+export interface ReceivingStations {
+  id: number;
+  name: string;
+}
+
 export interface Bags {
   bags: Bag[];
   points: number;
 }
+
 export interface Bag {
   id: number;
   name: string;
--- src/app/ubs-admin/ubs-admin.module.ts
+++ src/app/ubs-admin/ubs-admin.module.ts
@@ -2,6 +2,9 @@ import { AdminTableService } from './services/admin-table.service';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { UbsAdminTableComponent } from './components/ubs-admin-table/ubs-admin-table.component';
+import { UbsSidebarComponent } from './components/ubs-sidebar/ubs-sidebar.component';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
@@ -9,11 +12,12 @@ import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
 import { MaterialModule } from '../material.module';
 import { SharedModule } from '../shared/shared.module';
-import { UBSAdminRoutingModule } from './ubs-admin-routing.module';
-import { UbsAdminTableComponent } from './components/ubs-admin-table/ubs-admin-table.component';
-import { UbsSidebarComponent } from './components/ubs-sidebar/ubs-sidebar.component';
 import { UbsHeaderComponent } from './components/ubs-header/ubs-header.component';
 import { UbsAdminComponent } from './ubs-admin.component';
+import { UbsAdminEmployeeComponent } from './components/ubs-admin-employee/ubs-admin-employee.component';
+import { UbsAdminEmployeeCardComponent } from './components/ubs-admin-employee/ubs-admin-employee-card/ubs-admin-employee-card.component';
+import { PaginationComponent } from './components/shared/components/pagination/pagination.component';
+import { UBSAdminRoutingModule } from './ubs-admin-routing.module';
 import { UbsClientProfilePageComponent } from './components/ubs-client-profile-page/ubs-client-profile-page.component';
 import { UbsAdminResponsiblePersonsComponent } from './components/ubs-admin-responsible-persons/ubs-admin-responsible-persons.component';
 import { UbsAdminExportDetailsComponent } from './components/ubs-admin-export-details/ubs-admin-export-details.component';
@@ -26,10 +30,13 @@ import { UbsAdminAddressDetailsComponent } from './components/ubs-admin-address-
 
 @NgModule({
   declarations: [
+    PaginationComponent,
     UbsAdminTableComponent,
     UbsSidebarComponent,
     UbsHeaderComponent,
     UbsAdminComponent,
+    UbsAdminEmployeeComponent,
+    UbsAdminEmployeeCardComponent,
     UbsClientProfilePageComponent,
     UbsAdminOrderComponent,
     UbsAdminAddressDetailsComponent,
@@ -49,6 +56,8 @@ import { UbsAdminAddressDetailsComponent } from './components/ubs-admin-address-
     UBSAdminRoutingModule,
     FormsModule,
     ReactiveFormsModule,
+    UBSAdminRoutingModule,
+    NgxPaginationModule,
     TranslateModule.forChild({
       loader: {
         provide: TranslateLoader,
