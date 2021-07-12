c2b895881c82f04bcde7be5bb4962abc90833cf0
Rostik Doroshenko <69986847+rostikdoroshenko@users.noreply.github.com>
2021-07-13 00:29:28 +0300

    Update ubs-client-profile-page.component.ts

--- src/app/ubs-admin/components/ubs-client-profile-page/ubs-client-profile-page.component.ts
+++ src/app/ubs-admin/components/ubs-client-profile-page/ubs-client-profile-page.component.ts
@@ -14,9 +14,10 @@ import { UserProfile } from '../../models/ubs-admin.interface';
 export class UbsClientProfilePageComponent implements OnInit {
   userForm: FormGroup;
   public googleIcon = SignInIcons.picGoogle;
-  public editing = false;
-  public fetching = false;
+  public isEditing = false;
+  public isFetching = false;
   public userId: number;
+  public phoneMask = '+{38} 000 000 00 00';
   private readonly regexp = /^([a-zA-ZА-Яа-яЄЇҐа-їєґ '-])+$/iu;
   private readonly emailRegex = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
@@ -56,7 +57,7 @@ export class UbsClientProfilePageComponent implements OnInit {
       firstname: new FormControl(this.userProfile.recipientName, [Validators.required, Validators.pattern(this.regexp)]),
       surname: new FormControl(this.userProfile.recipientSurname, [Validators.required, Validators.pattern(this.regexp)]),
       email: new FormControl(this.userProfile.recipientEmail, [Validators.required, Validators.pattern(this.emailRegex)]),
-      phoneNumber: new FormControl(`+380${this.userProfile.recipientPhone}`, [Validators.required, Validators.pattern('[+ 0-9 ]{13}')]),
+      phoneNumber: new FormControl(`+38 0${this.userProfile.recipientPhone}`, [Validators.required, Validators.minLength(12)]),
       city: new FormControl(this.userProfile.addressDto.city, [
         Validators.required,
         Validators.pattern(this.regexp),
@@ -76,36 +77,36 @@ export class UbsClientProfilePageComponent implements OnInit {
         Validators.maxLength(20)
       ])
     });
-    this.fetching = false;
+    this.isFetching = false;
   }
 
   public onEdit() {
-    this.editing = true;
-    this.fetching = false;
+    this.isEditing = true;
+    this.isFetching = false;
   }
 
   public onCancel() {
     this.userInit();
-    this.editing = false;
+    this.isEditing = false;
   }
 
   public onSubmit() {
     if (this.userForm.valid) {
-      this.fetching = true;
+      this.isFetching = true;
       this.userProfile.recipientName = this.userForm.value.firstname;
       this.userProfile.recipientSurname = this.userForm.value.surname;
       this.userProfile.recipientEmail = this.userForm.value.email;
-      this.userProfile.recipientPhone = this.userForm.value.phoneNumber.slice(4);
+      this.userProfile.recipientPhone = this.userForm.value.phoneNumber.substr(-9, 9);
       this.userProfile.addressDto.city = this.userForm.value.city;
       this.userProfile.addressDto.street = this.userForm.value.street;
       this.userProfile.addressDto.houseNumber = this.userForm.value.houseNumber;
       this.userProfile.addressDto.houseCorpus = this.userForm.value.houseCorpus;
       this.userProfile.addressDto.entranceNumber = this.userForm.value.entranceNumber;
       this.userProfile.addressDto.district = this.userForm.value.district;
-      this.editing = false;
-      this.fetching = false;
+      this.isEditing = false;
+      this.isFetching = false;
     } else {
-      this.editing = true;
+      this.isEditing = true;
     }
   }
 }
