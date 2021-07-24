ce73162155db5773d9a35969e98bff55008fe674
gavayec13 <gavayec13@gmail.com>
2021-07-24 16:18:44 +0300

    fixed

--- src/app/main/component/user/components/habit/add-new-habit/habit-invite-friends/habit-invite-friends-pop-up/habit-invite-friends-pop-up.component.ts
+++ src/app/main/component/user/components/habit/add-new-habit/habit-invite-friends/habit-invite-friends-pop-up/habit-invite-friends-pop-up.component.ts
@@ -10,6 +10,8 @@ import { UserFriendsService } from '@global-user/services/user-friends.service';
 export class HabitInviteFriendsPopUpComponent implements OnInit {
   userId: number;
   friends: FriendModel[];
+  allAdd: boolean = false;
+  addedFriends: FriendModel[] = [];
 
   constructor(private userFriendsService: UserFriendsService) {}
 
@@ -24,7 +26,34 @@ export class HabitInviteFriendsPopUpComponent implements OnInit {
 
   getFriends() {
     this.userFriendsService.getAllFriends(this.userId).subscribe((data: FriendArrayModel) => {
-      (this.friends = data.page), console.log(this.friends);
+      this.friends = data.page;
     });
+  }
+
+  updateAllAdd() {
+    this.allAdd = this.friends != null && this.friends.every((friend) => friend.added);
+  }
+
+  someAdd(): boolean {
+    if (this.friends == null) {
+      return false;
+    }
+    return this.friends.filter((friend) => friend.added).length > 0 && !this.allAdd;
+  }
+
+  setAll(added: boolean) {
+    this.allAdd = added;
+    if (this.friends == null) {
+      return;
+    }
+    this.friends.forEach((friend) => (friend.added = added));
+  }
+
+  setAddedFriends() {
+    this.friends.forEach((friend) => (friend.added ? this.userFriendsService.addedFriendsToHabit(friend) : null));
+  }
+
+  inviteFriends() {
+    this.setAddedFriends();
   }
 }
--- src/app/main/component/user/components/habit/add-new-habit/habit-invite-friends/habit-invite-friends.component.ts
+++ src/app/main/component/user/components/habit/add-new-habit/habit-invite-friends/habit-invite-friends.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material';
-import { FriendArrayModel, FriendModel } from '@global-user/models/friend.model';
+import { FriendModel } from '@global-user/models/friend.model';
 import { UserFriendsService } from '@global-user/services/user-friends.service';
 import { HabitInviteFriendsPopUpComponent } from './habit-invite-friends-pop-up/habit-invite-friends-pop-up.component';
 
@@ -10,11 +10,19 @@ import { HabitInviteFriendsPopUpComponent } from './habit-invite-friends-pop-up/
   styleUrls: ['./habit-invite-friends.component.scss']
 })
 export class HabitInviteFriendsComponent {
+  userId: number;
+  addedFriends: FriendModel[] = [];
+
   constructor(private userFriendsService: UserFriendsService, private dialog: MatDialog) {}
 
   openInviteFriendsDialog() {
     this.dialog.open(HabitInviteFriendsPopUpComponent, {
       hasBackdrop: true
     });
+    this.dialog.afterAllClosed.subscribe(() => this.getAddedFriends());
+  }
+
+  getAddedFriends() {
+    this.addedFriends = this.userFriendsService.addedFriends;
   }
 }
