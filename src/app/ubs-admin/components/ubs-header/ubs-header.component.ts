79303e502731bc4c718823c2d634690f4b81dab0
Artem <artem.dyadyushko@gmail.com>
2021-07-27 17:49:49 +0300

    update styles

--- src/app/ubs-admin/components/ubs-header/ubs-header.component.ts
+++ src/app/ubs-admin/components/ubs-header/ubs-header.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { ubsHeaderIcons } from '../ubs-image-pathes/ubs-header-icons';
 
 @Component({
   selector: 'app-ubs-header',
   templateUrl: './ubs-header.component.html',
   styleUrls: ['./ubs-header.component.scss']
 })
-export class UbsHeaderComponent {}
+export class UbsHeaderComponent {
+  ubsHeaderIcons = ubsHeaderIcons;
+}
---
+++ src/app/ubs-admin/components/ubs-image-pathes/ubs-admin-table.ts
@@ -0,0 +1,3 @@
+export const ubsAdminTable = {
+  arrowDown: 'assets/img/arrow-down.svg'
+};
