6463b83db5e91757d7bf8892c94c7792c11acfd1
Yurii Shevchuk <34710231+YuriShevchuk@users.noreply.github.com>
2021-08-02 19:15:04 +0300

    Feature/#2787 order-status & export-details (#1009)
    * add json
    
    * add translation & some refactor
    
    * skip tests
    
    * minor changes for readability
    
    * add export-details
    
    * add order-status dropdown
    
    * fix lint

--- src/app/ubs-admin/components/ubs-admin-order-status/ubs-admin-order-status.component.ts
+++ src/app/ubs-admin/components/ubs-admin-order-status/ubs-admin-order-status.component.ts
@@ -1,8 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Subject } from 'rxjs';
+import { OrderService } from '../../services/order.service';
+import { IDetailStatus } from '../../models/ubs-admin.interface';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-ubs-admin-order-status',
   templateUrl: './ubs-admin-order-status.component.html',
   styleUrls: ['./ubs-admin-order-status.component.scss']
 })
-export class UbsAdminOrderStatusComponent {}
+export class UbsAdminOrderStatusComponent implements OnInit, OnDestroy {
+  public orderId = 893;
+  public detailStatusForm: FormGroup;
+  public detailStatus: IDetailStatus;
+  private destroy$: Subject<boolean> = new Subject<boolean>();
+
+  constructor(private fb: FormBuilder, private orderService: OrderService) {}
+
+  ngOnInit(): void {
+    this.initForm();
+    this.getOrderDetailStatus();
+  }
+
+  public initForm(): void {
+    this.detailStatusForm = this.fb.group({
+      date: [''],
+      orderStatus: this.fb.array([]),
+      paymentStatus: this.fb.array([])
+    });
+  }
+
+  public patchFormData(): void {
+    this.detailStatusForm.patchValue(this.detailStatus);
+  }
+
+  public getOrderDetailStatus(): void {
+    this.orderService
+      .getOrderDetailStatus(this.orderId)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((data: IDetailStatus) => {
+        this.detailStatus = data;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+}
