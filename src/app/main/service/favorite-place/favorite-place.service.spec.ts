110ff90e865d18c8f747c622c41698b61b7384a3
Nazar <hlyvka777@gmail.com>
2021-08-26 20:26:00 +0300

    tests for favorite-place service

--- src/app/main/service/favorite-place/favorite-place.service.spec.ts
+++ src/app/main/service/favorite-place/favorite-place.service.spec.ts
@@ -1,14 +1,15 @@
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-import { FavoritePlace } from '@global-models/favorite-place/favorite-place';
 import { Place } from '../../component/places/models/place';
-import { favoritePlaceLink } from '../../links';
+import { favoritePlaceLink, placeLink } from '../../links';
+import { FavoritePlace } from '../../model/favorite-place/favorite-place';
 
 import { FavoritePlaceService } from './favorite-place.service';
 
 fdescribe('FavoritePlaceService', () => {
   let service: FavoritePlaceService;
   let httpMock: HttpTestingController;
+  let favoritePlace: FavoritePlace;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -18,6 +19,7 @@ fdescribe('FavoritePlaceService', () => {
 
     service = TestBed.inject(FavoritePlaceService);
     httpMock = TestBed.inject(HttpTestingController);
+    favoritePlace = { name: 'Park', placeId: 1 };
   });
 
   afterEach(() => {
@@ -40,7 +42,6 @@ fdescribe('FavoritePlaceService', () => {
     });
 
     const request = httpMock.expectOne(favoritePlaceLink);
-
     expect(request.request.method).toBe('GET');
 
     request.flush(expectedData);
@@ -64,9 +65,43 @@ fdescribe('FavoritePlaceService', () => {
     });
 
     const request = httpMock.expectOne(favoritePlaceLink + 'favorite/' + 1);
-
     expect(request.request.method).toBe('GET');
 
     request.flush(place);
+  });
+
+  it('saveFavoritePlace should post data', () => {
+    service.saveFavoritePlace(favoritePlace).subscribe((res) => {
+      expect(res).toEqual(favoritePlace);
+    });
+
+    const request = httpMock.expectOne(placeLink + 'save/favorite/');
+    expect(request.request.method).toBe('POST');
+    expect(request.request.body).toEqual(favoritePlace);
+
+    request.flush(favoritePlace);
+  });
+
+  it('updateFavoritePlace should put data', () => {
+    service.updateFavoritePlace(favoritePlace).subscribe((res) => {
+      expect(res).toEqual(favoritePlace);
+    });
+
+    const request = httpMock.expectOne(favoritePlaceLink);
+    expect(request.request.method).toBe('PUT');
+    expect(request.request.body).toEqual(favoritePlace);
+
+    request.flush(favoritePlace);
+  });
+
+  it('deleteFavoritePlace should delete data by id', () => {
+    service.deleteFavoritePlace(1).subscribe((res) => {
+      expect(res).toBe(1);
+    });
+
+    const request = httpMock.expectOne(favoritePlaceLink + 1);
+    expect(request.request.method).toBe('DELETE');
+
+    request.flush(1);
   });
 });
