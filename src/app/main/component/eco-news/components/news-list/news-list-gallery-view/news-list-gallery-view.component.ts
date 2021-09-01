7d54b12b532394e7429536afe24176621a04270e
Oleg Postolovskyi <newartmask@gmail.com>
2021-09-01 13:31:47 +0300

    Added date localization for news list gallery and news list list

--- src/app/main/component/eco-news/components/news-list/news-list-gallery-view/news-list-gallery-view.component.ts
+++ src/app/main/component/eco-news/components/news-list/news-list-gallery-view/news-list-gallery-view.component.ts
@@ -1,12 +1,13 @@
 import { ecoNewsIcons } from './../../../../../image-pathes/profile-icons';
 import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
 import { EcoNewsModel } from '@eco-news-models/eco-news-model';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-news-list-gallery-view',
   templateUrl: './news-list-gallery-view.component.html',
   styleUrls: ['./news-list-gallery-view.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NewsListGalleryViewComponent {
   @Input() ecoNewsModel: EcoNewsModel;
@@ -14,11 +15,13 @@ export class NewsListGalleryViewComponent {
   public profileIcons = ecoNewsIcons;
   public newsImage: string;
 
+  constructor(public translate: TranslateService) {}
+
   public checkNewsImage(): string {
     this.newsImage =
-    this.ecoNewsModel.imagePath && this.ecoNewsModel.imagePath !== ' '
-      ? this.ecoNewsModel.imagePath
-      : this.profileIcons.newsDefaultPictureList;
-    return  this.newsImage;
+      this.ecoNewsModel.imagePath && this.ecoNewsModel.imagePath !== ' '
+        ? this.ecoNewsModel.imagePath
+        : this.profileIcons.newsDefaultPictureList;
+    return this.newsImage;
   }
 }
