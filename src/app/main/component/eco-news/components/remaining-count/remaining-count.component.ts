20c3603dc2da8680bbedbef3c1c43e9b59d6258b
Oleg Postolovskyi <newartmask@gmail.com>
2021-09-02 16:46:58 +0300

    Refactored localization for english

--- src/app/main/component/eco-news/components/remaining-count/remaining-count.component.ts
+++ src/app/main/component/eco-news/components/remaining-count/remaining-count.component.ts
@@ -1,10 +1,13 @@
 import { Component, Input } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-remaining-count',
   templateUrl: './remaining-count.component.html',
-  styleUrls: ['./remaining-count.component.scss'],
+  styleUrls: ['./remaining-count.component.scss']
 })
 export class RemainingCountComponent {
   @Input() public remainingCount = 0;
+
+  constructor(public translate: TranslateService) {}
 }
