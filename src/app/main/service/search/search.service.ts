9b1787af24c7fad361dbbb8f411f1040377b8ed0
nazarf1 <88323205+nazarf1@users.noreply.github.com>
2021-09-10 19:38:08 +0300

    Update search.service.ts

--- src/app/main/service/search/search.service.ts
+++ src/app/main/service/search/search.service.ts
@@ -19,7 +19,6 @@ export class SearchService {
   public allElements: SearchDto;
 
   public getAllResults(searchQuery: string, lang: string): Observable<SearchModel> {
-    // return this.http.get<SearchModel>(backendMockSearchAll);
     return this.http.get<SearchModel>(`${this.backEndLink}/search?searchQuery=${searchQuery}&lang=${lang}`);
   }
 
