58e0d4a4028c806871e1c17d304e31800df3c0c9
Oleg Postolovskyi <34275837+NewArtMask@users.noreply.github.com>
2021-09-20 00:10:02 +0300

    Update remaining-count.component.ts

--- src/app/main/component/eco-news/components/remaining-count/remaining-count.component.ts
+++ src/app/main/component/eco-news/components/remaining-count/remaining-count.component.ts
@@ -8,18 +8,17 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class RemainingCountComponent {
   @Input() public remainingCount = 0;
-
-  constructor(public translate: TranslateService) {}
-
-  setWordDeclension(count: string): string {
     const lastOne = -1;
     const lastTwo = -2;
     const suitableLastNumber = '1';
     const unsuitableLastNumber = '11';
     const suitableLastNumbers = '234';
     const unsuitableLastNumbers = ['12', '13', '14'];
-    
-    if (count.slice(lastOne) === suitableLastNumber && count.slice(lastTwo) !== unsuitableLastNumber) { return 'a'; } 
+
+  constructor(public translate: TranslateService) {}
+
+  setWordDeclension(count: string): string {
+    if (count.slice(lastOne) === suitableLastNumber && count.slice(lastTwo) !== unsuitableLastNumber) { return 'a'; }
     if (suitableLastNumbers.includes(count.slice(lastOne)) && !unsuitableLastNumbers.includes(count.slice(lastTwo))) { return 'b'; }
     return '';
   }
