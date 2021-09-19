c796405626eb38847a1b6e5b5f2f65ccdca0f026
Oleg Postolovskyi <34275837+NewArtMask@users.noreply.github.com>
2021-09-19 23:59:14 +0300

    Refactored RemainingCountComponent

--- src/app/main/component/eco-news/components/remaining-count/remaining-count.component.ts
+++ src/app/main/component/eco-news/components/remaining-count/remaining-count.component.ts
@@ -12,12 +12,15 @@ export class RemainingCountComponent {
   constructor(public translate: TranslateService) {}
 
   setWordDeclension(count: string): string {
-    if (count.slice(-1) === '1' && count.slice(-2) !== '11') {
-      return 'ends_on_1';
-    } else if ('234'.includes(count.slice(-1)) && !['12', '13', '14'].includes(count.slice(-2))) {
-      return 'ends_on_2_3_4';
-    } else {
-      return '';
-    }
+    const lastOne = -1;
+    const lastTwo = -2;
+    const suitableLastNumber = '1';
+    const unsuitableLastNumber = '11';
+    const suitableLastNumbers = '234';
+    const unsuitableLastNumbers = ['12', '13', '14'];
+    
+    if (count.slice(lastOne) === suitableLastNumber && count.slice(lastTwo) !== unsuitableLastNumber) { return 'a'; } 
+    if (suitableLastNumbers.includes(count.slice(lastOne)) && !unsuitableLastNumbers.includes(count.slice(lastTwo))) { return 'b'; }
+    return '';
   }
 }
