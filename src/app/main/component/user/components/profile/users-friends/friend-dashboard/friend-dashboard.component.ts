1849d0f39e8dd3bac03bce221a929eb8a2e2580e
Roman Bishko <bishko86@yahoo.com>
2021-09-27 15:08:57 +0300

    created search field and its methods

--- src/app/main/component/user/components/profile/users-friends/friend-dashboard/friend-dashboard.component.ts
+++ src/app/main/component/user/components/profile/users-friends/friend-dashboard/friend-dashboard.component.ts
@@ -1,25 +1,62 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { LocalStorageService } from '@global-service/localstorage/local-storage.service';
 import { TranslateService } from '@ngx-translate/core';
 import { Subject, Subscription } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { debounceTime, takeUntil } from 'rxjs/operators';
+import { AllFriendsComponent } from './all-friends/all-friends.component';
+import { RecommendedFriendsComponent } from './recommended-friends/recommended-friends.component';
+import { FriendRequestsComponent } from './friend-requests/friend-requests.component';
+import { searchIcon } from '../../../../../../image-pathes/places-icons';
 
 @Component({
   selector: 'app-friend-dashboard',
   templateUrl: './friend-dashboard.component.html',
-  styleUrls: ['./friend-dashboard.component.scss'],
+  styleUrls: ['./friend-dashboard.component.scss']
 })
 export class FriendDashboardComponent implements OnInit, OnDestroy {
   public userId: number;
   public langChangeSub: Subscription;
   public destroy$ = new Subject();
-
+  public searchTerm$: Subject<string> = new Subject();
+  public searchIcon = searchIcon;
+  public hideInput = true;
   constructor(private localStorageService: LocalStorageService, private translate: TranslateService) {}
+
+  @ViewChild(RouterOutlet) outlet: RouterOutlet;
 
   ngOnInit() {
     this.initUser();
     this.subscribeToLangChange();
     this.bindLang(this.localStorageService.getCurrentLanguage());
+    this.preventFrequentQuery();
+    this.hideInputField();
+  }
+
+  preventFrequentQuery() {
+    this.searchTerm$.pipe(debounceTime(400), takeUntil(this.destroy$)).subscribe((value) => {
+      this.searchForFriends(value);
+    });
+  }
+
+  public onInput(input): void {
+    if (input.value === '') return;
+    this.searchTerm$.next(input.value);
+  }
+
+  public searchForFriends(searchText: string): void {
+    if (this.outlet.component instanceof RecommendedFriendsComponent) {
+      this.outlet.component.findUserByName(searchText);
+    }
+    if (this.outlet.component instanceof AllFriendsComponent) {
+      this.outlet.component.findFriendByName(searchText);
+    }
+  }
+
+  public hideInputField() {
+    setTimeout(() => {
+      this.hideInput = this.outlet?.component instanceof FriendRequestsComponent;
+    });
   }
 
   public initUser(): void {
@@ -38,5 +75,6 @@ export class FriendDashboardComponent implements OnInit, OnDestroy {
     this.langChangeSub.unsubscribe();
     this.destroy$.next(true);
     this.destroy$.complete();
+    this.searchTerm$.complete();
   }
 }
