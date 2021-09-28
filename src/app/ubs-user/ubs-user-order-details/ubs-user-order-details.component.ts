60bf5e202131ab33174b2a33bfa32880f2b7b404
Khrystyna Pryslupska <71782246+KhrystynaPr@users.noreply.github.com>
2021-09-28 18:48:41 +0300

    remove empty constructor

--- src/app/ubs-user/ubs-user-order-details/ubs-user-order-details.component.ts
+++ src/app/ubs-user/ubs-user-order-details/ubs-user-order-details.component.ts
@@ -1,15 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-ubs-user-order-details',
   templateUrl: './ubs-user-order-details.component.html',
   styleUrls: ['./ubs-user-order-details.component.scss']
 })
-export class UbsUserOrderDetailsComponent implements OnInit {
+export class UbsUserOrderDetailsComponent {
   @Input()
   order: any;
-
-  constructor() {}
-
-  ngOnInit() {}
 }
