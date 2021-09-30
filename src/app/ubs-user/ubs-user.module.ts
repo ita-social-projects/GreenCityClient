caf30af04eb5a42b0612ece449eada358a2cc15b
Khrystyna Pryslupska <christina_seniuta@ukr.net>
2021-09-30 17:20:37 +0300

    move and rename ubs-client-bonuses component

--- src/app/ubs-user/ubs-user.module.ts
+++ src/app/ubs-user/ubs-user.module.ts
@@ -5,17 +5,19 @@ import { HttpClient } from '@angular/common/http';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { SharedModule } from '../shared/shared.module';
-import { UbsUserOrdersListComponent } from '../ubs-user/ubs-user-orders-list/ubs-user-orders-list.component';
-import { UbsUserOrderDetailsComponent } from '../ubs-user/ubs-user-order-details/ubs-user-order-details.component';
-import { UbsUserOrdersComponent } from '../ubs-user/ubs-user-orders/ubs-user-orders.component';
+import { UbsUserOrdersListComponent } from './ubs-user-orders-list/ubs-user-orders-list.component';
+import { UbsUserOrderDetailsComponent } from './ubs-user-order-details/ubs-user-order-details.component';
+import { UbsUserOrdersComponent } from './ubs-user-orders/ubs-user-orders.component';
 import { UbsUserRoutingModule } from './ubs-user-routing.module';
 import { UbsUserComponent } from './ubs-user.component';
 import { UbsUserSidebarComponent } from './ubs-user-sidebar/ubs-user-sidebar.component';
+import { UbsUserBonusesComponent } from './ubs-user-bonuses/ubs-user-bonuses.component';
 import { MaterialModule } from '../material.module';
 
 @NgModule({
   declarations: [
     UbsUserSidebarComponent,
+    UbsUserBonusesComponent,
     UbsUserComponent,
     UbsUserOrderDetailsComponent,
     UbsUserOrdersComponent,
