510a243b34c46de17f80d2170be8458240b7080e
Flash5482 <48418496+Flash5482@users.noreply.github.com>
2021-10-01 00:46:22 +0300

    Update ubs-base-sidebar.component.ts

--- src/app/shared/ubs-base-sidebar/ubs-base-sidebar.component.ts
+++ src/app/shared/ubs-base-sidebar/ubs-base-sidebar.component.ts
@@ -64,7 +64,7 @@ export class UbsBaseSidebarComponent implements AfterViewInit, OnInit, OnDestroy
       });
     }, 0);
   }
-  
+
   ngOnDestroy() {
     this.destroy.next();
     this.destroy.unsubscribe();
