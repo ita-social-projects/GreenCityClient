1aef78ff4279cc6be24125e35abd8e232fae9f01
Flash5482 <48418496+Flash5482@users.noreply.github.com>
2021-10-03 01:53:50 +0300

    add change language

--- src/app/ubs-user/ubs-user-messages/ubs-user-messages.component.ts
+++ src/app/ubs-user/ubs-user-messages/ubs-user-messages.component.ts
@@ -3,7 +3,7 @@ import { UserMessagesService } from '../services/user-messages.service';
 import { LocalStorageService } from '@global-service/localstorage/local-storage.service';
 import { NotificationBody } from '../../ubs-admin/models/ubs-user.model';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Subject } from 'rxjs';
+import { ReplaySubject, Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 
 @Component({
@@ -19,11 +19,11 @@ export class UbsUserMessagesComponent implements OnInit, OnDestroy {
   page = 1;
   count = 0;
   pageSize = 10;
-  language = this.localStorage.getCurrentLanguage();
   isLoadSpinner: boolean;
   isLoadSmallSpinner: boolean;
   isLoadBar: boolean;
   public countOfMessages: number;
+  private destroyed$: ReplaySubject<any> = new ReplaySubject<any>(1);
   destroy: Subject<boolean> = new Subject<boolean>();
   localization = {
     title: 'ubs-user-notification.title',
@@ -40,16 +40,19 @@ export class UbsUserMessagesComponent implements OnInit, OnDestroy {
   ) {}
 
   ngOnInit() {
-    this.language = this.localStorage.getCurrentLanguage();
-    this.isLoadSpinner = true;
     this.route.params.subscribe((val) => {
       this.page = +this.route.snapshot.paramMap.get('pageId');
-      this.fetchNotification();
+      this.subscribeToLangChange();
     });
+  }
+  
+  private subscribeToLangChange() {
+    this.localStorageService.languageBehaviourSubject.pipe(takeUntil(this.destroyed$)).subscribe(() => this.fetchNotification());
   }
 
   fetchNotification(): void {
-    this.userMessagesService.getNotification(this.language, this.page - 1, this.pageSize)
+    this.isLoadBar = true;
+    this.userMessagesService.getNotification(this.page - 1, this.pageSize)
       .pipe(takeUntil(this.destroy))
       .subscribe(
       (response) => {
@@ -81,7 +84,7 @@ export class UbsUserMessagesComponent implements OnInit, OnDestroy {
         }
       });
       this.isLoadSmallSpinner = true;
-      this.userMessagesService.setReadNotification(notificationId, this.language)
+      this.userMessagesService.setReadNotification(notificationId)
         .pipe(takeUntil(this.destroy))
         .subscribe((response) => {
         const findNotification = this.notifications.find((item) => item.id === notificationId);
@@ -93,7 +96,6 @@ export class UbsUserMessagesComponent implements OnInit, OnDestroy {
   }
 
   onTableDataChange(event) {
-    this.isLoadBar = true;
     this.router.navigate(['/ubs-user/messages/' + event]);
   }
 
