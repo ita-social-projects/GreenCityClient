7be34e36cfa2495231886b52c381e9f3be370257
Flash5482 <48418496+Flash5482@users.noreply.github.com>
2021-10-03 10:39:26 +0300

    Update ubs-user-routing.module.ts

--- src/app/ubs-user/ubs-user-routing.module.ts
+++ src/app/ubs-user/ubs-user-routing.module.ts
@@ -5,6 +5,7 @@ import { UbsUserComponent } from './ubs-user.component';
 import { UbsUserOrdersComponent } from './ubs-user-orders/ubs-user-orders.component';
 import { UbsUserMessagesComponent } from './ubs-user-messages/ubs-user-messages.component';
 import { UbsUserBonusesComponent } from './ubs-user-bonuses/ubs-user-bonuses.component';
+import { UbsClientBonusesComponent } from '../ubs-admin/components/ubs-client-bonuses/ubs-client-bonuses.component';
 const routes: Routes = [
   {
     path: '',
