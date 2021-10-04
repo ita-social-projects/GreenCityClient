91a4fb38099f912e3abd5ff489ac3f7c03b7e3a5
Yura935 <boyko.yuriy16@gmail.com>
2021-10-04 09:24:56 +0300

    Bugfix/#1785/edit-details-habit

--- src/app/main/component/user/components/habit/add-new-habit/habit-edit-shopping-list/habit-edit-shopping-list.component.ts
+++ src/app/main/component/user/components/habit/add-new-habit/habit-edit-shopping-list/habit-edit-shopping-list.component.ts
@@ -94,13 +94,14 @@ export class HabitEditShoppingListComponent implements OnInit, OnDestroy {
           this.getListItems(false);
           return;
         }
-        for (const assigned of response) {
-          if (assigned.habit.id === this.habitId) {
-            this.getListItems(true);
-            break;
-          }
-          this.getListItems(false);
-        }
+        // for (const assigned of response) {
+        //   if (assigned.habit.id === this.habitId) {
+        //     this.getListItems(true);
+        //     break;
+        //   }
+        //   this.getListItems(false);
+        // }
+        response.some((assigned) => assigned.habit.id === this.habitId) ? this.getListItems(true) : this.getListItems(false);
       });
   }
 
