45b0d331e7a21e7e4cc1bdb0939d6b0864eafbc8
Roman Bishko <bishko86@yahoo.com>
2021-10-05 12:42:10 +0300

    fixed unit test

--- src/app/main/component/ubs/components/ubs-order-details/ubs-order-location-popup/ubs-order-location-popup.component.spec.ts
+++ src/app/main/component/ubs/components/ubs-order-details/ubs-order-location-popup/ubs-order-location-popup.component.spec.ts
@@ -1,17 +1,19 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogRef } from '@angular/material/dialog';
 import { RouterTestingModule } from '@angular/router/testing';
 import { TranslateModule } from '@ngx-translate/core';
 
 import { UbsOrderLocationPopupComponent } from './ubs-order-location-popup.component';
 
-describe('UbsOrderLocationPopupComponent', () => {
+fdescribe('UbsOrderLocationPopupComponent', () => {
   let component: UbsOrderLocationPopupComponent;
   let fixture: ComponentFixture<UbsOrderLocationPopupComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [UbsOrderLocationPopupComponent],
+      providers: [{ provide: MatDialogRef, useValue: {} }],
       imports: [RouterTestingModule, HttpClientTestingModule, TranslateModule.forRoot()]
     }).compileComponents();
   }));
