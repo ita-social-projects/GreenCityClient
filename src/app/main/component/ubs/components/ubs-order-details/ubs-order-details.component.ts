dee3176140fa9d9a3c5dffa3af0a9a71bdcff260
Roman Bishko <bishko86@yahoo.com>
2021-10-11 23:39:44 +0300

    filtered locations by language

--- src/app/main/component/ubs/components/ubs-order-details/ubs-order-details.component.ts
+++ src/app/main/component/ubs/components/ubs-order-details/ubs-order-details.component.ts
@@ -108,11 +108,15 @@ export class UBSOrderDetailsComponent extends FormBaseComponent implements OnIni
       .addLocation(selectedLocation)
       .pipe(take(1))
       .subscribe(() => {
-        this.currentLocation = this.locations.find((loc) => loc.id === this.selectedLocationId).name;
+        this.setCurrentLocation(this.currentLanguage);
         this.isFetching = false;
         this.changeLocation = false;
         this.orderService.completedLocation(true);
       });
+  }
+
+  private setCurrentLocation(currentLanguage: string): void {
+    this.currentLocation = this.locations.find((loc) => loc.id === this.selectedLocationId && loc.languageCode === currentLanguage).name;
   }
 
   getFormValues(): boolean {
@@ -143,10 +147,10 @@ export class UBSOrderDetailsComponent extends FormBaseComponent implements OnIni
       .subscribe((res) => {
         if (res.data) {
           this.locations = res.data;
-          this.selectedLocationId = this.locations[0].id;
-          this.currentLocation = res.data[0].name;
-          this.isDialogOpen = false;
+          this.selectedLocationId = res.locationId;
+          this.setCurrentLocation(res.currentLanguage);
         }
+        this.isDialogOpen = false;
       });
   }
 
@@ -168,6 +172,7 @@ export class UBSOrderDetailsComponent extends FormBaseComponent implements OnIni
   private subscribeToLangChange(): void {
     this.localStorageService.languageSubject.pipe(takeUntil(this.destroy)).subscribe(() => {
       this.currentLanguage = this.localStorageService.getCurrentLanguage();
+      this.setCurrentLocation(this.currentLanguage);
       const inputsQuantity = [];
       this.bags.forEach((a) => {
         inputsQuantity.push(a.quantity === undefined || a.quantity === null ? null : a.quantity);
