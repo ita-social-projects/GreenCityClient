2c795b746d0669efe5e738fd4ed6662960efba46
Roman Bishko <bishko86@yahoo.com>
2021-10-18 13:28:51 +0300

    added new components to view friends profile

---
+++ src/app/main/component/user/components/profile/users-friends/friend-dashboard/friend-profile-page/friend-profile-dashboard/friend-profile-dashboard.component.spec.ts
@@ -0,0 +1,44 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { UserFriendsService } from '@global-user/services/user-friends.service';
+import { TranslateModule } from '@ngx-translate/core';
+import { of } from 'rxjs';
+import { FriendProfileDashboardComponent } from './friend-profile-dashboard.component';
+
+describe('FriendProfileDashboardComponent', () => {
+  let component: FriendProfileDashboardComponent;
+  let fixture: ComponentFixture<FriendProfileDashboardComponent>;
+  let userFriendsServiceMock: UserFriendsService;
+  userFriendsServiceMock = jasmine.createSpyObj('UserFriendsService', {
+    getAllFriends: of({})
+  });
+  let activatedRouteMock = {
+    snapshot: {
+      params: {
+        userId: 1,
+        id: 2
+      }
+    }
+  };
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [FriendProfileDashboardComponent],
+      providers: [
+        { provide: UserFriendsService, useValue: userFriendsServiceMock },
+        { provide: ActivatedRoute, useValue: activatedRouteMock }
+      ],
+      imports: [TranslateModule.forRoot()]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FriendProfileDashboardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
