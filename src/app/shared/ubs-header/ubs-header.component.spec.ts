4771d4443ac54477261997353776456bde548988
Svitlana <vsvitlana11@gmail.com>
2021-10-24 18:15:54 +0300

    localization

--- src/app/shared/ubs-header/ubs-header.component.spec.ts
+++ src/app/shared/ubs-header/ubs-header.component.spec.ts
@@ -1,5 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { TranslateModule } from '@ngx-translate/core';
 import { Language } from 'src/app/main/i18n/Language';
 import { LanguageService } from 'src/app/main/i18n/language.service';
 
@@ -20,7 +21,7 @@ describe('UbsHeaderComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [UbsHeaderComponent],
-      imports: [RouterTestingModule],
+      imports: [RouterTestingModule, TranslateModule.forRoot()],
       providers: [{ provide: LanguageService, useValue: languageServiceMock }]
     }).compileComponents();
   }));
