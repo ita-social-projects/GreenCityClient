a484fbd03ea07ad60b1606103b670266e52e35a4
Yura935 <boyko.yuriy16@gmail.com>
2021-11-02 10:42:34 +0200

    Added some unit tests

--- src/app/ubs-user/services/user-orders.service.spec.ts
+++ src/app/ubs-user/services/user-orders.service.spec.ts
@@ -1,16 +1,33 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { UserOrdersService } from './user-orders.service';
 
 describe('UserOrdersService', () => {
-  beforeEach(() =>
+  let httpMock: HttpTestingController;
+  const url = 'https://greencity-ubs.azurewebsites.net/ubs/client';
+  let service: UserOrdersService;
+  beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule]
-    })
-  );
+    });
+    service = TestBed.inject(UserOrdersService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  });
 
   it('should be created', () => {
-    const service: UserOrdersService = TestBed.inject(UserOrdersService);
     expect(service).toBeTruthy();
+  });
+
+  it('should return all users orders on ua language', () => {
+    const lang = 1;
+    service.getAllUserOrders().subscribe((data) => {
+      expect(data).toBeDefined();
+    });
+    const req = httpMock.expectOne(`${url}/get-all-orders-data/${lang}`);
+    expect(req.request.method).toBe('GET');
   });
 });
