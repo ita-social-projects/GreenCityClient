b3e4a77d0693a0a44cfdb6953bef77385c430a52
Khrystyna Pryslupska <christina_seniuta@ukr.net>
2021-11-03 14:55:17 +0200

    fixed test

--- src/app/ubs-admin/components/ubs-admin-order-history/ubs-admin-order-history.component.spec.ts
+++ src/app/ubs-admin/components/ubs-admin-order-history/ubs-admin-order-history.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TranslateModule } from '@ngx-translate/core';
 import { UbsAdminOrderHistoryComponent } from './ubs-admin-order-history.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('UbsAdminOrderHistoryComponent', () => {
   let component: UbsAdminOrderHistoryComponent;
@@ -8,6 +9,7 @@ describe('UbsAdminOrderHistoryComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, TranslateModule.forRoot()],
       declarations: [UbsAdminOrderHistoryComponent]
     }).compileComponents();
   }));
