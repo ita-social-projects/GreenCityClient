726ffa0efb4630ff1e5f373baccebc4afb889b76
Khrystyna Pryslupska <christina_seniuta@ukr.net>
2021-11-03 14:48:26 +0200

    recieved data from backend

--- src/app/ubs-admin/components/ubs-admin-order-history/ubs-admin-order-history.component.ts
+++ src/app/ubs-admin/components/ubs-admin-order-history/ubs-admin-order-history.component.ts
@@ -1,17 +1,39 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, OnInit, Input } from '@angular/core';
 import { Subject } from 'rxjs';
+import { OrderService } from '../../services/order.service';
+import { IOrderHistory } from '../../models/ubs-admin.interface';
+import { takeUntil } from 'rxjs/operators';
+
 @Component({
   selector: 'app-ubs-admin-order-history',
   templateUrl: './ubs-admin-order-history.component.html',
   styleUrls: ['./ubs-admin-order-history.component.scss']
 })
-export class UbsAdminOrderHistoryComponent implements OnDestroy {
+export class UbsAdminOrderHistoryComponent implements OnInit, OnDestroy {
+  @Input() order;
+
   private destroy$: Subject<boolean> = new Subject<boolean>();
   pageOpen: boolean;
-  constructor() {}
+  orderHistory;
+
+  constructor(private orderService: OrderService) {}
+
+  ngOnInit() {
+    this.getOrderHistory(this.order.id);
+    console.log(this.orderHistory);
+  }
 
   openDetails() {
     this.pageOpen = !this.pageOpen;
+  }
+
+  getOrderHistory(orderId: number) {
+    this.orderService
+      .getOrderHistory(orderId)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((data: IOrderHistory) => {
+        this.orderHistory = data;
+      });
   }
 
   ngOnDestroy(): void {
