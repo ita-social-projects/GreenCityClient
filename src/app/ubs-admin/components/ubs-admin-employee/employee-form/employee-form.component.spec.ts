78379d98f927e65defd18c5015969da88caed463
Oleg Postolovskyi <newartmask@gmail.com>
2021-11-04 00:05:04 +0200

    should create

--- src/app/ubs-admin/components/ubs-admin-employee/employee-form/employee-form.component.spec.ts
+++ src/app/ubs-admin/components/ubs-admin-employee/employee-form/employee-form.component.spec.ts
@@ -1,14 +1,21 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder } from '@angular/forms';
+import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { TranslateModule } from '@ngx-translate/core';
 
 import { EmployeeFormComponent } from './employee-form.component';
 
 describe('EmployeeFormComponent', () => {
   let component: EmployeeFormComponent;
   let fixture: ComponentFixture<EmployeeFormComponent>;
+  const matDialogRefMock = jasmine.createSpyObj('matDialogRefMock', ['close']);
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [EmployeeFormComponent]
+      declarations: [EmployeeFormComponent],
+      imports: [HttpClientTestingModule, MatDialogModule, TranslateModule.forRoot()],
+      providers: [{ provide: MatDialogRef, useValue: matDialogRefMock }, { provide: MAT_DIALOG_DATA, useValue: {} }, FormBuilder]
     }).compileComponents();
   }));
 
