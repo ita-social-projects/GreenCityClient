9cf60bf54c536acdfc3b37db029e6ba5edd5b1bb
Flash5482 <andruuu5482@gmail.com>
2021-11-09 13:54:56 +0200

    fix test

--- src/app/ubs-user/services/user-messages-service.spec.ts
+++ src/app/ubs-user/services/user-messages-service.spec.ts
@@ -1,10 +1,10 @@
-import {UserMessagesService} from './user-messages.service';
-import {TestBed} from '@angular/core/testing';
-import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
-import {Notifications} from '../../ubs-admin/models/ubs-user.model';
-import {HttpErrorResponse} from '@angular/common/http';
+import { UserMessagesService } from './user-messages.service';
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { Notifications } from '../../ubs-admin/models/ubs-user.model';
+import { environment } from '@environment/environment';
 
-
+const url = environment.backendUbsLink;
 const expectNotification: Notifications = {
   page: [
     {
@@ -26,9 +26,14 @@ const expectNotification: Notifications = {
   currentPage: 0,
   totalPages: 1
 };
+const expectBodyNotification = {
+  body: 'WWWWW',
+  title: 'Unpaid order'
+};
 const appServiceNotificationSpy = jasmine.createSpyObj('UserMessagesService', {
   getNotification: expectNotification,
-  getCountUnreadNotification: 100
+  getCountUnreadNotification: 100,
+  setReadNotification: expectBodyNotification
 });
 describe('UserMessagesService', () => {
   const lang = 'ua';
@@ -49,35 +54,32 @@ describe('UserMessagesService', () => {
     expect(serviceNotification).toBeDefined();
   });
 
-  it('should return expected Notifications (HttpClient called once)', (done: DoneFn) => {
-    appServiceNotificationSpy.getNotification.and.returnValue(expectNotification);
-    expect(appServiceNotificationSpy.getNotification(0, 2)).toBe(expectNotification, 'expected Notification');
-    done();
-  });
-
-  it('should return count of unread notifications  ', (done: DoneFn) => {
-    const expectCountUnreadNotification = 100;
-    appServiceNotificationSpy.getCountUnreadNotification.and.returnValue(100);
-    expect(appServiceNotificationSpy.getCountUnreadNotification()).toBe(expectCountUnreadNotification, 'Shod be count of item');
-    done();
-  });
-
-  it('can test HttpClient.get', (done: DoneFn) => {
-    const message = 'Session expired';
-    serviceNotification.getCountUnreadNotification().subscribe(
-      () => fail('should fail with the 401 error'),
-      (err: HttpErrorResponse) => {
-        expect(err.status).toBe(400, 'status');
-        expect(err.error).toBe(message, 'message');
-        done();
-      }
-    );
-    const req = httpMock.expectOne(`${serviceNotification.url}/notifications/quantityUnreadenNotifications`);
-    expect(req.request.method).toBe('GET');
-    req.flush(message, {
-      status: 400,
-      statusText: 'Unauthorized'
+  it('should return expected Notifications (HttpClient called once)', () => {
+    serviceNotification.getNotification(0, 2).subscribe((notifications) => {
+      expect(notifications).toEqual(expectNotification);
     });
+    const req = httpMock.expectOne(`${url}/notifications?lang=${serviceNotification.language}&page=0&size=2`);
+    expect(req.request.method).toBe('GET');
+    req.flush(expectNotification);
+  });
+
+  it('should return count of unread notifications  ', () => {
+    const expectCountUnreadNotification = 100;
+    serviceNotification.getCountUnreadNotification().subscribe((response) => {
+      expect(response).toEqual(expectCountUnreadNotification);
+    });
+    const req = httpMock.expectOne(`${url}/notifications/quantityUnreadenNotifications`);
+    expect(req.request.method).toBe('GET');
+    req.flush(expectCountUnreadNotification);
+  });
+
+  it('should change status notification unread or read and should return body', () => {
+    serviceNotification.setReadNotification(1).subscribe((response) => {
+      expect(response).toEqual(expectBodyNotification);
+    });
+    const req = httpMock.expectOne(`${url}/notifications/1?lang=${serviceNotification.language}`);
+    expect(req.request.method).toBe('POST');
+    req.flush(expectBodyNotification);
   });
 
   afterEach(() => {
