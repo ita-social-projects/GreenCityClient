286c72f17fedd86456517055000cf3bb660b5b57
NazarDobriy <nazaranddobriy@gmail.com>
2021-11-11 12:14:28 +0200

    Add method changePassword

--- src/app/main/service/auth/change-password.service.ts
+++ src/app/main/service/auth/change-password.service.ts
@@ -1,14 +1,29 @@
 import { Injectable } from '@angular/core';
-import { updatePasswordLink } from '../../links';
-import { HttpClient } from '@angular/common/http';
+import { changePasswordLink, updatePasswordLink } from '../../links';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { RestoreDto } from '../../model/restroreDto';
 import { Observable } from 'rxjs';
+import { LocalStorageService } from '@global-service/localstorage/local-storage.service';
+import { UpdatePasswordDto } from '@global-models/updatePasswordDto';
 
 @Injectable({ providedIn: 'root' })
 export class ChangePasswordService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private localStorageService: LocalStorageService) {}
+
+  private accessToken: string = localStorage.getItem('accessToken');
+
+  private httpOptions = {
+    headers: new HttpHeaders({
+      Authorization: 'my-auth-token'
+    })
+  };
 
   public restorePassword(dto: RestoreDto): Observable<object> {
     return this.http.post<object>(updatePasswordLink, dto);
+  }
+
+  public changePassword(updatePasswordDto: UpdatePasswordDto): Observable<object> {
+    this.httpOptions.headers.set('Authorization', `Bearer ${this.accessToken}`);
+    return this.http.post<object>(changePasswordLink, updatePasswordDto, this.httpOptions);
   }
 }
