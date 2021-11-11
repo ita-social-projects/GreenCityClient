2dc5bf141fae5cc01defed127938f1e3b60b0cbe
NazarDobriy <nazaranddobriy@gmail.com>
2021-11-11 23:47:01 +0200

    Change request on put to change password

--- src/app/main/service/auth/change-password.service.ts
+++ src/app/main/service/auth/change-password.service.ts
@@ -3,27 +3,23 @@ import { changePasswordLink, updatePasswordLink } from '../../links';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { RestoreDto } from '../../model/restroreDto';
 import { Observable } from 'rxjs';
-import { LocalStorageService } from '@global-service/localstorage/local-storage.service';
 import { UpdatePasswordDto } from '@global-models/updatePasswordDto';
 
 @Injectable({ providedIn: 'root' })
 export class ChangePasswordService {
-  constructor(private http: HttpClient, private localStorageService: LocalStorageService) {}
+  constructor(private http: HttpClient) {}
 
   private accessToken: string = localStorage.getItem('accessToken');
-
-  private httpOptions = {
-    headers: new HttpHeaders({
-      Authorization: 'my-auth-token'
-    })
-  };
 
   public restorePassword(dto: RestoreDto): Observable<object> {
     return this.http.post<object>(updatePasswordLink, dto);
   }
 
   public changePassword(updatePasswordDto: UpdatePasswordDto): Observable<object> {
-    this.httpOptions.headers.set('Authorization', `Bearer ${this.accessToken}`);
-    return this.http.post<object>(changePasswordLink, updatePasswordDto, this.httpOptions);
+    const myHeaders = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${this.accessToken}`
+    });
+    return this.http.put<object>(changePasswordLink, updatePasswordDto, { headers: myHeaders });
   }
 }
