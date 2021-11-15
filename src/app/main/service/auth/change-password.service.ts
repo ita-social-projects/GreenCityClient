608ce6d8b3f20405709b2e6f857150ac421b67d2
NazarDobriy <nazaranddobriy@gmail.com>
2021-11-15 12:01:56 +0200

    Use interceptor for adding access token

--- src/app/main/service/auth/change-password.service.ts
+++ src/app/main/service/auth/change-password.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { changePasswordLink, updatePasswordLink } from '../../links';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { RestoreDto } from '../../model/restroreDto';
 import { Observable } from 'rxjs';
 import { UpdatePasswordDto } from '@global-models/updatePasswordDto';
@@ -9,17 +9,11 @@ import { UpdatePasswordDto } from '@global-models/updatePasswordDto';
 export class ChangePasswordService {
   constructor(private http: HttpClient) {}
 
-  private accessToken: string = localStorage.getItem('accessToken');
-
   public restorePassword(dto: RestoreDto): Observable<object> {
     return this.http.post<object>(updatePasswordLink, dto);
   }
 
   public changePassword(updatePasswordDto: UpdatePasswordDto): Observable<object> {
-    const myHeaders = new HttpHeaders({
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${this.accessToken}`
-    });
-    return this.http.put<object>(changePasswordLink, updatePasswordDto, { headers: myHeaders });
+    return this.http.put<UpdatePasswordDto>(changePasswordLink, updatePasswordDto);
   }
 }
