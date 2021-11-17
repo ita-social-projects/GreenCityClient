ee2783c7b312d20b60f7895d1745cdff2dc4fbf8
Oleg Postolovskyi <newartmask@gmail.com>
2021-11-17 18:31:03 +0200

    Added http request for deleting employee

--- src/app/ubs-admin/components/ubs-admin-employee/ubs-admin-employee-card/ubs-admin-employee-card.component.ts
+++ src/app/ubs-admin/components/ubs-admin-employee/ubs-admin-employee-card/ubs-admin-employee-card.component.ts
@@ -3,6 +3,8 @@ import { Page } from '../../../models/ubs-admin.interface';
 import { MatDialog } from '@angular/material/dialog';
 import { EmployeeFormComponent } from '../employee-form/employee-form.component';
 import { DialogPopUpComponent } from '../../shared/components/dialog-pop-up/dialog-pop-up.component';
+import { take } from 'rxjs/operators';
+import { UbsAdminEmployeeService } from 'src/app/ubs-admin/services/ubs-admin-employee.service';
 
 @Component({
   selector: 'app-ubs-admin-employee-card',
@@ -11,7 +13,7 @@ import { DialogPopUpComponent } from '../../shared/components/dialog-pop-up/dial
 })
 export class UbsAdminEmployeeCardComponent {
   @Input() data: Page;
-  constructor(private dialog: MatDialog) {}
+  constructor(private dialog: MatDialog, private ubsAdminEmployeeService: UbsAdminEmployeeService) {}
 
   openModal() {
     this.dialog.open(EmployeeFormComponent, {
@@ -24,12 +26,21 @@ export class UbsAdminEmployeeCardComponent {
   }
 
   deleteEmployee() {
-    this.dialog.open(DialogPopUpComponent, {
+    const matDialogRef = this.dialog.open(DialogPopUpComponent, {
       // data: this.data,
       hasBackdrop: true,
       closeOnNavigation: true,
       disableClose: true,
       panelClass: ''
     });
+
+    matDialogRef
+      .afterClosed()
+      .pipe(take(1))
+      .subscribe((res) => {
+        if (res) {
+          this.ubsAdminEmployeeService.deleteEmployee(this.data.id).subscribe();
+        }
+      });
   }
 }
