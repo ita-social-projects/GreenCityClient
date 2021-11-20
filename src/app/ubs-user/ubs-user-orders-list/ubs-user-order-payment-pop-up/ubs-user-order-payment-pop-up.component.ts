c6c1b2fbbf77912b21bb8b1f54cda46e16fdd798
NazarDobriy <nazaranddobriy@gmail.com>
2021-11-20 13:09:01 +0200

    Change styles for input and add mask for input

--- src/app/ubs-user/ubs-user-orders-list/ubs-user-order-payment-pop-up/ubs-user-order-payment-pop-up.component.ts
+++ src/app/ubs-user/ubs-user-orders-list/ubs-user-order-payment-pop-up/ubs-user-order-payment-pop-up.component.ts
@@ -6,14 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ubs-user-order-payment-pop-up.component.scss']
 })
 export class UbsUserOrderPaymentPopUpComponent implements OnInit {
+  public selectedRadio = 'no';
+  public certificatePattern = /(?!0000)\d{4}-(?!0000)\d{4}/;
+  public certificateMask = '0000-0000';
+
   constructor() {}
 
-  public selectedRadio = 'no';
+  ngOnInit(): void {}
 
   public radioChange(e) {
     console.log(this.selectedRadio);
     console.log(e);
   }
-
-  ngOnInit(): void {}
 }
