bd0810db5231835eefe757e059e4b51dc18a411b
NazarDobriy <nazaranddobriy@gmail.com>
2021-11-20 21:42:03 +0200

    Create form group

--- src/app/ubs-user/ubs-user-orders-list/ubs-user-order-payment-pop-up/ubs-user-order-payment-pop-up.component.ts
+++ src/app/ubs-user/ubs-user-orders-list/ubs-user-order-payment-pop-up/ubs-user-order-payment-pop-up.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { OrderService } from 'src/app/main/component/ubs/services/order.service';
 
 @Component({
@@ -10,12 +11,24 @@ export class UbsUserOrderPaymentPopUpComponent implements OnInit {
   public totalSum: number = 0.0;
   public bonusValue: number = 0.0;
   public selectedRadio: string = 'no';
-  public certificatePattern: object = /(?!0000)\d{4}-(?!0000)\d{4}/;
+  public certificatePattern = /(?!0000)\d{4}-(?!0000)\d{4}/;
   public certificateMask: string = '0000-0000';
+  public orderDetailsForm: FormGroup;
 
-  constructor(private orderService: OrderService) {}
+  constructor(private fb: FormBuilder, private orderService: OrderService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  public initForm() {
+    this.orderDetailsForm = this.fb.group({
+      bonus: new FormControl('no', [Validators.required]),
+      paymentSystem: new FormControl('Fondy', [Validators.required]),
+      orderSum: new FormControl(0, [Validators.required, Validators.min(0)]),
+      certificate: ['', [Validators.required, Validators.minLength(8), Validators.pattern(this.certificatePattern)]]
+    });
+  }
 
   public radioChange(e) {
     console.log(this.selectedRadio);
