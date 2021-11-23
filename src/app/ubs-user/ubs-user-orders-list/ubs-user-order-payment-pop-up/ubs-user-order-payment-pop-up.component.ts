1ddbaf23daedca18d95f17b4f378b631aee5cd57
NazarDobriy <nazaranddobriy@gmail.com>
2021-11-23 18:13:49 +0200

    Add certificates

--- src/app/ubs-user/ubs-user-orders-list/ubs-user-order-payment-pop-up/ubs-user-order-payment-pop-up.component.ts
+++ src/app/ubs-user/ubs-user-orders-list/ubs-user-order-payment-pop-up/ubs-user-order-payment-pop-up.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { OrderService } from 'src/app/main/component/ubs/services/order.service';
 
@@ -15,6 +15,10 @@ export class UbsUserOrderPaymentPopUpComponent implements OnInit {
   public certificatePattern = /(?!0000)\d{4}-(?!0000)\d{4}/;
   public certificateMask: string = '0000-0000';
   public orderDetailsForm: FormGroup;
+  public certificates: any = [];
+  public certStatuses: boolean[] = [];
+  public cancelCertBtn: boolean = false;
+  public certificateError: boolean = false;
 
   constructor(private fb: FormBuilder, private orderService: OrderService, @Inject(MAT_DIALOG_DATA) public data: any) {}
 
@@ -27,7 +31,51 @@ export class UbsUserOrderPaymentPopUpComponent implements OnInit {
     this.orderDetailsForm = this.fb.group({
       bonus: new FormControl('no', [Validators.required]),
       paymentSystem: new FormControl('Fondy', [Validators.required]),
-      certificate: ['', [Validators.required, Validators.minLength(8), Validators.pattern(this.certificatePattern)]]
+      formArrayCertificates: this.fb.array([new FormControl('', [Validators.minLength(8), Validators.pattern(this.certificatePattern)])])
     });
+  }
+
+  get formArrayCertificates() {
+    return this.orderDetailsForm.get('formArrayCertificates') as FormArray;
+  }
+
+  public certificateSubmit(index: number): void {
+    if (!this.certificates.includes(this.formArrayCertificates.value[index])) {
+      this.certificates.push(this.formArrayCertificates.value[index]);
+      this.certStatuses.push(true);
+      this.calculateCertificates(this.certificates);
+    }
+  }
+
+  public calculateCertificates(arr): void {
+    this.cancelCertBtn = true;
+    console.log(arr);
+    if (arr.length > 0) {
+      arr.forEach((certificate, index) => {
+        this.orderService.processCertificate(certificate).subscribe(
+          (responce) => {
+            console.log(responce);
+            console.log('Index = ', index);
+            if (responce.certificateStatus === 'ACTIVE') {
+              this.totalSum -= responce.certificatePoints;
+            } else {
+              this.certificateError = true;
+            }
+          },
+          (error) => {
+            if (error.status === 404) {
+              arr.splice(index, 1);
+              this.certificateError = true;
+            }
+          }
+        );
+      });
+    }
+  }
+
+  public deleteCertificate(index: number): void {
+    this.certificateError = false;
+    this.certificates.splice(index, 1);
+    this.cancelCertBtn = false;
   }
 }
