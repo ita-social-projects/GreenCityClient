89f99fa5c520ca5ccaf88442281d47f1a23d7b2f
Oleg Postolovskyi <newartmask@gmail.com>
2021-11-24 15:07:56 +0200

    Imported needed modules for tests

--- src/app/ubs-admin/components/ubs-admin-employee/ubs-admin-employee.component.spec.ts
+++ src/app/ubs-admin/components/ubs-admin-employee/ubs-admin-employee.component.spec.ts
@@ -1,20 +1,33 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { TranslateModule } from '@ngx-translate/core';
+import { NgxPaginationModule, PaginatePipe } from 'ngx-pagination';
 
 import { UbsAdminEmployeeComponent } from './ubs-admin-employee.component';
 
-xdescribe('UbsAdminEmployeeComponent', () => {
+fdescribe('UbsAdminEmployeeComponent', () => {
   let component: UbsAdminEmployeeComponent;
   let fixture: ComponentFixture<UbsAdminEmployeeComponent>;
+  let activatedRoute: ActivatedRoute;
+  let dialog: MatDialog;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [UbsAdminEmployeeComponent]
+      declarations: [UbsAdminEmployeeComponent, PaginatePipe],
+      imports: [TranslateModule.forRoot(), RouterTestingModule, HttpClientTestingModule, MatDialogModule, NgxPaginationModule],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(UbsAdminEmployeeComponent);
     component = fixture.componentInstance;
+    activatedRoute = TestBed.inject(ActivatedRoute);
+    dialog = TestBed.inject(MatDialog);
     fixture.detectChanges();
   });
 
