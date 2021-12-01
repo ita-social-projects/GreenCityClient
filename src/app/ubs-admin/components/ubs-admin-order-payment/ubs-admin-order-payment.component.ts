89d7d2c74ac724b8f17ae547237b59f875e0fdfc
RybakY <randomnuy@gmail.com>
2021-12-01 10:02:31 +0200

    adding dynamic values to payment details

--- src/app/ubs-admin/components/ubs-admin-order-payment/ubs-admin-order-payment.component.ts
+++ src/app/ubs-admin/components/ubs-admin-order-payment/ubs-admin-order-payment.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { IPaymentInfo, IPaymentInfoDtos } from '../../models/ubs-admin.interface';
 
 @Component({
   selector: 'app-ubs-admin-order-payment',
   templateUrl: './ubs-admin-order-payment.component.html',
   styleUrls: ['./ubs-admin-order-payment.component.scss']
 })
-export class UbsAdminOrderPaymentComponent {
+export class UbsAdminOrderPaymentComponent implements OnInit {
+  @Input() orderInfo;
+  paidAmount: number;
+  unPaidAmount: number;
   pageOpen: boolean;
+  paymentInfo: IPaymentInfo;
+  paymentsArray: IPaymentInfoDtos[];
+
+  ngOnInit() {
+    this.paymentInfo = this.orderInfo.paymentTableInfoDto;
+    this.paymentsArray = this.orderInfo.paymentTableInfoDto.paymentInfoDtos;
+    this.paidAmount = this.paymentInfo.paidAmount;
+    this.unPaidAmount = this.paymentInfo.unPaidAmount;
+  }
 
   openDetails() {
     this.pageOpen = !this.pageOpen;
