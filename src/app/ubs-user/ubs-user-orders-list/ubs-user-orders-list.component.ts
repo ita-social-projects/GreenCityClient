cf71da0bb41df6e912ed4ea55af0ac65e30d1d20
Boblyk Nazar <boblyk.nazar@gmail.com>
2021-12-06 10:46:44 +0200

    refactoring

--- src/app/ubs-user/ubs-user-orders-list/ubs-user-orders-list.component.ts
+++ src/app/ubs-user/ubs-user-orders-list/ubs-user-orders-list.component.ts
@@ -30,13 +30,7 @@ export class UbsUserOrdersListComponent {
   }
 
   changeCard(id: number) {
-    this.orders.forEach((order) => {
-      if (order.generalOrderInfo.id === id) {
-        order.extend = !order.extend;
-      } else if (order.generalOrderInfo.id !== id) {
-        order.extend = false;
-      }
-    });
+    this.orders.forEach((order) => (order.extend = order.generalOrderInfo.id === id ? !order.extend : false));
   }
 
   deleteCard(orderId: number) {
