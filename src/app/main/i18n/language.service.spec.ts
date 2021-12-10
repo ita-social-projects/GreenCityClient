e191e5cd4f44df417bc4f78f13de148d604e4b36
Oleg Postolovskyi <newartmask@gmail.com>
2021-12-10 23:27:31 +0200

    Added unit tests to LanguageService

--- src/app/main/i18n/language.service.spec.ts
+++ src/app/main/i18n/language.service.spec.ts
@@ -1,10 +1,10 @@
 import { TestBed } from '@angular/core/testing';
 import { LocalStorageService } from '@global-service/localstorage/local-storage.service';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
-
+import { Language } from './Language';
 import { LanguageService } from './language.service';
 
-fdescribe('LanguageService', () => {
+describe('LanguageService', () => {
   let service: LanguageService;
   let localStorageService: LocalStorageService;
   let translate: TranslateService;
@@ -12,9 +12,8 @@ fdescribe('LanguageService', () => {
   let setCurrentLanguageMock: any;
   let setDefaultLangMock: any;
 
-  const langMap = 'langMap';
-  const monthMap = 'monthMap';
   const getLanguageByString = 'getLanguageByString';
+  const defaultLanguage = 'defaultLanguage';
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -27,8 +26,6 @@ fdescribe('LanguageService', () => {
     getCurrentLanguageMock = spyOn(localStorageService, 'getCurrentLanguage');
     setCurrentLanguageMock = spyOn(localStorageService, 'setCurrentLanguage');
     setDefaultLangMock = spyOn(translate, 'setDefaultLang');
-    spyOn(service[langMap], 'set');
-    spyOn(service[monthMap], 'set');
   });
 
   it('should be created', () => {
@@ -53,5 +50,38 @@ fdescribe('LanguageService', () => {
     getCurrentLanguageMock.and.returnValue('ua');
     const spy = service.getCurrentLanguage();
     expect(spy).toBe('ua');
+  });
+
+  it('getLanguageByString should return the language', () => {
+    const spy = service[getLanguageByString]('ua');
+    expect(spy).toBe('ua');
+  });
+
+  it('getLanguageByString should return default language', () => {
+    service[defaultLanguage] = Language.UA;
+    const spy = service[getLanguageByString]('de');
+    expect(spy).toBe('ua');
+  });
+
+  it('getLocalizedMonth should return the month', () => {
+    getCurrentLanguageMock.and.returnValue('ua');
+    const spy = service.getLocalizedMonth(2);
+    expect(spy).toBe('березня');
+  });
+
+  it('setCurrentLanguage and setDefaultLang should be called in changeCurrentLanguage', () => {
+    service.changeCurrentLanguage(Language.UA);
+    expect(setDefaultLangMock).toHaveBeenCalledWith('ua');
+    expect(setCurrentLanguageMock).toHaveBeenCalledWith('ua');
+  });
+
+  it('getLanguageId should return the value', () => {
+    service.synqLanguageArr = [
+      { id: 111, code: 'ua' },
+      { id: 222, code: 'en' },
+      { id: 333, code: 'ru' }
+    ];
+    const spy = service.getLanguageId(Language.UA);
+    expect(spy).toBe(111);
   });
 });
