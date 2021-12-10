b655d153c6fb85e04c8fea944994b7259b42d5dc
Oleg Postolovskyi <newartmask@gmail.com>
2021-12-10 18:35:19 +0200

    Unskipped test file

--- src/app/main/i18n/language.service.spec.ts
+++ src/app/main/i18n/language.service.spec.ts
@@ -1,9 +1,14 @@
 import { TestBed } from '@angular/core/testing';
+import { TranslateModule } from '@ngx-translate/core';
 
 import { LanguageService } from './language.service';
 
-describe('LanguageService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+fdescribe('LanguageService', () => {
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [TranslateModule.forRoot()]
+    })
+  );
 
   it('should be created', () => {
     const service: LanguageService = TestBed.get(LanguageService);
