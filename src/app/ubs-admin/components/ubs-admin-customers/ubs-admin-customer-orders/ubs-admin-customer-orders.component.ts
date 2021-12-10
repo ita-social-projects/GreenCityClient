4048ce419b88b727d18d7a613c593fad260d371f
TarasKuzma1999 <taras.kuzma6@gmail.com>
2021-12-10 17:28:12 +0200

    conected table to resize derective

--- src/app/ubs-admin/components/ubs-admin-customers/ubs-admin-customer-orders/ubs-admin-customer-orders.component.ts
+++ src/app/ubs-admin/components/ubs-admin-customers/ubs-admin-customer-orders/ubs-admin-customer-orders.component.ts
@@ -1,18 +1,8 @@
 import { takeUntil } from 'rxjs/operators';
 import { Subject } from 'rxjs';
-import { MatTable, MatTableDataSource } from '@angular/material/table';
+import { MatTableDataSource } from '@angular/material/table';
 import { ActivatedRoute, Router } from '@angular/router';
-import {
-  Component,
-  OnInit,
-  HostListener,
-  Renderer2,
-  ViewChild,
-  ElementRef,
-  ChangeDetectorRef,
-  AfterViewChecked,
-  OnDestroy
-} from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef, AfterViewChecked, OnDestroy } from '@angular/core';
 import { columnsParamsOrders } from './../columnsParams';
 import { AdminCustomersService } from 'src/app/ubs-admin/services/admin-customers.service';
 import { ICustomerOrdersTable } from './../../../models/customer-orders-table.model';
@@ -26,13 +16,6 @@ import { LocalStorageService } from './../../../../main/service/localstorage/loc
 export class UbsAdminCustomerOrdersComponent implements OnInit, AfterViewChecked, OnDestroy {
   private id: string;
   private destroy$: Subject<boolean> = new Subject<boolean>();
-  private pressed = false;
-  private currentResizeIndex: number;
-  private startX: number;
-  private startWidth: number;
-  private isResizingRight: boolean;
-  private resizableMousemove: () => void;
-  private resizableMouseup: () => void;
   private sortingColumn: string;
   private sortingType: string;
   private page = 0;
@@ -48,10 +31,7 @@ export class UbsAdminCustomerOrdersComponent implements OnInit, AfterViewChecked
   public isCanScroll = false;
   public isTableLoading = false;
 
-  @ViewChild(MatTable, { read: ElementRef }) private matTableRef: ElementRef;
-
   constructor(
-    private renderer: Renderer2,
     private route: ActivatedRoute,
     private router: Router,
     private adminCustomerService: AdminCustomersService,
@@ -70,7 +50,6 @@ export class UbsAdminCustomerOrdersComponent implements OnInit, AfterViewChecked
 
   ngAfterViewChecked() {
     if (!this.isLoading) {
-      this.setTableResize(this.matTableRef.nativeElement.clientWidth);
       this.cdr.detectChanges();
     }
   }
@@ -78,6 +57,7 @@ export class UbsAdminCustomerOrdersComponent implements OnInit, AfterViewChecked
   private getOrders() {
     this.route.params.subscribe((params) => {
       this.id = params.id;
+      this.page = 0;
       this.adminCustomerService
         .getCustomerOrders(this.id, this.page, this.sortingColumn || 'id', this.sortingType || 'ASC')
         .pipe(takeUntil(this.destroy$))
@@ -139,88 +119,6 @@ export class UbsAdminCustomerOrdersComponent implements OnInit, AfterViewChecked
     this.router.navigate([]).then((result) => {
       window.open(`/#/ubs-admin/order/${id}`, '_blank');
     });
-  }
-
-  //////////// resize logic
-  public onResizeColumn(event: any, index: number) {
-    this.checkResizing(event, index);
-    this.currentResizeIndex = index;
-    this.pressed = true;
-    this.startX = event.pageX;
-    this.startWidth = event.target.clientWidth;
-    event.preventDefault();
-    this.mouseMove(index);
-  }
-
-  private setTableResize(tableWidth: number) {
-    let totWidth = 0;
-    this.columns.forEach((column) => {
-      totWidth += column.width;
-    });
-    const scale = (tableWidth - 5) / totWidth;
-    this.columns.forEach((column) => {
-      column.width *= scale;
-      this.setColumnWidth(column);
-    });
-  }
-
-  private checkResizing(event: any, index: any) {
-    const cellData = this.getCellData(index);
-    this.isResizingRight =
-      index === 0 || (Math.abs(event.pageX - cellData.right) < cellData.width / 2 && index !== this.columns.length - 1);
-  }
-
-  private getCellData(index: number) {
-    const headerRow = this.matTableRef.nativeElement.children[0];
-    const cell = headerRow.children[index];
-    return cell.getBoundingClientRect();
-  }
-
-  private mouseMove(index: number) {
-    this.resizableMousemove = this.renderer.listen('document', 'mousemove', (event) => {
-      if (this.pressed && event.buttons) {
-        const dx = this.isResizingRight ? event.pageX - this.startX : -event.pageX + this.startX;
-        const width = this.startWidth + dx;
-        if (this.currentResizeIndex === index && width > 100) {
-          this.setColumnWidthChanges(index, width);
-        }
-      }
-    });
-    this.resizableMouseup = this.renderer.listen('document', 'mouseup', (event) => {
-      if (this.pressed) {
-        this.pressed = false;
-        this.currentResizeIndex = -1;
-        this.resizableMousemove();
-        this.resizableMouseup();
-      }
-    });
-  }
-
-  private setColumnWidthChanges(index: number, width: number) {
-    const orgWidth = this.columns[index].width;
-    const dx = width - orgWidth;
-    if (dx !== 0) {
-      const j = this.isResizingRight ? index + 1 : index - 1;
-      const newWidth = this.columns[j].width - dx;
-      if (newWidth > 50) {
-        this.columns[index].width = width;
-        this.setColumnWidth(this.columns[index]);
-        this.columns[j].width = newWidth;
-        this.setColumnWidth(this.columns[j]);
-      }
-    }
-  }
-
-  private setColumnWidth(column: any) {
-    const columnEls = Array.from(document.getElementsByClassName('mat-column-' + column.title.key));
-    columnEls.forEach((el: any) => {
-      el.style.width = column.width + 'px';
-    });
-  }
-
-  @HostListener('window:resize', ['$event'])
-  onResize() {
-    this.setTableResize(this.matTableRef.nativeElement.clientWidth);
   }
 
   ngOnDestroy() {
--- src/app/ubs-admin/derictives/resize-table-columns.directive.ts
+++ src/app/ubs-admin/derictives/resize-table-columns.directive.ts
@@ -1,21 +1,15 @@
 import { Directive, OnInit, Renderer2, Input, ElementRef } from '@angular/core';
 
 @Directive({
-  selector: '[resizeColumn]'
+  selector: '[appResizeColumn]'
 })
 export class ResizeColumnDirective implements OnInit {
-  @Input('resizeColumn') resizable: boolean;
-
-  @Input() index: number;
+  @Input('appResizeColumn') params: { resizable: boolean; index: number };
 
   private startX: number;
-
   private startWidth: number;
-
   private column: HTMLElement;
-
   private table: HTMLElement;
-
   private pressed: boolean;
 
   constructor(private renderer: Renderer2, private el: ElementRef) {
@@ -23,7 +17,7 @@ export class ResizeColumnDirective implements OnInit {
   }
 
   ngOnInit() {
-    if (this.resizable) {
+    if (this.params.resizable) {
       const row = this.renderer.parentNode(this.column);
       const thead = this.renderer.parentNode(row);
       this.table = this.renderer.parentNode(thead);
@@ -47,18 +41,11 @@ export class ResizeColumnDirective implements OnInit {
     const offset = 35;
     if (this.pressed && event.buttons) {
       this.renderer.addClass(this.table, 'resizing');
-
-      // Calculate width of column
-      let width = this.startWidth + (event.pageX - this.startX - offset);
-
+      const width = this.startWidth + (event.pageX - this.startX - offset);
       const tableCells = Array.from(this.table.querySelectorAll('.mat-row')).map((row: any) =>
-        row.querySelectorAll('.mat-cell').item(this.index)
+        row.querySelectorAll('.mat-cell').item(this.params.index)
       );
-
-      // Set table header width
       this.renderer.setStyle(this.column, 'width', `${width}px`);
-
-      // Set table cells width
       for (const cell of tableCells) {
         this.renderer.setStyle(cell, 'width', `${width}px`);
       }
