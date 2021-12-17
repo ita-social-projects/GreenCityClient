62562dd58e717de259d533f9a006b5033b5fdd1d
Oleg Postolovskyi <newartmask@gmail.com>
2021-12-17 18:59:18 +0200

    Added tests

--- src/app/ubs-admin/components/shared/components/show-imgs-pop-up/show-imgs-pop-up.component.spec.ts
+++ src/app/ubs-admin/components/shared/components/show-imgs-pop-up/show-imgs-pop-up.component.spec.ts
@@ -1,4 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { TranslateModule } from '@ngx-translate/core';
+import { of } from 'rxjs';
 
 import { ShowImgsPopUpComponent } from './show-imgs-pop-up.component';
 
@@ -6,9 +9,33 @@ describe('ShowImgsPopUpComponent', () => {
   let component: ShowImgsPopUpComponent;
   let fixture: ComponentFixture<ShowImgsPopUpComponent>;
 
+  const dialogRefStub = {
+    keydownEvents() {
+      return of();
+    },
+    backdropClick() {
+      return of();
+    },
+    close() {}
+  };
+
+  const popupDataStub = {
+    imgIndex: 1,
+    images: [
+      { src: 'fakeFoto1', label: null, name: 'fakeName1' },
+      { src: 'fakeFoto2', label: null, name: 'fakeName2' },
+      { src: null, label: null, name: null }
+    ]
+  };
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ShowImgsPopUpComponent]
+      declarations: [ShowImgsPopUpComponent],
+      imports: [MatDialogModule, TranslateModule.forRoot()],
+      providers: [
+        { provide: MatDialogRef, useValue: dialogRefStub },
+        { provide: MAT_DIALOG_DATA, useValue: popupDataStub }
+      ]
     }).compileComponents();
   }));
 
@@ -20,5 +47,17 @@ describe('ShowImgsPopUpComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('nextImg increment index', () => {
+    component.imgIndex = 1;
+    component.nextImg(true);
+    expect(component.imgIndex).toBe(0);
+  });
+
+  it('nextImg dencrement index', () => {
+    component.imgIndex = 0;
+    component.nextImg(false);
+    expect(component.imgIndex).toBe(1);
   });
 });
