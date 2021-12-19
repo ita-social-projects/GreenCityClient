38f33503cfb4c1abfcaaac8834113abd37f8159f
NazarDobriy <nazaranddobriy@gmail.com>
2021-12-20 00:01:35 +0200

    Remove OnInit and for loop

--- src/app/ubs-user/ubs-user-orders-list/ubs-user-order-cancel-pop-up/ubs-user-order-cancel-pop-up.component.ts
+++ src/app/ubs-user/ubs-user-orders-list/ubs-user-order-cancel-pop-up/ubs-user-order-cancel-pop-up.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { UserOrdersService } from '../../services/user-orders.service';
 import { IOrderData } from '../models/IOrderData.interface';
@@ -8,18 +8,11 @@ import { IOrderData } from '../models/IOrderData.interface';
   templateUrl: './ubs-user-order-cancel-pop-up.component.html',
   styleUrls: ['./ubs-user-order-cancel-pop-up.component.scss']
 })
-export class UbsUserOrderCancelPopUpComponent implements OnInit {
+export class UbsUserOrderCancelPopUpComponent {
   constructor(private userOrdersService: UserOrdersService, @Inject(MAT_DIALOG_DATA) public data: IOrderData) {}
-
-  ngOnInit(): void {}
 
   public deleteCard(): void {
     this.userOrdersService.deleteOrder(this.data.orderId).subscribe();
-    for (let i = 0; i < this.data.orders.length; i++) {
-      if (this.data.orders[i].generalOrderInfo.id === this.data.orderId) {
-        this.data.orders.splice(i, 1);
-        break;
-      }
-    }
+    this.data.orders = this.data.orders.filter((order) => order.generalOrderInfo.id !== this.data.orderId);
   }
 }
