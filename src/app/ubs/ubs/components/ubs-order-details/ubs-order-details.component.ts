e1d59afe583f69d0eb60600ddfaa3b21ebd29b0c
Oleg Postolovskyi <newartmask@gmail.com>
2022-01-05 16:50:55 +0200

    Added if statement and setTimeout to takeOrderData

--- src/app/ubs/ubs/components/ubs-order-details/ubs-order-details.component.ts
+++ src/app/ubs/ubs/components/ubs-order-details/ubs-order-details.component.ts
@@ -218,14 +218,19 @@ export class UBSOrderDetailsComponent extends FormBaseComponent implements OnIni
         this.defaultPoints = this.points;
         this.certificateLeft = orderData.points;
         this.bags.forEach((bag) => {
-          bag.quantity = bag.quantity === undefined ? null : bag.quantity;
-          this.orderDetailsForm.addControl('quantity' + String(bag.id), new FormControl('', [Validators.min(0), Validators.max(999)]));
-          const quantity = bag.quantity === null ? '' : +bag.quantity;
-          const valueName = 'quantity' + String(bag.id);
-          this.orderDetailsForm.controls[valueName].setValue(quantity);
+          if (bag.code === this.currentLanguage) {
+            bag.quantity = bag.quantity === undefined ? null : bag.quantity;
+            this.orderDetailsForm.addControl('quantity' + String(bag.id), new FormControl('', [Validators.min(0), Validators.max(999)]));
+            const quantity = bag.quantity === null ? '' : +bag.quantity;
+            const valueName = 'quantity' + String(bag.id);
+            this.orderDetailsForm.controls[valueName].setValue(quantity);
+          }
         });
         this.filterBags();
         this.isFetching = false;
+        setTimeout(() => {
+          this.shareFormService.changeOrderDetails();
+        }, 0);
       });
   }
 
