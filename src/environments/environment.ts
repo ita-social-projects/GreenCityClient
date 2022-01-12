3cc8f597e11c8e0abf58d75b008b3e75f9d1b8d9
Oleg Postolovskyi <34275837+NewArtMask@users.noreply.github.com>
2022-01-12 18:25:06 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,15 +2,15 @@ export const environment = {
   production: false,
   apiKeys: 'AIzaSyDanBliCzAuCZrsq67FeKEs3vqAilUD_is',
   apiMapKey: 'AIzaSyB3xs7Kczo46LFcQRFKPMdrE0lU4qsR_S4',
-  backendLink: 'https://192.168.137.146:8080/',
-  backendUserLink: 'https://192.168.137.146:8060/',
-  backendUbsLink: 'https://192.168.137.146:8050',
+  backendLink: 'http://192.168.137.146:8080/',
+  backendUserLink: 'http://192.168.137.146:8060/',
+  backendUbsLink: 'http://192.168.137.146:8050',
   frontendLink: 'http://192.168.137.146:4200',
-  socket: 'https://192.168.137.146/socket',
+  socket: 'http://192.168.137.146/socket',
   firebaseConfig: {
     apiKey: 'AIzaSyDSVxahTHqdffRX2upKIMXCCjEBUYoHX8E',
     authDomain: 'greencity-9bdb7.firebaseapp.com',
-    databaseURL: 'https://192.168.137.146:5432',
+    databaseURL: 'http://192.168.137.146:5432',
     projectId: 'greencity-9bdb7',
     storageBucket: 'greencity-9bdb7.appspot.com',
     messagingSenderId: '3763960182',
@@ -18,8 +18,8 @@ export const environment = {
     measurementId: 'G-CCHRKQ4R0S'
   },
   ubsAdmin: {
-    backendEmployeesLink: 'https://192.168.137.146:8060/admin/ubs-employee/getAll-active-employees',
-    backendUbsAdminLink: 'https://192.168.137.146:8060/ubs'
+    backendEmployeesLink: 'http://192.168.137.146:8050/admin/ubs-employee/getAll-active-employees',
+    backendUbsAdminLink: 'http://192.168.137.146:8050/ubs'
   },
   googleClientId: '129513550972-eu9ej46rviv1ac8q14at62t2k5qon1pu.apps.googleusercontent.com',
   agmCoreModuleApiKey: 'AIzaSyC7q2v0VgRy60dAoItfv3IJhfJQEEoeqCI'
