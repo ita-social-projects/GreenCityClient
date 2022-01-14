7644093292d05480d5078dfa12d4ee9186bedef7
Oleg Postolovskyi <newartmask@gmail.com>
2022-01-14 18:26:18 +0200

    Unit tests

--- src/app/ubs/ubs-admin/components/add-payment/add-payment.component.spec.ts
+++ src/app/ubs/ubs-admin/components/add-payment/add-payment.component.spec.ts
@@ -2,6 +2,7 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormBuilder } from '@angular/forms';
 import { MatDialog, MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { LocalStorageService } from '@global-service/localstorage/local-storage.service';
 import { TranslateModule } from '@ngx-translate/core';
 import { of } from 'rxjs';
 import { OrderService } from '../../services/order.service';
@@ -11,6 +12,7 @@ import { AddPaymentComponent } from './add-payment.component';
 fdescribe('AddPaymentComponent', () => {
   let component: AddPaymentComponent;
   let fixture: ComponentFixture<AddPaymentComponent>;
+  let localeStorageService: LocalStorageService;
   const matDialogRefMock = () => ({
     close: () => ({})
   });
@@ -21,6 +23,11 @@ fdescribe('AddPaymentComponent', () => {
     orderId: 735,
     viewMode: false,
     payment: null
+  };
+  const dataFileMock = new File([''], 'test-file.jpeg');
+  const fakeFileHandle = {
+    file: dataFileMock,
+    url: 'fakeUrl'
   };
   const orderServiceMock = jasmine.createSpyObj('orderService', ['addPaymentManually', 'updatePaymentManually', 'deleteManualPayment']);
   orderServiceMock.addPaymentManually.and.returnValue(of(true));
@@ -44,6 +51,8 @@ fdescribe('AddPaymentComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(AddPaymentComponent);
     component = fixture.componentInstance;
+    localeStorageService = TestBed.inject(LocalStorageService);
+    (localeStorageService as any).firstNameBehaviourSubject = of('fakeName');
     fixture.detectChanges();
   });
 
@@ -77,5 +86,58 @@ fdescribe('AddPaymentComponent', () => {
 
   it(`isInitialImageChanged has default value`, () => {
     expect(component.isInitialImageChanged).toBeFalsy();
+  });
+
+  describe('ngOnInit', () => {
+    it('makes expected calls', () => {
+      const spy = spyOn(component, 'initForm');
+      component.ngOnInit();
+      expect(spy).toHaveBeenCalled();
+      expect(component.adminName).toBe('fakeName');
+      expect(component.orderId).toBe(735);
+      expect(component.viewMode).toBeFalsy();
+      expect(component.payment).toBe(null);
+    });
+  });
+
+  describe('save', () => {
+    it('makes expected calls', () => {
+      const spy = spyOn(component, 'processPayment');
+      component.save();
+      expect(spy).toHaveBeenCalled();
+    });
+  });
+
+  describe('removeImage', () => {
+    it(`is not edit mode`, () => {
+      component.imagePreview = { src: 'fakePaht', name: 'fakeName' };
+      component.file = fakeFileHandle;
+      component.removeImage();
+      expect(component.imagePreview.name).toBe(null);
+      expect(component.imagePreview.src).toBe(null);
+      expect(component.file).toBe(null);
+    });
+
+    it(`is edit mode`, () => {
+      component.editMode = true;
+      component.payment = { imagePath: 'fakePath' } as any;
+      component.imagePreview = { src: 'fakePath', name: 'fakeName' };
+      component.file = fakeFileHandle;
+      component.removeImage();
+      expect(component.imagePreview.name).toBe(null);
+      expect(component.imagePreview.src).toBe(null);
+      expect(component.file).toBe(null);
+      expect(component.isInitialImageChanged).toBeTruthy();
+    });
+  });
+
+  describe('editPayment', () => {
+    xit(`makes expected calls`, () => {
+      component.imagePreview = { src: 'fakePaht', name: 'fakeName' };
+      component.editPayment();
+      // expect(component.imagePreview.name).toBe(null);
+      // expect(component.imagePreview.src).toBe(null);
+      // expect(component.file).toBe(null);
+    });
   });
 });
