9b111388583f2c9ea2cdfd984c9117091d6bc07a
Oleg Postolovskyi <newartmask@gmail.com>
2022-01-14 15:43:25 +0200

    Added tests

--- src/app/ubs/ubs-admin/components/add-payment/add-payment.component.spec.ts
+++ src/app/ubs/ubs-admin/components/add-payment/add-payment.component.spec.ts
@@ -3,15 +3,29 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormBuilder } from '@angular/forms';
 import { MatDialog, MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { TranslateModule } from '@ngx-translate/core';
+import { of } from 'rxjs';
+import { OrderService } from '../../services/order.service';
 
 import { AddPaymentComponent } from './add-payment.component';
 
 fdescribe('AddPaymentComponent', () => {
   let component: AddPaymentComponent;
   let fixture: ComponentFixture<AddPaymentComponent>;
-  const matDialogRefMock = {};
-  const matDialogMock = {};
-  const mockedData = {};
+  const matDialogRefMock = () => ({
+    close: () => ({})
+  });
+  const matDialogMock = () => ({
+    open: () => of(true)
+  });
+  const mockedData = {
+    orderId: 735,
+    viewMode: false,
+    payment: null
+  };
+  const orderServiceMock = jasmine.createSpyObj('orderService', ['addPaymentManually', 'updatePaymentManually', 'deleteManualPayment']);
+  orderServiceMock.addPaymentManually.and.returnValue(of(true));
+  orderServiceMock.updatePaymentManually.and.returnValue(of(true));
+  orderServiceMock.deleteManualPayment.and.returnValue(of(true));
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -21,6 +35,7 @@ fdescribe('AddPaymentComponent', () => {
         { provide: MatDialogRef, useValue: matDialogRefMock },
         { provide: MatDialog, useValue: matDialogMock },
         { provide: MAT_DIALOG_DATA, useValue: mockedData },
+        { provide: OrderService, useValue: orderServiceMock },
         FormBuilder
       ]
     }).compileComponents();
@@ -34,5 +49,33 @@ fdescribe('AddPaymentComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it(`editMode has default value`, () => {
+    expect(component.editMode).toBeFalsy();
+  });
+
+  it(`isDeleting has default value`, () => {
+    expect(component.isDeleting).toBeFalsy();
+  });
+
+  it(`isUploading has default value`, () => {
+    expect(component.isUploading).toBeFalsy();
+  });
+
+  it(`isImageSizeError has default value`, () => {
+    expect(component.isImageSizeError).toBeFalsy();
+  });
+
+  it(`isImageTypeError has default value`, () => {
+    expect(component.isImageTypeError).toBeFalsy();
+  });
+
+  it(`isInitialDataChanged has default value`, () => {
+    expect(component.isInitialDataChanged).toBeFalsy();
+  });
+
+  it(`isInitialImageChanged has default value`, () => {
+    expect(component.isInitialImageChanged).toBeFalsy();
   });
 });
