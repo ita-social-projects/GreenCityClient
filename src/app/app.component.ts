41af564d54bccfd11604e4e7741342a314c341d9
Oleg Postolovskyi <newartmask@gmail.com>
2022-01-18 14:16:48 +0200

    Implemented app reloading when updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private updates: SwUpdate) {
+    updates.available.subscribe((event) => {
+      updates.activateUpdate().then(() => document.location.reload());
+    });
+  }
+}
