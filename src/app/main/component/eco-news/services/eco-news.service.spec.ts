2c137c43de3277dba2f2b1c0632299ecbeaa4c28
olga210500 <olakovalisko@gmail.com>
2022-01-20 14:59:35 +0200

    changed endpoint

--- src/app/main/component/eco-news/services/eco-news.service.spec.ts
+++ src/app/main/component/eco-news/services/eco-news.service.spec.ts
@@ -52,7 +52,7 @@ describe('EcoNewsService', () => {
       expect(data).toBe(tagMock);
     });
 
-    const req = httpTestingController.expectOne(`${environment.backendLink}econews/tags/all?lang=en`);
+    const req = httpTestingController.expectOne(`${environment.backendLink}tags/search?lang?lang=en&type=ECO_NEWS`);
     expect(req.request.method).toEqual('GET');
     req.flush(tagMock);
   });
