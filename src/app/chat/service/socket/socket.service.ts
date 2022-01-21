41ee8987ebf0bda94b34d6c1c075582f5678255b
TarasKuzma1999 <taras.kuzma6@gmail.com>
2022-01-21 10:39:46 +0200

    added headers with id

--- src/app/chat/service/socket/socket.service.ts
+++ src/app/chat/service/socket/socket.service.ts
@@ -1,3 +1,4 @@
+import { LocalStorageService } from '@global-service/localstorage/local-storage.service';
 import { Injectable } from '@angular/core';
 import * as SockJS from 'sockjs-client';
 import { environment } from '@environment/environment';
@@ -13,10 +14,12 @@ export class SocketService {
   private socket: WebSocket;
   private stompClient: CompatClient;
   private backendSocketLink = `${environment.socket}`;
+  private userId: string;
 
-  constructor(private chatsService: ChatsService) {}
+  constructor(private chatsService: ChatsService, private localStorageService: LocalStorageService) {}
 
   public connect() {
+    this.userId = this.localStorageService.getUserId().toString();
     this.socket = new SockJS(this.backendSocketLink);
     this.stompClient = Stomp.over(() => this.socket);
     this.stompClient.connect(
@@ -27,21 +30,29 @@ export class SocketService {
   }
 
   private onConnected() {
-    this.stompClient.subscribe('/room/message/chat-messages', (data: IMessage) => {
-      // TODO bad logic, you might not be sitting in chat where message landed FIXIT
-      const newMessage: Message = JSON.parse(data.body);
-      console.log(newMessage);
-      const messages = this.chatsService.chatsMessages[newMessage.roomId];
-      if (messages) {
-        messages.page.push(newMessage);
-        this.chatsService.currentChatMessagesStream$.next(messages.page);
-      }
-    });
-    this.stompClient.subscribe('/message/new-participant', (participant) => {
-      console.log(participant);
-      const newChatParticipant: User = JSON.parse(participant.body);
-      this.chatsService.currentChat.participants.push(newChatParticipant);
-    });
+    this.stompClient.subscribe(
+      '/room/message/chat-messages',
+      (data: IMessage) => {
+        // TODO bad logic, you might not be sitting in chat where message landed FIXIT
+        const newMessage: Message = JSON.parse(data.body);
+        console.log(newMessage);
+        const messages = this.chatsService.chatsMessages[newMessage.roomId];
+        if (messages) {
+          messages.page.push(newMessage);
+          this.chatsService.currentChatMessagesStream$.next(messages.page);
+        }
+      },
+      { id: this.userId }
+    );
+    this.stompClient.subscribe(
+      '/message/new-participant',
+      (participant) => {
+        console.log(participant);
+        const newChatParticipant: User = JSON.parse(participant.body);
+        this.chatsService.currentChat.participants.push(newChatParticipant);
+      },
+      { id: this.userId }
+    );
   }
 
   private onError(error) {
@@ -62,7 +73,7 @@ export class SocketService {
 
   createNewChat() {
     const newChatInfo = {
-      currentUserId: 21,
+      currentUserId: 22,
       participantsIds: 18
     };
     this.stompClient.send(`/app/chat/user`, {}, JSON.stringify(newChatInfo));
