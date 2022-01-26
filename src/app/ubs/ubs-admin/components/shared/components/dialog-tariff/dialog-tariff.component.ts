9e82d5bd5f058cd6bb5f2e59fe1d73a5593ef7c6
Svitlana <vsvitlana11@gmail.com>
2022-01-26 22:00:30 +0200

    fix css linter

--- src/app/ubs/ubs-admin/components/shared/components/dialog-tariff/dialog-tariff.component.ts
+++ src/app/ubs/ubs-admin/components/shared/components/dialog-tariff/dialog-tariff.component.ts
@@ -1,23 +1,4 @@
-import {
-  ChangeDetectorRef,
-  Component,
-  ElementRef,
-  Inject,
-  Input,
-  OnInit,
-  Output,
-  QueryList,
-  TemplateRef,
-  ViewChild,
-  ViewChildren
-} from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { LocalStorageService } from '@global-service/localstorage/local-storage.service';
-import { takeUntil } from 'rxjs/operators';
-import { Subject } from 'rxjs';
-import { DatePipe } from '@angular/common';
-import { TariffsService } from 'src/app/ubs/ubs-admin/services/tariffs.service';
+import { Component, Input, OnInit, TemplateRef } from '@angular/core';
 
 @Component({
   selector: 'app-dialog-tariff',
