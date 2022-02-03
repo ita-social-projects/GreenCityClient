099b2286761463b0b56c2d1f3eaf6a7f08c88db9
Oleg Postolovskyi <newartmask@gmail.com>
2022-02-03 14:48:33 +0200

    Created unit tests for LocalizedCurrencyPipe

---
+++ src/app/shared/localized-currency-pipe/localized-currency.pipe.spec.ts
@@ -0,0 +1,39 @@
+import { TestBed, async } from '@angular/core/testing';
+import { LocalStorageService } from '@global-service/localstorage/local-storage.service';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { of } from 'rxjs';
+import { LocalizedCurrencyPipe } from './localized-currency.pipe';
+
+describe('LocalizedCurrencyPipe', () => {
+  let pipe: LocalizedCurrencyPipe;
+  const localStorageServiceMock = jasmine.createSpyObj('LocalStorageService', ['getCurrentLanguage']);
+  localStorageServiceMock.getCurrentLanguage = () => 'en';
+  const fakeDefaultLangObj = { lang: 'en' };
+  const translateMock = jasmine.createSpyObj('translate', ['']);
+  translateMock.defaultLang = 'en';
+  translateMock.onDefaultLangChange = of(fakeDefaultLangObj);
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [LocalizedCurrencyPipe],
+      imports: [TranslateModule.forRoot()],
+      providers: [
+        { provide: TranslateService, useValue: translateMock },
+        { provide: LocalStorageService, useValue: LocalStorageService }
+      ]
+    }).compileComponents();
+    pipe = new LocalizedCurrencyPipe(translateMock, localStorageServiceMock);
+  }));
+
+  it('create an instance', () => {
+    expect(pipe).toBeTruthy();
+  });
+
+  it('lang images has default value', () => {
+    expect((pipe as any).lang).toBe('en');
+  });
+
+  it('transform', () => {
+    expect(pipe.transform(111)).toBe('111 UAH');
+  });
+});
