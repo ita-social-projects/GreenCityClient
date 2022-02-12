28b5c6de196336ae18eefb35cf393c8ebae524ba
olga210500 <olakovalisko@gmail.com>
2022-02-12 17:26:40 +0200

    Added pop-up for several orders

--- src/app/ubs/ubs-admin/components/ubs-admin-several-orders-pop-up/ubs-admin-several-orders-pop-up.component.ts
+++ src/app/ubs/ubs-admin/components/ubs-admin-several-orders-pop-up/ubs-admin-several-orders-pop-up.component.ts
@@ -1,12 +1,18 @@
+import { formatDate } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { takeUntil } from 'rxjs/operators';
+import { IExportDetails } from '../../models/ubs-admin.interface';
 import { fromSelect, toSelect } from '../ubs-admin-table/table-cell-time/table-cell-time-range';
-
+import { LocalStorageService } from '@global-service/localstorage/local-storage.service';
+import { TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-ubs-admin-several-orders-pop-up',
   templateUrl: './ubs-admin-several-orders-pop-up.component.html',
   styleUrls: ['./ubs-admin-several-orders-pop-up.component.scss']
 })
 export class UbsAdminSeveralOrdersPopUpComponent implements OnInit {
+  currentLanguage: string;
   public showTimePicker = false;
   public fromSelect: string[];
   public toSelect: string[];
@@ -14,14 +20,40 @@ export class UbsAdminSeveralOrdersPopUpComponent implements OnInit {
   public toInput: string;
   public from: string;
   public to: string;
+  exportInfo: IExportDetails;
+  ordersForm: FormGroup;
 
-  constructor() {}
+  constructor(private fb: FormBuilder, private localStorageService: LocalStorageService, private translate: TranslateService) {}
   setExportTime() {
     this.showTimePicker = true;
     this.fromSelect = fromSelect;
     this.toSelect = toSelect;
   }
+  ngOnInit(): void {
+    this.localStorageService.languageBehaviourSubject.subscribe((lang) => {
+      this.currentLanguage = lang;
+      this.translate.setDefaultLang(lang);
+    });
+  }
+  initForm() {
+    // const currentEmployees = this.responsiblePersonInfo.currentPositionEmployees;
+    // this.overpayment = 0;
+    this.ordersForm = this.fb.group({
+      exportDetailsDto: this.fb.group({
+        dateExport: this.exportInfo.dateExport ? formatDate(this.exportInfo.dateExport, 'yyyy-MM-dd', this.currentLanguage) : '',
+        timeDeliveryFrom: this.parseTimeToStr(this.exportInfo.timeDeliveryFrom),
+        timeDeliveryTo: this.parseTimeToStr(this.exportInfo.timeDeliveryTo),
+        receivingStation: this.exportInfo.receivingStation
+      }),
 
+      responsiblePersonsForm: this.fb.group({
+        callManager: null,
+        logistician: null,
+        navigator: null,
+        driver: null
+      })
+    });
+  }
   onTimeFromChange() {
     const fromIdx = fromSelect.indexOf(this.fromInput);
     this.toSelect = toSelect.slice(fromIdx);
@@ -42,5 +74,7 @@ export class UbsAdminSeveralOrdersPopUpComponent implements OnInit {
     this.toInput = this.to;
     this.showTimePicker = false;
   }
-  ngOnInit(): void {}
+  parseTimeToStr(dateStr: string) {
+    return dateStr ? formatDate(dateStr, 'HH:mm', this.currentLanguage) : '';
+  }
 }
