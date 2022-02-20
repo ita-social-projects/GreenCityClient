0bf2613a61d19eec0d0e0fd024d365f4b5c40123
Oleg Postolovskyi <newartmask@gmail.com>
2022-02-20 20:00:15 +0200

    Codesmell

--- src/app/main/component/user/services/format-date.service.ts
+++ src/app/main/component/user/services/format-date.service.ts
@@ -4,8 +4,6 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class FormatDateService {
-  constructor() {}
-
   public formatDate(date: string): string {
     const dateArr = date.split('/');
     return [dateArr[2], dateArr[0].length < 2 ? '0' + dateArr[0] : dateArr[0], dateArr[1].length < 2 ? '0' + dateArr[1] : dateArr[1]].join(
