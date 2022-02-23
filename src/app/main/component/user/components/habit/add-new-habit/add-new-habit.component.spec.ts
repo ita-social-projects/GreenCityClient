4e16dab2e4d1639f401e7df8bcc64113aabe4636
Svyatoslav <s1aukaa@gmail.com>
2022-02-23 14:57:06 +0200

    3.0

--- src/app/main/component/user/components/habit/add-new-habit/add-new-habit.component.spec.ts
+++ src/app/main/component/user/components/habit/add-new-habit/add-new-habit.component.spec.ts
@@ -1,16 +1,17 @@
 import { MatSnackBarComponent } from '@global-errors/mat-snack-bar/mat-snack-bar.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AddNewHabitComponent } from './add-new-habit.component';
-import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { LocalStorageService } from '@global-service/localstorage/local-storage.service';
 import { ShoppingListService } from './habit-edit-shopping-list/shopping-list.service';
 import { HabitService } from '@global-service/habit/habit.service';
 import { HabitAssignService } from '@global-service/habit-assign/habit-assign.service';
-import { of, Subject, Subscription } from 'rxjs';
+import { of, Subject } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
+import { HabitResponseInterface } from 'src/app/main/interface/habit/habit-assign.interface';
 
 fdescribe('AddNewHabitComponent', () => {
   let component: AddNewHabitComponent;
@@ -18,14 +19,23 @@ fdescribe('AddNewHabitComponent', () => {
   let matSnackBarMock: MatSnackBarComponent;
   let fakeHabitAssignService: HabitAssignService;
   let fakeLocalStorageService: LocalStorageService;
+  const mockHabitResponse: HabitResponseInterface = {
+    complexity: 2,
+    defaultDuration: 2,
+    habitTranslation: {
+      description: 'test',
+      habitItem: 'test',
+      languageCode: 'test',
+      name: 'test'
+    },
+    id: 2,
+    image: 'test',
+    shoppingListItems: []
+  };
   const mockActivatedRoute = {
     params: of({ habitId: 2 })
-    // params: {
-    //   subscribe: of({ habitId: 2 })
-    // }
   };
   fakeHabitAssignService = jasmine.createSpyObj('fakeHabitAssignService', {
-    // getAssignedHabits: of('mock'),
     getAssignedHabits: of([
       {
         habit: {
@@ -34,21 +44,14 @@ fdescribe('AddNewHabitComponent', () => {
       }
     ]),
     deleteHabitById: of('test'),
-    getCustomHabit: of('test')
+    getCustomHabit: of(mockHabitResponse),
+    getHabitById: of(mockHabitResponse)
   });
-  // fakeHabitAssignService = jasmine.createSpyObj('fakeHabitAssignService', [
-  //   'getAssignedHabits',
-  //   'deleteHabitById',
-  //   'getCustomHabit',
-  //   'assignCustomHabit',
-  //   'updateHabit'
-  // ]);
   fakeLocalStorageService = jasmine.createSpyObj('fakeLocalStorageService', {
     getCurrentLanguage: () => 'ua'
-    // 'languageSubject': of('ua')
   });
   fakeLocalStorageService.languageSubject = new Subject();
-  // fakeLocalStorageService.languageSubject = of({'ua'});
+  fakeLocalStorageService.languageSubject.next('ua');
   matSnackBarMock = jasmine.createSpyObj('MatSnackBarComponent', ['openSnackBar']);
   matSnackBarMock.openSnackBar = (type: string) => {};
 
@@ -64,7 +67,7 @@ fdescribe('AddNewHabitComponent', () => {
         { provide: LocalStorageService, useValue: fakeLocalStorageService },
         { provide: ActivatedRoute, useValue: mockActivatedRoute }
       ],
-      schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA]
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     }).compileComponents();
   }));
 
@@ -79,27 +82,19 @@ fdescribe('AddNewHabitComponent', () => {
   });
 
   it('should create', () => {
-    // spyOn(component, 'ngOnInit').and.returnValue();
     expect(component).toBeTruthy();
   });
 
   it('method deleteHabbit should navigate and openSnackBar', (done) => {
-    component.userId = '33';
-    component.habitId = 33;
-    fixture.detectChanges();
+    const mockId = 33;
     // @ts-ignore
-    spyOn(component.snackBar, 'openSnackBar');
-    // // const spy = spyOn(component.snackBar, 'openSnackBar').and.returnValue();
-    fakeHabitAssignService.deleteHabitById(component.habitId).subscribe(() => {
-      console.log('DONE');
+    spyOn(component.snackBar, 'openSnackBar').and.returnValue();
+    fakeHabitAssignService.deleteHabitById(mockId).subscribe(() => {
       // @ts-ignore
       expect(component.snackBar.openSnackBar).toHaveBeenCalledWith('habitDeleted2');
       done();
     });
     component.deleteHabit();
-    // tick();
-
-    // expect(spy).toHaveBeenCalledWith("habitDeleted222");
   });
 
   it('ngOnDestroy should unsubscribe from subscription', () => {
