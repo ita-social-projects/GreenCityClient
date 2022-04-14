12e161a408a1a7a3708b2c1710d3cb418bfa142f
TurchynAnastasiia <nastya.melnychenko@gmail.com>
2022-04-14 17:32:13 +0300

    Write unit test to admin component

--- src/app/main/component/admin/admin.component.ts
+++ src/app/main/component/admin/admin.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { JwtService } from '../../service/jwt/jwt.service';
-import { HttpClient } from '@angular/common/http';
+// import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-admin',
-  templateUrl: './admin.component.html',
+  templateUrl: './admin.component.html'
 })
 export class AdminComponent implements OnInit {
   userRole: string;
 
-  constructor(private http: HttpClient, private jwtService: JwtService) {}
+  constructor(private jwtService: JwtService) {}
 
   ngOnInit() {
     this.userRole = this.jwtService.getUserRole();
