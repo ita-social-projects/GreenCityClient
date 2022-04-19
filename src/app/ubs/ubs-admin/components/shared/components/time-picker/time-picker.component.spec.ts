9461344429aad8605739c34e4033a33cf15f3886
olga210500 <olakovalisko@gmail.com>
2022-04-19 20:47:01 +0300

    Fixed test

--- src/app/ubs/ubs-admin/components/shared/components/time-picker/time-picker.component.spec.ts
+++ src/app/ubs/ubs-admin/components/shared/components/time-picker/time-picker.component.spec.ts
@@ -1,14 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { TranslateModule } from '@ngx-translate/core';
 
 import { TimePickerComponent } from './time-picker.component';
 
-describe('TimePickerComponent', () => {
+fdescribe('TimePickerComponent', () => {
   let component: TimePickerComponent;
   let fixture: ComponentFixture<TimePickerComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [TimePickerComponent]
+      declarations: [TimePickerComponent],
+      imports: [TranslateModule.forRoot()]
     }).compileComponents();
   }));
 
