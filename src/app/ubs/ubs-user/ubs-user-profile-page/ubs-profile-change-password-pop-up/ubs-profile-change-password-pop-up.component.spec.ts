0bf8a6a64ad6d524702da95c1d8eacbafd2e4938
TurchynAnastasiia <nastya.melnychenko@gmail.com>
2022-04-19 23:31:26 +0300

    add test

---
+++ src/app/ubs/ubs-user/ubs-user-profile-page/ubs-profile-change-password-pop-up/ubs-profile-change-password-pop-up.component.spec.ts
@@ -0,0 +1,75 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ChangePasswordService } from '@global-service/auth/change-password.service';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TranslateModule } from '@ngx-translate/core';
+
+import { UbsProfileChangePasswordPopUpComponent } from './ubs-profile-change-password-pop-up.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { UpdatePasswordDto } from '@global-models/updatePasswordDto';
+import { of } from 'rxjs';
+
+describe('UbsProfileChangePasswordPopUpComponent', () => {
+  let component: UbsProfileChangePasswordPopUpComponent;
+  let fixture: ComponentFixture<UbsProfileChangePasswordPopUpComponent>;
+
+  const changePasswordServiceFake = jasmine.createSpyObj('ChangePasswordService', ['changePassword']);
+  changePasswordServiceFake.changePassword.and.returnValue(of({}));
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [UbsProfileChangePasswordPopUpComponent],
+      imports: [TranslateModule.forRoot(), ReactiveFormsModule, FormsModule],
+      providers: [
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+        { provide: ChangePasswordService, useValue: changePasswordServiceFake },
+        FormBuilder
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UbsProfileChangePasswordPopUpComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('form invalid when empty', () => {
+    expect(component.formConfig.valid).toBeFalsy();
+  });
+  it('ngOnint calls initForm()', () => {
+    const spyInitForm = spyOn(component, 'initForm');
+    component.ngOnInit();
+    expect(spyInitForm).toHaveBeenCalled();
+  });
+
+  it('initForm should create', () => {
+    const initFormFake = {
+      password: '',
+      currentPassword: '',
+      confirmPassword: ''
+    };
+    component.initForm();
+    expect(component.formConfig.value).toEqual(initFormFake);
+  });
+
+  it('submitting a form', () => {
+    expect(component.formConfig.valid).toBeFalsy();
+    component.formConfig.controls['password'].setValue('Test!2334');
+    component.formConfig.controls['currentPassword'].setValue('Test!2334dfff');
+    component.formConfig.controls['confirmPassword'].setValue('Test!2334');
+    expect(component.formConfig.valid).toBeTruthy();
+
+    const updatePasswordDto: UpdatePasswordDto = component.formConfig.value;
+
+    component.onSubmit();
+    expect(updatePasswordDto.password).toBe('Test!2334');
+    expect(updatePasswordDto.currentPassword).toBe('Test!2334dfff');
+    expect(updatePasswordDto.confirmPassword).toBe('Test!2334');
+  });
+});
