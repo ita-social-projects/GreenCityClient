846c094e3c9b3f74a234861ee669380f2d731d6a
Oleksandr <voronko.oleksandr@gmail.com>
2022-05-04 12:48:26 +0300

    delete comments from unit-test

--- src/app/main/component/user/components/habit/add-new-habit/habit-duration/habit-duration.component.spec.ts
+++ src/app/main/component/user/components/habit/add-new-habit/habit-duration/habit-duration.component.spec.ts
@@ -1,19 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { TranslateModule } from '@ngx-translate/core';
-import { CUSTOM_ELEMENTS_SCHEMA, Renderer2 } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { HabitDurationComponent } from './habit-duration.component';
-import { By } from '@angular/platform-browser';
 
 fdescribe('HabitDurationComponent', () => {
   let component: HabitDurationComponent;
   let fixture: ComponentFixture<HabitDurationComponent>;
-  // const fakeRenderer2 =  jasmine.createSpyObj('fakeFirstDep', ['start']);
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [HabitDurationComponent],
       imports: [TranslateModule.forRoot()],
-      // providers: [{provide: Renderer2, useValue: fakeRenderer2}],
       schemas: [CUSTOM_ELEMENTS_SCHEMA]
     }).compileComponents();
   }));
@@ -21,7 +18,6 @@ fdescribe('HabitDurationComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(HabitDurationComponent);
     component = fixture.componentInstance;
-    // fakeRenderer2.start.and.callFake()
     fixture.detectChanges();
   });
 
@@ -34,14 +30,6 @@ fdescribe('HabitDurationComponent', () => {
     component.updateDuration();
     expect(event).toHaveBeenCalled();
   });
-
-  // it(' component should be send event', () => {
-  //   const event = spyOn(component.changeDuration, 'emit' );
-  //   const input = fixture.debugElement.query(By.css('input'));
-  //   event.calls.reset();
-  //   input.nativeElement.input();
-  //   expect(event).toHaveBeenCalledWith();
-  // });
 
   it('should create', () => {
     expect(component).toBeTruthy();
