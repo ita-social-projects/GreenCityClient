b893c6548496200f80033937044510c576c9715b
TurchynAnastasiia <nastya.melnychenko@gmail.com>
2022-05-12 15:51:34 +0300

    add test to error component

--- src/app/main/component/auth/components/error/error.component.spec.ts
+++ src/app/main/component/auth/components/error/error.component.spec.ts
@@ -12,48 +12,44 @@ describe('error component', () => {
   it('Shoud call ngOnChanges hook', () => {
     component.formElement.setErrors({ symbolInvalid: true });
 
-    // @ts-ignore
-    const spy = spyOn(component, 'getType');
+    const spy = spyOn(component as any, 'getType');
     component.ngOnChanges();
     expect(spy).toHaveBeenCalled();
   });
 
   it('Shoud return error type', () => {
     component.formElement.setErrors({ required: true });
-    // @ts-ignore
-    component.getType();
+    (component as any).getType();
 
     expect(component.errorMessage).toBe('user.auth.sign-in.password-is-required');
   });
 
   it('Error: get email message', () => {
-    // @ts-ignore
-    expect(component.getErrorMsg[`email`]()).toBe('user.auth.sign-in.email-is-required');
+    expect((component as any).getErrorMsg[`email`]()).toBe('user.auth.sign-in.email-is-required');
   });
 
   it('Error: get required message', () => {
-    // @ts-ignore
-    expect(component.getErrorMsg[`required`]()).toBe('user.auth.sign-in.password-is-required');
+    expect((component as any).getErrorMsg[`required`]()).toBe('user.auth.sign-in.password-is-required');
   });
 
   it('Error: get passwordMismatch message', () => {
-    // @ts-ignore
-    expect(component.getErrorMsg[`passwordMismatch`]()).toBe('user.auth.sign-up.password-match');
+    expect((component as any).getErrorMsg[`passwordMismatch`]()).toBe('user.auth.sign-up.password-match');
   });
 
   it('Error: get minlength message', () => {
-    // @ts-ignore
-    expect(component.getErrorMsg[`minlength`]()).toBe('user.auth.sign-in.password-must-be-at-least-8-characters-long');
+    expect((component as any).getErrorMsg[`minlength`]()).toBe('user.auth.sign-in.password-must-be-at-least-8-characters-long');
+  });
+
+  it('Error: get maxlength message', () => {
+    expect((component as any).getErrorMsg[`maxlength`]()).toBe('user.auth.sign-up.too-long-password');
   });
 
   it('Error: get value for password', () => {
     component.controlName = 'password';
-    // @ts-ignore
-    expect(component.getErrorMsg[`symbolInvalid`]()).toBe('user.auth.sign-up.password-symbols-error');
+    expect((component as any).getErrorMsg[`symbolInvalid`]()).toBe('user.auth.sign-up.password-symbols-error');
   });
 
   it('Error: get value for password', () => {
-    // @ts-ignore
-    expect(component.getErrorMsg[`symbolInvalid`]()).toBe('user.auth.sign-up.user-name-size');
+    expect((component as any).getErrorMsg[`symbolInvalid`]()).toBe('user.auth.sign-up.user-name-size');
   });
 });
