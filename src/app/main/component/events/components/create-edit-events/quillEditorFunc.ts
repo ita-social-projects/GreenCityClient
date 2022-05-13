4f8df2623fb8dad326e5cb32eafaae50cc0c288d
Mukola Kovalyshyn <m.kovalushun@gmail.com>
2022-05-13 15:43:25 +0300

    refactor

--- src/app/main/component/events/components/create-edit-events/quillEditorFunc.ts
+++ src/app/main/component/events/components/create-edit-events/quillEditorFunc.ts
@@ -6,23 +6,6 @@ export const checkImages = (editorContent: string) => {
   }
 };
 
-export const dataURLtoFile = (dataUrl) => {
-  const arr = dataUrl.split(',');
-  const mime = arr[0].match(/:(.*?);/)[1];
-  const filename = `image.${mime.split('/')[1]}`;
-  const baseStr = atob(arr[1]);
-  let n = baseStr.length;
-  const u8arr = new Uint8Array(n);
-
-  while (n--) {
-    u8arr[n] = baseStr.charCodeAt(n);
-  }
-
-  return new File([u8arr], filename, {
-    type: mime
-  });
-};
-
 export const quillConfig = {
   'emoji-shortname': true,
   'emoji-textarea': false,
