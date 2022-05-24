14b6f6546eb0703a59b535723e1e0afc87714f08
Oleksandr <voronko.oleksandr@gmail.com>
2022-05-24 22:16:04 +0300

    add function reduce for count certificates, change Interface IUserOrderInfo

--- src/app/ubs/ubs-user/ubs-user-order-details/ubs-user-order-details.component.ts
+++ src/app/ubs/ubs-user/ubs-user-order-details/ubs-user-order-details.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input, OnDestroy } from '@angular/core';
+import { Component, Input, OnDestroy, ValueSansProvider } from '@angular/core';
 import { LocalStorageService } from '@global-service/localstorage/local-storage.service';
+import { AnyFn } from '@ngrx/store/src/selector';
+import { AnySoaRecord } from 'dns';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { IUserOrderInfo, CheckPaymentStatus } from '../ubs-user-orders-list/models/UserOrder.interface';
@@ -14,6 +16,7 @@ export class UbsUserOrderDetailsComponent implements OnDestroy {
   order: IUserOrderInfo;
   public currentLanguage: string;
   private destroy$: Subject<boolean> = new Subject<boolean>();
+  public points: any;
 
   constructor(private localStorageService: LocalStorageService) {
     this.currentLanguage = this.localStorageService.getCurrentLanguage();
@@ -23,6 +26,10 @@ export class UbsUserOrderDetailsComponent implements OnDestroy {
   }
   isPaid(order: IUserOrderInfo): boolean {
     return order.paymentStatus === CheckPaymentStatus.PAID;
+  }
+
+  reduceCertificate(order: IUserOrderInfo): any {
+    return (this.points = order.certificate.reduce((acc, item) => acc + item.points, 0));
   }
 
   ngOnDestroy(): void {
