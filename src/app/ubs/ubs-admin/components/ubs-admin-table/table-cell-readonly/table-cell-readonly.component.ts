f613c2f04b485e412bab3aad1b4caa5d13708fed
Oleg Postolovskyi <newartmask@gmail.com>
2022-07-14 16:22:41 +0300

    Added localization to en

--- src/app/ubs/ubs-admin/components/ubs-admin-table/table-cell-readonly/table-cell-readonly.component.ts
+++ src/app/ubs/ubs-admin/components/ubs-admin-table/table-cell-readonly/table-cell-readonly.component.ts
@@ -1,14 +1,32 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
+import { IColumnBelonging } from '../../../models/ubs-admin.interface';
 
 @Component({
   selector: 'app-table-cell-readonly',
   templateUrl: './table-cell-readonly.component.html',
   styleUrls: ['./table-cell-readonly.component.scss']
 })
-export class TableCellReadonlyComponent {
-  @Input() title;
-  @Input() lang;
-  @Input() date;
+export class TableCellReadonlyComponent implements OnInit, OnChanges {
+  @Input() title: string | number | { ua: string; en: string } | null;
+  @Input() optional: IColumnBelonging[];
+  @Input() lang: string;
+  @Input() date: string;
+  @Input() key: string;
+  public dataObj: IColumnBelonging = null;
+  public data: string | number | { ua: string; en: string } | null;
+
+  ngOnInit(): void {
+    if (this.optional?.length) {
+      this.dataObj = this.optional.filter((item) => item.key === this.title)[0];
+    }
+  }
+
+  ngOnChanges(): void {
+    this.data =
+      this.key === 'bagsAmount' && this.lang === 'en'
+        ? (this.title as string).toLowerCase().replace(/л|шт/gi, (el) => (el === 'л' ? 'L' : 'p'))
+        : this.title;
+  }
 
   showTooltip(title: any, tooltip: any) {
     const maxWidthTitle = 112;
