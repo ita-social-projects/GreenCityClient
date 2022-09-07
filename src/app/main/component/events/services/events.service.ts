b82c3a3b9812aebba12c895b70c22301d23cff1c
Taras Slovak <slovak.taras@gmail.com>
2022-09-07 15:39:37 +0300

    fixed

--- src/app/main/component/events/services/events.service.ts
+++ src/app/main/component/events/services/events.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, OnDestroy } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable, ReplaySubject } from 'rxjs';
 import { environment } from '@environment/environment';
 
