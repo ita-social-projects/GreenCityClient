36c15821012f99a0358e1535865c1ccc9d59fcc0
Oleksii <zaytsev961st@gmail.com>
2022-09-16 02:32:53 +0300

    pagination->infinite scroll

--- src/app/ubs/ubs-user/services/user-orders.service.ts
+++ src/app/ubs/ubs-user/services/user-orders.service.ts
@@ -19,6 +19,20 @@ export class UserOrdersService {
         : 'DONE&status=CANCELED';
     return this.http.get<IUserOrdersInfo>(`${this.url}/user-orders?page=${page}&size=${itemsPerPage}&status=${ordersStatusesParams}`);
   }
+
+  getCurrentUserOrders(page: number, itemsPerPage: number): Observable<IUserOrdersInfo> {
+    const ordersStatusesParams =
+      'ADJUSTMENT&status=BROUGHT_IT_HIMSELF&status=FORMED&status=CONFIRMED&status=ON_THE_ROUTE&status=NOT_TAKEN_OUT';
+    const url = `${this.url}/user-orders?page=${page}&size=${itemsPerPage}&status=${ordersStatusesParams}`;
+    return this.http.get<IUserOrdersInfo>(url);
+  }
+
+  getClosedUserOrders(page: number, itemsPerPage: number): Observable<IUserOrdersInfo> {
+    const ordersStatusesParams = 'DONE&status=CANCELED';
+    const url = `${this.url}/user-orders?page=${page}&size=${itemsPerPage}&status=${ordersStatusesParams}`;
+    return this.http.get<IUserOrdersInfo>(url);
+  }
+
   public deleteOrder(orderId: number): Observable<object> {
     return this.http.delete<object>(`${this.url}/delete-order/${orderId}`);
   }
--- src/app/ubs/ubs-user/ubs-user-orders/ubs-user-orders.component.ts
+++ src/app/ubs/ubs-user/ubs-user-orders/ubs-user-orders.component.ts
@@ -1,11 +1,9 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { catchError, takeUntil } from 'rxjs/operators';
-import { Subject, throwError } from 'rxjs';
+import { Subject, forkJoin } from 'rxjs';
 import { MatSnackBarComponent } from '@global-errors/mat-snack-bar/mat-snack-bar.component';
 import { UserOrdersService } from '../services/user-orders.service';
 import { Router } from '@angular/router';
 import { BonusesService } from '../ubs-user-bonuses/services/bonuses.service';
-import { IBonus } from '../ubs-user-bonuses/models/IBonus.interface';
 import { IUserOrderInfo } from '../ubs-user-orders-list/models/UserOrder.interface';
 import { TranslateService } from '@ngx-translate/core';
 
@@ -16,18 +14,16 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class UbsUserOrdersComponent implements OnInit, OnDestroy {
   destroy: Subject<boolean> = new Subject<boolean>();
-  orders: IUserOrderInfo[];
-  currentOrders: IUserOrderInfo[];
-  orderHistory: IUserOrderInfo[];
+  currentOrders: IUserOrderInfo[] = [];
+  closedOrders: IUserOrderInfo[] = [];
   bonuses: number;
-  loadingOrders = false;
-  loadingBonuses = false;
-  page = 0;
-  numberOfCurrentOrders: number;
-  numberOfHistoryOrders: number;
-  currentOrdersOnPage = 10;
-  historyOrdersOnPage = 10;
-  isMoreThenOnePage: boolean;
+  loading = true;
+  currentOrdersLoadedPage = 1;
+  closedOrdersLoadedPage = 1;
+  ordersPerPage = 10;
+  totalCurrentOrdersPages: number;
+  totalClosedOrdersPages: number;
+  currentTabIdx = 0;
 
   constructor(
     private router: Router,
@@ -37,59 +33,72 @@ export class UbsUserOrdersComponent implements OnInit, OnDestroy {
     private translate: TranslateService
   ) {}
 
-  onPageChange(e) {
-    this.page = e;
-    const numberOfCurrenordersLeft = this.numberOfCurrentOrders - (e - 1) * 10;
-    const numberOfHistoryOrdersLeft = this.numberOfHistoryOrders - (e - 1) * 10;
-    this.currentOrdersOnPage = numberOfCurrenordersLeft < 10 ? this.currentOrdersOnPage : 10;
-    this.historyOrdersOnPage = numberOfHistoryOrdersLeft < 10 ? this.historyOrdersOnPage : 10;
-    this.getOrders(e - 1, this.currentOrdersOnPage, 'current');
-    this.getOrders(e - 1, this.historyOrdersOnPage, 'history');
+  onTabChange(newTabIdx) {
+    this.currentTabIdx = newTabIdx;
+  }
+
+  onScroll() {
+    if (this.currentTabIdx === 0) {
+      if (this.currentOrdersLoadedPage + 1 > this.totalCurrentOrdersPages) {
+        return;
+      }
+      this.currentOrdersLoadedPage += 1;
+      this.loadCurrentOrders(this.currentOrdersLoadedPage, this.ordersPerPage);
+      return;
+    }
+    if (this.closedOrdersLoadedPage + 1 > this.totalClosedOrdersPages) {
+      return;
+    }
+    this.closedOrdersLoadedPage += 1;
+    this.loadClosedOrders(this.closedOrdersLoadedPage, this.ordersPerPage);
+  }
+
+  loadCurrentOrders(page, ordersPerPage) {
+    this.userOrdersService.getCurrentUserOrders(page - 1, ordersPerPage).subscribe({
+      next: (data) => {
+        this.currentOrders = [...this.currentOrders, ...data.page];
+        this.totalCurrentOrdersPages = data.totalPages;
+      },
+      error: (err) => this.displayError(err)
+    });
+  }
+
+  loadClosedOrders(page, ordersPerPage) {
+    this.userOrdersService.getClosedUserOrders(page - 1, ordersPerPage).subscribe({
+      next: (data) => {
+        this.closedOrders = [...this.closedOrders, ...data.page];
+        this.totalClosedOrdersPages = data.totalPages;
+      },
+      error: (err) => this.displayError(err)
+    });
   }
 
   redirectToOrder() {
     this.router.navigate(['ubs', 'order']);
   }
 
-  public loading(): boolean {
-    return this.loadingOrders || this.loadingBonuses;
-  }
-
   ngOnInit() {
-    this.getOrders(0, 10, 'current');
-    this.getOrders(0, 10, 'history');
-    this.bonusesService.getUserBonuses().subscribe((responce: IBonus) => {
-      this.bonuses = responce.points;
-      this.loadingBonuses = true;
+    forkJoin([
+      this.userOrdersService.getCurrentUserOrders(0, this.ordersPerPage),
+      this.userOrdersService.getClosedUserOrders(0, this.ordersPerPage),
+      this.bonusesService.getUserBonuses()
+    ]).subscribe({
+      next: (results) => {
+        const [current, closed, bonuses] = results;
+        this.currentOrders = current.page ?? [];
+        this.closedOrders = closed.page ?? [];
+        this.bonuses = bonuses.points ?? 0;
+        this.totalCurrentOrdersPages = current.totalPages;
+        this.totalClosedOrdersPages = closed.totalPages;
+        this.loading = false;
+      },
+      error: (err) => this.displayError(err)
     });
   }
 
-  getOrders(pageNumber: number, ordersOnPage: number, table: string) {
-    this.userOrdersService
-      .getAllUserOrders(pageNumber, ordersOnPage, table)
-      .pipe(
-        takeUntil(this.destroy),
-        catchError((err) => {
-          const errorMessage = this.translate.instant('snack-bar.error.default');
-          this.snackBar.openSnackBar(errorMessage);
-          return throwError(err);
-        })
-      )
-      .subscribe((item) => {
-        if (pageNumber === 0) {
-          this.numberOfCurrentOrders = table === 'current' ? item.totalElements : this.numberOfCurrentOrders;
-          this.numberOfHistoryOrders = table === 'history' ? item.totalElements : this.numberOfHistoryOrders;
-        }
-        this.orders = item.page;
-        this.currentOrders = table === 'current' ? this.orders : this.currentOrders;
-        this.orderHistory = table === 'history' ? this.orders : this.orderHistory;
-
-        this.loadingOrders = !!this.currentOrders;
-
-        if (this.numberOfCurrentOrders && this.numberOfHistoryOrders) {
-          this.isMoreThenOnePage = this.numberOfCurrentOrders > 10 || this.numberOfHistoryOrders > 10;
-        }
-      });
+  displayError(error) {
+    const errorMessage = this.translate.instant('snack-bar.error.default');
+    this.snackBar.openSnackBar(errorMessage);
   }
 
   ngOnDestroy() {
