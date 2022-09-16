4e1065eea31f40374a8b0721a01fab5463fc9968
Oleksii <zaytsev961st@gmail.com>
2022-09-17 00:46:16 +0300

    tests

--- src/app/ubs/ubs-user/ubs-user-orders/ubs-user-orders.component.spec.ts
+++ src/app/ubs/ubs-user/ubs-user-orders/ubs-user-orders.component.spec.ts
@@ -1,20 +1,21 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
-import { LocalizedCurrencyPipe } from 'src/app/shared/localized-currency-pipe/localized-currency.pipe';
-import { RouterModule, Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
-import { UbsUserOrdersComponent } from './ubs-user-orders.component';
+import { LocalizedCurrencyPipe } from 'src/app/shared/localized-currency-pipe/localized-currency.pipe';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+import { UbsUserOrdersComponent } from './ubs-user-orders.component';
 import { TranslateModule } from '@ngx-translate/core';
 import { MatSnackBarComponent } from '@global-errors/mat-snack-bar/mat-snack-bar.component';
 
-import { Subject, of } from 'rxjs';
+import { of, throwError } from 'rxjs';
 
 import { UserOrdersService } from '../services/user-orders.service';
 import { BonusesService } from '../ubs-user-bonuses/services/bonuses.service';
 import { APP_BASE_HREF } from '@angular/common';
-import { NgxPaginationModule } from 'ngx-pagination';
+import { By } from '@angular/platform-browser';
 
 describe('UbsUserOrdersComponent', () => {
   let component: UbsUserOrdersComponent;
@@ -37,98 +38,77 @@ describe('UbsUserOrdersComponent', () => {
     paymentStatus: 'Paid'
   };
 
+  const fakeCurrentOrdersData = [fakeOrder1, fakeOrder2];
+  const fakeClosedOrdersData = [fakeOrder1];
+
   const MatSnackBarMock = jasmine.createSpyObj('MatSnackBarComponent', ['openSnackBar']);
 
-  const userOrderServiceMock = jasmine.createSpyObj('userOrderService', ['getAllUserOrders']);
+  const userOrderServiceMock = jasmine.createSpyObj('userOrderService', ['getCurrentUserOrders', 'getClosedUserOrders']);
 
-  userOrderServiceMock.getAllUserOrders.and.returnValue(of({ page: [fakeOrder1, fakeOrder2] }));
+  userOrderServiceMock.getCurrentUserOrders.and.returnValue(of({ page: fakeCurrentOrdersData }));
+  userOrderServiceMock.getClosedUserOrders.and.returnValue(of({ page: fakeClosedOrdersData }));
+
+  const userOrderServiceFailureMock = jasmine.createSpyObj('userOrderService', ['getCurrentUserOrders', 'getClosedUserOrders']);
+  userOrderServiceFailureMock.getCurrentUserOrders.and.returnValue(of({ page: fakeCurrentOrdersData }));
+  userOrderServiceFailureMock.getClosedUserOrders.and.returnValue(throwError('error!'));
+
+  const userOrderServiceNoOrdersMock = jasmine.createSpyObj('userOrderService', ['getCurrentUserOrders', 'getClosedUserOrders']);
+  userOrderServiceNoOrdersMock.getCurrentUserOrders.and.returnValue(of({ page: [] }));
+  userOrderServiceNoOrdersMock.getClosedUserOrders.and.returnValue(of({ page: [] }));
 
   const bonusesServiceMock = jasmine.createSpyObj('bonusesService', ['getUserBonuses']);
 
-  bonusesServiceMock.getUserBonuses.and.returnValue(
-    of({
-      points: 5
-    })
-  );
+  bonusesServiceMock.getUserBonuses.and.returnValue(of({ points: 5 }));
+
+  const routerSpy = { navigate: jasmine.createSpy('navigate') };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [UbsUserOrdersComponent, LocalizedCurrencyPipe],
-      imports: [TranslateModule.forRoot(), NgxPaginationModule, HttpClientTestingModule, RouterModule.forRoot([])],
+      imports: [TranslateModule.forRoot(), HttpClientTestingModule, InfiniteScrollModule, RouterModule.forRoot([])],
       providers: [
+        { provide: Router, useValue: routerSpy },
         { provide: MatSnackBarComponent, useValue: MatSnackBarMock },
         { provide: UserOrdersService, useValue: userOrderServiceMock },
         { provide: BonusesService, useValue: bonusesServiceMock },
         { provide: APP_BASE_HREF, useValue: '/' }
       ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA]
-    }).compileComponents();
+    });
   }));
 
-  beforeEach(() => {
+  const buildComponent = () => {
+    TestBed.compileComponents();
     fixture = TestBed.createComponent(UbsUserOrdersComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+  };
 
   it('should create', () => {
+    buildComponent();
     expect(component).toBeDefined();
   });
 
-  it('router should been called', () => {
-    spyOn((component as any).router, 'navigate');
-    component.redirectToOrder();
-    expect((component as any).router.navigate).toHaveBeenCalledWith(['ubs', 'order']);
+  it('click on new order button should navigate user to /ubs/order', () => {
+    buildComponent();
+    const newOrderButton = fixture.debugElement.query(By.css('.btn_new_order')).nativeElement;
+    newOrderButton.click();
+    expect(routerSpy.navigate).toHaveBeenCalledWith(['ubs', 'order']);
   });
 
-  it('loading should return true', () => {
-    component.loadingOrders = true;
-    component.loadingBonuses = true;
-    const isLoadingTrue = component.loading();
-    expect(isLoadingTrue).toBe(true);
-  });
-
-  it('loading should return false', () => {
-    component.loadingOrders = false;
-    component.loadingBonuses = false;
-    const isLoadingTrue = component.loading();
-    expect(isLoadingTrue).toBeFalsy();
-  });
-
-  it('should call getAllUserOrders and getUserBonuses ', () => {
-    component.loadingBonuses = false;
+  it('should display a message if there are no orders', () => {
+    TestBed.overrideProvider(UserOrdersService, { useValue: userOrderServiceNoOrdersMock });
+    buildComponent();
     component.ngOnInit();
-    expect((component as any).userOrdersService.getAllUserOrders).toHaveBeenCalled();
-    expect((component as any).bonusesService.getUserBonuses).toHaveBeenCalled();
-    expect(component.loadingBonuses).toBe(true);
+    fixture.detectChanges();
+    const noOrdersElement = fixture.debugElement.query(By.css('.if_empty')).nativeElement;
+    expect(noOrdersElement.textContent).toContain('user-orders.no-orders');
   });
 
-  it('getOrders should call getAllUserOrders ', () => {
-    component.getOrders(0, 10, 'current');
-    expect((component as any).userOrdersService.getAllUserOrders).toHaveBeenCalledWith(0, 10, 'current');
-  });
-
-  it('getOrders should call getAllUserOrders ', () => {
-    component.getOrders(0, 10, 'history');
-    expect((component as any).userOrdersService.getAllUserOrders).toHaveBeenCalledWith(0, 10, 'history');
-  });
-
-  it('onPageChange should call getCurrentOrders ', () => {
-    spyOn(component as any, 'getOrders');
-    component.numberOfCurrentOrders = 10;
-    component.numberOfHistoryOrders = 10;
-    component.onPageChange(1);
-
-    expect((component as any).getOrders).toHaveBeenCalledWith(0, 10, 'current');
-    expect((component as any).getOrders).toHaveBeenCalledWith(0, 10, 'history');
-  });
-
-  it('destroy Subject should be closed after ngOnDestroy()', () => {
-    component.destroy = new Subject<boolean>();
-    spyOn(component.destroy, 'unsubscribe');
-    component.ngOnDestroy();
-    fixture.componentInstance.ngOnDestroy();
-
-    expect(component.destroy.unsubscribe).toHaveBeenCalledTimes(2);
+  it('should open a snackbar with error message if error occurs when loading data', () => {
+    TestBed.overrideProvider(UserOrdersService, { useValue: userOrderServiceFailureMock });
+    buildComponent();
+    component.ngOnInit();
+    expect((component as any).snackBar.openSnackBar).toHaveBeenCalledWith('snack-bar.error.default');
   });
 });
