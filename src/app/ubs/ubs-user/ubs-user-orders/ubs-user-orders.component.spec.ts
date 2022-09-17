c03b3e109584f6794f67c0aebdfdd1955f1d8221
Oleksii <zaytsev961st@gmail.com>
2022-09-18 01:04:24 +0300

    test for loading on scroll

--- src/app/ubs/ubs-user/ubs-user-orders/ubs-user-orders.component.spec.ts
+++ src/app/ubs/ubs-user/ubs-user-orders/ubs-user-orders.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
@@ -6,7 +7,7 @@ import { Router, RouterModule } from '@angular/router';
 
 import { LocalizedCurrencyPipe } from 'src/app/shared/localized-currency-pipe/localized-currency.pipe';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+import { InfiniteScrollDirective, InfiniteScrollModule } from 'ngx-infinite-scroll';
 import { UbsUserOrdersComponent } from './ubs-user-orders.component';
 import { TranslateModule } from '@ngx-translate/core';
 import { MatSnackBarComponent } from '@global-errors/mat-snack-bar/mat-snack-bar.component';
@@ -40,6 +41,7 @@ describe('UbsUserOrdersComponent', () => {
   };
 
   const fakeCurrentOrdersData = new Array(10).fill(fakeOrder1);
+  const fakeCurrentOrdersDataPage2 = new Array(5).fill(fakeOrder2);
   const fakeClosedOrdersData = [fakeOrder2];
 
   const RouterMock = jasmine.createSpyObj('Router', ['navigate']);
@@ -47,7 +49,7 @@ describe('UbsUserOrdersComponent', () => {
   const MatSnackBarMock = jasmine.createSpyObj('MatSnackBarComponent', ['openSnackBar']);
 
   const userOrderServiceMock = {
-    getCurrentUserOrders: () => of({ page: fakeCurrentOrdersData }),
+    getCurrentUserOrders: (page) => of({ page: page === 0 ? fakeCurrentOrdersData : fakeCurrentOrdersDataPage2 }),
     getClosedUserOrders: () => of({ page: fakeClosedOrdersData })
   };
 
@@ -67,7 +69,7 @@ describe('UbsUserOrdersComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [UbsUserOrdersComponent, LocalizedCurrencyPipe],
+      declarations: [UbsUserOrdersComponent, LocalizedCurrencyPipe, InfiniteScrollDirective],
       imports: [TranslateModule.forRoot(), HttpClientTestingModule, InfiniteScrollModule, RouterModule.forRoot([])],
       providers: [
         { provide: Router, useValue: RouterMock },
@@ -80,45 +82,56 @@ describe('UbsUserOrdersComponent', () => {
     });
   }));
 
-  const buildComponent = () => {
-    TestBed.compileComponents();
+  const buildComponent = async () => {
+    await TestBed.compileComponents();
     fixture = TestBed.createComponent(UbsUserOrdersComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   };
 
-  it('should create', () => {
-    buildComponent();
+  it('should create', async () => {
+    await buildComponent();
     expect(component).toBeDefined();
   });
 
-  it('should navigate user to /ubs/order when clicking on new order button ', () => {
-    buildComponent();
+  it('should navigate user to /ubs/order when clicking on new order button ', async () => {
+    await buildComponent();
     const newOrderButton = fixture.debugElement.query(By.css('.btn_new_order')).nativeElement;
     newOrderButton.click();
     expect(RouterMock.navigate).toHaveBeenCalledWith(['ubs', 'order']);
   });
 
-  it('should render ubs-user-orders-list component with correct inputs if there are current orders on init', () => {
-    buildComponent();
+  it('should render ubs-user-orders-list component with correct inputs if there are current orders on init', async () => {
+    await buildComponent();
     component.ngOnInit();
     const list = fixture.debugElement.query(By.css('app-ubs-user-orders-list'));
     expect(list).toBeTruthy();
-    expect(JSON.stringify(list.properties.orders)).toBe(JSON.stringify(fakeCurrentOrdersData));
+    expect(list.properties.orders).toEqual(fakeCurrentOrdersData);
   });
 
-  it('should display a message if there are no orders', () => {
+  it('should render list with more orders on scroll', async () => {
+    await buildComponent();
+    component.ngOnInit();
+    const container = fixture.debugElement.query(By.directive(InfiniteScrollDirective));
+    container.triggerEventHandler('scrolled', null);
+    fixture.detectChanges();
+    const list = fixture.debugElement.query(By.css('app-ubs-user-orders-list'));
+    expect(list).toBeTruthy();
+    expect(list.properties.orders).toEqual([...fakeCurrentOrdersData, ...fakeCurrentOrdersDataPage2]);
+  });
+
+  it('should display a message if there are no orders', async () => {
     TestBed.overrideProvider(UserOrdersService, { useValue: userOrderServiceNoOrdersMock });
-    buildComponent();
+    await buildComponent();
     component.ngOnInit();
     fixture.detectChanges();
     const noOrdersElement = fixture.debugElement.query(By.css('.if_empty')).nativeElement;
     expect(noOrdersElement.textContent).toContain('user-orders.no-orders');
   });
 
-  it('should open a snackbar with error message if error occurs when loading data', () => {
+  it('should open a snackbar with error message if error occurs when loading data', async () => {
     TestBed.overrideProvider(UserOrdersService, { useValue: userOrderServiceFailureMock });
-    buildComponent();
+    await buildComponent();
     component.ngOnInit();
     expect(MatSnackBarMock.openSnackBar).toHaveBeenCalledWith('snack-bar.error.default');
   });
