494aec9b4bbd08c59c15d975b0746279730b20cb
KhristinaVasko <tinavasko0904@gmail.com>
2022-09-19 15:36:25 +0300

    test fix

--- src/app/main/pipe/first-string-word/first-string-word.pipe.spec.ts
+++ src/app/main/pipe/first-string-word/first-string-word.pipe.spec.ts
@@ -1,13 +1,17 @@
 import { FirstStringWordPipe } from './first-string-word.pipe';
 
 describe('FirstStringWordPipe', () => {
-  const pipe = new FirstStringWordPipe();
+  let pipe: FirstStringWordPipe;
+
+  beforeEach(() => {
+    pipe = new FirstStringWordPipe();
+  });
 
   it('create an instance', () => {
     expect(pipe).toBeTruthy();
   });
 
-  it('should display only first name in user name', () => {
+  it('transforms "Firstname Lastname" to "Firstname"', () => {
     const name = 'Firstname Lastname';
     const result = pipe.transform(name);
     expect(result).toBe('Firstname');
