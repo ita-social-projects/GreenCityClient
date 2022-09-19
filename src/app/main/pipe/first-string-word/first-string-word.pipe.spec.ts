abb19bc9c43de4b0d4c0cc10067e4872f329e10b
KhristinaVasko <tinavasko0904@gmail.com>
2022-09-19 13:08:16 +0300

    tests

--- src/app/main/pipe/first-string-word/first-string-word.pipe.spec.ts
+++ src/app/main/pipe/first-string-word/first-string-word.pipe.spec.ts
@@ -1,8 +1,13 @@
 import { FirstStringWordPipe } from './first-string-word.pipe';
 
 describe('FirstStringWordPipe', () => {
+  const pipe = new FirstStringWordPipe();
+
   it('create an instance', () => {
-    const pipe = new FirstStringWordPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('transforms "Firstname Lastname" to "Firstname"', () => {
+    expect(pipe.transform('Firstname Lastname')).toBe('Firstname');
   });
 });
