0e65223eeb6682be4abca82f819102f2fad89fe9
Taras <sokha2411@gmail.com>
2022-09-26 17:15:38 +0300

    Another part of tests is added

--- src/app/ubs/ubs-admin/components/ubs-admin-tariffs/ubs-admin-tariffs-pricing-page/ubs-admin-tariffs-add-tariff-service-pop-up/ubs-admin-tariffs-add-tariff-service-pop-up.component.spec.ts
+++ src/app/ubs/ubs-admin/components/ubs-admin-tariffs/ubs-admin-tariffs-pricing-page/ubs-admin-tariffs-add-tariff-service-pop-up/ubs-admin-tariffs-add-tariff-service-pop-up.component.spec.ts
@@ -6,6 +6,9 @@ import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { UbsAdminTariffsAddTariffServicePopUpComponent } from './ubs-admin-tariffs-add-tariff-service-pop-up.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ModalTextComponent } from '../../../shared/components/modal-text/modal-text.component';
+import { of } from 'rxjs';
+import { ServerTranslatePipe } from 'src/app/shared/translate-pipe/translate-pipe.pipe';
 
 describe('UbsAdminTariffsAddTariffServicePopupComponent', () => {
   let component: UbsAdminTariffsAddTariffServicePopUpComponent;
@@ -13,7 +16,7 @@ describe('UbsAdminTariffsAddTariffServicePopupComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [UbsAdminTariffsAddTariffServicePopUpComponent],
+      declarations: [UbsAdminTariffsAddTariffServicePopUpComponent, ServerTranslatePipe, ModalTextComponent],
       imports: [TranslateModule.forRoot(), HttpClientTestingModule, MatDialogModule, ReactiveFormsModule, BrowserAnimationsModule],
       providers: [{ provide: MAT_DIALOG_DATA, useValue: {} }, FormBuilder, { provide: MatDialogRef, useValue: {} }],
       schemas: [CUSTOM_ELEMENTS_SCHEMA]
@@ -31,16 +34,19 @@ describe('UbsAdminTariffsAddTariffServicePopupComponent', () => {
   });
 
   const matDialogMock = jasmine.createSpyObj('matDialog', ['open']);
+  // const fakeMatDialogRef = jasmine.createSpyObj(['close', 'afterClosed']);
+  // fakeMatDialogRef.afterClosed.and.returnValue(of(true));
+
   it('Check whether method onCancel called with proper args', () => {
-    // component.onCancel();
-    // expect(matDialogMock.open).toHaveBeenCalledWith(ModalTextComponent, {
-    //   hasBackdrop: true,
-    //   panelClass: 'address-matDialog-styles-w-100',
-    //   data: {
-    //     name: 'cancel',
-    //     text: 'modal-text.cancel-message',
-    //     action: 'modal-text.yes'
-    //   }
-    // });
+    /*component.onCancel();
+    expect(matDialogMock.open).toHaveBeenCalledWith(ModalTextComponent, {
+      hasBackdrop: true,
+      panelClass: 'address-matDialog-styles-w-100',
+      data: {
+        name: 'cancel',
+        text: 'modal-text.cancel-message',
+        action: 'modal-text.yes'
+      }
+    });*/
   });
 });
