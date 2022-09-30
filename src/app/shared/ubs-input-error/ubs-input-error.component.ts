cd466cddcc993639af13a61bbee4c03b718df0b8
Мар'яна <mariana@MacBook-Air-Marana.local>
2022-09-30 10:20:40 +0300

    Fixed the message about wrong maxLength of house number/ corp number/ entrance number/ comment to the  address

--- src/app/shared/ubs-input-error/ubs-input-error.component.ts
+++ src/app/shared/ubs-input-error/ubs-input-error.component.ts
@@ -25,6 +25,9 @@ export class UBSInputErrorComponent implements OnChanges {
     email: 'input-error.email-wrong',
     minlength: 'input-error.minlength-short',
     maxlength: 'input-error.max-length',
+    maxlengthEntrance: 'input-error.max-length-entrance',
+    maxlengthHouse: 'input-error.max-length-house',
+    maxlengthComment: 'input-error.max-length-comment',
     pattern: 'input-error.pattern',
     required: 'input-error.required',
     wrongNumber: 'input-error.number-wrong',
@@ -38,6 +41,18 @@ export class UBSInputErrorComponent implements OnChanges {
   private getType() {
     for (const error in this.validationErrors) {
       if (this.formElement.errors[error]) {
+        console.log(this.formElement.errors.maxlength.requiredLength);
+        if (this.formElement.errors.maxlength.requiredLength === 2) {
+          this.errorMessage = this.validationErrors.maxlengthEntrance;
+          break;
+        } else if (this.formElement.errors.maxlength.requiredLength === 8) {
+          this.errorMessage = this.validationErrors.maxlengthHouse;
+          break;
+        } else if (this.formElement.errors.maxlength.requiredLength === 255) {
+          this.errorMessage = this.validationErrors.maxlengthComment;
+          break;
+        }
+
         this.errorMessage = this.validationErrors[error];
         break;
       }
