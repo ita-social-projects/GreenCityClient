873ac6673d447d62c26760716eb89c0dc7264e58
vitaliitrehubov <tregubovvakharkov@gmail.com>
2022-10-03 11:06:24 +0300

    minor fix

--- src/app/shared/translate-date-pipe/translate-date.pipe.ts
+++ src/app/shared/translate-date-pipe/translate-date.pipe.ts
@@ -10,10 +10,15 @@ export class TranslateDatePipe implements PipeTransform {
   constructor(private languageService: LanguageService) {}
 
   transform(date: string): string {
-    const [day, month, year] = date?.split(' ');
+    if (!date) {
+      return '';
+    }
+
+    const [day, month, year] = date.split(' ');
     const monthIndex = Number(month) - 1;
     let localizedMonth = this.languageService.getLocalizedMonth(monthIndex);
     localizedMonth = localizedMonth[0].toUpperCase() + localizedMonth.slice(1);
+
     return `${day} ${localizedMonth} ${year}`;
   }
 }
