61655d21709463b0859b1237fbdd4f64d34d5e7b
vitaliitrehubov <tregubovvakharkov@gmail.com>
2022-10-03 21:00:30 +0300

    add tests

--- src/app/ubs/ubs-admin/components/ubs-admin-several-orders-pop-up/ubs-admin-several-orders-pop-up.component.spec.ts
+++ src/app/ubs/ubs-admin/components/ubs-admin-several-orders-pop-up/ubs-admin-several-orders-pop-up.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { TranslateModule } from '@ngx-translate/core';
 import { FormBuilder } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
@@ -29,13 +29,28 @@ describe('UbsAdminSeveralOrdersPopUpComponent', () => {
     }
   };
 
-  const orderInfo = {
+  const orderInfoEmpty = {
     exportDetailsDto: {
       timeDeliveryFrom: null,
       timeDeliveryTo: null,
       dateExport: null,
       receivingStationId: null,
       allReceivingStations: []
+    },
+    employeePositionDtoRequest: {
+      currentPositionEmployees: null,
+      allPositionsEmployees: null,
+      orderId: 0
+    }
+  };
+
+  const orderInfoFilledIn = {
+    exportDetailsDto: {
+      timeDeliveryFrom: '2022:02:10T10:00',
+      timeDeliveryTo: '2022:02:10T20:00',
+      dateExport: '22.12.2022',
+      receivingStationId: 0,
+      allReceivingStations: [{ id: 0, name: 'Default' }]
     },
     employeePositionDtoRequest: {
       currentPositionEmployees: null,
@@ -63,14 +78,24 @@ describe('UbsAdminSeveralOrdersPopUpComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('initForm()', () => {
+  it('initForm with null values if no info provided', () => {
     component.initForm(emptyFormValue);
-    expect(component.ordersForm.value).toEqual(emptyFormValue);
+    const { exportDetailsDto, responsiblePersonsForm } = emptyFormValue;
+    const { exportDetailsDto: formExportDetails, responsiblePersonsForm: formResponsiblePersons } = component.ordersForm.value;
+
+    expect(formExportDetails.receivingStationId).toEqual(exportDetailsDto.receivingStationId);
+    expect(formExportDetails.dateExport).toEqual(exportDetailsDto.dateExport);
+    expect(formExportDetails.timeDeliveryFrom).toEqual(exportDetailsDto.timeDeliveryFrom);
+    expect(formExportDetails.timeDeliveryTo).toEqual(exportDetailsDto.timeDeliveryTo);
+    expect(formResponsiblePersons.responsibleCaller).toEqual(responsiblePersonsForm.responsibleCaller);
+    expect(formResponsiblePersons.responsibleLogicMan).toEqual(responsiblePersonsForm.responsibleLogicMan);
+    expect(formResponsiblePersons.responsibleNavigator).toEqual(responsiblePersonsForm.responsibleNavigator);
+    expect(formResponsiblePersons.responsibleDriver).toEqual(responsiblePersonsForm.responsibleDriver);
   });
 
   it('getInitialFormValues()', () => {
     spyOn(component, 'getInitialFormValues').and.callFake(() => emptyFormValue);
-    expect(component.getInitialFormValues(orderInfo)).toEqual(emptyFormValue);
+    expect(component.getInitialFormValues(orderInfoEmpty)).toEqual(emptyFormValue);
   });
 
   it('getEmployeeById()', () => {
@@ -82,15 +107,37 @@ describe('UbsAdminSeveralOrdersPopUpComponent', () => {
     expect(component.ordersForm.value).toEqual(emptyFormValue);
   });
 
-  it('ordersService', () => {
-    orderServiceMock.getOrderInfo.and.returnValue(of(orderInfo));
+  it('ordersService with empty order info', () => {
+    orderServiceMock.getOrderInfo.and.returnValue(of(orderInfoEmpty));
     component.ngOnInit();
     expect(orderServiceMock.getOrderInfo).toHaveBeenCalled();
   });
+
+  it('should fill in ordersForm with correct values', fakeAsync(() => {
+    orderServiceMock.getOrderInfo.and.returnValue(of(orderInfoFilledIn));
+    component.ngOnInit();
+
+    const { exportDetailsDto: formExportDetails } = component.ordersForm.value;
+    const { exportDetailsDto } = orderInfoFilledIn;
+
+    const timeDeliveryFrom = exportDetailsDto.timeDeliveryFrom.split('T')[1];
+    const timeDeliveryTo = exportDetailsDto.timeDeliveryTo.split('T')[1];
+    const receivingStationId = exportDetailsDto.allReceivingStations[exportDetailsDto.receivingStationId]?.name;
+
+    expect(formExportDetails.dateExport).toEqual(orderInfoFilledIn.exportDetailsDto.dateExport);
+    expect(formExportDetails.timeDeliveryFrom).toEqual(timeDeliveryFrom);
+    expect(formExportDetails.timeDeliveryTo).toEqual(timeDeliveryTo);
+    expect(formExportDetails.receivingStationId).toEqual(receivingStationId);
+  }));
 
   it('loadOrderInfo()', () => {
     const spy = spyOn(component, 'loadOrderInfo');
     component.ngOnInit();
     expect(spy).toHaveBeenCalledTimes(1);
   });
+
+  it('should set isLoading to false after loading order info', fakeAsync(() => {
+    component.ngOnInit();
+    expect(component.isLoading).toEqual(false);
+  }));
 });
