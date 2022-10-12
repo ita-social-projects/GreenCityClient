e9f1836178fa6a694378f9efe8715f6858efd27e
katia9261 <101433204+katia9261@users.noreply.github.com>
2022-10-12 17:06:22 +0300

    Revert "#4587: Fix comments of @vitaliitrehubov"
    This reverts commit a7eff1eafed686de2ff68df029b2089b2fcd2736.

--- src/app/main/component/events/components/create-edit-events/create-edit-events.component.ts
+++ src/app/main/component/events/components/create-edit-events/create-edit-events.component.ts
@@ -43,8 +43,8 @@ export class CreateEditEventsComponent implements OnInit, OnDestroy {
   public isTagValid: boolean;
   public isAddressFill = true;
   public eventFormGroup: FormGroup;
-  public isImageSizeError: boolean;
-  public isImageTypeError = false;
+  isImageSizeError: boolean;
+  isImageTypeError = false;
 
   private imgArray: Array<File> = [];
   private pipe = new DatePipe('en-US');
@@ -131,8 +131,8 @@ export class CreateEditEventsComponent implements OnInit, OnDestroy {
   }
 
   public getImageTosend(imageArr: Array<File>): void {
-    this.checkFileExtension(imageArr[-1]);
     this.imgArray = [...imageArr];
+    this.checkFileExtension(imageArr);
   }
 
   public getImagesToDelete(imagesSrc: Array<string>): void {
@@ -254,7 +254,6 @@ export class CreateEditEventsComponent implements OnInit, OnDestroy {
 
   private checkFileExtension(file: any): void {
     this.isImageSizeError = file.size >= 10000000;
-
     this.isImageTypeError = !(file.type === 'image/jpeg' || file.type === 'image/png');
   }
 
