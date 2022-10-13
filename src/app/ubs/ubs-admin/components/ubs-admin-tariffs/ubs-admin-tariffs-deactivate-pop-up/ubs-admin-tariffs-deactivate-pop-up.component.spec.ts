c55160c6992fe946b9132cf320d21a12f87b8162
KhristinaVasko <tinavasko0904@gmail.com>
2022-10-14 02:02:39 +0300

    Deactivate card add Select region input + tests

--- src/app/ubs/ubs-admin/components/ubs-admin-tariffs/ubs-admin-tariffs-deactivate-pop-up/ubs-admin-tariffs-deactivate-pop-up.component.spec.ts
+++ src/app/ubs/ubs-admin/components/ubs-admin-tariffs/ubs-admin-tariffs-deactivate-pop-up/ubs-admin-tariffs-deactivate-pop-up.component.spec.ts
@@ -1,18 +1,61 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { ReactiveFormsModule } from '@angular/forms';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { MatDialog, MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { TranslateModule } from '@ngx-translate/core';
+import { of, Subject } from 'rxjs';
 
 import { UbsAdminTariffsDeactivatePopUpComponent } from './ubs-admin-tariffs-deactivate-pop-up.component';
+import { LocalStorageService } from '@global-service/localstorage/local-storage.service';
+import { TariffsService } from '../../../services/tariffs.service';
+import { Store } from '@ngrx/store';
 
 describe('UbsAdminTariffsDeactivatePopUpComponent', () => {
   let component: UbsAdminTariffsDeactivatePopUpComponent;
   let fixture: ComponentFixture<UbsAdminTariffsDeactivatePopUpComponent>;
 
+  const stationItem = {
+    name: 'Фейк',
+    id: 0
+  };
+  const fakeStation = {
+    id: 1,
+    name: 'fake'
+  };
+  const eventMockStation = {
+    option: {
+      value: 'fake'
+    }
+  };
+
+  const matDialogMock = jasmine.createSpyObj('matDialog', ['open']);
+  matDialogMock.open.and.returnValue({ afterClosed: () => of(true) });
+  const fakeMatDialogRef = jasmine.createSpyObj(['close', 'afterClosed']);
+  fakeMatDialogRef.afterClosed.and.returnValue(of(true));
+
+  const tariffsServiceMock = jasmine.createSpyObj('tariffsServiceMock', ['getAllStations']);
+  tariffsServiceMock.getAllStations.and.returnValue(of([fakeStation]));
+  const localStorageServiceStub = () => ({
+    firstNameBehaviourSubject: { pipe: () => of('fakeName') }
+  });
+
+  const storeMock = jasmine.createSpyObj('store', ['select', 'dispatch']);
+  storeMock.select.and.returnValue(of());
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [UbsAdminTariffsDeactivatePopUpComponent],
-      imports: [MatDialogModule, TranslateModule.forRoot(), ReactiveFormsModule]
+      imports: [MatDialogModule, TranslateModule.forRoot(), ReactiveFormsModule],
+      providers: [
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+        { provide: MatDialog, useValue: matDialogMock },
+        { provide: MatDialogRef, useValue: fakeMatDialogRef },
+        { provide: LocalStorageService, useFactory: localStorageServiceStub },
+        { provide: TariffsService, useValue: tariffsServiceMock },
+        { provide: Store, useValue: storeMock },
+        FormBuilder
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     }).compileComponents();
   }));
 
@@ -24,5 +67,82 @@ describe('UbsAdminTariffsDeactivatePopUpComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call methods in OnInit', () => {
+    const spy1 = spyOn(component, 'getReceivingStation');
+    const spy2 = spyOn(component, 'setStationPlaceholder');
+    component.ngOnInit();
+    expect(spy1).toHaveBeenCalled();
+    expect(spy2).toHaveBeenCalled();
+  });
+
+  it('should get all stations', () => {
+    component.getReceivingStation();
+    expect(component.stations).toEqual([fakeStation]);
+  });
+
+  it('should add new selected station if it does not exist in list', () => {
+    component.selectedStation = [{ name: 'station', id: 0 }];
+    component.onSelectStation(eventMockStation as any);
+    expect(component.selectedStation).toEqual([
+      { name: 'station', id: 0 },
+      { name: 'fake', id: 1 }
+    ]);
+  });
+
+  it('should remove selected station if it exists in list', () => {
+    component.selectedStation = [
+      { name: 'station', id: 0 },
+      { name: 'fake', id: 1 }
+    ];
+    component.onSelectStation(eventMockStation as any);
+    expect(component.selectedStation).toEqual([{ name: 'station', id: 0 }]);
+  });
+
+  it('should empty station value onSelectStation method', () => {
+    component.onSelectStation(eventMockStation as any);
+    expect(component.station.value).toEqual('');
+    expect(component.blurOnOption).toEqual(false);
+  });
+
+  it('should set station placeholder', () => {
+    component.selectedStation = [stationItem];
+    component.setStationPlaceholder();
+    expect(component.stationPlaceholder).toEqual('1 вибрано');
+  });
+
+  it('should set station placeholder', () => {
+    component.selectedStation = [];
+    component.setStationPlaceholder();
+    expect(component.stationPlaceholder).toEqual('ubs-tariffs.placeholder-choose-station');
+  });
+
+  it('should  change blurOnOption', () => {
+    const eventMock = {
+      relatedTarget: {
+        localName: 'mat-option'
+      }
+    };
+    component.onBlur(eventMock);
+    expect(component.blurOnOption).toEqual(true);
+  });
+
+  it('should not change blurOnOption', () => {
+    const eventMock = {
+      relatedTarget: {
+        localName: ''
+      }
+    };
+    component.onBlur(eventMock);
+    expect(component.blurOnOption).toEqual(false);
+  });
+
+  it('destroy Subject should be closed after ngOnDestroy()', () => {
+    const unsubscribe = 'unsubscribe';
+    component[unsubscribe] = new Subject<boolean>();
+    spyOn(component[unsubscribe], 'complete');
+    component.ngOnDestroy();
+    expect(component[unsubscribe].complete).toHaveBeenCalledTimes(1);
   });
 });
--- src/app/ubs/ubs-admin/components/ubs-admin-tariffs/ubs-admin-tariffs-deactivate-pop-up/ubs-admin-tariffs-deactivate-pop-up.component.ts
+++ src/app/ubs/ubs-admin/components/ubs-admin-tariffs/ubs-admin-tariffs-deactivate-pop-up/ubs-admin-tariffs-deactivate-pop-up.component.ts
@@ -1,13 +1,155 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-
+import { TariffsService } from '../../../services/tariffs.service';
+import { FormBuilder, ValidatorFn, Validators } from '@angular/forms';
+import { LocalStorageService } from '@global-service/localstorage/local-storage.service';
+import { Subject } from 'rxjs';
+import { map, startWith, takeUntil } from 'rxjs/operators';
+import { MatAutocompleteTrigger } from '@angular/material/autocomplete';
+import { DatePipe } from '@angular/common';
 @Component({
   selector: 'app-ubs-admin-tariffs-deactivate-pop-up',
   templateUrl: './ubs-admin-tariffs-deactivate-pop-up.component.html',
   styleUrls: ['./ubs-admin-tariffs-deactivate-pop-up.component.scss']
 })
-export class UbsAdminTariffsDeactivatePopUpComponent implements OnInit {
-  constructor(private translate: TranslateService) {}
 
-  ngOnInit(): void {}
+//@ts-ignore
+export class UbsAdminTariffsDeactivatePopUpComponent implements OnInit {
+  DeactivateCardForm = this.fb.group({
+    courier: ['', Validators.required],
+    station: ['', Validators.required],
+    region: ['', Validators.required],
+    city: [{ value: '', disabled: true }, [Validators.maxLength(40), Validators.required]]
+  });
+
+  public name: string;
+  public datePipe = new DatePipe('ua');
+  public newDate = this.datePipe.transform(new Date(), 'MMM dd, yyyy');
+
+  public selectedStation = [];
+  public blurOnOption = false;
+  public stationPlaceholder: string;
+  public stations;
+  public filteredStations;
+  unsubscribe: Subject<any> = new Subject();
+
+  public icons = {
+    arrowDown: '././assets/img/ubs-tariff/arrow-down.svg',
+    cross: '././assets/img/ubs/cross.svg'
+  };
+
+  get station() {
+    return this.DeactivateCardForm.get('station');
+  }
+  get courier() {
+    return this.DeactivateCardForm.get('courier');
+  }
+  get region() {
+    return this.DeactivateCardForm.get('region');
+  }
+  get city() {
+    return this.DeactivateCardForm.get('city');
+  }
+
+  constructor(
+    private fb: FormBuilder,
+    private translate: TranslateService,
+    private tariffsService: TariffsService,
+    private localeStorageService: LocalStorageService
+  ) {}
+
+  ngOnInit(): void {
+    this.localeStorageService.firstNameBehaviourSubject.pipe(takeUntil(this.unsubscribe)).subscribe((firstName) => {
+      this.name = firstName;
+    });
+    this.setStationPlaceholder();
+    this.getReceivingStation();
+  }
+
+  public onBlur(event): void {
+    if (event.relatedTarget.localName === 'mat-option') {
+      this.blurOnOption = true;
+    }
+  }
+
+  public checkStation(item): boolean {
+    return this.selectedStation.map((it) => it.name).includes(item);
+  }
+
+  openAuto(event: Event, trigger: MatAutocompleteTrigger, flag: boolean): void {
+    if (!flag) {
+      event.stopPropagation();
+      trigger.openPanel();
+    }
+  }
+
+  public setStationPlaceholder(): void {
+    if (this.selectedStation.length) {
+      this.stationPlaceholder = this.selectedStation.length + ' вибрано';
+    } else {
+      this.translate.get('ubs-tariffs.placeholder-choose-station').subscribe((data) => (this.stationPlaceholder = data));
+    }
+  }
+
+  public onSelectStation(event, trigger?: MatAutocompleteTrigger): void {
+    this.station.clearValidators();
+    this.station.updateValueAndValidity();
+    this.blurOnOption = false;
+    const selectedValue = this.stations.find((ob) => ob.name === event.option.value);
+    const tempItem = {
+      name: selectedValue.name,
+      id: selectedValue.id
+    };
+    const newValue = event.option.value;
+    if (this.selectedStation.map((it) => it.name).includes(newValue)) {
+      this.selectedStation = this.selectedStation.filter((item) => item.name !== newValue);
+    } else {
+      this.selectedStation.push(tempItem);
+    }
+    this.station.setValidators(this.stationValidator());
+    this.station.setValue('');
+    this.setStationPlaceholder();
+    if (trigger) {
+      requestAnimationFrame(() => {
+        trigger.openPanel();
+      });
+    }
+  }
+
+  public _filterOptions(name: string, items: any[]): any[] {
+    const filterValue = name.toLowerCase();
+    return items.filter((option) => option.toLowerCase().includes(filterValue));
+  }
+
+  public stationValidator(): ValidatorFn {
+    let error;
+    if (!this.selectedStation.length) {
+      error = this.station.setErrors({ emptySelectedStation: true });
+    }
+    return error;
+  }
+
+  public getReceivingStation(): void {
+    this.tariffsService
+      .getAllStations()
+      .pipe(takeUntil(this.unsubscribe))
+      .subscribe((res) => {
+        this.stations = res;
+        const stationsName = this.stations.map((it) => it.name);
+        this.station.valueChanges
+          .pipe(
+            startWith(''),
+            map((value: string) => this._filterOptions(value, stationsName))
+          )
+          .subscribe((data) => {
+            this.filteredStations = data;
+            this.station.setValidators(this.stationValidator());
+          });
+      });
+  }
+
+  public ngOnDestroy(): void {
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
+  }
 }
