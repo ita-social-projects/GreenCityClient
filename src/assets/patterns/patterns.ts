924a2cc393f35a55f0ac89019467535e66c61061
katia9261 <101433204+katia9261@users.noreply.github.com>
2022-11-15 10:38:40 +0200

    #4618: try another pattern

--- src/assets/patterns/patterns.ts
+++ src/assets/patterns/patterns.ts
@@ -29,7 +29,7 @@ export const Patterns = {
   ubsCityPattern: /^([a-zа-яїєґі '-]){1,30}/iu,
   ubsWithDigitPattern: /^([a-zа-яїєґі0-9 '-])+$/iu,
   ubsHouseNumberPattern: /^([a-zа-яїєґі0-9]([-,/]?))+$/iu,
-  ubsNameAndSernamePattern: /^([[a-zа-яїєґі]+(['-]+[a-zа-яїєґі])?){1,30}$/iu,
+  ubsNameAndSernamePattern: /^[a-zа-яїєґі](?!.*-.*-)(?!.*'.*')[-' a-zа-яїєґі]{0,28}[a-zа-яїєґі]$/iu,
   adminPhone: '^\\+?3?8?(0\\d{9})$'
 };
 
