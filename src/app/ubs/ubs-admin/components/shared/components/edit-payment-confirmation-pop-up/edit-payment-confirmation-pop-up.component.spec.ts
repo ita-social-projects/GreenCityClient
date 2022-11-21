f7a8da88ed29daf610edc3b52f2bdd3feedcb2ae
katia9261 <101433204+katia9261@users.noreply.github.com>
2022-11-21 17:12:13 +0200

    4715: add pop-up and tests

---
+++ src/app/ubs/ubs-admin/components/shared/components/edit-payment-confirmation-pop-up/edit-payment-confirmation-pop-up.component.spec.ts
@@ -0,0 +1,84 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
+import { TranslateModule } from '@ngx-translate/core';
+import { of } from 'rxjs';
+
+import { EditPaymentConfirmationPopUpComponent } from './edit-payment-confirmation-pop-up.component';
+
+describe('EditPaymentConfirmationPopUpComponent', () => {
+  let component: EditPaymentConfirmationPopUpComponent;
+  let fixture: ComponentFixture<EditPaymentConfirmationPopUpComponent>;
+  const fakeTitles = {
+    popupTitle: 'popupTitle',
+    popupConfirm: 'popupSubtitle',
+    popupCancel: 'popupSubtitle'
+  };
+  const dialogRefStub = {
+    keydownEvents() {
+      return of();
+    },
+    backdropClick() {
+      return of();
+    },
+    close() {}
+  };
+  const matDialogRef = 'matDialogRef';
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [EditPaymentConfirmationPopUpComponent],
+      imports: [TranslateModule.forRoot(), MatDialogModule, BrowserDynamicTestingModule],
+      providers: [
+        { provide: MatDialogRef, useValue: dialogRefStub },
+        { provide: MAT_DIALOG_DATA, useValue: fakeTitles }
+      ]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EditPaymentConfirmationPopUpComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should keydownEvents be called in ngOnInit', () => {
+    const spy = spyOn(component[matDialogRef], 'keydownEvents').and.returnValue(of());
+    component.ngOnInit();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should backdropClick be called in ngOnInit', () => {
+    const spy = spyOn(component[matDialogRef], 'backdropClick').and.returnValue(of());
+    component.ngOnInit();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should set titles after setTitles method', () => {
+    component.ngOnInit();
+
+    expect(component.popupTitle).toBe(fakeTitles.popupTitle);
+    expect(component.popupCancel).toBe(fakeTitles.popupCancel);
+    expect(component.popupConfirm).toBe(fakeTitles.popupConfirm);
+  });
+
+  it('should call close on matDialogRef', () => {
+    const spy = spyOn(component[matDialogRef], 'close');
+    component.userReply(true);
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should cancel streams after ngOnDestroy', () => {
+    const destroy$ = 'destroy$';
+    const nextSpy = spyOn(component.destroy$, 'next');
+    const completeSpy = spyOn(component.destroy$, 'complete');
+    component.ngOnDestroy();
+
+    expect(nextSpy).toHaveBeenCalled();
+    expect(completeSpy).toHaveBeenCalled();
+  });
+});
---
+++ src/app/ubs/ubs-admin/components/shared/components/edit-payment-confirmation-pop-up/edit-payment-confirmation-pop-up.component.ts
@@ -0,0 +1,51 @@
+import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-edit-payment-confirmation-pop-up',
+  templateUrl: './edit-payment-confirmation-pop-up.component.html',
+  styleUrls: ['./edit-payment-confirmation-pop-up.component.scss']
+})
+export class EditPaymentConfirmationPopUpComponent implements OnInit {
+  public destroy$: Subject<boolean> = new Subject<boolean>();
+  popupTitle: string;
+  popupConfirm: string;
+  popupCancel: string;
+
+  constructor(private matDialogRef: MatDialogRef<EditPaymentConfirmationPopUpComponent>, @Inject(MAT_DIALOG_DATA) public modalData: any) {}
+
+  ngOnInit(): void {
+    this.popupTitle = this.modalData.popupTitle;
+    this.popupConfirm = this.modalData.popupConfirm;
+    this.popupCancel = this.modalData.popupCancel;
+
+    this.matDialogRef
+      .keydownEvents()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((event) => {
+        if (event.key === 'Escape') {
+          this.userReply(false);
+        }
+        if (event.key === 'Enter') {
+          this.userReply(true);
+        }
+      });
+    this.matDialogRef
+      .backdropClick()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(() => {
+        this.userReply(false);
+      });
+  }
+
+  public userReply(res: boolean): void {
+    this.matDialogRef.close(res);
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+}
