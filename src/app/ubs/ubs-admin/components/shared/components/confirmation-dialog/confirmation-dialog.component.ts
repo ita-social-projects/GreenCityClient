7f58cc529a325eb6a5f6dac52b83be7e7ed59e09
Oleksii Zaitsev <81097502+zaitsev-oleksii@users.noreply.github.com>
2022-11-28 14:46:12 +0200

    code smell

--- src/app/ubs/ubs-admin/components/shared/components/confirmation-dialog/confirmation-dialog.component.ts
+++ src/app/ubs/ubs-admin/components/shared/components/confirmation-dialog/confirmation-dialog.component.ts
@@ -1,6 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-confirmation-dialog',
