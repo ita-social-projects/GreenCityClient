19e3cceac589b6180292aa7f6d9fa9cb62df3acb
atsa21 <ts21ann@gmail.com>
2022-12-12 16:20:16 +0200

    bugfix/#4250 Homepage buttons 'Start forming a habit' click

--- src/app/main/component/home/components/stat-row/stat-row.component.ts
+++ src/app/main/component/home/components/stat-row/stat-row.component.ts
@@ -1,14 +1,27 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { CheckTokenService } from '@global-service/auth/check-token/check-token.service';
+import { LocalStorageService } from '@global-service/localstorage/local-storage.service';
 
 @Component({
   selector: 'app-stat-row',
   templateUrl: './stat-row.component.html',
-  styleUrls: ['./stat-row.component.scss'],
+  styleUrls: ['./stat-row.component.scss']
 })
 export class StatRowComponent {
   /* TODO Replace with entity*/
   @Input() stat: { action; caption; count; question; iconPath; locationText };
   @Input() index: number;
 
+  userId: number;
+
   readonly locationImage = 'assets/img/icon/location-icon.png';
+
+  constructor(private router: Router, private checkTokenservice: CheckTokenService, private localService: LocalStorageService) {
+    this.localService.userIdBehaviourSubject.subscribe((userId) => (this.userId = userId));
+  }
+
+  startHabit() {
+    this.userId ? this.router.navigate(['/profile', this.userId]) : this.checkTokenservice.openAuthModalWindow();
+  }
 }
