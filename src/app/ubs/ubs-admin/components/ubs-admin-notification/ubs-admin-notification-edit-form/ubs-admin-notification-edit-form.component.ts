dc2cf253406cd70b0c2d5eece6ce7f174c727a35
Oleksii Zaitsev <81097502+zaitsev-oleksii@users.noreply.github.com>
2022-12-12 09:06:04 +0200

    removed unused import in notification-edit-form

--- src/app/ubs/ubs-admin/components/ubs-admin-notification/ubs-admin-notification-edit-form/ubs-admin-notification-edit-form.component.ts
+++ src/app/ubs/ubs-admin/components/ubs-admin-notification/ubs-admin-notification-edit-form/ubs-admin-notification-edit-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
