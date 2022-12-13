72a17e820ed032399602bcbbb2e639aa63c9351f
katia9261 <101433204+katia9261@users.noreply.github.com>
2022-12-13 15:25:42 +0200

    #4817: redirect to friend's page

--- src/app/main/component/user/components/profile/users-friends/friend-dashboard/friend-requests/request-item/request-item.component.ts
+++ src/app/main/component/user/components/profile/users-friends/friend-dashboard/friend-requests/request-item/request-item.component.ts
@@ -1,16 +1,21 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { FriendModel } from '@global-user/models/friend.model';
+import { Router } from '@angular/router';
+import { LocalStorageService } from '@global-service/localstorage/local-storage.service';
 
 @Component({
   selector: 'app-request-item',
   templateUrl: './request-item.component.html',
-  styleUrls: ['./request-item.component.scss'],
+  styleUrls: ['./request-item.component.scss']
 })
 export class RequestItemComponent {
   @Input() request: FriendModel;
   @Input() btnName: string;
   @Output() acceptEvent = new EventEmitter<number>();
   @Output() declineEvent = new EventEmitter<number>();
+  private userId: number;
+
+  constructor(private router: Router, private localeStorageService: LocalStorageService) {}
 
   public accept(id: number) {
     this.acceptEvent.emit(id);
@@ -18,5 +23,21 @@ export class RequestItemComponent {
 
   public decline(id: number) {
     this.declineEvent.emit(id);
+  }
+
+  public clickHandler(event: MouseEvent): void {
+    const target = event.target as HTMLElement;
+    if (target.tagName === 'button') {
+      return;
+    } else {
+      this.redirectToFriendPage();
+    }
+  }
+
+  private redirectToFriendPage(): void {
+    this.userId = this.localeStorageService.getUserId();
+    this.router.navigate(['profile', this.userId, 'friends', this.request.name, this.request.id], {
+      queryParams: { tab: 'All firends', index: 3 }
+    });
   }
 }
