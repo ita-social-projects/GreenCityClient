e258842eeac17f6f68010b22a0632e490038c149
katia9261 <101433204+katia9261@users.noreply.github.com>
2022-12-14 10:10:16 +0200

    #4817: delete unused imports

--- src/app/main/component/user/components/profile/users-friends/friend-dashboard/friend-requests/request-item/request-item.component.spec.ts
+++ src/app/main/component/user/components/profile/users-friends/friend-dashboard/friend-requests/request-item/request-item.component.spec.ts
@@ -1,6 +1,6 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
-import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { TranslateModule } from '@ngx-translate/core';
 import { By } from '@angular/platform-browser';
