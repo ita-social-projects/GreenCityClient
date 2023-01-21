5f287f3e2b18bdaf777003cab100659e202226ef
Мар'яна <mariana@MacBook-Air-Marana.local>
2023-01-21 11:45:02 +0200

    Fixed lint error

--- src/assets/google-script/google-script.ts
+++ src/assets/google-script/google-script.ts
@@ -2,20 +2,20 @@ import { Injectable } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class GoogleScript {
-  mainUrl = `https://maps.googleapis.com/maps/api/js?key=AIzaSyB3xs7Kczo46LFcQRFKPMdrE0lU4qsR_S4&callback=initMap&libraries=places&language=`;
+  url = `https://maps.googleapis.com/maps/api/js?key=AIzaSyB3xs7Kczo46LFcQRFKPMdrE0lU4qsR_S4&callback=initMap&libraries=places&language=`;
 
   load(lang: string): void {
     const googleScript: HTMLScriptElement = document.querySelector('#googleMaps');
 
     if (googleScript) {
-      googleScript.src = this.mainUrl + lang;
+      googleScript.src = this.url + lang;
     }
     if (!googleScript) {
       this.initMap();
       const google = document.createElement('script');
       google.type = 'text/javascript';
       google.id = 'googleMaps';
-      google.setAttribute('src', this.mainUrl + lang);
+      google.setAttribute('src', this.url + lang);
       document.getElementsByTagName('head')[0].appendChild(google);
     }
   }
