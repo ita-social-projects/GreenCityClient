31f6f5847bc0f6e33758c914a92650a7269ef205
atsa21 <ts21ann@gmail.com>
2023-01-26 17:36:48 +0200

    lint fix

--- src/app/ubs/ubs-admin/components/ubs-admin-tariffs/ubs-admin-tariffs-location-pop-up/ubs-admin-tariffs-location-pop-up.component.ts
+++ src/app/ubs/ubs-admin/components/ubs-admin-tariffs/ubs-admin-tariffs-location-pop-up/ubs-admin-tariffs-location-pop-up.component.ts
@@ -267,9 +267,9 @@ export class UbsAdminTariffsLocationPopUpComponent implements OnInit, AfterViewC
     this.setTranslation(event.place_id, this.englishRegion, this.checkLanguage('en', 'uk'));
   }
 
-  setTranslation(place_id: string, abstractControl: any, lang: string): void {
+  setTranslation(id: string, abstractControl: any, lang: string): void {
     const request = {
-      placeId: place_id,
+      placeId: id,
       language: lang
     };
     this.placeService.getDetails(request, (placeDetails) => {
