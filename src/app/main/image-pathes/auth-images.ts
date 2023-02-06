ffe676a195b65d3d12b57a9403f61a7a3d29edcd
katia9261 <101433204+katia9261@users.noreply.github.com>
2023-02-06 14:39:42 +0200

    fix login img

--- src/app/main/image-pathes/auth-images.ts
+++ src/app/main/image-pathes/auth-images.ts
@@ -1,5 +1,5 @@
 export const authImages = {
-  mainImage: 'assets/img/auth/auth-lock-open.png',
+  mainImage: 'assets/img/auth/auth-img.png',
   cross: 'assets/img/auth/cross.svg',
   hiddenEye: 'assets/img/auth/hided_eye.svg',
   openEye: 'assets/img/auth/open_eye.svg',
