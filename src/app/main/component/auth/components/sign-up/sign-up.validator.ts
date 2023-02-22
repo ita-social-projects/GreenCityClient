7f735d7915056bc3ffb067e61db12aecfc35f795
Nadiia <nadiia.stoiko@gmail.com>
2023-02-22 13:54:07 +0200

    added changes at ConfirmPasswordValidator function

--- src/app/main/component/auth/components/sign-up/sign-up.validator.ts
+++ src/app/main/component/auth/components/sign-up/sign-up.validator.ts
@@ -7,7 +7,7 @@ export function ConfirmPasswordValidator(controlName: string, matchingControlNam
     const matchingControl = formGroup.controls[matchingControlName];
     if (matchingControl.value === '') {
       matchingControl.setErrors({ required: true });
-    } else if (control.value === '' && matchingControl.value.length > 0) {
+    } else if (!control.value && matchingControl.value.length) {
       matchingControl.setErrors({ passwordIsEmpty: true });
     } else {
       if (control.value !== matchingControl.value) {
