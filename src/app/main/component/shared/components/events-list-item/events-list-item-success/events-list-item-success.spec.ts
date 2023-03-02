bea08bbf5e5a2adba00d798367c4caa45d3c81ac
Kateryna <katyakravchuk123@gmail.com>
2023-03-02 15:52:05 +0200

    added translation

--- src/app/main/component/shared/components/events-list-item/events-list-item-success/events-list-item-success.spec.ts
+++ src/app/main/component/shared/components/events-list-item/events-list-item-success/events-list-item-success.spec.ts
@@ -1,20 +1,66 @@
+import { Language } from './../../../../../i18n/Language';
 import { EventsListItemSuccessComponent } from './events-list-item-success';
-import { BsModalRef } from 'ngx-bootstrap/modal';
-import { Injector, Injectable } from '@angular/core';
+import { BsModalRef, ModalModule } from 'ngx-bootstrap/modal';
+import { Injectable, EventEmitter } from '@angular/core';
 import { TestBed, async, ComponentFixture, fakeAsync, tick } from '@angular/core/testing';
 import { MatDialogModule } from '@angular/material/dialog';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { LocalStorageService } from '@global-service/localstorage/local-storage.service';
+import { of, Subject } from 'rxjs';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+@Injectable()
+class TranslationServiceStub {
+  public onLangChange = new EventEmitter<any>();
+  public onTranslationChange = new EventEmitter<any>();
+  public onDefaultLangChange = new EventEmitter<any>();
+  public addLangs(langs: string[]) {}
+  public getLangs() {
+    return 'en-us';
+  }
+  public getBrowserLang() {
+    return '';
+  }
+  public getBrowserCultureLang() {
+    return '';
+  }
+  public use(lang: string) {
+    return '';
+  }
+  public get(key: any): any {
+    return of(key);
+  }
+  public setDefaultLang() {
+    return true;
+  }
+}
 describe('EventsListItemSuccessComponent', () => {
   let component: EventsListItemSuccessComponent;
   let fixture: ComponentFixture<EventsListItemSuccessComponent>;
+
+  const mockLang = 'ua';
+
+  let translateServiceMock: TranslateService;
+  translateServiceMock = jasmine.createSpyObj('TranslateService', ['setDefaultLang']);
+  translateServiceMock.setDefaultLang = (lang: string) => of();
+  translateServiceMock.get = () => of(true);
+
+  let localStorageServiceMock: LocalStorageService;
+  localStorageServiceMock = jasmine.createSpyObj('LocalStorageService', ['languageSubject', 'getCurrentLanguage']);
+  localStorageServiceMock.languageSubject = new Subject();
+  localStorageServiceMock.getCurrentLanguage = () => mockLang as Language;
 
   const MockBsModalRef = jasmine.createSpyObj('bsModalRef', ['hide']);
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [EventsListItemSuccessComponent],
-      providers: [{ provide: BsModalRef, useValue: MockBsModalRef }],
-      imports: [MatDialogModule]
+      providers: [
+        { provide: BsModalRef, useValue: MockBsModalRef },
+        { provide: TranslateService, useClass: TranslationServiceStub },
+        { provide: LocalStorageService, useValue: localStorageServiceMock }
+      ],
+      imports: [MatDialogModule, TranslateModule.forRoot(), ModalModule.forRoot(), BrowserAnimationsModule]
     }).compileComponents();
   }));
 
@@ -57,6 +103,20 @@ describe('EventsListItemSuccessComponent', () => {
       jasmine.clock().tick(500);
       expect(component.openAuthModalWindow).toHaveBeenCalled();
       jasmine.clock().uninstall();
+    });
+  });
+
+  describe('ngOnInit', () => {
+    it(`subscribeToLangChange should be called in ngOnInit`, () => {
+      const subscribeToLangChangeSpy = spyOn(component, 'subscribeToLangChange');
+      component.ngOnInit();
+      expect(subscribeToLangChangeSpy).toHaveBeenCalled();
+    });
+
+    it(`bindLang should be called in ngOnInit`, () => {
+      const bindLangSpy = spyOn(component, 'subscribeToLangChange');
+      component.ngOnInit();
+      expect(bindLangSpy).toHaveBeenCalled();
     });
   });
 });
--- src/app/main/component/shared/components/events-list-item/events-list-item-success/events-list-item-success.ts
+++ src/app/main/component/shared/components/events-list-item/events-list-item-success/events-list-item-success.ts
@@ -1,21 +1,35 @@
-import { Component, Injector } from '@angular/core';
+import { Component, Injector, OnInit } from '@angular/core';
 import { BsModalRef } from 'ngx-bootstrap/modal';
 import { AuthModalComponent } from '@global-auth/auth-modal/auth-modal.component';
 import { MatDialog } from '@angular/material/dialog';
+import { TranslateService } from '@ngx-translate/core';
+import { LocalStorageService } from '@global-service/localstorage/local-storage.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-event-list-item-success',
   templateUrl: 'events-list-item-success.html',
   styleUrls: ['events-list-item-success.scss']
 })
-export class EventsListItemSuccessComponent {
+export class EventsListItemSuccessComponent implements OnInit {
   public id: number;
   public isRegistered: boolean;
   public elementName: string;
   public dialog: MatDialog;
+  public langChangeSub: Subscription;
 
-  constructor(public bsModalRef: BsModalRef, public injector: Injector) {
+  constructor(
+    public bsModalRef: BsModalRef,
+    public injector: Injector,
+    private translate: TranslateService,
+    private localStorageService: LocalStorageService
+  ) {
     this.dialog = injector.get(MatDialog);
+  }
+
+  ngOnInit() {
+    this.subscribeToLangChange();
+    this.bindLang(this.localStorageService.getCurrentLanguage());
   }
 
   public modalBtn(): void {
@@ -37,5 +51,13 @@ export class EventsListItemSuccessComponent {
         popUpName: page
       }
     });
+  }
+
+  public bindLang(lang: string): void {
+    this.translate.setDefaultLang(lang);
+  }
+
+  public subscribeToLangChange(): void {
+    this.langChangeSub = this.localStorageService.languageSubject.subscribe(this.bindLang.bind(this));
   }
 }
