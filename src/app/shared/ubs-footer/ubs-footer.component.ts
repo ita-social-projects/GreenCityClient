f55d31826baa482509c85e18899d63810703ba4e
katia9261 <101433204+katia9261@users.noreply.github.com>
2023-03-14 14:16:56 +0200

    fix small bags

--- src/app/shared/ubs-footer/ubs-footer.component.ts
+++ src/app/shared/ubs-footer/ubs-footer.component.ts
@@ -14,8 +14,7 @@ export class UbsFooterComponent {
   public currentYear = new Date().getFullYear();
   public ubsNavLinks = [
     { name: 'user.lower-nav-bar.sorting-rules', route: 'https://nowaste.com.ua/yak-sortyvaty-na-karantuni/', url: true },
-    { name: 'user.lower-nav-bar.eco-shop', route: 'https://shop.nowaste.com.ua/', url: true },
-    { name: 'Green City', route: '/#/greenCity', url: false }
+    { name: 'user.lower-nav-bar.eco-shop', route: 'https://shop.nowaste.com.ua/', url: true }
   ];
 
   constructor(private dialog: MatDialog) {}
