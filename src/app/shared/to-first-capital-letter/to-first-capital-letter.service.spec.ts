dc6968e048087ba9b513c842401634aa152f49c0
atsa21 <ts21ann@gmail.com>
2023-03-15 13:41:14 +0200

    added test

--- src/app/shared/to-first-capital-letter/to-first-capital-letter.service.spec.ts
+++ src/app/shared/to-first-capital-letter/to-first-capital-letter.service.spec.ts
@@ -13,4 +13,9 @@ describe('ToFirstCapitalLetterService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should convert value that just first letter will be capital', () => {
+    const res = service.convFirstLetterToCapital('street Name');
+    expect(res).toBe('Street name');
+  });
 });
