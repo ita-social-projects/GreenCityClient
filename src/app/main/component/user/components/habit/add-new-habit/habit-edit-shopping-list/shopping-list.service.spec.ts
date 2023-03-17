f5652bb82756ac8583f43402af978ca02a09a9d9
atsa21 <ts21ann@gmail.com>
2023-03-17 16:26:00 +0200

    cover shop list service by unit test

--- src/app/main/component/user/components/habit/add-new-habit/habit-edit-shopping-list/shopping-list.service.spec.ts
+++ src/app/main/component/user/components/habit/add-new-habit/habit-edit-shopping-list/shopping-list.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { ShoppingListService } from './shopping-list.service';
-import { AllShoppingLists, ShoppingList } from '@global-user/models/shoppinglist.model';
+import { AllShoppingLists, CustomShoppingItem, HabitUpdateShopList, ShoppingList } from '@global-user/models/shoppinglist.model';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { environment } from '@environment/environment.js';
 
@@ -12,11 +12,14 @@ describe('ShoppingListService', () => {
 
   const mainLink = environment.backendLink;
 
+  const mockCustomItem: CustomShoppingItem = {
+    text: 'New item'
+  };
+
   const mockItem: ShoppingList = {
     id: 1,
     status: 'INPROGRESS',
-    text: 'string',
-    selected: false
+    text: 'Item 1'
   };
 
   const mockList: ShoppingList[] = [
@@ -49,6 +52,25 @@ describe('ShoppingListService', () => {
         text: 'Item 1'
       }
     ]
+  };
+
+  const mockUpdateHabitList: HabitUpdateShopList = {
+    habitId: 2,
+    standartShopList: [
+      {
+        id: 1,
+        status: 'INPROGRESS',
+        text: 'Item 1'
+      }
+    ],
+    customShopList: [
+      {
+        id: 2,
+        status: 'INPROGRESS',
+        text: 'Item 2'
+      }
+    ],
+    lang: 'ua'
   };
 
   beforeEach(() => {
@@ -100,5 +122,41 @@ describe('ShoppingListService', () => {
     expect(req.request.responseType).toEqual('json');
     expect(req.request.method).toBe('GET');
     req.flush(mockAllShopList);
+  });
+
+  it('should add Habit Custom Shop List', () => {
+    service.addHabitCustomShopList(1, 2, [mockCustomItem]).subscribe((data) => {
+      expect(data).toEqual(mockList);
+    });
+    const req = httpMock.expectOne(`${mainLink}custom/shopping-list-items/1/2/custom-shopping-list-items`);
+    expect(req.request.method).toBe('POST');
+    req.flush(mockList);
+  });
+
+  it('should update Standard Shop Item Status', () => {
+    service.updateStandardShopItemStatus(mockItem, 'ua').subscribe((data) => {
+      expect(data).toEqual(mockList);
+    });
+    const req = httpMock.expectOne(`${mainLink}user/shopping-list-items/1/status/INPROGRESS?lang=ua`);
+    expect(req.request.method).toBe('PATCH');
+    req.flush(mockList);
+  });
+
+  it('should update Custom Shop Item Status', () => {
+    service.updateCustomShopItemStatus(1, mockItem).subscribe((data) => {
+      expect(data).toEqual(mockItem);
+    });
+    const req = httpMock.expectOne(`${mainLink}custom/shopping-list-items/1/custom-shopping-list-items?itemId=1&status=INPROGRESS`);
+    expect(req.request.method).toBe('PATCH');
+    req.flush(mockItem);
+  });
+
+  it('should update Habit Shop List', () => {
+    service.updateHabitShopList(mockUpdateHabitList).subscribe((data) => {
+      expect(data).toEqual(null);
+    });
+    const req = httpMock.expectOne(`${mainLink}habit/assign/2/allUserAndCustomList?lang=ua`);
+    expect(req.request.method).toBe('PUT');
+    req.flush(null);
   });
 });
