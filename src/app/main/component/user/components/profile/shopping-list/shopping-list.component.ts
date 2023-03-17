e15002e49b19049d0304a2524d2f00f154b039b5
atsa21 <ts21ann@gmail.com>
2023-03-17 18:45:00 +0200

    add comment

--- src/app/main/component/user/components/profile/shopping-list/shopping-list.component.ts
+++ src/app/main/component/user/components/profile/shopping-list/shopping-list.component.ts
@@ -43,7 +43,8 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
       .getCustomShopList(this.userId)
       .pipe(takeUntil(this.destroy$))
       .subscribe((res: ShoppingList[]) => {
-        this.shoppingList = res.filter((el) => el.status === 'INPROGRESS'); //will be removed after add new GET on backend
+        this.shoppingList = res.filter((el) => el.status === 'INPROGRESS');
+        //will be removed after add new GET controller on backend ^
         this.shoppingList.forEach((el) => (el.custom = true));
         this.getShoppingList();
       });
