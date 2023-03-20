067212c1e56be60746a443f3f0e0893aa6f119a1
katia9261 <101433204+katia9261@users.noreply.github.com>
2023-03-20 10:59:14 +0200

    fix for cancel reason

--- src/app/ubs/ubs-admin/components/add-order-cancellation-reason/add-order-cancellation-reason.component.ts
+++ src/app/ubs/ubs-admin/components/add-order-cancellation-reason/add-order-cancellation-reason.component.ts
@@ -77,7 +77,7 @@ export class AddOrderCancellationReasonComponent implements OnInit {
   }
 
   public disableButton(): boolean {
-    const isInvalidCommentForm = this.commentForm.invalid && this.commentForm.touched && !this.cancellationReason;
+    const isInvalidCommentForm = this.commentForm.invalid && this.commentForm.touched && this.cancellationReason === 'OTHER';
     const isOtherCancellationReasonInvalid = this.cancellationReason === 'OTHER' && !this.commentForm.get('cancellationComment').value;
 
     if (isInvalidCommentForm || isOtherCancellationReasonInvalid) {
