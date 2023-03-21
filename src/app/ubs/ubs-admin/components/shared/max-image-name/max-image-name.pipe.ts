2458475072c305ee314029bc8fef887a46a58cc9
katia9261 <101433204+katia9261@users.noreply.github.com>
2023-03-21 13:18:18 +0200

    fixed tests

--- src/app/ubs/ubs-admin/components/shared/max-image-name/max-image-name.pipe.ts
+++ src/app/ubs/ubs-admin/components/shared/max-image-name/max-image-name.pipe.ts
@@ -5,6 +5,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class MaxImageNamePipe implements PipeTransform {
   transform(imageName: string, maxImgNameLength: number = 15): string {
+    if (!imageName) {
+      return '';
+    }
     return imageName.length > maxImgNameLength ? imageName.slice(0, maxImgNameLength) + '...' : imageName;
   }
 }
