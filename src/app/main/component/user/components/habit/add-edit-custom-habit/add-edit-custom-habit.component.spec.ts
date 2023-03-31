8e68fbc426d273ddff8ad4888ca68e65d66ec54a
atsa21 <ts21ann@gmail.com>
2023-03-31 19:46:04 +0300

    added unit tests

--- src/app/main/component/user/components/habit/add-edit-custom-habit/add-edit-custom-habit.component.spec.ts
+++ src/app/main/component/user/components/habit/add-edit-custom-habit/add-edit-custom-habit.component.spec.ts
@@ -7,26 +7,29 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { HabitService } from '@global-service/habit/habit.service';
 import { ReactiveFormsModule } from '@angular/forms';
-import { of, Subject } from 'rxjs';
+import { of, Subject, BehaviorSubject } from 'rxjs';
 import { TagInterface } from '@shared/components/tag-filter/tag-filter.model';
 import { LocalStorageService } from '@global-service/localstorage/local-storage.service';
 import { Router } from '@angular/router';
+import { Language } from 'src/app/main/i18n/Language';
+import { ShoppingList } from '@global-user/models/shoppinglist.model';
 
-describe('AddEditCustomHabitComponent', () => {
+fdescribe('AddEditCustomHabitComponent', () => {
   let component: AddEditCustomHabitComponent;
   let fixture: ComponentFixture<AddEditCustomHabitComponent>;
 
   const tagsMock: TagInterface[] = [{ id: 1, name: 'Tag', nameUa: 'Тег', isActive: true }];
 
-  const localStorageServiceMock = jasmine.createSpyObj('localStorageService', {
-    getCurrentLanguage: () => 'ua'
-  });
+  const localStorageServiceMock = jasmine.createSpyObj('localStorageService', ['getUserId', 'getCurrentLanguage']);
+
   localStorageServiceMock.getUserId = () => 2;
   localStorageServiceMock.languageSubject = new Subject<string>();
-  localStorageServiceMock.languageSubject.next('ua');
+  localStorageServiceMock.languageBehaviourSubject = new BehaviorSubject('ua');
+  localStorageServiceMock.getCurrentLanguage = () => 'ua' as Language;
 
   const habitServiceMock = jasmine.createSpyObj('fakeHabitAssignService', ['getAllTags', 'addCustomHabit']);
   habitServiceMock.getAllTags = () => of(tagsMock);
+  habitServiceMock.addCustomHabit = () => of(null);
 
   const languageServiceMock = jasmine.createSpyObj('languageService', ['getLangValue']);
   languageServiceMock.getLangValue.and.returnValue('fakeTag');
@@ -54,5 +57,92 @@ describe('AddEditCustomHabitComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call methods onInit', () => {
+    const spy1 = spyOn(component as any, 'getUserId');
+    const spy2 = spyOn(component as any, 'initForm');
+    const spy3 = spyOn(component as any, 'getHabitTags');
+    const spy4 = spyOn(component as any, 'subscribeToLangChange');
+    component.ngOnInit();
+    expect(spy1).toHaveBeenCalled();
+    expect(spy2).toHaveBeenCalled();
+    expect(spy3).toHaveBeenCalled();
+    expect(spy4).toHaveBeenCalled();
+  });
+
+  it('getUserId should set userId', () => {
+    (component as any).getUserId();
+    expect((component as any).userId).toBe(2);
+  });
+
+  it('should return title Control on getControl', () => {
+    (component as any).initForm();
+    const title = component.getControl('title');
+    expect(title).toEqual(component.habitForm.get('title'));
+  });
+
+  it('subscribeToLangChange should set current language', () => {
+    (component as any).subscribeToLangChange();
+    expect((component as any).currentLang).toBe('ua');
+  });
+
+  it('getStars should return right star image', () => {
+    let starImage = component.getStars(1, 3);
+    expect(starImage).toBe('assets/img/icon/star-1.png');
+    starImage = component.getStars(3, 2);
+    expect(starImage).toBe('assets/img/icon/star-2.png');
+  });
+
+  it('should set duration after get it from child component', () => {
+    (component as any).initForm();
+    component.getDuration(15);
+    expect(component.habitForm.get('duration').value).toBe(15);
+  });
+
+  it('should set shopList after get it from child component', () => {
+    const newShopList: ShoppingList[] = [
+      {
+        id: 1,
+        status: 'INPROGRESS',
+        text: 'Some item',
+        selected: true,
+        custom: true
+      }
+    ];
+    const convertedList: ShoppingList[] = [{ id: 1, status: 'INPROGRESS', text: 'Some item' }];
+    (component as any).initForm();
+    component.getShopList(newShopList);
+    expect(component.newList).toEqual(convertedList);
+    expect(component.habitForm.get('shopList').value).toEqual(convertedList);
+  });
+
+  it('should set TagList after get it from child component', () => {
+    (component as any).initForm();
+    component.getTagsList(tagsMock);
+    expect(component.selectedTagsList).toEqual(['fakeTag']);
+    expect(component.habitForm.get('tags').value).toEqual(['fakeTag']);
+  });
+
+  it('goToAllHabits should navigate to all habits page', () => {
+    (component as any).userId = 2;
+    component.goToAllHabits();
+    expect(routerMock.navigate).toHaveBeenCalledWith(['/profile/2/allhabits']);
+  });
+
+  it('should set tagsList on getHabitTags', () => {
+    (component as any).getHabitTags();
+    expect(component.tagsList).toEqual(tagsMock);
+  });
+
+  it('should get value by language', () => {
+    const valueByLang = (component as any).getLangValue('fakeTag', 'fakeTagEn');
+    expect(valueByLang).toBe('fakeTag');
+  });
+
+  it('should call goToAllHabits on addHabit', () => {
+    const spy = spyOn(component, 'goToAllHabits');
+    component.addHabit();
+    expect(spy).toHaveBeenCalled();
   });
 });
