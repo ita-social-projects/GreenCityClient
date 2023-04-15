c1844a3bac08e50600631a884cb4ba110d67d204
atsa21 <ts21ann@gmail.com>
2023-04-15 10:27:52 +0300

    cover location service by test

--- src/app/main/service/location/location.spec.ts
+++ src/app/main/service/location/location.spec.ts
@@ -15,4 +15,22 @@ describe('LocationService', () => {
   it('should be created', () => {
     expect(locations).toBeTruthy();
   });
+
+  it('should get auto district', () => {
+    const placeDetails = {
+      address_components: [{ long_name: 'Name of District' }]
+    };
+    const convertedAddress = locations.getDistrictAuto(placeDetails, 'en');
+    expect(convertedAddress).toBe('Name of district');
+  });
+
+  it('should convert value that only first letter will be capital letter', () => {
+    const convertedAddress = locations.convFirstLetterToCapital('Name of District');
+    expect(convertedAddress).toBe('Name of district');
+  });
+
+  it('should add House Number To Address', () => {
+    const convertedAddress = locations.addHouseNumToAddress('Skovorody street, Kyiv, Ukraine', 2);
+    expect(convertedAddress).toBe('Skovorody street, 2, Kyiv, Ukraine');
+  });
 });
