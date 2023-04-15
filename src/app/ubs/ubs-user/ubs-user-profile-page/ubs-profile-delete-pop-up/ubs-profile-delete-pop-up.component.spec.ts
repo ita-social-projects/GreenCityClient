2d57e8847289bd062ecfd8e074f66d5d2b59e4ca
Kateryna <katyakravchuk123@gmail.com>
2023-04-15 22:12:01 +0300

    changed tests

--- src/app/ubs/ubs-user/ubs-user-profile-page/ubs-profile-delete-pop-up/ubs-profile-delete-pop-up.component.spec.ts
+++ src/app/ubs/ubs-user/ubs-user-profile-page/ubs-profile-delete-pop-up/ubs-profile-delete-pop-up.component.spec.ts
@@ -1,31 +1,30 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { TranslateModule } from '@ngx-translate/core';
-import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDialog, MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
+
 import { UbsProfileDeletePopUpComponent } from './ubs-profile-delete-pop-up.component';
 
 describe('UbsProfileDeletePopUpComponent', () => {
   let component: UbsProfileDeletePopUpComponent;
   let fixture: ComponentFixture<UbsProfileDeletePopUpComponent>;
-  const dialogMock = {
-    open: () => {},
-    close: () => {}
-  };
   const viewModeInputs = {
     isAddressDelete: {}
   };
-  const matDialogRefStub = jasmine.createSpyObj('matDialogRefStub', ['close']);
-  matDialogRefStub.close = () => 'Close window please';
+  class MatDialogMock {
+    open() {
+      return {};
+    }
+  }
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [UbsProfileDeletePopUpComponent],
       providers: [
-        { prodive: MatDialog, useValue: dialogMock },
-        { provide: MatDialogRef, useValue: matDialogRefStub },
+        { prodive: MatDialog, useClass: MatDialogMock },
         { provide: MAT_DIALOG_DATA, useValue: viewModeInputs }
       ],
-      imports: [TranslateModule.forRoot()],
+      imports: [TranslateModule.forRoot(), MatDialogModule],
       schemas: [CUSTOM_ELEMENTS_SCHEMA]
     }).compileComponents();
   }));
@@ -34,5 +33,9 @@ describe('UbsProfileDeletePopUpComponent', () => {
     fixture = TestBed.createComponent(UbsProfileDeletePopUpComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
   });
 });
