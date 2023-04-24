d4ecbf40c740c39bab67714d30ca0758bb71c132
katia9261 <101433204+katia9261@users.noreply.github.com>
2023-04-25 01:50:07 +0300

    refactoring and fixing some bugs

--- src/app/focus-detection.service.ts
+++ src/app/focus-detection.service.ts
@@ -1,79 +1,79 @@
 import { Injectable } from '@angular/core';
 
+const MATERIAL_COMPONENT_SELECTORS = [
+  '.mat-button',
+  '.mat-raised-button',
+  '.mat-flat-button',
+  '.mat-icon-button',
+  '.mat-fab',
+  '.mat-mini-fab',
+  '.mat-menu-item',
+  '.mat-list-item',
+  '.mat-radio-button',
+  '.mat-checkbox',
+  '.mat-slider',
+  '.mat-slide-toggle',
+  '.mat-select'
+].join(', ');
+
+const NAVIGATION_KEYS = ['Tab', 'ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'];
+
+const OUTLINE_STYLE = {
+  color: '#000',
+  width: '2px',
+  type: 'solid'
+};
+
+const OUTLINE_STYLE_STRING = `${OUTLINE_STYLE.width} ${OUTLINE_STYLE.type} ${OUTLINE_STYLE.color}`;
+
 @Injectable({
   providedIn: 'root'
 })
 export class FocusDetectionService {
   private focusByKeyboard = false;
-  materialComponentSelectors = [
-    '.mat-button',
-    '.mat-raised-button',
-    '.mat-flat-button',
-    '.mat-icon-button',
-    '.mat-fab',
-    '.mat-mini-fab',
-    '.mat-menu-item',
-    '.mat-list-item',
-    '.mat-radio-button',
-    '.mat-checkbox',
-    '.mat-slider',
-    '.mat-slide-toggle',
-    '.mat-select',
-    'button'
-  ].join(', ');
 
   constructor() {
     this.setupFocusDetection();
   }
 
   private setupFocusDetection(): void {
-    document.addEventListener('mousedown', (event) => {
-      if (
-        event.target instanceof HTMLInputElement ||
-        event.target instanceof HTMLSelectElement ||
-        event.target instanceof HTMLButtonElement ||
-        (event.target as HTMLElement).matches(this.materialComponentSelectors)
-      ) {
-        (event.target as HTMLElement).style.outline = 'none';
-      }
-    });
+    document.addEventListener('mousedown', this.onMouseDown);
+    document.addEventListener('keydown', this.onKeyDown);
+    document.addEventListener('focusin', this.onFocusIn);
+    document.addEventListener('focusout', this.onFocusOut);
+  }
 
-    document.addEventListener('keydown', (event) => {
-      const navigationKeys = ['Tab', 'ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'];
-      if (navigationKeys.includes(event.key)) {
-        this.focusByKeyboard = true;
-      } else {
-        this.focusByKeyboard = false;
-      }
+  private onMouseDown = (event: MouseEvent): void => {
+    this.focusByKeyboard = false;
+    this.updateOutline(event.target as HTMLElement, 'none');
+  };
 
-      if (
-        this.focusByKeyboard &&
-        (event.target instanceof HTMLInputElement ||
-          event.target instanceof HTMLSelectElement ||
-          event.target instanceof HTMLButtonElement ||
-          (event.target as HTMLElement).matches(this.materialComponentSelectors))
-      ) {
-        (event.target as HTMLElement).style.outline = '2px solid black !important';
-      }
-    });
+  private onKeyDown = (event: KeyboardEvent): void => {
+    this.focusByKeyboard = NAVIGATION_KEYS.includes(event.key);
+    this.updateOutline(event.target as HTMLElement, `${OUTLINE_STYLE_STRING} !important`);
+  };
 
-    document.addEventListener('focusin', (event) => {
-      if (
-        this.focusByKeyboard &&
-        (event.target instanceof HTMLInputElement || event.target instanceof HTMLSelectElement || event.target instanceof HTMLButtonElement)
-      ) {
-        (event.target as HTMLElement).style.outline = '2px solid black';
-      }
-    });
+  private onFocusIn = (event: FocusEvent): void => {
+    this.updateOutline(event.target as HTMLElement, this.focusByKeyboard ? `${OUTLINE_STYLE_STRING}` : 'none');
+  };
 
-    document.addEventListener('focusout', (event) => {
-      if (
-        event.target instanceof HTMLInputElement ||
-        event.target instanceof HTMLSelectElement ||
-        event.target instanceof HTMLButtonElement
-      ) {
-        (event.target as HTMLElement).style.outline = 'none';
-      }
-    });
+  private onFocusOut = (event: FocusEvent): void => {
+    this.updateOutline(event.target as HTMLElement, 'none');
+  };
+
+  private updateOutline(target: HTMLElement, style: string): void {
+    if (this.isTargetElement(target)) {
+      target.style.outline = style;
+      console.log('Update outline', target, this.focusByKeyboard);
+    }
+  }
+
+  private isTargetElement(target: HTMLElement): boolean {
+    return (
+      target instanceof HTMLInputElement ||
+      target instanceof HTMLSelectElement ||
+      target instanceof HTMLButtonElement ||
+      target.matches(MATERIAL_COMPONENT_SELECTORS)
+    );
   }
 }
