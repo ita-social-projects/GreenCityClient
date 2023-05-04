34a0aa5794edc018a2302ade103e068b7512bdb2
katia9261 <101433204+katia9261@users.noreply.github.com>
2023-05-04 15:31:52 +0300

    tests

--- src/app/ubs/ubs-user/ubs-user-profile-page/ubs-profile-delete-pop-up/ubs-profile-delete-pop-up.component.spec.ts
+++ src/app/ubs/ubs-user/ubs-user-profile-page/ubs-profile-delete-pop-up/ubs-profile-delete-pop-up.component.spec.ts
@@ -1,31 +1,40 @@
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { TranslateModule } from '@ngx-translate/core';
-import { MatDialog, MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
-
+import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { UbsProfileDeletePopUpComponent } from './ubs-profile-delete-pop-up.component';
+import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
+import { of } from 'rxjs';
 
 describe('UbsProfileDeletePopUpComponent', () => {
   let component: UbsProfileDeletePopUpComponent;
   let fixture: ComponentFixture<UbsProfileDeletePopUpComponent>;
-  const viewModeInputs = {
-    isAddressDelete: {}
+
+  const fakeTitles = {
+    popupTitle: 'popupTitle',
+    popupSubtitle: 'popupSubtitle',
+    popupConfirm: 'popupSubtitle',
+    popupCancel: 'popupSubtitle'
   };
-  class MatDialogMock {
-    open() {
-      return {};
-    }
-  }
+
+  const dialogRefStub = {
+    keydownEvents() {
+      return of();
+    },
+    backdropClick() {
+      return of();
+    },
+    close() {}
+  };
+  const matDialogRef = 'matDialogRef';
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [UbsProfileDeletePopUpComponent],
+      imports: [TranslateModule.forRoot(), MatDialogModule, BrowserDynamicTestingModule],
       providers: [
-        { prodive: MatDialog, useClass: MatDialogMock },
-        { provide: MAT_DIALOG_DATA, useValue: viewModeInputs }
-      ],
-      imports: [TranslateModule.forRoot(), MatDialogModule],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA]
+        { provide: MatDialogRef, useValue: dialogRefStub },
+        { provide: MAT_DIALOG_DATA, useValue: fakeTitles }
+      ]
     }).compileComponents();
   }));
 
@@ -37,5 +46,42 @@ describe('UbsProfileDeletePopUpComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should setTitles be called in ngOnInit', () => {
+    const spy = spyOn(UbsProfileDeletePopUpComponent.prototype as any, 'setTitles');
+    component.ngOnInit();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should keydownEvents be called in ngOnInit', () => {
+    const spy = spyOn(component[matDialogRef], 'keydownEvents').and.returnValue(of());
+    component.ngOnInit();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should backdropClick be called in ngOnInit', () => {
+    const spy = spyOn(component[matDialogRef], 'backdropClick').and.returnValue(of());
+    component.ngOnInit();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should set titles after setTitles method', () => {
+    component.popupTitle = 'fake';
+    component.popupSubtitle = 'fake';
+    component.popupCancel = 'fake';
+    component.popupConfirm = 'fake';
+    const setTitles = 'setTitles';
+    component[setTitles]();
+    expect(component.popupTitle).toBe(fakeTitles.popupTitle);
+    expect(component.popupSubtitle).toBe(fakeTitles.popupSubtitle);
+    expect(component.popupCancel).toBe(fakeTitles.popupCancel);
+    expect(component.popupConfirm).toBe(fakeTitles.popupConfirm);
+  });
+
+  it('should call close on matDialogRef', () => {
+    const spy = spyOn(component[matDialogRef], 'close');
+    component.userReply(true);
+    expect(spy).toHaveBeenCalledWith(true);
   });
 });
