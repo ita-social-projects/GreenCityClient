635503701d2e0c1124e9c2897cd8d0da1736be97
atsa21 <ts21ann@gmail.com>
2023-05-25 11:30:52 +0300

    added unit tests

--- src/app/main/component/user/components/profile/users-friends/friend-dashboard/friend-item/friend-item.component.spec.ts
+++ src/app/main/component/user/components/profile/users-friends/friend-dashboard/friend-item/friend-item.component.spec.ts
@@ -1,5 +1,4 @@
 import { MatDialogModule } from '@angular/material/dialog';
-import { ChatsService } from './../../../../../../../../chat/service/chats/chats.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
@@ -51,5 +50,39 @@ describe('FriendItemComponent', () => {
     const spy = spyOn(component.declineEvent, 'emit');
     component.declineFriend();
     expect(spy).toHaveBeenCalledWith(1);
+  });
+
+  it('should call checkButtons when target is a button', () => {
+    const mockEvent: Partial<MouseEvent> = {
+      target: document.createElement('button')
+    };
+    const spy = spyOn(component as any, 'checkButtons');
+
+    component.clickHandler(mockEvent as MouseEvent);
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should call showMutualFriends when target is a span and userId is not set', () => {
+    const mockEvent: Partial<MouseEvent> = {
+      target: document.createElement('span')
+    };
+    component.userId = null;
+    const spy = spyOn(component as any, 'showMutualFriends');
+
+    component.clickHandler(mockEvent as MouseEvent);
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should call toUsersInfo when target is div', () => {
+    const mockEvent: Partial<MouseEvent> = {
+      target: document.createElement('div')
+    };
+    const spy = spyOn(component as any, 'toUsersInfo');
+
+    component.clickHandler(mockEvent as MouseEvent);
+
+    expect(spy).toHaveBeenCalled();
   });
 });
