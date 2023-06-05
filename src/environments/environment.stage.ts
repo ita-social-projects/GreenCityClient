bbc53159ff4c787a5e6208d7254d28c34d4e87b3
danylohlynskyi <danylohlynsky@gmail.com>
2023-06-05 02:25:52 -0400

    Updated env variables

--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -2,12 +2,12 @@ export const environment = {
   production: true,
   apiKeys: 'AIzaSyDanBliCzAuCZrsq67FeKEs3vqAilUD_is',
   apiMapKey: 'AIzaSyB3xs7Kczo46LFcQRFKPMdrE0lU4qsR_S4',
-  backendLink: 'https://greencity.testgreencity.ga/',
+  backendLink: 'https://greencity.greencity.social/',
   backendChatLink: 'https://greencity-chat.testgreencity.ga/',
-  backendUserLink: 'https://greencity-user.testgreencity.ga/',
-  backendUbsLink: 'https://greencity-ubs.testgreencity.ga',
-  frontendLink: 'https://www.testgreencity.ga/',
-  socket: 'https://greencity.testgreencity.ga/socket',
+  backendUserLink: 'https://greencity-user.greencity.social/',
+  backendUbsLink: 'https://greencity-ubs.greencity.social',
+  frontendLink: 'https://www.greencity.social/',
+  socket: 'https://greencity.greencity.social/socket',
   chatSocket: 'https://greencity-chat.testgreencity.ga/socket',
   firebaseConfig: {
     apiKey: 'AIzaSyDSVxahTHqdffRX2upKIMXCCjEBUYoHX8E',
@@ -20,7 +20,7 @@ export const environment = {
     measurementId: 'G-CCHRKQ4R0S'
   },
   ubsAdmin: {
-    backendUbsAdminLink: 'https://greencity-ubs.testgreencity.ga/ubs'
+    backendUbsAdminLink: 'https://greencity-ubs.greencity.social/ubs'
   },
   googleClientId: '129513550972-eu9ej46rviv1ac8q14at62t2k5qon1pu.apps.googleusercontent.com',
   agmCoreModuleApiKey: 'AIzaSyCdBtR8O8eazfWUSdyb5O2cnL32uJtWUwA'
