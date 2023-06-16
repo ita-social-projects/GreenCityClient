679c7e17fef79aff6f4b11eb3d6b44762cf760a6
Tadei-Mykola <tmukola2006@gmail.com>
2023-06-16 16:36:53 +0300

    bugfix/#5208

--- src/app/ubs/ubs-admin/components/ubs-admin-order-details-form/ubs-admin-order-details-form.component.ts
+++ src/app/ubs/ubs-admin/components/ubs-admin-order-details-form/ubs-admin-order-details-form.component.ts
@@ -336,6 +336,10 @@ export class UbsAdminOrderDetailsFormComponent implements OnInit, OnChanges {
     return currentAmountOfNumbersFromShop < this.LIMIT_OF_ECO_SHOP_NUMBERS;
   }
 
+  checkEmptyInput(value: string): boolean {
+    return value.length === 0;
+  }
+
   addOrderNumberFromShop(): void {
     const arr = this.orderDetailsForm.controls.storeOrderNumbers as FormArray;
     arr.push(new FormControl('', [Validators.required, Validators.pattern(Patterns.ordersPattern)]));
