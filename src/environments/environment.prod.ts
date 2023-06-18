dbd92ac4b9dd3eb893e619a2c7cba5e71216a9d2
Skyere <70713060+Skyere@users.noreply.github.com>
2023-06-18 13:39:48 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiKeys: 'AIzaSyAkfqpSGHVE28AjQoPcZFZd8AaPB_RU39k',
-  apiMapKey: 'AIzaSyCU0ArzZlZ3n0pLq4o9MJy29LPT5DBMk4Y',
+  apiMapKey: 'AIzaSyCHj-foTxtWg0hud8sbF6lPTmT0BYdAwFE',
   backendLink: 'https://greencity.pick-up.city/',
   backendChatLink: 'https://greencity-chat.pick-up.city/',
   backendUserLink: 'https://greencity-user.pick-up.city/',
