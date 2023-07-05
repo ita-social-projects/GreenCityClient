4cfacf98b916f543194503149f6c9e4c6e6b752c
Oksana Filipchuk <oksanafilipchuk@MacBook-Air-Oksana.local>
2023-07-06 00:47:21 +0300

    fix pipe

--- src/app/shared/correct-unit-pipe/correct-unit.pipe.ts
+++ src/app/shared/correct-unit-pipe/correct-unit.pipe.ts
@@ -5,18 +5,18 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class CorrectUnitPipe implements PipeTransform {
   transform(unit: string, value: number, lang: string): string {
+    let result = unit;
     if (lang === 'ua') {
-      let result = unit;
       if (value % 10 === 1 && value !== 11) {
-        result = result += '.singular';
+        result += '.singular';
       } else if ([2, 3, 4].some((el) => el === value % 10)) {
-        result = result += '.plural.units-2-3-4';
+        result += '.plural.units-2-3-4';
       } else {
-        result = result += '.plural.units-more-5';
+        result += '.plural.units-more-5';
       }
       return result;
     }
-
-    return value === 1 ? (unit += '.singular') : (unit += '.plural');
+    result = value === 1 ? `${unit}.singular` : `${unit}.plural`;
+    return result;
   }
 }
