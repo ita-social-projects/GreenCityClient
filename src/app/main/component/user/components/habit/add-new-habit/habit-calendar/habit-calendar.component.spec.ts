4b14e4bed92ada2d3568ecd198acb2edf53a040f
Oksana Filipchuk <oksanafilipchuk@MacBook-Air-Oksana.local>
2023-07-05 11:35:46 +0300

    added tests to habit calendar

---
+++ src/app/main/component/user/components/habit/add-new-habit/habit-calendar/habit-calendar.component.spec.ts
@@ -0,0 +1,84 @@
+import { HabitCalendarComponent } from './habit-calendar.component';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TranslateService } from '@ngx-translate/core';
+import { CalendarBaseComponent } from '@shared/components';
+import { LanguageService } from 'src/app/main/i18n/language.service';
+import { Injectable, EventEmitter } from '@angular/core';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatDialogModule } from '@angular/material/dialog';
+import { CalendarInterface } from '@global-user/components/profile/calendar/calendar-interface';
+import { By } from '@angular/platform-browser';
+import { Observable, Subject } from 'rxjs';
+import { fakeAsync, tick } from '@angular/core/testing';
+
+@Injectable()
+class TranslationServiceStub {
+  private subject = new Subject();
+  public onDefaultLangChange = new EventEmitter<any>();
+  public unsubscribe = new EventEmitter<any>();
+  public getTranslation() {
+    return this.subject.asObservable();
+  }
+  public getDefaultLang() {}
+}
+
+describe('HabitCalendarComponent', () => {
+  let component: HabitCalendarComponent;
+  let fixture: ComponentFixture<HabitCalendarComponent>;
+
+  beforeEach(async () => {
+    TestBed.configureTestingModule({
+      declarations: [HabitCalendarComponent, CalendarBaseComponent],
+      imports: [HttpClientTestingModule, MatDialogModule],
+      providers: [
+        { provide: TranslateService, useClass: TranslationServiceStub },
+        { provide: LanguageService, useValue: {} }
+      ]
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HabitCalendarComponent);
+    component = fixture.componentInstance;
+    component.calendarDay = [
+      {
+        date: new Date('Tue Jul 04 2023 10:49:04 GMT+0300'),
+        numberOfDate: 2,
+        year: 2023,
+        month: 6,
+        firstDay: 5,
+        dayName: '',
+        totalDaysInMonth: 31,
+        hasHabitsInProgress: true,
+        areHabitsDone: false,
+        isCurrentDayActive: false
+      }
+    ];
+    component.monthView = true;
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should show habits after clicking on day item', fakeAsync(() => {
+    const spy = spyOn(component, 'showHabits');
+    fixture.detectChanges();
+    const dayEl = fixture.debugElement.query(By.css('.calendar-grid-day'));
+    dayEl.triggerEventHandler('click', null);
+    tick();
+    fixture.detectChanges();
+
+    expect(spy).toHaveBeenCalled();
+  }));
+
+  it('should show habits if day has habits in progress', fakeAsync(() => {
+    const spy = spyOn(component, 'openDialogDayHabits');
+    fixture.detectChanges();
+    const dayEl = fixture.debugElement.query(By.css('.calendar-grid-day'));
+    dayEl.triggerEventHandler('click', null);
+    tick();
+    fixture.detectChanges();
+    expect(spy).not.toHaveBeenCalled();
+  }));
+});
