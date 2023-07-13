d534c801706abacee003a23664d7c390e1f2fa04
Марта <marta@MacBook-Air-Marta.local>
2023-07-13 11:34:44 +0300

    removed type annotation

--- src/app/main/component/events/services/events.service.ts
+++ src/app/main/component/events/services/events.service.ts
@@ -84,14 +84,14 @@ export class EventsService implements OnDestroy {
   }
 
   getDistanceFromLatLonInKm(lat1: number, lon1: number, lat2: number, lon2: number): number {
-    const R: number = 6371; // Radius of the earth in km
-    const dLat: number = this.deg2rad(lat2 - lat1); // deg2rad below
-    const dLon: number = this.deg2rad(lon2 - lon1);
-    const a: number =
+    const R = 6371; // Radius of the earth in km
+    const dLat = this.deg2rad(lat2 - lat1); // deg2rad below
+    const dLon = this.deg2rad(lon2 - lon1);
+    const a =
       Math.sin(dLat / 2) * Math.sin(dLat / 2) +
       Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
-    const c: number = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    const d: number = R * c; // Distance in km
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const d = R * c; // Distance in km
     return d; // distance returned
   }
 
