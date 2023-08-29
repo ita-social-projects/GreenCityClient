b95be4fcce0dc7b2efcf7cebe4fd73b55b089566
Марта <marta@MacBook-Air-Marta.local>
2023-08-29 12:04:54 +0300

    added custom tooltip directive

--- src/app/main/component/user/components/shared/user-shared.module.ts
+++ src/app/main/component/user/components/shared/user-shared.module.ts
@@ -6,10 +6,10 @@ import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { CommonModule } from '@angular/common';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { UserProfileImageComponent } from './components/user-profile-image/user-profile-image.component';
-import { HoverTextDirective } from './hover-text.directive';
+import { CustomTooltipDirective } from './custom-tooltip.directive';
 
 @NgModule({
-  declarations: [HabitsGalleryViewComponent, UserProfileImageComponent, HoverTextDirective],
+  declarations: [HabitsGalleryViewComponent, UserProfileImageComponent, CustomTooltipDirective],
   imports: [
     CommonModule,
     TranslateModule.forChild({
@@ -21,7 +21,7 @@ import { HoverTextDirective } from './hover-text.directive';
     }),
     MDBBootstrapModule
   ],
-  exports: [TranslateModule, MDBBootstrapModule, HabitsGalleryViewComponent, UserProfileImageComponent, HoverTextDirective]
+  exports: [TranslateModule, MDBBootstrapModule, HabitsGalleryViewComponent, UserProfileImageComponent, CustomTooltipDirective]
 })
 export class UserSharedModule {}
 
