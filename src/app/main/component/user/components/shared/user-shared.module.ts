7318edbc72e1bd7185179541bfbfc442437d2439
Марта <marta@MacBook-Air-Marta.local>
2023-08-30 15:04:14 +0300

    remove comments

--- src/app/main/component/user/components/shared/user-shared.module.ts
+++ src/app/main/component/user/components/shared/user-shared.module.ts
@@ -6,7 +6,6 @@ import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { CommonModule } from '@angular/common';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { UserProfileImageComponent } from './components/user-profile-image/user-profile-image.component';
-//import { MaxTextLengthPipe } from 'src/app/ubs/ubs-admin/components/shared/max-text-length/max-text-length.pipe';
 import { CustomTooltipDirective } from './directives/custom-tooltip/custom-tooltip.directive';
 
 @NgModule({
@@ -22,14 +21,7 @@ import { CustomTooltipDirective } from './directives/custom-tooltip/custom-toolt
     }),
     MDBBootstrapModule
   ],
-  exports: [
-    TranslateModule,
-    MDBBootstrapModule,
-    HabitsGalleryViewComponent,
-    UserProfileImageComponent,
-    // MaxTextLengthPipe,
-    CustomTooltipDirective
-  ]
+  exports: [TranslateModule, MDBBootstrapModule, HabitsGalleryViewComponent, UserProfileImageComponent, CustomTooltipDirective]
 })
 export class UserSharedModule {}
 
