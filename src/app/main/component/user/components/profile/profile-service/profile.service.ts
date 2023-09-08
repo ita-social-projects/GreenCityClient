b8eb69af458ff8abeaf386571ec09d1f529d047a
Марта <marta@MacBook-Air-Marta.local>
2023-09-08 14:24:24 +0300

    added enum with rate points

--- src/app/main/component/user/components/profile/profile-service/profile.service.ts
+++ src/app/main/component/user/components/profile/profile-service/profile.service.ts
@@ -2,12 +2,14 @@ import { EcoPlaces } from '@user-models/ecoPlaces.model';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { CardModel } from '@user-models/card.model';
-import { HttpClient } from '@angular/common/http';
+import { UserRatingModel } from '@global-user/models/rating.model';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { LocalStorageService } from '@global-service/localstorage/local-storage.service';
 import { ProfileStatistics } from '@user-models/profile-statistiscs';
 import { EditProfileModel } from '@user-models/edit-profile.model';
 import { LanguageService } from 'src/app/main/i18n/language.service';
 import { mainLink, mainUserLink } from '../../../../../links';
+import { dataTypes } from 'src/app/main/component/comments/models/comments-model';
 
 @Injectable({
   providedIn: 'root'
@@ -24,6 +26,12 @@ export class ProfileService {
     instagram: './assets/img/icon/instagram-icon.svg',
     twitter: './assets/img/icon/twitter-icon.svg',
     youtube: './assets/img/icon/youtube-icon.svg'
+  };
+
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
   };
 
   constructor(private http: HttpClient, private localStorageService: LocalStorageService, private languageService: LanguageService) {}
