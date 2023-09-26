8894d84e359b21d66990005056a7177312565018
Pavlo Sobkiv <pavlo.sobkiv@gmail.com>
2023-09-26 12:45:09 +0300

    Done refactoring

--- src/app/main/component/user/components/profile/users-achievements/users-achievements.component.ts
+++ src/app/main/component/user/components/profile/users-achievements/users-achievements.component.ts
@@ -8,18 +8,11 @@ import { PROFILE_IMAGES } from 'src/app/main/image-pathes/profile-images';
 })
 export class UsersAchievementsComponent implements OnChanges {
   public achievementsImages = PROFILE_IMAGES.achs;
-  public ukrLang: boolean;
-  public engLang: boolean;
+  public isEngLang: boolean;
 
   @Input() currLang: string;
 
   ngOnChanges(): void {
-    if (this.currLang === 'en') {
-      this.engLang = true;
-      this.ukrLang = false;
-    } else {
-      this.ukrLang = true;
-      this.engLang = false;
-    }
+    this.isEngLang = this.currLang === 'en';
   }
 }
