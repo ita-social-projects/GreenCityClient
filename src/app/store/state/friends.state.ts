03ad8381d261e6082063f97ebe80f22c5b1aa1a8
Kateryna <katyakravchuk123@gmail.com>
2023-10-09 13:07:51 +0300

    added changes to state

--- src/app/store/state/friends.state.ts
+++ src/app/store/state/friends.state.ts
@@ -3,8 +3,6 @@ import { FriendArrayModel, FriendModel } from '@global-user/models/friend.model'
 export interface IFriendState {
   FriendList: FriendArrayModel | null;
   FriendRequestList: FriendArrayModel | null;
-  FriendAcceptedList: FriendModel[] | null;
-  FriendDeclinedList: FriendModel[] | null;
   FriendsStayInRequestList: FriendModel[] | null;
   FriendsStayInFriendsList: FriendModel[] | null;
   error: string | null;
@@ -13,8 +11,6 @@ export interface IFriendState {
 export const initialFriendState: IFriendState = {
   FriendList: null,
   FriendRequestList: null,
-  FriendAcceptedList: null,
-  FriendDeclinedList: null,
   FriendsStayInRequestList: null,
   FriendsStayInFriendsList: null,
   error: null
