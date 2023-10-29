2b4661e34c6bb80741ccf0168ed8020ad6721fa0
igorv404 <igorvilkov404@gmail.com>
2023-10-30 01:32:50 +0200

    fixed mistaken cleaning of To-do list in my habit edit

--- src/app/main/component/user/components/habit/add-new-habit/habit-edit-shopping-list/habit-edit-shopping-list.component.ts
+++ src/app/main/component/user/components/habit/add-new-habit/habit-edit-shopping-list/habit-edit-shopping-list.component.ts
@@ -57,6 +57,7 @@ export class HabitEditShoppingListComponent implements OnInit, AfterViewChecked,
   ngAfterViewChecked(): void {
     if (this.shopList && this.isEditing) {
       this.shopList = this.shopList.map((el) => ({ ...el, selected: el.status === TodoStatus.inprogress }));
+      this.newList.emit(this.shopList);
     }
     this.placeItemInOrder();
     this.cdr.detectChanges();
