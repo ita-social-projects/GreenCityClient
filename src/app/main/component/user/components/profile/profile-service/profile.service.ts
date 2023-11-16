8bf9a2319e9122664df2fc3ef506452efeaf456a
ShirayDan <dan.shirayy@gmail.com>
2023-11-16 15:49:03 +0200

    fixed solution according to the object access via string literals is disallowed

--- src/app/main/component/user/components/profile/profile-service/profile.service.ts
+++ src/app/main/component/user/components/profile/profile-service/profile.service.ts
@@ -55,9 +55,9 @@ export class ProfileService {
   public getSocialImage(socialNetwork: string): string {
     const value = socialNetwork;
     const icons = JSON.parse(JSON.stringify(this.icons));
-    delete icons['edit'];
-    delete icons['add'];
-    delete icons['delete'];
+    delete icons.edit;
+    delete icons.add;
+    delete icons.delete;
     let imgPath = icons.defaultIcon;
     Object.keys(icons).forEach((icon) => {
       if (value.toLowerCase().includes(icon)) {
