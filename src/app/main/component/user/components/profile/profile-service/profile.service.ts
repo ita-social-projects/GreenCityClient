846e1ec101ebaa5c5ced2d1b8b18c84c13bdf003
ShirayDan <dan.shirayy@gmail.com>
2023-11-19 18:36:50 +0200

    removing unused constant

--- src/app/main/component/user/components/profile/profile-service/profile.service.ts
+++ src/app/main/component/user/components/profile/profile-service/profile.service.ts
@@ -53,14 +53,13 @@ export class ProfileService {
   }
 
   public getSocialImage(socialNetwork: string): string {
-    const value = socialNetwork;
     const icons = JSON.parse(JSON.stringify(this.icons));
     delete icons.edit;
     delete icons.add;
     delete icons.delete;
     let imgPath = icons.defaultIcon;
     Object.keys(icons).forEach((icon) => {
-      if (value.toLowerCase().includes(icon)) {
+      if (socialNetwork.toLowerCase().includes(icon)) {
         imgPath = icons[icon];
       }
     });
