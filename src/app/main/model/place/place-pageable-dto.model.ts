5085607457213511c2a369c3db54c779506570ef
lemoncat <xar4ak7even5@gmail.com>
2023-11-23 09:40:19 +0200

    remove unused code

--- src/app/main/model/place/place-pageable-dto.model.ts
+++
@@ -1,7 +0,0 @@
-import { AdminPlace } from '../../component/admin/models/admin-place.model';
-
-export class PlacePageableDto {
-  page: AdminPlace[];
-  totalElements: number;
-  currentPage: number;
-}
