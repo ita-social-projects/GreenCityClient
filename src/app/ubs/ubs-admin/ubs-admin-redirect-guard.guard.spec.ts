d502e8a5b81765bd9f14a5b7aac8e04641131d2c
ShirayDan <dan.shirayy@gmail.com>
2023-11-29 13:32:10 +0200

    fixing tests

--- src/app/ubs/ubs-admin/ubs-admin-redirect-guard.guard.spec.ts
+++ src/app/ubs/ubs-admin/ubs-admin-redirect-guard.guard.spec.ts
@@ -1,22 +1,24 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { JwtService } from '@global-service/jwt/jwt.service';
 import { BehaviorSubject } from 'rxjs';
-
+import { Router } from '@angular/router';
 import { UbsAdminRedirectGuard } from './ubs-admin-redirect-guard.guard';
 
 describe('UbsAdminRedirectGuard', () => {
   let redirectGuard: UbsAdminRedirectGuard;
-
+  let router: Router;
   let jwtServiceMock: JwtService;
   jwtServiceMock = jasmine.createSpyObj('JwtService', ['getUserRole']);
   jwtServiceMock.getUserRole = () => 'true';
   jwtServiceMock.userRole$ = new BehaviorSubject('test');
-
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [{ provide: JwtService, useValue: jwtServiceMock }]
+      imports: [RouterTestingModule],
+      providers: [redirectGuard, { provide: JwtService, useValue: jwtServiceMock }]
     });
     redirectGuard = TestBed.inject(UbsAdminRedirectGuard);
+    router = TestBed.inject(Router);
   });
 
   it('should be created', () => {
