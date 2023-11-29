00aafe6cee7be16667f64d62776cda5c4367cfb6
ShirayDan <dan.shirayy@gmail.com>
2023-11-29 13:54:43 +0200

    fixing tests 2

--- src/app/ubs/ubs-admin/ubs-admin-redirect-guard.guard.spec.ts
+++ src/app/ubs/ubs-admin/ubs-admin-redirect-guard.guard.spec.ts
@@ -1,27 +1,40 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { JwtService } from '@global-service/jwt/jwt.service';
-import { BehaviorSubject } from 'rxjs';
-import { Router } from '@angular/router';
 import { UbsAdminRedirectGuard } from './ubs-admin-redirect-guard.guard';
+import { JwtService } from '@global-service/jwt/jwt.service';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 
 describe('UbsAdminRedirectGuard', () => {
-  let redirectGuard: UbsAdminRedirectGuard;
+  let guard: UbsAdminRedirectGuard;
   let router: Router;
-  let jwtServiceMock: JwtService;
-  jwtServiceMock = jasmine.createSpyObj('JwtService', ['getUserRole']);
-  jwtServiceMock.getUserRole = () => 'true';
-  jwtServiceMock.userRole$ = new BehaviorSubject('test');
+  let jwtService: JwtService;
+
+  const routerSpy = { navigate: jasmine.createSpy('navigate') };
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [RouterTestingModule],
-      providers: [redirectGuard, { provide: JwtService, useValue: jwtServiceMock }]
+      providers: [UbsAdminRedirectGuard, JwtService, { provide: Router, useValue: routerSpy }]
     });
-    redirectGuard = TestBed.inject(UbsAdminRedirectGuard);
+    guard = TestBed.inject(UbsAdminRedirectGuard);
     router = TestBed.inject(Router);
+    jwtService = TestBed.inject(JwtService);
   });
 
-  it('should be created', () => {
-    expect(redirectGuard).toBeTruthy();
+  it('should allow access for a non-admin user', () => {
+    spyOn(jwtService, 'getUserRole').and.returnValue('ROLE_USER');
+    const result = guard.canActivate(new ActivatedRouteSnapshot(), <RouterStateSnapshot>{ url: '' });
+
+    expect(result).toBeTruthy();
+  });
+
+  it('should redirect to /ubs-admin/orders for an admin user', () => {
+    spyOn(jwtService, 'getUserRole').and.returnValue('ROLE_UBS_EMPLOYEE');
+
+    const navigateSpy = spyOn(router, 'navigate');
+    const result = guard.canActivate(new ActivatedRouteSnapshot(), <RouterStateSnapshot>{ url: '' });
+
+    expect(result).toBeFalsy();
+    expect(navigateSpy).toHaveBeenCalledWith(['ubs-admin', 'orders']);
   });
 });
