e8b5a9bbe9c67284299f207673dafc88cd709876
ShirayDan <dan.shirayy@gmail.com>
2023-12-15 18:25:45 +0200

    added some tests

--- src/app/main/component/user/components/shared/directives/custom-tooltip.directive.spec.ts
+++ src/app/main/component/user/components/shared/directives/custom-tooltip.directive.spec.ts
@@ -1,19 +1,71 @@
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
+// import { Component, DebugElement } from '@angular/core';
+// import { By } from '@angular/platform-browser';
+// import { CustomTooltipDirective } from './custom-tooltip.directive';
+
+// class TooltipServiceMock {
+//   showTooltip() {}
+//   hide() {}
+// }
+
+// @Component({
+//   template: ` <div [appCustomTooltip]="tooltipContent" [tooltip]="tooltipService" [font]="font"></div> `
+// })
+// class TestComponent {
+//   tooltipContent = 'This is a tooltip';
+//   tooltipService = new TooltipServiceMock();
+//   font = '12px Arial';
+// }
+
+// describe('CustomTooltipDirective', () => {
+//   let component: TestComponent;
+//   let fixture: ComponentFixture<TestComponent>;
+//   let directiveElement: DebugElement;
+
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [CustomTooltipDirective, TestComponent]
+//     });
+
+//     fixture = TestBed.createComponent(TestComponent);
+//     component = fixture.componentInstance;
+//     directiveElement = fixture.debugElement.query(By.directive(CustomTooltipDirective));
+
+//     fixture.detectChanges();
+//   });
+
+//   it('should create tooltip on mouseover', () => {
+//     const hostElement = directiveElement.query(By.directive(CustomTooltipDirective)).nativeElement;
+//     hostElement.dispatchEvent(new MouseEvent('mouseover'));
+//     fixture.detectChanges();
+//     const tooltipElement = hostElement.querySelector('.tooltipClass');
+//     expect(tooltipElement).toBeTruthy();
+//   });
+
+//   it('should hide tooltip on mouse leave', () => {
+//     const tooltipSpy = spyOn(component.tooltipService, 'hide');
+//     directiveElement.triggerEventHandler('mouseleave', new MouseEvent('mouseleave', { bubbles: true }));
+
+//     fixture.detectChanges();
+
+//     expect(tooltipSpy).toHaveBeenCalled();
+//   });
+// });
+
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Component, DebugElement } from '@angular/core';
 import { By } from '@angular/platform-browser';
 import { CustomTooltipDirective } from './custom-tooltip.directive';
 
-class TooltipServiceMock {
-  show() {}
-  hide() {}
-}
-
 @Component({
-  template: ` <div [appCustomTooltip]="tooltipContent" [tooltip]="tooltipService" [font]="font"></div> `
+  template: `<div appCustomTooltip [tooltipContent]="tooltipContent" [tooltip]="tooltip" [font]="font"></div>`
 })
 class TestComponent {
-  tooltipContent = 'This is a tooltip';
-  tooltipService = new TooltipServiceMock();
+  tooltipContent = 'Test Tooltip';
+  tooltip = {
+    showTooltip: jasmine.createSpy('showTooltip'),
+    hide: jasmine.createSpy('hide')
+  };
   font = '16px Arial';
 }
 
@@ -25,21 +77,28 @@ describe('CustomTooltipDirective', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [CustomTooltipDirective, TestComponent]
-    });
+    }).compileComponents();
 
     fixture = TestBed.createComponent(TestComponent);
     component = fixture.componentInstance;
     directiveElement = fixture.debugElement.query(By.directive(CustomTooltipDirective));
-
     fixture.detectChanges();
   });
 
-  it('should hide tooltip on mouse leave', () => {
-    const tooltipSpy = spyOn(component.tooltipService, 'hide');
-    directiveElement.triggerEventHandler('mouseleave', new MouseEvent('mouseleave', { bubbles: true }));
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 
+  it('should hide tooltip on mouse enter if text width does not exceed container width', () => {
+    const eventMock = { target: { offsetWidth: 200, innerText: 'Some text' } };
+    directiveElement.triggerEventHandler('mouseenter', eventMock);
     fixture.detectChanges();
+    expect(component.tooltip.hide).toHaveBeenCalled();
+  });
 
-    expect(tooltipSpy).toHaveBeenCalled();
+  it('should hide tooltip on mouse leave', () => {
+    directiveElement.triggerEventHandler('mouseleave', null);
+    fixture.detectChanges();
+    expect(component.tooltip.hide).toHaveBeenCalled();
   });
 });
