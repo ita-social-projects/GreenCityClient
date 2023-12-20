6e64c6d7b1295bf80db601b1aa0fd73c43c001e5
Pavlo Sobkiv <pavlo.sobkiv@gmail.com>
2023-12-20 16:57:31 +0200

    Improved readability

--- src/app/main/component/events/components/event-date-time-picker/event-date-time-picker.component.ts
+++ src/app/main/component/events/components/event-date-time-picker/event-date-time-picker.component.ts
@@ -113,9 +113,6 @@ export class EventDateTimePickerComponent implements OnInit, OnChanges, OnDestro
       this.readyToApplyLocation = true;
       this.setDataEditing();
     }
-    if (this.appliedForAllLocations) {
-      this.applyLocationForAllDays();
-    }
 
     this.langService.getCurrentLangObs().subscribe((_) => {
       this.getCoordinates();
