2f85f5ff579bb293ef9d6fa8c0d22a818f9d9dd8
Kamila <jonjungkook66@gmail.com>
2023-12-27 19:57:16 +0200

    fixed bug with empty messages

---
+++ src/app/chat/component/chat/chat.component.spec.ts
@@ -0,0 +1,54 @@
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { ChatComponent } from './chat.component';
+import { ChatsService } from '../../service/chats/chats.service';
+import { SocketService } from '../../service/socket/socket.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+
+describe('ChatComponent', () => {
+  let component: ChatComponent;
+  let fixture: ComponentFixture<ChatComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ChatComponent ],
+      imports: [HttpClientTestingModule],
+      providers: [
+        ChatsService,
+        SocketService,
+      ],
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ChatComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should toggle showEmojiPicker on toggleEmojiPicker method', () => {
+    component.showEmojiPicker = false;
+
+    component.toggleEmojiPicker();
+    expect(component.showEmojiPicker).toBe(true);
+
+    component.toggleEmojiPicker();
+    expect(component.showEmojiPicker).toBe(false);
+  });
+
+  it('should add emoji to messageControl on addEmoji method', () => {
+    const emojiEvent = { emoji: { native: '😊' } };
+    const initialMessage = 'Hello';
+
+    component.messageControl.setValue(initialMessage);
+
+    component.addEmoji(emojiEvent);
+    const expectedMessage = initialMessage + emojiEvent.emoji.native;
+
+    expect(component.messageControl.value).toBe(expectedMessage);
+  });
+});
