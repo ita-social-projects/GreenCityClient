760089192ca18dfa1a0f4832acbb623caf20b696
ShirayDan <dan.shirayy@gmail.com>
2024-01-18 16:30:43 +0200

    removed commited code

--- src/app/main/component/user/components/habit/add-new-habit/habit-duration/habit-duration.component.ts
+++ src/app/main/component/user/components/habit/add-new-habit/habit-duration/habit-duration.component.ts
@@ -1,65 +1,4 @@
-// import { Component, ElementRef, EventEmitter, Input, OnInit, Output } from '@angular/core';
-// import { LanguageService } from 'src/app/main/i18n/language.service';
-// import { MatSliderChange } from '@angular/material/slider';
-// import { Subscription } from 'rxjs';
-// import { TranslateService } from '@ngx-translate/core';
-// @Component({
-//   selector: 'app-habit-duration',
-//   templateUrl: './habit-duration.component.html',
-//   styleUrls: ['./habit-duration.component.scss']
-// })
-// export class HabitDurationComponent implements OnInit {
-//   @Input() habitDurationInitial: number;
-//   @Output() changeDuration = new EventEmitter<number>();
-//   public newDuration = 7;
-//   public currentLang = '';
-//   public thumbTextEl;
-//   public langChangeSub: Subscription;
-//   public days = 'd';
-
-//   constructor(private elm: ElementRef, private langService: LanguageService, public translate: TranslateService) {}
-
-//   ngOnInit() {
-//     this.subscribeToLangChange();
-//     this.newDuration = this.habitDurationInitial;
-//     console.log(this.newDuration);
-
-//     this.currentLang = this.langService.getCurrentLanguage();
-//     this.thumbTextEl = this.elm.nativeElement.getElementsByClassName('mat-slider-thumb-label-text')[0];
-
-//     if (this.currentLang === 'ua') {
-//       this.thumbTextEl.textContent = this.newDuration + 'дн';
-//     } else {
-//       this.thumbTextEl.textContent = this.newDuration + 'd';
-//     }
-//   }
-
-//   ngOnDestroy() {
-//     if (this.langChangeSub) {
-//       this.langChangeSub.unsubscribe();
-//     }
-//   }
-
-//   public updateDuration() {
-//     this.changeDuration.emit(this.newDuration);
-//   }
-
-//   public updateInput(event) {
-//     const sliderChangeEvent = event as MatSliderChange;
-//     const newSliderValue = sliderChangeEvent.value;
-//     this.thumbTextEl.textContent = newSliderValue + this.days;
-//   }
-
-//   public subscribeToLangChange(): void {
-//     this.langChangeSub = this.translate.onDefaultLangChange.subscribe((res) => {
-//       const translations = res.translations;
-//       this.days = translations.user.habit.duration.day;
-//       this.thumbTextEl.textContent = this.newDuration + this.days;
-//     });
-//   }
-// }
-
-import { Component, ElementRef, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, OnDestroy } from '@angular/core';
 import { LanguageService } from 'src/app/main/i18n/language.service';
 import { MatSliderChange } from '@angular/material/slider';
 import { Subscription } from 'rxjs';
@@ -69,7 +8,7 @@ import { TranslateService } from '@ngx-translate/core';
   templateUrl: './habit-duration.component.html',
   styleUrls: ['./habit-duration.component.scss']
 })
-export class HabitDurationComponent implements OnInit {
+export class HabitDurationComponent implements OnInit, OnDestroy {
   @Input() habitDurationInitial: number;
   @Output() changeDuration = new EventEmitter<number>();
   public newDuration = 7;
