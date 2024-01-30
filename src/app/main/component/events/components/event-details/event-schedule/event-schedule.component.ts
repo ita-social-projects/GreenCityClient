a946d9a51f98ed4ea56e36d4d981ad4f1c078dc8
ShirayDan <dan.shirayy@gmail.com>
2024-01-30 17:42:25 +0200

    added opportunity to see the link in pop up

--- src/app/main/component/events/components/event-details/event-schedule/event-schedule.component.ts
+++ src/app/main/component/events/components/event-details/event-schedule/event-schedule.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { LanguageService } from 'src/app/main/i18n/language.service';
 import { EventsService } from '../../../services/events.service';
 import { Coordinates } from '../../../models/events.interface';
@@ -11,15 +11,12 @@ export class EventScheduleComponent {
   icons = {
     clock: 'assets/img/events/clock.svg',
     location: 'assets/img/events/location.svg',
-    ellipsis: 'assets/img/events/ellipsis.svg'
+    ellipsis: 'assets/img/events/ellipsis.svg',
+    link: 'assets/img/events/link.svg'
   };
 
   @Input() days = [];
   constructor(private langService: LanguageService, public eventService: EventsService) {}
-
-  public getLangValue(uaValue: string, enValue: string): string {
-    return this.langService.getLangValue(uaValue, enValue) as string;
-  }
 
   public getAddress(location: Coordinates): string {
     return this.eventService.getFormattedAddress(location);
