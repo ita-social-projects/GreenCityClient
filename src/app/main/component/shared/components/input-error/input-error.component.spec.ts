d05cbaeaa329dc26a94bd1e53e49b93bee7007c8
ShirayDan <dan.shirayy@gmail.com>
2024-02-07 16:33:13 +0200

    add some tests

--- src/app/main/component/shared/components/input-error/input-error.component.spec.ts
+++ src/app/main/component/shared/components/input-error/input-error.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { InputErrorComponent } from './input-error.component';
 import { TranslateModule } from '@ngx-translate/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
 
 describe('InputErrorComponent', () => {
   let component: InputErrorComponent;
@@ -32,5 +32,60 @@ describe('InputErrorComponent', () => {
     const spy = spyOn(component, 'getType');
     component.ngOnInit();
     expect(spy).toHaveBeenCalled();
+  });
+
+  it('should display required error message if form control is empty and isEvent is true', () => {
+    component.formElement = new FormControl('', Validators.required);
+    component.isEvent = true;
+    component.date = false;
+    component.getType();
+    expect(component.errorMessage).toEqual('create-event.title-required');
+  });
+
+  it('should display required error message if form control is empty and isStartEventError, numberDate, date are true', () => {
+    component.formElement = new FormControl('', Validators.required);
+    component.isStartEventError = true;
+    component.numberDate = true;
+    component.date = true;
+    component.getType();
+    expect(component.errorMessage).toEqual('create-event.start-time-end-time-and-datepicker-not-correct');
+  });
+
+  it('should display required error message if form control is empty and numberDate, date are true', () => {
+    component.formElement = new FormControl('', Validators.required);
+    component.numberDate = true;
+    component.date = true;
+    component.getType();
+    expect(component.errorMessage).toEqual('create-event.datepicker-not-correct-date-required');
+  });
+
+  it('should display required error message if form control is empty and isStartEventError, date are true', () => {
+    component.formElement = new FormControl('', Validators.required);
+    component.isStartEventError = true;
+    component.date = true;
+    component.getType();
+    expect(component.errorMessage).toEqual('create-event.start-time-and-end-time-not-correct');
+  });
+
+  it('should display required error message if form control is empty and isStartEventError, numberDate are true', () => {
+    component.formElement = new FormControl('', Validators.required);
+    component.isStartEventError = true;
+    component.numberDate = true;
+    component.getType();
+    expect(component.errorMessage).toEqual('create-event.datepicker-and-start-time-not-correct');
+  });
+
+  it('should display required error message if form control is empty and isStartEventError is true', () => {
+    component.formElement = new FormControl('', Validators.required);
+    component.isStartEventError = true;
+    component.getType();
+    expect(component.errorMessage).toEqual('create-event.start-time-required');
+  });
+
+  it('should display required error message if form control is empty and numberDate is true', () => {
+    component.formElement = new FormControl('', Validators.required);
+    component.numberDate = true;
+    component.getType();
+    expect(component.errorMessage).toEqual('create-event.datepicker-not-correct');
   });
 });
