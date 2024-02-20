def85ccb90dde26a7e0c090920cfb91ee2f79dc4
Oksana Filipchuk <filipchuk.oksana@gmail.com>
2024-02-21 00:05:35 +0200

    add user notification service

---
+++ src/app/main/component/user/models/notification.model.ts
@@ -0,0 +1,27 @@
+export interface NotificationArrayModel {
+  currentPage: number;
+  first: boolean;
+  hasNext: boolean;
+  hasPrevious: boolean;
+  last: boolean;
+  number: number;
+  page: NotificationModel[];
+  totalElements: number;
+  totalPages: number;
+}
+
+export interface NotificationModel {
+  actionUserId: number;
+  actionUserText: string;
+  bodyText: string;
+  message: string;
+  notificationId: number;
+  notificationType: string;
+  projectName: string;
+  secondMessage: string;
+  secondMessageId: number;
+  targetId: number;
+  time: any;
+  titleText: string;
+  viewed: boolean;
+}
