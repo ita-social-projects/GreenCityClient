b88bc45da017d5fc8033a55474b2f3642aadad68
Oksana Filipchuk <filipchuk.oksana@gmail.com>
2024-03-29 16:31:54 +0200

    add user-socket url to environment file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,8 @@ export const environment = {
   backendUserLink: 'https://greencity-user.greencity.social/',
   backendUbsLink: 'https://greencity-ubs.greencity.social',
   frontendLink: 'https://www.greencity.social/',
-  socket: 'https://greencity-user.greencity.social/socket',
+  socket: 'https://greencity.greencity.social/socket',
+  userSocket: 'https://greencity-user.greencity.social/socket',
   chatSocket: 'https://greencity-chat.greencity.social/socket',
   ubsAdmin: {
     backendUbsAdminLink: 'https://greencity-ubs.greencity.social/ubs'
