b6be5badfcececc75b93ff348cf8c05fab0f0769
Olena Voitsekhivska <mirkbog@gmail.com>
2024-04-17 02:12:46 +0300

    css refactoring

--- src/app/main/component/about/components/vision-card/vision-card.component.spec.ts
+++ src/app/main/component/about/components/vision-card/vision-card.component.spec.ts
@@ -1,6 +1,35 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { VisionCardComponent } from './vision-card.component';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { CUSTOM_ELEMENTS_SCHEMA, EventEmitter, Injectable, NO_ERRORS_SCHEMA } from '@angular/core';
+import { of } from 'rxjs';
+
+@Injectable()
+class TranslationServiceStub {
+  public onLangChange = new EventEmitter<any>();
+  public onTranslationChange = new EventEmitter<any>();
+  public onDefaultLangChange = new EventEmitter<any>();
+  public addLangs(langs: string[]) {}
+  public getLangs() {
+    return 'en-us';
+  }
+  public getBrowserLang() {
+    return '';
+  }
+  public getBrowserCultureLang() {
+    return '';
+  }
+  public use(lang: string) {
+    return '';
+  }
+  public get(key: any): any {
+    return of(key);
+  }
+  public setDefaultLang() {
+    return true;
+  }
+}
 
 describe('VisionCardComponent', () => {
   let component: VisionCardComponent;
@@ -8,7 +37,10 @@ describe('VisionCardComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [VisionCardComponent]
+      declarations: [VisionCardComponent],
+      imports: [TranslateModule.forRoot()],
+      providers: [{ provide: TranslateService, useClass: TranslationServiceStub }],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
     }).compileComponents();
   }));
 
