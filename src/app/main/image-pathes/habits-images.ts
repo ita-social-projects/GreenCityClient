26a0e1857dc8462fb5e4cf979414a9db7cb780ed
Lokankara <poliyakpavlo@gmail.com>
2024-04-29 15:16:39 +0300

    Modify material style  - fix filter dropdown  - add path icons

--- src/app/main/image-pathes/habits-images.ts
+++ src/app/main/image-pathes/habits-images.ts
@@ -1,3 +1,8 @@
 export const habitImages = {
   defaultImage: 'assets/img/habits/default-habit-image.png'
 };
+
+export const starIcons = {
+  whiteStar: 'assets/events-icons/star-empthy.svg',
+  greenStar: 'assets/events-icons/star-filled.svg'
+};
