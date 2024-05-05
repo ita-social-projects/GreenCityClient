3b0cd308e1dbaf2732614e7e39ba6b78e0f66068
lemoncat <xar4ak7even5@gmail.com>
2024-05-05 18:47:13 +0300

    remove duplications

--- src/app/ubs/ubs-admin/models/non-sortable-columns.model.ts
+++ src/app/ubs/ubs-admin/models/non-sortable-columns.model.ts
@@ -11,13 +11,11 @@ export const nonSortableColumns = [
   'idOrderFromShop',
   'phone_number',
   'recipient_phone',
-  'address',
   'comment_to_address_for_client',
   'order_certificate_code',
   'comment_for_order_by_client',
   'comments_for_order',
   'id_order_from_shop',
-  'recipientPhone',
   'recipientEmail',
   'orderId',
   'points'
