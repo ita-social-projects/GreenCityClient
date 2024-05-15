6b5a13ed0f27ae83d3ca5574a4775b114f383404
Olena Voitsekhivska <mirkbog@gmail.com>
2024-05-15 23:41:56 +0300

    removed redundant commented out code

--- src/app/ubs/ubs-user/ubs-user-bonuses/ubs-user-bonuses.component.ts
+++ src/app/ubs/ubs-user/ubs-user-bonuses/ubs-user-bonuses.component.ts
@@ -17,23 +17,9 @@ import { LocalStorageService } from '@global-service/localstorage/local-storage.
 export class UbsUserBonusesComponent implements OnInit, OnDestroy {
   displayedColumns: string[] = ['dateOfEnrollment', 'amount', 'reason'];
   dataSource = new MatTableDataSource<BonusModel>();
-  // totalBonuses: number;
-  totalBonuses = 0;
-  // isLoading = true;
-  isLoading = false;
-  // bonusesList: BonusModel[] = [];
-  bonusesList: BonusModel[] = [
-    { amount: 500, dateOfEnrollment: new Date('2023-01-15'), numberOfOrder: 25 },
-    { amount: 320, dateOfEnrollment: new Date('2023-05-20'), numberOfOrder: 12 },
-    { amount: 750, dateOfEnrollment: new Date('2023-02-08'), numberOfOrder: 35 },
-    { amount: 180, dateOfEnrollment: new Date('2023-04-03'), numberOfOrder: 8 },
-    { amount: 920, dateOfEnrollment: new Date('2023-03-10'), numberOfOrder: 42 },
-    { amount: 280, dateOfEnrollment: new Date('2023-06-18'), numberOfOrder: 17 },
-    { amount: 600, dateOfEnrollment: new Date('2023-07-22'), numberOfOrder: 29 },
-    { amount: 420, dateOfEnrollment: new Date('2023-08-05'), numberOfOrder: 21 },
-    { amount: 850, dateOfEnrollment: new Date('2023-09-14'), numberOfOrder: 38 },
-    { amount: 190, dateOfEnrollment: new Date('2023-10-30'), numberOfOrder: 14 }
-  ];
+  totalBonuses: number;
+  isLoading = true;
+  bonusesList: BonusModel[] = [];
 
   destroy: Subject<boolean> = new Subject<boolean>();
 
@@ -47,23 +33,23 @@ export class UbsUserBonusesComponent implements OnInit, OnDestroy {
   }
 
   getBonusesData() {
-    // this.isLoading = true;
-    // this.bonusesService
-    //   .getUserBonusesWithPaymentHistory()
-    //   .pipe(takeUntil(this.destroy))
-    //   .subscribe(
-    //     (res: BonusesModel) => {
-    //       this.bonusesList = res.ubsUserBonuses;
-    //       this.dataSource.data = res.ubsUserBonuses;
-    //       this.totalBonuses = res.userBonuses;
-    //       this.isLoading = false;
-    //     },
-    //     (error) => {
-    //       this.isLoading = false;
-    //       this.snackBar.openSnackBar('Oops, something went wrong. Please reload page or try again later.');
-    //       return throwError(error);
-    //     }
-    //   );
+    this.isLoading = true;
+    this.bonusesService
+      .getUserBonusesWithPaymentHistory()
+      .pipe(takeUntil(this.destroy))
+      .subscribe(
+        (res: BonusesModel) => {
+          this.bonusesList = res.ubsUserBonuses;
+          this.dataSource.data = res.ubsUserBonuses;
+          this.totalBonuses = res.userBonuses;
+          this.isLoading = false;
+        },
+        (error) => {
+          this.isLoading = false;
+          this.snackBar.openSnackBar('Oops, something went wrong. Please reload page or try again later.');
+          return throwError(error);
+        }
+      );
   }
 
   sortData(sort: Sort) {
--- src/app/ubs/ubs-user/ubs-user-messages/ubs-user-messages.component.ts
+++ src/app/ubs/ubs-user/ubs-user-messages/ubs-user-messages.component.ts
@@ -35,73 +35,6 @@ export class UbsUserMessagesComponent implements OnInit, OnDestroy {
     time: 'ubs-user-notification.title-table.time'
   };
 
-  mockData: NotificationBody[] = [
-    {
-      id: 1,
-      orderId: 101,
-      notificationTime: '2024-05-06T08:00:00',
-      read: false,
-      title: 'New Order Received',
-      body: 'A new order has been received. Please process it promptly.'
-    },
-    {
-      id: 2,
-      orderId: 102,
-      notificationTime: '2024-05-05T15:30:00',
-      read: true,
-      title: 'Order Shipped',
-      body: 'Your order has been shipped and is on its way to you.'
-    },
-    {
-      id: 3,
-      orderId: 103,
-      notificationTime: '2024-05-04T10:45:00',
-      read: false,
-      title: 'Payment Received',
-      body: 'Payment for your order has been successfully received.'
-    },
-    {
-      id: 4,
-      orderId: 104,
-      notificationTime: '2024-05-03T12:20:00',
-      read: false,
-      title: 'Order Delivered',
-      body: 'Your order has been delivered to the specified address.'
-    },
-    {
-      id: 5,
-      orderId: 105,
-      notificationTime: '2024-05-02T09:10:00',
-      read: true,
-      title: 'Order Cancelled',
-      body: 'Unfortunately, your order has been cancelled.'
-    },
-    {
-      id: 6,
-      orderId: 106,
-      notificationTime: '2024-05-01T17:55:00',
-      read: true,
-      title: 'Order Updated',
-      body: 'There has been an update regarding your order.'
-    },
-    {
-      id: 7,
-      orderId: 107,
-      notificationTime: '2024-04-30T14:15:00',
-      read: false,
-      title: 'New Message Received',
-      body: 'You have received a new message. Please check your inbox.'
-    },
-    {
-      id: 8,
-      orderId: 108,
-      notificationTime: '2024-04-29T11:25:00',
-      read: true,
-      title: 'Order Refunded',
-      body: 'Your order has been refunded successfully.'
-    }
-  ];
-
   constructor(
     private userMessagesService: UserMessagesService,
     private localStorageService: LocalStorageService,
@@ -135,9 +68,6 @@ export class UbsUserMessagesComponent implements OnInit, OnDestroy {
       .subscribe(
         (response) => {
           this.notifications = response.page;
-          //REMOVE MOCK!!!
-          this.notifications = this.mockData;
-          //END MOCK DATA
           this.count = response.totalElements;
           this.isAnyMessages = this.notifications.length > 0;
           this.isLoadSpinner = this.isLoadBar = false;
@@ -164,21 +94,21 @@ export class UbsUserMessagesComponent implements OnInit, OnDestroy {
           item.read = true;
         }
       });
-      // this.isLoadSmallSpinner = true;
-      // this.userMessagesService
-      //   .setReadNotification(notificationId)
-      //   .pipe(takeUntil(this.destroy))
-      //   .subscribe((response) => {
-      //     const findNotification = this.notifications.find((item) => item.id === notificationId);
-      //     findNotification.body = response.body;
-      //     findNotification.images = response.images;
-      //     findNotification.isOpen = true;
-      //     this.isLoadSmallSpinner = false;
-      //     if (findNotification.images) {
-      //       const images = response.images.map((url) => ({ src: url, label: null, name: null }));
-      //       this.images.splice(0, response.images.length, ...images);
-      //     }
-      //   });
+      this.isLoadSmallSpinner = true;
+      this.userMessagesService
+        .setReadNotification(notificationId)
+        .pipe(takeUntil(this.destroy))
+        .subscribe((response) => {
+          const findNotification = this.notifications.find((item) => item.id === notificationId);
+          findNotification.body = response.body;
+          findNotification.images = response.images;
+          findNotification.isOpen = true;
+          this.isLoadSmallSpinner = false;
+          if (findNotification.images) {
+            const images = response.images.map((url) => ({ src: url, label: null, name: null }));
+            this.images.splice(0, response.images.length, ...images);
+          }
+        });
     }
   }
 
