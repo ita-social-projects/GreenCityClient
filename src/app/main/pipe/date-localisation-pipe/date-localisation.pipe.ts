9374e54391884da270afab86261ee372f957c9e6
Lokankara <poliyakpavlo@gmail.com>
2024-05-24 10:26:41 +0300

    Modify validator for date

--- src/app/main/pipe/date-localisation-pipe/date-localisation.pipe.ts
+++ src/app/main/pipe/date-localisation-pipe/date-localisation.pipe.ts
@@ -1,29 +1,22 @@
-import { OnDestroy, Pipe, PipeTransform } from '@angular/core';
+import { Pipe, PipeTransform } from '@angular/core';
 import { formatDate } from '@angular/common';
-import { Subject } from 'rxjs';
 import { LocalStorageService } from '../../service/localstorage/local-storage.service';
-import { takeUntil } from 'rxjs/operators';
+import { take } from 'rxjs/operators';
 
 @Pipe({
   name: 'dateLocalisation',
   pure: false
 })
-export class DateLocalisationPipe implements PipeTransform, OnDestroy {
-  private destroy$: Subject<boolean> = new Subject<boolean>();
+export class DateLocalisationPipe implements PipeTransform {
   private locale: string = this.localStorageService.getCurrentLanguage();
 
   constructor(private localStorageService: LocalStorageService) {
     this.locale = this.localStorageService.getCurrentLanguage();
-    this.localStorageService.languageSubject.pipe(takeUntil(this.destroy$)).subscribe((lang) => (this.locale = lang));
+    this.localStorageService.languageSubject.pipe(take(1)).subscribe((lang) => (this.locale = lang));
   }
 
   transform(date: string | Date): string {
     date = !date ? Date.now().toString() : date;
     return formatDate(date, 'mediumDate', this.locale);
-  }
-
-  ngOnDestroy() {
-    this.destroy$.next(true);
-    this.destroy$.complete();
   }
 }
