2869e160b2cb35c9394220053928b65d4e3b1e6f
Oksana Filipchuk <filipchuk.oksana@gmail.com>
2024-06-14 10:15:52 +0300

    update environment.stage.ts

--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -2,16 +2,16 @@ export const environment = {
   production: true,
   apiKeys: 'AIzaSyDanBliCzAuCZrsq67FeKEs3vqAilUD_is',
   apiMapKey: 'AIzaSyCU0ArzZlZ3n0pLq4o9MJy29LPT5DBMk4Y',
-  backendLink: 'https://greencity.greencity.social/',
-  backendChatLink: 'https://greencity-chat.greencity.social/',
-  backendUserLink: 'https://greencity-user.greencity.social/',
-  backendUbsLink: 'https://greencity-ubs.greencity.social',
-  frontendLink: 'https://www.greencity.social/',
-  socket: 'https://greencity.greencity.social/socket',
-  userSocket: 'https://greencity-user.greencity.social/socket',
-  chatSocket: 'https://greencity-chat.greencity.social/socket',
+  backendLink: 'https://greencity.greencity.cx.ua/',
+  backendChatLink: 'https://greencity-chat.greencity.cx.ua/',
+  backendUserLink: 'https://greencity-user.greencity.cx.ua/',
+  backendUbsLink: 'https://greencity-ubs.greencity.cx.ua',
+  frontendLink: 'https://www.greencity.cx.ua/',
+  socket: 'https://greencity.greencity.cx.ua/socket',
+  userSocket: 'https://greencity-user.greencity.cx.ua/socket',
+  chatSocket: 'https://greencity-chat.greencity.cx.ua/socket',
   ubsAdmin: {
-    backendUbsAdminLink: 'https://greencity-ubs.greencity.social/ubs'
+    backendUbsAdminLink: 'https://greencity-ubs.greencity.cx.ua/ubs'
   },
   googleClientId: '129513550972-eu9ej46rviv1ac8q14at62t2k5qon1pu.apps.googleusercontent.com',
   agmCoreModuleApiKey: 'AIzaSyCdBtR8O8eazfWUSdyb5O2cnL32uJtWUwA'
