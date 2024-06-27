8c25b29aa7baf243cdf87f8a5e162eea725d0cd2
Tadei-Mykola <tmukola2006@gmail.com>
2024-06-27 18:11:48 +0300

    Fix import

--- src/app/main/service/socket/socket.service.ts
+++ src/app/main/service/socket/socket.service.ts
@@ -1,11 +1,10 @@
 import { Injectable, OnDestroy } from '@angular/core';
 import { environment } from '../../../../environments/environment';
 import { BehaviorSubject, Observable } from 'rxjs';
-
+import SockJS from 'sockjs-client';
 import { Stomp, StompSubscription } from '@stomp/stompjs';
 import { SocketClientState, SocketConnection } from './socket-state.enum';
 import { filter, first, switchMap } from 'rxjs/operators';
-import SockJS from 'sockjs-client';
 
 @Injectable({
   providedIn: 'root'
