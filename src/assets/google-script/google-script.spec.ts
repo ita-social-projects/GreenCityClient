4ef2833c9d7af6f8778f40bd053446eb53685f77
Lokankara <poliyakpavlo@gmail.com>
2024-07-17 20:50:54 +0300

    Add default map values

--- src/assets/google-script/google-script.spec.ts
+++ src/assets/google-script/google-script.spec.ts
@@ -22,7 +22,10 @@ describe('GoogleScript', () => {
 
     if (!window.initMap) {
       window.initMap = () => {
-        googleScript.initMap();
+        new google.maps.Map(document.getElementById('map'), {
+          center: { lat: -34.397, lng: 150.644 },
+          zoom: 8
+        });
       };
     }
   });
