2293fb0e01e522092131e163650d88c9b6ebb013
Oksana Filipchuk <filipchuk.oksana@gmail.com>
2024-08-19 01:11:40 +0300

    test

--- src/app/chat/component/chat-popup/chat-popup.component.spec.ts
+++ src/app/chat/component/chat-popup/chat-popup.component.spec.ts
@@ -5,10 +5,11 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { ChatPopupComponent } from './chat-popup.component';
 import { LocalStorageService } from '@global-service/localstorage/local-storage.service';
-import { of } from 'rxjs';
+import { of, Subject } from 'rxjs';
 import { JwtService } from '@global-service/jwt/jwt.service';
 import { OrderService } from '@ubs/ubs/services/order.service';
 import { TranslateModule } from '@ngx-translate/core';
+import { ReferenceDirective } from '../../directive/reference/reference.directive';
 
 describe('ChatPopupComponent', () => {
   let component: ChatPopupComponent;
@@ -23,7 +24,7 @@ describe('ChatPopupComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ChatPopupComponent],
+      declarations: [ChatPopupComponent, ReferenceDirective],
       imports: [HttpClientTestingModule, TranslateModule.forRoot(), MatDialogModule],
       providers: [
         { provide: UserService, useValue: {} },
@@ -40,5 +41,37 @@ describe('ChatPopupComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should load chats if user is  authorized', () => {
+    const spyConnect = spyOn((component as any).socketService, 'connect');
+    const spyLoadChats = spyOn(component, 'loadChats');
+    localStorageServiceMock.userIdBehaviourSubject = of(1);
+    component.ngOnInit();
+    expect((component as any).userId).toBe(1);
+    expect((component as any).isUbsAdmin).toBeTrue();
+    expect(spyConnect).toHaveBeenCalled();
+    expect(spyLoadChats).toHaveBeenCalled();
+  });
+
+  it('should not load chats', () => {
+    const spyConnect = spyOn((component as any).socketService, 'connect');
+    const spyLoadChats = spyOn(component, 'loadChats');
+    localStorageServiceMock.userIdBehaviourSubject = of(null);
+    expect(spyConnect).not.toHaveBeenCalled();
+    expect(spyLoadChats).not.toHaveBeenCalled();
+  });
+
+  it('should reset and unsubscribe', () => {
+    (component as any).commonService.newMessageWindowRequireCloseStream$ = new Subject();
+    const spyUnsub = spyOn((component as any).socketService, 'unsubscribeAll');
+    const spyResetData = spyOn((component as any).chatsService, 'resetData');
+    const spyWindow = spyOn((component as any).commonService.newMessageWindowRequireCloseStream$, 'next');
+    localStorageServiceMock.userIdBehaviourSubject = of(null);
+    component.ngOnInit();
+    expect(spyUnsub).toHaveBeenCalled();
+    expect(spyResetData).toHaveBeenCalled();
+    expect(component.isOpen).toBeFalse();
+    expect(spyWindow).toHaveBeenCalledWith(true);
   });
 });
