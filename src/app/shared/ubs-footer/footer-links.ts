bbf6159b0c111133d13fd055b769e8ea4ce5d816
lemoncat <xar4ak7even5@gmail.com>
2024-08-20 08:49:19 +0300

    feature: add link to user agreement for user

--- src/app/shared/ubs-footer/footer-links.ts
+++ src/app/shared/ubs-footer/footer-links.ts
@@ -1,7 +1,8 @@
 export const ubsNavLinks = [
   { name: 'user.lower-nav-bar.sorting-rules', route: 'https://nowaste.com.ua/sort-station/', url: true },
+  { name: 'user.lower-nav-bar.user-agreement', route: '/ubs/user-agreement', url: false },
   { name: 'user.lower-nav-bar.eco-shop', route: 'https://shop.nowaste.com.ua/', url: true },
-  { name: 'Green City', route: '/#/greenCity', url: false }
+  { name: 'Green City', route: '/greenCity', url: false }
 ];
 
 export const socialLinks = [
