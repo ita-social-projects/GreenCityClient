f8b018bb1ab23bd713cdfb20ba483a559f0522e8
kovalsofiia1 <kovalsofiia1@gmail.com>
2024-09-05 21:00:21 +0300

    fix: refactor constants

--- src/app/main/component/eco-news/components/news-list/news-list-list-view/breakpoints.ts
+++ src/app/main/component/eco-news/components/news-list/news-list-list-view/breakpoints.ts
@@ -1,26 +1,13 @@
-export const possibleDescHeight = {
-  32: 'tree-row',
-  64: 'two-row',
-  96: 'one-row',
-  128: 'hide'
-};
+export enum TitleHeights {
+  oneRow = 32,
+  twoRows = 64,
+  threeRows = 96,
+  fourRows = 128
+}
 
-export const possibleTitleHeight = {
-  smallHeight: {
-    26: 'one-row',
-    52: 'two-row',
-    78: 'tree-row',
-    104: 'two-row'
-  },
-  bigHeight: {
-    24: 'one-row',
-    26: 'one-row',
-    48: 'two-row',
-    52: 'two-row',
-    72: 'tree-row',
-    78: 'tree-row',
-    96: 'tree-row',
-    100: 'tree-row',
-    104: 'tree-row'
-  }
+export const possibleDescHeight = {
+  [TitleHeights.oneRow]: 'tree-row',
+  [TitleHeights.twoRows]: 'two-row',
+  [TitleHeights.threeRows]: 'one-row',
+  [TitleHeights.fourRows]: 'hide'
 };
