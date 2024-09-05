8f6457a073296a379a0b618bad4652e269ce17f3
Denys Leon <leonden0046@gmail.com>
2024-09-05 16:55:46 +0300

    fix

--- src/app/main/component/comments/components/reply-comment/reply-comment.component.spec.ts
+++ src/app/main/component/comments/components/reply-comment/reply-comment.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { TranslateModule } from '@ngx-translate/core';
 
 import { ReplyCommentComponent } from './reply-comment.component';
+import { CommentsDTO } from '../../models/comments-model';
 
 describe('ReplyCommentComponent', () => {
   let component: ReplyCommentComponent;
@@ -17,24 +18,11 @@ describe('ReplyCommentComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ReplyCommentComponent);
     component = fixture.componentInstance;
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
-  });
-
-  it('should set the srcset to reply image when isAddingReply is true', () => {
-    component.isAddingReply = true;
-    component.writeReply();
-
-    expect(component.reply.nativeElement.srcset).toBe('assets/img/comments/reply.svg');
-  });
-
-  it('should set the srcset to replying image when isAddingReply is false', () => {
-    component.isAddingReply = false;
-    component.writeReply();
-
-    expect(component.reply.nativeElement.srcset).toBe('assets/img/comments/reply-green.svg');
   });
 });
