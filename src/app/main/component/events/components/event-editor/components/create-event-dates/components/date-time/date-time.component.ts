fd8cffe6bbe59270a0ae05bbba11be706ab05d60
kovalsofiia1 <kovalsofiia1@gmail.com>
2024-09-10 16:02:45 +0300

    feat: add fields

--- src/app/main/component/events/components/event-editor/components/create-event-dates/components/date-time/date-time.component.ts
+++ src/app/main/component/events/components/event-editor/components/create-event-dates/components/date-time/date-time.component.ts
@@ -45,6 +45,24 @@ export class DateTimeComponent implements OnInit, OnDestroy {
   isDateInThePast = false;
   isDateEmpty = false;
 
+  months = [
+    { value: 0, label: 'January' },
+    { value: 1, label: 'February' },
+    { value: 2, label: 'March' },
+    { value: 3, label: 'April' },
+    { value: 4, label: 'May' },
+    { value: 5, label: 'June' },
+    { value: 6, label: 'July' },
+    { value: 7, label: 'August' },
+    { value: 8, label: 'September' },
+    { value: 9, label: 'October' },
+    { value: 10, label: 'November' },
+    { value: 11, label: 'December' }
+  ];
+
+  minYear = this.today.getFullYear();
+  maxYear = this.minYear + 10;
+
   constructor(
     private fb: FormBuilder,
     private bridge: FormBridgeService,
