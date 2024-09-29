17a53d0e1d052e451795b8d2a318b636b77b6d55
yuliia_karas <yuliiakaras03@gmail.com>
2024-09-29 16:37:26 +0300

    refactor functionality in achievement-item

--- src/app/main/component/user/components/profile/users-achievements/achievement-item/achievement-item.component.ts
+++ src/app/main/component/user/components/profile/users-achievements/achievement-item/achievement-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AchievementDto } from '@global-models/achievement/AchievementDto';
 import { AchievementCategoryDto } from '@global-models/achievementCategory/achievementCategoryDto.model';
 import { LanguageService } from 'src/app/main/i18n/language.service';
@@ -9,13 +9,46 @@ import { PROFILE_IMAGES } from 'src/app/main/image-pathes/profile-images';
   templateUrl: './achievement-item.component.html',
   styleUrls: ['./achievement-item.component.scss']
 })
-export class AchievementItemComponent {
+export class AchievementItemComponent implements OnInit {
   @Input() item: AchievementDto | AchievementCategoryDto;
   @Input() isCategory = false;
   @Output() selectItem = new EventEmitter<AchievementDto | AchievementCategoryDto>();
   achievementsImages = PROFILE_IMAGES.achs;
+  itemToUse: {
+    categoryId: number;
+    title: string;
+    titleEn: string;
+    achieved: number;
+    totalQuantity: number;
+  };
 
   constructor(public langService: LanguageService) {}
+
+  ngOnInit(): void {
+    this.itemToUse = this.transformItem(this.item);
+  }
+
+  transformItem(item: AchievementDto | AchievementCategoryDto) {
+    if (this.isCategory) {
+      const categoryItem = item as AchievementCategoryDto;
+      return {
+        categoryId: categoryItem.id,
+        title: categoryItem.title,
+        titleEn: categoryItem.titleEn,
+        achieved: categoryItem.achieved,
+        totalQuantity: categoryItem.totalQuantity
+      };
+    } else {
+      const achievementItem = item as AchievementDto;
+      return {
+        categoryId: achievementItem.achievementCategory.id,
+        title: achievementItem.name,
+        titleEn: achievementItem.nameEng,
+        achieved: achievementItem.progress,
+        totalQuantity: achievementItem.condition
+      };
+    }
+  }
 
   onItemClick() {
     if (this.isCategory) {
@@ -24,49 +57,23 @@ export class AchievementItemComponent {
   }
 
   getProgressValue(): number {
-    if (this.isCategory) {
-      const categoryItem = this.item as AchievementCategoryDto;
-      return (categoryItem.achieved / categoryItem.totalQuantity) * 100;
-    } else {
-      const achievementItem = this.item as AchievementDto;
-      return (achievementItem.progress / achievementItem.condition) * 100;
-    }
+    return (this.itemToUse.achieved / this.itemToUse.totalQuantity) * 100;
   }
 
   getImagePath(): string {
-    if (this.isCategory) {
-      const categoryItem = this.item as AchievementCategoryDto;
-      return this.achievementsImages[categoryItem.id - 1];
-    } else {
-      const achievementItem = this.item as AchievementDto;
-      return this.achievementsImages[achievementItem.achievementCategory.id - 1];
-    }
+    return this.achievementsImages[this.itemToUse.categoryId - 1];
   }
 
   isGreyscale(): boolean {
-    if (this.isCategory) {
-      const categoryItem = this.item as AchievementCategoryDto;
-      return categoryItem.achieved < categoryItem.totalQuantity;
-    } else {
-      const achievementItem = this.item as AchievementDto;
-      return achievementItem.progress < achievementItem.condition;
-    }
+    return this.itemToUse.achieved < this.itemToUse.totalQuantity;
   }
 
   getTitle(): string {
-    return this.isCategory
-      ? this.langService.getLangValue((this.item as AchievementCategoryDto).title, (this.item as AchievementCategoryDto).titleEn)
-      : this.langService.getLangValue((this.item as AchievementDto).name, (this.item as AchievementDto).nameEng);
+    return this.langService.getLangValue(this.itemToUse.title, this.itemToUse.titleEn);
   }
 
   getStatistics(): string {
-    if (this.isCategory) {
-      const categoryItem = this.item as AchievementCategoryDto;
-      return `${categoryItem.achieved}/${categoryItem.totalQuantity}`;
-    } else {
-      const achievementItem = this.item as AchievementDto;
-      const achieved = Math.min(achievementItem.progress, achievementItem.condition);
-      return `${achieved}/${achievementItem.condition}`;
-    }
+    const achieved = Math.min(this.itemToUse.achieved, this.itemToUse.totalQuantity);
+    return `${achieved}/${this.itemToUse.totalQuantity}`;
   }
 }
