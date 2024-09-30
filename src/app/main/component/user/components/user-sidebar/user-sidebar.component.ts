f54b708ec5bb4f41773dda34a8b9386ce4582fdd
yuliia_karas <yuliiakaras03@gmail.com>
2024-09-30 13:24:17 +0300

    remove unused components

--- src/app/main/component/user/components/user-sidebar/user-sidebar.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-user-sidebar',
-  templateUrl: './user-sidebar.component.html',
-  styleUrls: ['./user-sidebar.component.scss']
-})
-export class UserSidebarComponent {}
