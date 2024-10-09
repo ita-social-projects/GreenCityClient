86fe2f0d5c693505a4257972448d42ccfb7148b9
kovalsofiia1 <kovalsofiia1@gmail.com>
2024-10-09 20:21:53 +0300

    feat: add tests

--- src/app/main/component/auth/components/error/error.component.spec.ts
+++ src/app/main/component/auth/components/error/error.component.spec.ts
@@ -1,10 +1,27 @@
 import { ErrorComponent } from './error.component';
+import { FormControl } from '@angular/forms';
 
-describe('error component', () => {
+describe('ErrorComponent', () => {
   let component: ErrorComponent;
 
   beforeEach(() => {
     component = new ErrorComponent();
+    component.control = new FormControl(); // Mocking the FormControl input
+  });
+
+  it('Error: get email required message', () => {
+    component.controlName = 'email';
+    expect(component['ERROR_MESSAGE'].required()).toBe('user.auth.sign-in.email-is-required');
+  });
+
+  it('Error: get firstName required message', () => {
+    component.controlName = 'firstName';
+    expect(component['ERROR_MESSAGE'].required()).toBe('user.auth.sign-in.name-is-required');
+  });
+
+  it('Error: get default required message', () => {
+    component.controlName = 'other';
+    expect(component['ERROR_MESSAGE'].required()).toBe('user.auth.sign-in.field-is-required');
   });
 
   it('Error: get email message', () => {
@@ -15,7 +32,7 @@ describe('error component', () => {
     expect(component['ERROR_MESSAGE'].passwordMismatch()).toBe('user.auth.sign-up.password-match');
   });
 
-  it('Error: get emptyPassword message', () => {
+  it('Error: get passwordIsEmpty message', () => {
     expect(component['ERROR_MESSAGE'].passwordIsEmpty()).toBe('user.auth.sign-up.password-match-is-empty');
   });
 
@@ -27,12 +44,13 @@ describe('error component', () => {
     expect(component['ERROR_MESSAGE'].maxlength()).toBe('user.auth.sign-in.too-long-password');
   });
 
-  it('Error: get value for password', () => {
+  it('Error: get symbolInvalid message for password', () => {
     component.controlName = 'password';
     expect(component['ERROR_MESSAGE'].symbolInvalid()).toBe('user.auth.sign-up.password-symbols-error');
   });
 
-  it('Error: get value for password', () => {
+  it('Error: get symbolInvalid message for other field', () => {
+    component.controlName = 'username';
     expect(component['ERROR_MESSAGE'].symbolInvalid()).toBe('user.auth.sign-up.user-name-size');
   });
 });
