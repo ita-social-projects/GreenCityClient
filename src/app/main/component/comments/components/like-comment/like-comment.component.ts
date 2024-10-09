34d9a41af7ccffdbc6af2e22d924943428b0910d
Anastasia <155824290+kryzanivska-nastya@users.noreply.github.com>
2024-10-09 13:47:33 +0300

    socket like comment

--- src/app/main/component/comments/components/like-comment/like-comment.component.ts
+++ src/app/main/component/comments/components/like-comment/like-comment.component.ts
@@ -42,17 +42,14 @@ export class LikeCommentComponent implements OnInit {
   }
 
   checkSocketMessageToSubscribe() {
-    if (this.router.url.includes('news')) {
+    const baseUrl = '/app/likeAndCount';
+    const routes = ['news', 'events', 'allhabits'];
+
+    const matchingRoute = routes.find((route) => this.router.url.includes(route));
+
+    if (matchingRoute) {
       this.socketMessageToSubscribe = `/topic/${this.comment.id}/comment`;
-      this.socketMessageToSend = '/app/likeAndCount';
-    }
-    if (this.router.url.includes('events')) {
-      this.socketMessageToSubscribe = `/topic/${this.comment.id}/eventComment`;
-      this.socketMessageToSend = '/app/eventCommentLikeAndCount';
-    }
-    if (this.router.url.includes('allhabits')) {
-      this.socketMessageToSubscribe = `/topic/${this.comment.id}/сomment`;
-      this.socketMessageToSend = '/app/likeAndCount';
+      this.socketMessageToSend = baseUrl;
     }
   }
 
