a9b9b02bb643a0f1d639808ec99713b16c93c59f
Anastasia <155824290+kryzanivska-nastya@users.noreply.github.com>
2024-10-10 21:11:32 +0300

    follow best practices for tests

--- src/app/main/service/habit-comments/habit-comments.service.spec.ts
+++ src/app/main/service/habit-comments/habit-comments.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { HabitCommentsService } from './habit-comments.service';
 import { environment } from '@environment/environment';
-import { HabitAddedCommentDTO, HabitCommentsModel } from '@global-user/components/habit/models/habits-comments.model';
+import { MOCK_HABIT_ADDED_COMMENT, MOCK_HABIT_COMMENTS_MODEL } from 'src/app/main/component/user/components/habit/mocks/habit-mock';
 
 describe('HabitCommentsService', () => {
   let service: HabitCommentsService;
@@ -24,64 +24,37 @@ describe('HabitCommentsService', () => {
   });
 
   it('should be created', () => {
-    const serviceNew: HabitCommentsService = TestBed.inject(HabitCommentsService);
-    expect(serviceNew).toBeTruthy();
+    expect(service).toBeTruthy();
   });
 
   it('should make POST request to add comment', () => {
-    const commentBody: HabitAddedCommentDTO = {
-      author: { id: 1, name: 'User', userProfilePicturePath: null },
-      id: 2,
-      text: 'Test comment',
-      modifiedDate: new Date().toISOString()
-    };
-
     service.addComment(1, 'Test comment', 0).subscribe((commentData) => {
-      expect(commentData).toEqual(commentBody);
+      expect(commentData).toEqual(MOCK_HABIT_ADDED_COMMENT);
     });
 
     const req = httpTestingController.expectOne(`${url}habits/1/comments`);
     expect(req.request.method).toEqual('POST');
-    req.flush(commentBody);
+    req.flush(MOCK_HABIT_ADDED_COMMENT);
   });
 
   it('should make GET request to get active comments by page', () => {
-    const commentData: HabitCommentsModel = {
-      currentPage: 0,
-      page: [
-        {
-          author: { id: 1, name: 'User', userProfilePicturePath: null },
-          id: 1,
-          text: 'Test comment',
-          likes: 10,
-          modifiedDate: new Date().toISOString(),
-          status: 'ORIGINAL',
-          replies: 0,
-          currentUserLiked: false
-        }
-      ],
-      totalElements: 1
-    };
-
     service.getActiveCommentsByPage(1, 0, 5).subscribe((data) => {
-      expect(data).toEqual(commentData);
+      expect(data).toEqual(MOCK_HABIT_COMMENTS_MODEL);
     });
 
     const req = httpTestingController.expectOne(`${url}habits/comments/active?habitId=1&page=0&size=5`);
     expect(req.request.method).toEqual('GET');
-    req.flush(commentData);
+    req.flush(MOCK_HABIT_COMMENTS_MODEL);
   });
 
   it('should make GET request to get comments count', () => {
-    const count = 10;
-
     service.getCommentsCount(1).subscribe((data) => {
-      expect(data).toEqual(count);
+      expect(data).toEqual(10);
     });
 
     const req = httpTestingController.expectOne(`${url}habits/1/comments/count`);
     expect(req.request.method).toEqual('GET');
-    req.flush(count);
+    req.flush(10);
   });
 
   it('should make DELETE request to delete comment', () => {
@@ -95,15 +68,13 @@ describe('HabitCommentsService', () => {
   });
 
   it('should make GET request to get replies amount', () => {
-    const repliesCount = 5;
-
     service.getRepliesAmount(1, 2).subscribe((data) => {
-      expect(data).toEqual(repliesCount);
+      expect(data).toEqual(5);
     });
 
     const req = httpTestingController.expectOne(`${url}habits/comments/2/replies/active/count`);
     expect(req.request.method).toEqual('GET');
-    req.flush(repliesCount);
+    req.flush(5);
   });
 
   it('should make PATCH request to edit a comment', () => {
@@ -118,30 +89,13 @@ describe('HabitCommentsService', () => {
   });
 
   it('should make GET request to get active replies by page', () => {
-    const repliesData: HabitCommentsModel = {
-      currentPage: 0,
-      page: [
-        {
-          author: { id: 1, name: 'User', userProfilePicturePath: null },
-          id: 1,
-          text: 'Test reply',
-          likes: 5,
-          modifiedDate: new Date().toISOString(),
-          status: 'ORIGINAL',
-          replies: 0,
-          currentUserLiked: false
-        }
-      ],
-      totalElements: 1
-    };
-
     service.getActiveRepliesByPage(1, 1, 0, 5).subscribe((data) => {
-      expect(data).toEqual(repliesData);
+      expect(data).toEqual(MOCK_HABIT_COMMENTS_MODEL);
     });
 
     const req = httpTestingController.expectOne(`${url}habits/comments/1/replies/active?page=0&size=5`);
     expect(req.request.method).toEqual('GET');
-    req.flush(repliesData);
+    req.flush(MOCK_HABIT_COMMENTS_MODEL);
   });
 
   it('should make POST request to like a comment', () => {
@@ -165,29 +119,12 @@ describe('HabitCommentsService', () => {
   });
 
   it('should make GET request to get comment by id', () => {
-    const commentData: HabitCommentsModel = {
-      currentPage: 0,
-      page: [
-        {
-          author: { id: 1, name: 'User', userProfilePicturePath: null },
-          id: 1,
-          text: 'Test comment',
-          likes: 5,
-          modifiedDate: new Date().toISOString(),
-          status: 'ORIGINAL',
-          replies: 0,
-          currentUserLiked: false
-        }
-      ],
-      totalElements: 1
-    };
-
     service.getCommentById(1, 1).subscribe((data) => {
-      expect(data).toEqual(commentData);
+      expect(data).toEqual(MOCK_HABIT_COMMENTS_MODEL);
     });
 
     const req = httpTestingController.expectOne(`${url}habits/comments/1`);
     expect(req.request.method).toEqual('GET');
-    req.flush(commentData);
+    req.flush(MOCK_HABIT_COMMENTS_MODEL);
   });
 });
