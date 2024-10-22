5e329571a0bdcaa6c0535f768c9f5c9b3d9ebf9c
Bozhen <89320434+bzhn@users.noreply.github.com>
2024-10-23 00:01:56 +0300

    Revert "wip: Hide environment variables"

---
+++ src/environments/environment copy.ts
@@ -0,0 +1,18 @@
+export const environment = {
+  production: false,
+  apiKeys: 'AIzaSyDanBliCzAuCZrsq67FeKEs3vqAilUD_is',
+  apiMapKey: 'AIzaSyCU0ArzZlZ3n0pLq4o9MJy29LPT5DBMk4Y',
+  backendLink: 'http://localhost:8085/',
+  backendChatLink: 'http://localhost:8085/',
+  backendUserLink: 'http://localhost:8065/',
+  backendUbsLink: 'http://localhost:8055/',
+  frontendLink: 'http://localhost:4200/',
+  socket: 'https://greencity.greencity.cx.ua/socket',
+  userSocket: 'https://greencity-user.greencity.cx.ua/socket',
+  chatSocket: 'https://greencity-chat.greencity.cx.ua/socket',
+  ubsAdmin: {
+    backendUbsAdminLink: 'https://greencity-ubs.greencity.cx.ua/ubs'
+  },
+  googleClientId: '129513550972-pg3ueh62gbde1jukmr49vem2rclde123.apps.googleusercontent.com',
+  agmCoreModuleApiKey: 'AIzaSyC7q2v0VgRy60dAoItfv3IJhfJQEEoeqCI'
+};
