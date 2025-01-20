f48227076ee34902645b79b70222fef9731b1d99
yuliia_karas <yuliiakaras03@gmail.com>
2025-01-20 13:08:24 +0200

    provide correct validation messages on house number field

--- src/app/shared/address-input/address-input.component.ts
+++ src/app/shared/address-input/address-input.component.ts
@@ -68,7 +68,7 @@ export class AddressInputComponent implements OnInit, AfterViewInit, OnDestroy, 
     maxZoom: 20
   };
 
-  private buildingPattern = Patterns.numericAndAlphabetic;
+  private buildingPattern = Patterns.ubsHouseNumberPattern;
   private $destroy: Subject<void> = new Subject();
   private viewInitialized = false;
 
@@ -268,7 +268,10 @@ export class AddressInputComponent implements OnInit, AfterViewInit, OnDestroy, 
       city: [this.addressData.getCity() ?? '', Validators.required],
       street: [this.addressData.getStreet() ?? '', Validators.required],
       district: [this.addressData.getDistrict() ?? '', Validators.required],
-      houseNumber: [this.address?.houseNumber ?? '', [Validators.required, Validators.pattern(this.buildingPattern)]],
+      houseNumber: [
+        this.address?.houseNumber ?? '',
+        [Validators.required, Validators.maxLength(10), Validators.pattern(this.buildingPattern)]
+      ],
       houseCorpus: [this.address?.houseCorpus ?? '', emptyOrValid([Validators.maxLength(4), Validators.pattern(this.buildingPattern)])],
       entranceNumber: [
         this.address?.entranceNumber ?? '',
