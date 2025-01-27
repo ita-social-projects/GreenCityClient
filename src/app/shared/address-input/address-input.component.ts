4b3ec8d95655af8aa5ff607142a42c08d45ad35a
IraHrechyn <hrechyniryna@gmail.com>
2025-01-27 22:57:50 +0200

    feat: add validation and error messages for address input fields

--- src/app/shared/address-input/address-input.component.ts
+++ src/app/shared/address-input/address-input.component.ts
@@ -68,10 +68,8 @@ export class AddressInputComponent implements OnInit, AfterViewInit, OnDestroy, 
     maxZoom: 20
   };
 
-  private readonly buildingPattern = Patterns.numericAndAlphabetic;
-  private readonly houseNumberPattern = Patterns.ubsHouseNumberPattern;
-  private readonly numericPattern = Patterns.numeric;
-  private readonly $destroy: Subject<void> = new Subject();
+  private buildingPattern = Patterns.ubsHouseNumberPattern;
+  private $destroy: Subject<void> = new Subject();
   private viewInitialized = false;
 
   autocompleteRegionRequest = {
@@ -272,12 +270,12 @@ export class AddressInputComponent implements OnInit, AfterViewInit, OnDestroy, 
       district: [this.addressData.getDistrict() ?? '', Validators.required],
       houseNumber: [
         this.address?.houseNumber ?? '',
-        [Validators.required, Validators.maxLength(10), Validators.pattern(this.houseNumberPattern)]
+        [Validators.required, Validators.maxLength(4), Validators.pattern(this.buildingPattern)]
       ],
       houseCorpus: [this.address?.houseCorpus ?? '', emptyOrValid([Validators.maxLength(4), Validators.pattern(this.buildingPattern)])],
       entranceNumber: [
         this.address?.entranceNumber ?? '',
-        emptyOrValid([Validators.maxLength(2), Validators.pattern(this.numericPattern)])
+        emptyOrValid([Validators.maxLength(2), Validators.pattern(this.buildingPattern)])
       ],
       placeId: [this.address?.placeId ?? ''],
       addressComment: [this.address?.addressComment ?? '', Validators.maxLength(255)]
@@ -391,6 +389,7 @@ export class AddressInputComponent implements OnInit, AfterViewInit, OnDestroy, 
 
   onHouseNumberChange(): void {
     this.addressData.setHouseNumber(this.houseNumber.value);
+    console.log(this.houseNumber.errors);
     this.OnChangeAndTouched();
 
     if (!this.district.value) {
