f26fe2d81dbb49315028ea919c4eda9e1b6acfc0
dxrknesss <imp0rt4ntduck@gmail.com>
2025-01-30 16:05:41 +0200

    fix: single quote imports

--- src/app/main/component/events/components/event-editor/event-editor.component.spec.ts
+++ src/app/main/component/events/components/event-editor/event-editor.component.spec.ts
@@ -1,18 +1,17 @@
-import { HarnessLoader } from "@angular/cdk/testing";
-import { TestbedHarnessEnvironment } from "@angular/cdk/testing/testbed";
-import { provideHttpClient } from "@angular/common/http";
-import { ComponentFixture, TestBed, tick } from "@angular/core/testing";
-import { FormBuilder } from "@angular/forms";
-import { MatDialogRef } from "@angular/material/dialog";
-import { provideNoopAnimations } from "@angular/platform-browser/animations";
-import { ActivatedRoute, provideRouter, Router } from "@angular/router";
-import { RouterTestingHarness, RouterTestingModule } from "@angular/router/testing";
-import { LocalStorageService } from "@global-service/localstorage/local-storage.service";
-import { provideMockStore } from "@ngrx/store/testing";
-import { Observable, of, Subscription } from "rxjs";
-import { EventsModule } from "../../events.module";
-import { EventStoreService } from "../../services/event-store.service";
-import { EventEditorComponent } from "./event-editor.component";
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { provideHttpClient } from '@angular/common/http';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { provideNoopAnimations } from '@angular/platform-browser/animations';
+import { ActivatedRoute, provideRouter } from '@angular/router';
+import { LocalStorageService } from '@global-service/localstorage/local-storage.service';
+import { provideMockStore } from '@ngrx/store/testing';
+import { of } from 'rxjs';
+import { EventsModule } from '../../events.module';
+import { EventStoreService } from '../../services/event-store.service';
+import { EventEditorComponent } from './event-editor.component';
 
 describe('EventEditorComponent', () => {
   let component: EventEditorComponent;
