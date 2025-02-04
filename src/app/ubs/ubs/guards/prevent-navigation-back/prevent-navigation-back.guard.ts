10964f64b5317dbd044f358c2ea62210b1ca7841
yuliia_karas <yuliiakaras03@gmail.com>
2025-02-04 12:52:05 +0200

    update deprecated functionality

--- src/app/ubs/ubs/guards/prevent-navigation-back/prevent-navigation-back.guard.ts
+++ src/app/ubs/ubs/guards/prevent-navigation-back/prevent-navigation-back.guard.ts
@@ -1,21 +1,16 @@
-import { Injectable } from '@angular/core';
+import { CanDeactivateFn } from '@angular/router';
+import { inject } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { CanDeactivate } from '@angular/router';
 import { ComponentCanDeactivate } from '@global-service/pending-changes-guard/pending-changes.guard';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class PreventNavigationBackGuard implements CanDeactivate<ComponentCanDeactivate> {
-  constructor(private dialog: MatDialog) {}
+export const PreventNavigationBackGuard: CanDeactivateFn<ComponentCanDeactivate> = () => {
+  const dialog = inject(MatDialog);
 
-  canDeactivate(component: ComponentCanDeactivate): boolean {
-    if (this.dialog.openDialogs.length) {
-      this.dialog.closeAll();
-      history.pushState(null, '');
-      return false;
-    } else {
-      return true;
-    }
+  if (dialog.openDialogs.length) {
+    dialog.closeAll();
+    history.pushState(null, '');
+    return false;
   }
-}
+
+  return true;
+};
