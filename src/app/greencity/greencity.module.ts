6edecd63930d8d594f3521d334b1822d7caf494d
yuliia_karas <yuliiakaras03@gmail.com>
2025-02-07 11:54:48 +0200

    fix home.module imports

--- src/app/greencity/greencity.module.ts
+++ src/app/greencity/greencity.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeModule } from './modules/home/home.module';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule]
+  imports: [CommonModule, HomeModule]
 })
 export class GreencityModule {}
--- src/app/greencity/modules/home/home.module.ts
+++ src/app/greencity/modules/home/home.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
-import { CoreModule } from 'src/app/main/component/core/core.module';
-import { HomepageComponent, EcoEventsComponent, StatRowComponent, StatRowsComponent, SubscribeComponent } from './components';
+import { CommonModule } from '@angular/common';
+import { CoreModule } from '../../../main/component/core/core.module';
+import { SharedMainModule } from '@shared/shared-main.module';
 import { EcoEventsItemComponent } from './components/eco-events/eco-events-item/eco-events-item.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 import { UnsubscribeComponent } from './components/unsubscribe/unsubscribe.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { EcoEventsComponent } from './components/eco-events/eco-events.component';
+import { StatRowComponent } from './components/stat-row/stat-row.component';
+import { StatRowsComponent } from './components/stat-rows/stat-rows.component';
+import { SubscribeComponent } from './components/subscribe/subscribe.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +21,7 @@ import { UnsubscribeComponent } from './components/unsubscribe/unsubscribe.compo
     EcoEventsItemComponent,
     UnsubscribeComponent
   ],
-  imports: [CoreModule],
+  imports: [CommonModule, CoreModule, SharedMainModule, SharedModule],
   exports: [HomepageComponent],
   providers: []
 })
