931b2287cea6c8c791e1522050715575b78b6d5e
yuliia_karas <yuliiakaras03@gmail.com>
2025-03-02 19:43:56 +0200

    update environments

---
+++ src/environments/environment.stage.ts
@@ -0,0 +1,20 @@
+export const environment = {
+  production: true,
+  enableUBS: true,
+  enableGreenCity: false,
+  apiKeys: 'AIzaSyDanBliCzAuCZrsq67FeKEs3vqAilUD_is',
+  apiMapKey: 'AIzaSyDaqjlemyxH1pLvF1EWfBDW2IPlgwJYXnU',
+  backendLink: 'https://greencity.testgreencity.ga/',
+  backendChatLink: 'https://greencity-chat.testgreencity.ga/',
+  backendUserLink: 'https://greencity-user.testgreencity.ga/',
+  backendUbsLink: 'https://greencity-ubs.testgreencity.ga',
+  frontendLink: 'https://www.testgreencity.ga/',
+  socket: 'https://greencity.testgreencity.ga/socket',
+  userSocket: 'https://greencity-user.greencity.cx.ua/socket', // new one: diff link
+  chatSocket: 'https://greencity-chat.testgreencity.ga/socket',
+  ubsAdmin: {
+    backendUbsAdminLink: 'https://greencity-ubs.testgreencity.ga/ubs'
+  },
+  googleClientId: '509138864931-6emuq5cv1rvqf33ivppod9jp8n24pjst.apps.googleusercontent.com',
+  agmCoreModuleApiKey: 'AIzaSyCdBtR8O8eazfWUSdyb5O2cnL32uJtWUwA'
+};
