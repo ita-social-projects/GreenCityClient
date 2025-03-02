dfd7a8bd6e3dda64c7189cc70aa55fcac6baad82
yuliia_karas <yuliiakaras03@gmail.com>
2025-03-02 19:43:56 +0200

    update environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,17 +3,18 @@ export const environment = {
   enableUBS: true,
   enableGreenCity: false,
   apiKeys: 'AIzaSyDanBliCzAuCZrsq67FeKEs3vqAilUD_is',
-  apiMapKey: 'AIzaSyDaqjlemyxH1pLvF1EWfBDW2IPlgwJYXnU',
+  apiMapKey: 'AIzaSyAUswsGJSIw2Z_SJ18xx7zsguZnaVfEdUo',
   backendLink: 'https://greencity.pick-up.city/',
   backendChatLink: 'https://greencity-chat.pick-up.city/',
   backendUserLink: 'https://greencity-user.pick-up.city/',
   backendUbsLink: 'https://greencity-ubs.pick-up.city',
   frontendLink: 'https://www.pick-up.city/',
   socket: 'https://greencity.pick-up.city/socket',
-  userSocket: 'https://greencity-user.greencity.cx.ua/socket',
+  userSocket: 'https://greencity-user.greencity.cx.ua/socket', // new one: diff link
   chatSocket: 'https://greencity-chat.pick-up.city/socket',
   ubsAdmin: {
     backendUbsAdminLink: 'https://greencity-ubs.pick-up.city/ubs'
   },
-  googleClientId: '129513550972-pg3ueh62gbde1jukmr49vem2rclde123.apps.googleusercontent.com'
+  googleClientId: '129513550972-pg3ueh62gbde1jukmr49vem2rclde123.apps.googleusercontent.com',
+  agmCoreModuleApiKey: 'AIzaSyC7q2v0VgRy60dAoItfv3IJhfJQEEoeqCI'
 };
