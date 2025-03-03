753e80916bbb499cc5b1da55f8c503b7433e74d8
yuliia_karas <yuliiakaras03@gmail.com>
2025-03-03 17:39:58 +0200

    update environments

--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -3,18 +3,17 @@ export const environment = {
   enableUBS: true,
   enableGreenCity: false,
   apiKeys: 'AIzaSyDanBliCzAuCZrsq67FeKEs3vqAilUD_is',
-  apiMapKey: 'AIzaSyDaqjlemyxH1pLvF1EWfBDW2IPlgwJYXnU',
-  backendLink: 'https://greencity.testgreencity.ga/',
-  backendChatLink: 'https://greencity-chat.testgreencity.ga/',
-  backendUserLink: 'https://greencity-user.testgreencity.ga/',
-  backendUbsLink: 'https://greencity-ubs.testgreencity.ga',
-  frontendLink: 'https://www.testgreencity.ga/',
-  socket: 'https://greencity.testgreencity.ga/socket',
-  userSocket: 'https://greencity-user.greencity.cx.ua/socket', // new one: diff link
-  chatSocket: 'https://greencity-chat.testgreencity.ga/socket',
+  apiMapKey: 'AIzaSyB3xs7Kczo46LFcQRFKPMdrE0lU4qsR_S4',
+  backendLink: 'https://greencity.greencity.cx.ua/',
+  backendChatLink: 'https://greencity-chat.greencity.cx.ua/',
+  backendUserLink: 'https://greencity-user.greencity.cx.ua/',
+  backendUbsLink: 'https://greencity-ubs.greencity.cx.ua',
+  frontendLink: 'https://www.greencity.cx.ua/',
+  socket: 'https://greencity.greencity.cx.ua/socket',
+  userSocket: 'https://greencity-user.greencity.cx.ua/socket',
+  chatSocket: 'https://greencity-chat.greencity.cx.ua/socket',
   ubsAdmin: {
-    backendUbsAdminLink: 'https://greencity-ubs.testgreencity.ga/ubs'
+    backendUbsAdminLink: 'https://greencity-ubs.greencity.cx.ua/ubs'
   },
-  googleClientId: '509138864931-6emuq5cv1rvqf33ivppod9jp8n24pjst.apps.googleusercontent.com',
-  agmCoreModuleApiKey: 'AIzaSyCdBtR8O8eazfWUSdyb5O2cnL32uJtWUwA'
+  googleClientId: '509138864931-6emuq5cv1rvqf33ivppod9jp8n24pjst.apps.googleusercontent.com'
 };
