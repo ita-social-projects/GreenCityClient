cdc9a5f35d34648ee6684dcf04e53455ba2de7d6
yuliia_karas <yuliiakaras03@gmail.com>
2025-03-28 11:46:42 +0200

    fix env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   enableUBS: true,
   enableGreenCity: false,
   apiKeys: 'AIzaSyDanBliCzAuCZrsq67FeKEs3vqAilUD_is',
-  apiMapKey: 'AIzaSyAUswsGJSIw2Z_SJ18xx7zsguZnaVfEdUo',
+  apiMapKey: 'AIzaSyDaqjlemyxH1pLvF1EWfBDW2IPlgwJYXnU',
   backendLink: 'https://greencity.pick-up.city/',
   backendChatLink: 'https://greencity-chat.pick-up.city/',
   backendUserLink: 'https://greencity-user.pick-up.city/',
@@ -12,9 +12,9 @@ export const environment = {
   socket: 'https://greencity.pick-up.city/socket',
   // TODO: change to pick-up if exists
   userSocket: 'https://greencity-user.greencity.cx.ua/socket',
-  chatSocket: 'https://greencity-chat.greencity.cx.ua/socket',
+  chatSocket: 'https://greencity-chat.pick-up.city/socket',
   ubsAdmin: {
-    backendUbsAdminLink: 'https://greencity-ubs.greencity.cx.ua/ubs'
+    backendUbsAdminLink: 'https://greencity-ubs.pick-up.city/ubs'
   },
   googleClientId: '129513550972-pg3ueh62gbde1jukmr49vem2rclde123.apps.googleusercontent.com'
 };
