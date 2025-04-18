53afc8958af368467d0f5a9e8c732682752fe717
IraHrechyn <142835111+IraHrechyn@users.noreply.github.com>
2025-04-18 11:15:48 +0300

    Update order.service.ts

--- src/app/ubs/ubs/services/order.service.ts
+++ src/app/ubs/ubs/services/order.service.ts
@@ -54,7 +54,6 @@ export class OrderService {
   }
 
   getOrderPdf(orderId: number, lang: string): Observable<Blob> {
-    console.log(orderId, lang);
     const url = `${this.url}/order/pdf/export`;
     const params = new HttpParams().set('id', orderId.toString()).set('lang', lang);
 
