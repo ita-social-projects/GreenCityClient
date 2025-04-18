e5b3db0cef9e89dbeea67b7db56253c78a2b143c
yuliia_karas <yuliiakaras03@gmail.com>
2025-04-18 13:24:06 +0300

    add table name serch, enhance styling

--- src/app/ubs/ubs-db-display/components/table-list/table-list.component.ts
+++ src/app/ubs/ubs-db-display/components/table-list/table-list.component.ts
@@ -8,4 +8,16 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 export class TableListComponent {
   @Input() tables: string[] = [];
   @Output() selectTable = new EventEmitter<string>();
+
+  searchTerm = '';
+  selectedTable: string | null = null;
+
+  get filteredTables(): string[] {
+    return this.tables.filter((table) => table.toLowerCase().includes(this.searchTerm.toLowerCase()));
+  }
+
+  onSelect(table: string): void {
+    this.selectedTable = table;
+    this.selectTable.emit(table);
+  }
 }
--- src/app/ubs/ubs-db-display/ubs-db-display.module.ts
+++ src/app/ubs/ubs-db-display/ubs-db-display.module.ts
@@ -4,9 +4,10 @@ import { DbTablesComponent } from './components/db-tables/db-tables.component';
 import { TableListComponent } from './components/table-list/table-list.component';
 import { TableViewComponent } from './components/table-view/table-view.component';
 import { UbsDbDisplayRoutingModule } from './ubs-db-display-routing.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [DbTablesComponent, TableListComponent, TableViewComponent],
-  imports: [CommonModule, UbsDbDisplayRoutingModule]
+  imports: [CommonModule, UbsDbDisplayRoutingModule, FormsModule]
 })
 export class UbsDbDisplayModule {}
