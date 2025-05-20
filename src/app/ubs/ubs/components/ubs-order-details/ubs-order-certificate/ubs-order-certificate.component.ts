f6bd2417704624e32c4f1557fcc2f8c7e17cf5c4
Andrew <andriy.kozovyi@gmail.com>
2025-05-20 20:13:16 +0300

    remode, as deactivated certificate

--- src/app/ubs/ubs/components/ubs-order-details/ubs-order-certificate/ubs-order-certificate.component.ts
+++ src/app/ubs/ubs/components/ubs-order-details/ubs-order-certificate/ubs-order-certificate.component.ts
@@ -122,9 +122,11 @@ export class UbsOrderCertificateComponent implements OnInit, OnDestroy {
 
   clearCertificates(): void {
     if (this.formArrayCertificates.length >= 0 && this.certificateSum > 0) {
+      const lastEnteredCertificate = this.certificates[0].code;
       for (let i = 0; i < this.formArrayCertificates.length; i++) {
         this.deleteCertificate(i);
       }
+      this.formArrayCertificates.at(0).setValue(lastEnteredCertificate);
     }
   }
 
