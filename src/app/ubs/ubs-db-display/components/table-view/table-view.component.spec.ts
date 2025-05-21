ec98b356d2092656062c131c6a7d639b27292910
yuliia_karas <yuliiakaras03@gmail.com>
2025-05-21 15:07:34 +0300

    cover with tests

--- src/app/ubs/ubs-db-display/components/table-view/table-view.component.spec.ts
+++ src/app/ubs/ubs-db-display/components/table-view/table-view.component.spec.ts
@@ -1,14 +1,42 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TableViewComponent } from './table-view.component';
+import { TableService } from '@ubs/ubs-db-display/services/table.service';
+import { of } from 'rxjs';
+import { TableDataResponse } from '@ubs/ubs-db-display/models/table.model';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 
 describe('TableViewComponent', () => {
   let component: TableViewComponent;
   let fixture: ComponentFixture<TableViewComponent>;
+  let mockTableService: jasmine.SpyObj<TableService>;
+
+  const mockResponsePage1: TableDataResponse = {
+    tableName: 'test-table',
+    currentPage: 0,
+    first: true,
+    hasNext: true,
+    hasPrevious: false,
+    last: false,
+    number: 0,
+    totalElements: 100,
+    totalPages: 2,
+    page: [{ id: '1', name: 'row1' }]
+  };
+
+  const mockResponsePage2: TableDataResponse = {
+    ...mockResponsePage1,
+    currentPage: 1,
+    page: [{ id: '2', name: 'row2' }],
+    hasNext: false
+  };
 
   beforeEach(() => {
+    mockTableService = jasmine.createSpyObj('TableService', ['getTableData']);
     TestBed.configureTestingModule({
-      declarations: [TableViewComponent]
+      declarations: [TableViewComponent],
+      imports: [InfiniteScrollModule],
+      providers: [{ provide: TableService, useValue: mockTableService }]
     });
     fixture = TestBed.createComponent(TableViewComponent);
     component = fixture.componentInstance;
@@ -17,5 +45,54 @@ describe('TableViewComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should load next page on scroll if hasNext is true and not loading', () => {
+    component.tableName = 'test-table';
+    component.tableDataResponse = {
+      ...mockResponsePage1,
+      currentPage: 0
+    };
+    component.isLoading = false;
+    mockTableService.getTableData.and.returnValue(of(mockResponsePage2));
+    component.onScroll();
+
+    expect(mockTableService.getTableData).toHaveBeenCalledWith('test-table', 1, 50);
+  });
+
+  it('should not call loadPage on scroll if already loading more', () => {
+    component.isLoading = true;
+    component.tableDataResponse = mockResponsePage1;
+
+    component.onScroll();
+
+    expect(mockTableService.getTableData).not.toHaveBeenCalled();
+  });
+
+  it('should not call loadPage if hasNext is false', () => {
+    component.isLoading = false;
+    component.tableDataResponse = {
+      ...mockResponsePage1,
+      hasNext: false
+    };
+
+    component.onScroll();
+
+    expect(mockTableService.getTableData).not.toHaveBeenCalled();
+  });
+
+  it('should set isLoadingMore properly when loading a non-initial page', () => {
+    component.tableName = 'test-table';
+    component.tableDataResponse = {
+      ...mockResponsePage1,
+      currentPage: 0,
+      hasNext: true
+    };
+
+    mockTableService.getTableData.and.returnValue(of(mockResponsePage2));
+    component.onScroll();
+
+    expect(component.isLoading).toBeFalse();
+    expect(component.tableDataResponse?.page.length).toBe(2);
   });
 });
