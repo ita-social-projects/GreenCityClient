b2ffa9ee053250bfeb12deb25a5d04eb3e11fd9a
yuliia_karas <yuliiakaras03@gmail.com>
2025-05-23 08:34:38 +0300

    add error handling

--- src/app/ubs/ubs-db-display/components/table-view/table-view.component.ts
+++ src/app/ubs/ubs-db-display/components/table-view/table-view.component.ts
@@ -1,18 +1,19 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy, SimpleChanges } from '@angular/core';
 import { TableDataResponse } from '@ubs/ubs-db-display/models/table.model';
 import { TableService } from '@ubs/ubs-db-display/services/table.service';
-import { finalize } from 'rxjs';
+import { catchError, finalize, Subject, takeUntil, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-table-view',
   templateUrl: './table-view.component.html',
   styleUrls: ['./table-view.component.scss']
 })
-export class TableViewComponent implements OnChanges {
+export class TableViewComponent implements OnChanges, OnDestroy {
   @Input() tableName: string | null = null;
   tableDataResponse: TableDataResponse | null = null;
   isLoading = false;
   pageLimit = 50;
+  private readonly destroy = new Subject<void>();
 
   constructor(private readonly tableService: TableService) {}
 
@@ -44,6 +45,11 @@ export class TableViewComponent implements OnChanges {
     this.tableService
       .getTableData(this.tableName, pageIndex, this.pageLimit)
       .pipe(
+        takeUntil(this.destroy),
+        catchError((error) => {
+          console.error('Error loading table data:', error);
+          return throwError(() => error);
+        }),
         finalize(() => {
           this.isLoading = false;
         })
@@ -60,5 +66,9 @@ export class TableViewComponent implements OnChanges {
     if (!this.isLoading && this.tableDataResponse.hasNext) {
       this.loadPage(this.tableDataResponse.currentPage + 1);
     }
+  }
+  ngOnDestroy() {
+    this.destroy.next();
+    this.destroy.complete();
   }
 }
