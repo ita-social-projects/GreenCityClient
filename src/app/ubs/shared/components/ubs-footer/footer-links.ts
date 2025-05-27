edd33f6927d363d8ed66d346af7c779a90c5a589
Tadei-Mykola <tmukola2006@gmail.com>
2025-05-27 15:34:37 +0300

    Delete greencity link from footer

--- src/app/ubs/shared/components/ubs-footer/footer-links.ts
+++ src/app/ubs/shared/components/ubs-footer/footer-links.ts
@@ -1,8 +1,7 @@
 export const ubsNavLinks = [
   { name: 'user.lower-nav-bar.sorting-rules', route: 'https://nowaste.com.ua/sort-station/', url: true },
   { name: 'footer.user-agreement', route: '/ubs/user-agreement', url: false },
-  { name: 'user.lower-nav-bar.eco-shop', route: 'https://shop.nowaste.com.ua/', url: true },
-  { name: 'Green City', route: '/greenCity', url: false }
+  { name: 'user.lower-nav-bar.eco-shop', route: 'https://shop.nowaste.com.ua/', url: true }
 ];
 
 export const socialLinks = [
