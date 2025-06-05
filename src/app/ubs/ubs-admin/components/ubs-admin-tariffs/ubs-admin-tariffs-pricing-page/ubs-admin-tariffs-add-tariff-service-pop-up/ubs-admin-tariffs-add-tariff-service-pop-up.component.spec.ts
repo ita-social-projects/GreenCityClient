1281766ba41f53cf9fdd4083cfd90c52cfd4bef3
Andrew <andriy.kozovyi@gmail.com>
2025-06-05 17:06:28 +0300

    coderabbit advice

--- src/app/ubs/ubs-admin/components/ubs-admin-tariffs/ubs-admin-tariffs-pricing-page/ubs-admin-tariffs-add-tariff-service-pop-up/ubs-admin-tariffs-add-tariff-service-pop-up.component.spec.ts
+++ src/app/ubs/ubs-admin/components/ubs-admin-tariffs/ubs-admin-tariffs-pricing-page/ubs-admin-tariffs-add-tariff-service-pop-up/ubs-admin-tariffs-add-tariff-service-pop-up.component.spec.ts
@@ -49,13 +49,13 @@ describe('UbsAdminTariffsAddTariffServicePopupComponent', () => {
     bagData: {
       capacity: 77,
       commission: 89,
-      description: 'Тест опис',
-      descriptionEng: 'Test descr',
+      descriptionUk: 'Тест опис',
+      descriptionEn: 'Test descr',
       fullPrice: 989,
       id: 20,
       limitIncluded: false,
-      name: 'Пластик',
-      nameEng: 'Plastic',
+      nameUk: 'Пластик',
+      nameEn: 'Plastic',
       price: 900
     },
     button: 'update'
