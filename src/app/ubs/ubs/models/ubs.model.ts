b9fba5f7e77340318308b00d6fd10162668627ae
Andrew <andriy.kozovyi@gmail.com>
2025-06-20 17:53:42 +0300

    order in district is important

--- src/app/ubs/ubs/models/ubs.model.ts
+++ src/app/ubs/ubs/models/ubs.model.ts
@@ -192,7 +192,7 @@ export class CAddressData {
   async setDistrict(place_id: string): Promise<void> {
     try {
       this.resetDistrict();
-      await this.setProperties('district', place_id, 'sublocality', 'administrative_area_level_2', 'locality');
+      await this.setProperties('district', place_id, 'locality', 'sublocality', 'administrative_area_level_2');
     } catch (error) {
       console.error('Error during setting district:', error);
     }
