9b489bf15448216bf2284a199def2937dba2a566
holus-bolus <ustinovoleksij@gmail.com>
2025-07-10 19:44:09 +0300

    Removed the unneccessary files

--- src/app/greencity/modules/chat/chat.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-chat',
-  templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.scss']
-})
-export class ChatComponent {}
