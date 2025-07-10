2a83ff057467098eb3bb0ac7cabb2f841c8398ea
holus-bolus <ustinovoleksij@gmail.com>
2025-07-10 19:34:25 +0300

    Changed this env

--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -1,20 +1,20 @@
 export const environment = {
-  production: false,
+  production: true,
   enableUBS: true,
   enableGreenCity: true,
   apiKeys: 'AIzaSyDanBliCzAuCZrsq67FeKEs3vqAilUD_is',
   apiMapKey: 'AIzaSyDaqjlemyxH1pLvF1EWfBDW2IPlgwJYXnU',
-  backendLink: 'http://localhost:8080/',
+  backendLink: 'https://greencity.greencity.cx.ua/',
   backendChatLink: 'https://greencity-chat.greencity.cx.ua/',
-  backendUserLink: 'http://localhost:8060/',
-  backendUbsLink: 'http://localhost:8050/',
+  backendUserLink: 'https://greencity-user.greencity.cx.ua/',
+  backendUbsLink: 'https://greencity-ubs.greencity.cx.ua',
   frontendLink: 'https://www.greencity.cx.ua/',
   socket: 'https://greencity.greencity.cx.ua/socket',
-  userSocket: 'http://localhost:8060/socket',
+  userSocket: 'https://greencity-user.greencity.cx.ua/socket',
   chatSocket: 'https://greencity-chat.greencity.cx.ua/socket',
   ubsAdmin: {
-    backendUbsAdminLink: 'http://localhost:8050/ubs'
+    backendUbsAdminLink: 'https://greencity-ubs.greencity.cx.ua/ubs'
   },
-  googleClientId: '129513550972-pg3ueh62gbde1jukmr49vem2rclde123.apps.googleusercontent.com',
-  agmCoreModuleApiKey: 'AIzaSyC7q2v0VgRy60dAoItfv3IJhfJQEEoeqCI'
+  googleClientId: '129513550972-eu9ej46rviv1ac8q14at62t2k5qon1pu.apps.googleusercontent.com',
+  agmCoreModuleApiKey: 'AIzaSyCdBtR8O8eazfWUSdyb5O2cnL32uJtWUwA'
 };
