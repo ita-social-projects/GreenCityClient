10c614864b081fa04ef969ec6ef0ae3ccc46219f
holus-bolus <ustinovoleksij@gmail.com>
2025-07-23 19:48:35 +0300

    Adjusted unit tests

--- src/app/chat/component/current-chat/current-chat.component.spec.ts
+++ src/app/chat/component/current-chat/current-chat.component.spec.ts
@@ -1,6 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TranslateService } from '@ngx-translate/core';
+import { of } from 'rxjs';
+import { Store } from '@ngrx/store';
 import { CurrentChatComponent } from './current-chat.component';
+
+class MockTranslateService {
+  get(key: any) {
+    return of(key);
+  }
+
+  instant(key: any) {
+    return key;
+  }
+
+  use(lang: string) {
+    return lang;
+  }
+}
+
+class MockStore {
+  select = jasmine.createSpy().and.returnValue(of({}));
+  dispatch = jasmine.createSpy();
+}
 
 describe('CurrentChatComponent', () => {
   let component: CurrentChatComponent;
@@ -8,7 +30,12 @@ describe('CurrentChatComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [CurrentChatComponent]
+      declarations: [CurrentChatComponent],
+      imports: [HttpClientTestingModule],
+      providers: [
+        { provide: TranslateService, useClass: MockTranslateService },
+        { provide: Store, useClass: MockStore }
+      ]
     }).compileComponents();
   });
 
