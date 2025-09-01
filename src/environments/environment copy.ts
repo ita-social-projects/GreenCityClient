7ba5cea5ee3f7b501313d5314718393f60358019
Andrew <andriy.kozovyi@gmail.com>
2025-09-01 17:09:43 +0300

    sockets fix for stage

--- src/environments/environment copy.ts
+++ src/environments/environment copy.ts
@@ -9,7 +9,7 @@ export const environment = {
   backendUserLink: 'http://localhost:8065/',
   backendUbsLink: 'http://localhost:8055/',
   frontendLink: 'http://localhost:4200/',
-  socket: 'https://greencity.greencity.cx.ua/socket',
+  socket: 'https://greencity-ubs.greencity.cx.ua/socket',
   userSocket: 'https://greencity-user.greencity.cx.ua/socket',
   chatSocket: 'https://greencity-chat.greencity.cx.ua/socket',
   ubsAdmin: {
--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -9,7 +9,7 @@ export const environment = {
   backendUserLink: 'https://greencity-user.greencity.cx.ua/',
   backendUbsLink: 'https://greencity-ubs.greencity.cx.ua',
   frontendLink: 'https://www.greencity.cx.ua/',
-  socket: 'https://greencity.greencity.cx.ua/socket',
+  socket: 'https://greencity-ubs.greencity.cx.ua/socket',
   userSocket: 'https://greencity-user.greencity.cx.ua/socket',
   chatSocket: 'https://greencity-chat.greencity.cx.ua/socket',
   ubsAdmin: {
