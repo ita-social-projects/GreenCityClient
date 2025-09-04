7c2252a09e67bac783c4a13f0022ccaffc0ed44d
Andrew <andriy.kozovyi@gmail.com>
2025-09-04 18:07:46 +0300

    reading env fix

--- scripts/setenv.ts
+++ scripts/setenv.ts
@@ -1,36 +1,27 @@
 const fs = require('fs');
 const envPath = require('path');
-const dotenv = require('dotenv');
 
 const envName = process.argv[2] || '';
-const envFilePath = envName ? `.env.${envName}` : '.env';
-
-const envConfig = dotenv.config({ path: envPath.resolve(process.cwd(), envFilePath) }).parsed;
-
-if (!envConfig) {
-  console.error(`Error: .env file not found at ${envFilePath}`);
-  process.exit(-1);
-}
 
 const environment = {
-  production: envConfig.PRODUCTION === 'true',
-  enableUBS: envConfig.ENABLE_UBS === 'true',
-  enableGreenCity: envConfig.ENABLE_GREENCITY === 'true',
-  apiKeys: envConfig.API_KEYS,
-  apiMapKey: envConfig.API_MAP_KEY,
-  backendLink: envConfig.BACKEND_LINK,
-  backendChatLink: envConfig.BACKEND_CHAT_LINK,
-  backendUserLink: envConfig.BACKEND_USER_LINK,
-  backendUbsLink: envConfig.BACKEND_UBS_LINK,
-  frontendLink: envConfig.FRONTEND_LINK,
-  socket: envConfig.SOCKET,
-  userSocket: envConfig.USER_SOCKET,
-  chatSocket: envConfig.CHAT_SOCKET,
+  production: process.env.PRODUCTION === 'true',
+  enableUBS: process.env.ENABLE_UBS === 'true',
+  enableGreenCity: process.env.ENABLE_GREENCITY === 'true',
+  apiKeys: process.env.API_KEYS,
+  apiMapKey: process.env.API_MAP_KEY,
+  backendLink: process.env.BACKEND_LINK,
+  backendChatLink: process.env.BACKEND_CHAT_LINK,
+  backendUserLink: process.env.BACKEND_USER_LINK,
+  backendUbsLink: process.env.BACKEND_UBS_LINK,
+  frontendLink: process.env.FRONTEND_LINK,
+  socket: process.env.SOCKET,
+  userSocket: process.env.USER_SOCKET,
+  chatSocket: process.env.CHAT_SOCKET,
   ubsAdmin: {
-    backendUbsAdminLink: envConfig.BACKEND_UBS_ADMIN_LINK
+    backendUbsAdminLink: process.env.BACKEND_UBS_ADMIN_LINK
   },
-  googleClientId: envConfig.GOOGLE_CLIENT_ID,
-  agmCoreModuleApiKey: envConfig.AGM_CORE_MODULE_API_KEY
+  googleClientId: process.env.GOOGLE_CLIENT_ID,
+  agmCoreModuleApiKey: process.env.AGM_CORE_MODULE_API_KEY
 };
 
 const environmentFileContent = `export const environment = ${JSON.stringify(environment, null, 2)};`;
@@ -48,3 +39,6 @@ console.log(`Successfully generated ${envPath.basename(environmentTsPath)}`);
 
 fs.writeFileSync(environmentStageTsPath, environmentFileContent);
 console.log(`Successfully generated ${envPath.basename(environmentStageTsPath)}`);
+
+console.log(`--- Content of ${envPath.basename(environmentTsPath)} ---`);
+console.log(fs.readFileSync(environmentTsPath, 'utf8'));
