7256c609a08f31eedd4514b79d4f35010ba2660b
Andrew <andriy.kozovyi@gmail.com>
2025-09-04 18:54:26 +0300

    fix

--- scripts/setenv.ts
+++ scripts/setenv.ts
@@ -5,49 +5,55 @@ const dotenv = require('dotenv');
 const envName = process.argv[2] || '';
 const envFile = envName ? `.env.${envName}` : '.env';
 
-const envFilePath = envPath.resolve(process.cwd(), envFile);
-const envConfig = dotenv.parse(fs.readFileSync(envFilePath));
+require('dotenv').config({ path: envPath.resolve(process.cwd(), envFile) });
 
-for (const k in envConfig) {
-  process.env[k] = envConfig[k];
-}
-
-const environment = {
-  production: process.env.PRODUCTION === 'true',
-  enableUBS: process.env.ENABLE_UBS === 'true',
-  enableGreenCity: process.env.ENABLE_GREENCITY === 'true',
-  apiKeys: process.env.API_KEYS,
-  apiMapKey: process.env.API_MAP_KEY,
-  backendLink: process.env.BACKEND_LINK,
-  backendChatLink: process.env.BACKEND_CHAT_LINK,
-  backendUserLink: process.env.BACKEND_USER_LINK,
-  backendUbsLink: process.env.BACKEND_UBS_LINK,
-  frontendLink: process.env.FRONTEND_LINK,
-  socket: process.env.SOCKET,
-  userSocket: process.env.USER_SOCKET,
-  chatSocket: process.env.CHAT_SOCKET,
+const environmentFileContent = `export const environment = {
+  production: ${process.env.PRODUCTION},
+  enableUBS: ${process.env.ENABLE_UBS},
+  enableGreenCity: ${process.env.ENABLE_GREENCITY},
+  apiKeys: '${process.env.API_KEYS}',
+  apiMapKey: '${process.env.API_MAP_KEY}',
+  backendLink: '${process.env.BACKEND_LINK}',
+  backendChatLink: '${process.env.BACKEND_CHAT_LINK}',
+  backendUserLink: '${process.env.BACKEND_USER_LINK}',
+  backendUbsLink: '${process.env.BACKEND_UBS_LINK}',
+  frontendLink: '${process.env.FRONTEND_LINK}',
+  socket: '${process.env.SOCKET}',
+  userSocket: '${process.env.USER_SOCKET}',
+  chatSocket: '${process.env.CHAT_SOCKET}',
   ubsAdmin: {
-    backendUbsAdminLink: process.env.BACKEND_UBS_ADMIN_LINK
+    backendUbsAdminLink: '${process.env.BACKEND_UBS_ADMIN_LINK}'
   },
-  googleClientId: process.env.GOOGLE_CLIENT_ID,
-  agmCoreModuleApiKey: process.env.AGM_CORE_MODULE_API_KEY
+  googleClientId: '${process.env.GOOGLE_CLIENT_ID}',
+  agmCoreModuleApiKey: '${process.env.AGM_CORE_MODULE_API_KEY}'
 };
-
-const environmentFileContent = `export const environment = ${JSON.stringify(environment, null, 2)};`;
+`;
 
 const environmentsDir = envPath.resolve(process.cwd(), './src/environments');
-const environmentTsPath = envPath.resolve(environmentsDir, `environment.ts`);
-const environmentStageTsPath = envPath.resolve(environmentsDir, `environment.${envName}.ts`);
+const environmentObligatoryPath = envPath.resolve(environmentsDir, `environment.ts`);
+const environmentPath = envPath.resolve(environmentsDir, `environment${envName ? `.${envName}` : ''}.ts`);
 
 if (!fs.existsSync(environmentsDir)) {
   fs.mkdirSync(environmentsDir, { recursive: true });
 }
 
-fs.writeFileSync(environmentTsPath, environmentFileContent);
-console.log(`Successfully generated ${envPath.basename(environmentTsPath)}`);
+fs.writeFile(environmentPath, environmentFileContent, (err) => {
+  if (err) {
+    console.error(err);
+    process.exit(-1);
+  }
+  console.log(`Successfully generated ${envPath.basename(environmentPath)}`);
+});
 
-fs.writeFileSync(environmentStageTsPath, environmentFileContent);
-console.log(`Successfully generated ${envPath.basename(environmentStageTsPath)}`);
+fs.writeFile(environmentObligatoryPath, environmentFileContent, (err) => {
+  if (err) {
+    console.error(err);
+    process.exit(-1);
+  }
+  console.log(`Successfully generated ${envPath.basename(environmentObligatoryPath)}`);
+});
 
-console.log(`--- Content of ${envPath.basename(environmentTsPath)} ---`);
-console.log(fs.readFileSync(environmentTsPath, 'utf8'));
+console.log(`--- Content of ${envPath.basename(environmentPath)} ---`);
+console.log(fs.readFileSync(environmentPath, 'utf8'));
+console.log(`--- Content of ${envPath.basename(environmentObligatoryPath)} ---`);
+console.log(fs.readFileSync(environmentObligatoryPath, 'utf8'));
