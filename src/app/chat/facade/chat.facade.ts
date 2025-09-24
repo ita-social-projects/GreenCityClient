cd61cc51db7c72784ad19b4f4a8810c312b675a6
anastasiakuznietsova <anastasiia.kuznietsova.shi.2023@lpnu.ua>
2025-09-24 18:24:42 +0300

    fixed sonar issue

--- src/app/chat/facade/chat.facade.ts
+++ src/app/chat/facade/chat.facade.ts
@@ -314,8 +314,8 @@ export class ChatFacade {
     this.api.sendMessage(sel.chatInternalId, text.trim(), file).subscribe({
       next: () => {
         const time = formatTimeOrDate(new Date().toISOString());
-        const imagePreview = file && file.type?.startsWith('image/') ? URL.createObjectURL(file) : null;
-        const filePreview = file && !file.type?.startsWith('image/') ? URL.createObjectURL(file) : null;
+        const imagePreview = file?.type?.startsWith('image/') ? URL.createObjectURL(file) : null;
+        const filePreview = file && !imagePreview ? URL.createObjectURL(file) : null;
 
         sel.messages.push({
           from: 'Me',
