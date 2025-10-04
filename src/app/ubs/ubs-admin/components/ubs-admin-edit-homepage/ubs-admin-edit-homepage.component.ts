c1399431684399517d0d8ed9b9360750c4cb4422
viktor <yu.viktor31@gmail.com>
2025-10-05 00:24:04 +0300

    feat: implement OnInit in ubs-admin-edit-homepage component

--- src/app/ubs/ubs-admin/components/ubs-admin-edit-homepage/ubs-admin-edit-homepage.component.ts
+++ src/app/ubs/ubs-admin/components/ubs-admin-edit-homepage/ubs-admin-edit-homepage.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { AdminUserAgreementService } from '@ubs/ubs-admin/services/admin-homepage-settings/admin-homepage-settings.service';
 import { THomepageContent } from '@ubs/ubs-admin/models/homepage-settings.interface';
 
@@ -7,7 +7,7 @@ import { THomepageContent } from '@ubs/ubs-admin/models/homepage-settings.interf
   templateUrl: './ubs-admin-edit-homepage.component.html',
   styleUrls: ['./ubs-admin-edit-homepage.component.scss']
 })
-export class UbsAdminEditHomepageComponent {
+export class UbsAdminEditHomepageComponent implements OnInit {
   private adminHomepageSettingsService: AdminUserAgreementService = inject(AdminUserAgreementService);
 
   homepageContent: THomepageContent;
