833d15182c9e8aec7216085dd83d412c68617972
witolDark <witalikspelina@gmail.com>
2025-10-24 16:36:46 +0300

    tests fix

--- src/app/ubs/ubs-admin/components/ubs-admin-order-status/ubs-admin-order-status.component.spec.ts
+++ src/app/ubs/ubs-admin/components/ubs-admin-order-status/ubs-admin-order-status.component.spec.ts
@@ -1,12 +1,11 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { TranslateModule } from '@ngx-translate/core';
 import { MatDialog } from '@angular/material/dialog';
-import { FormGroup, FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { of, Subject } from 'rxjs';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { of } from 'rxjs';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { GeneralInfoMock } from '../../services/orderInfoMock';
 import { OrderService } from '../../services/order.service';
-import { AddOrderCancellationReasonComponent } from '../add-order-cancellation-reason/add-order-cancellation-reason.component';
 import { UbsAdminOrderStatusComponent } from './ubs-admin-order-status.component';
 import { LanguageService } from 'src/app/shared/i18n/language.service';
 import { OrderStatus, PaymnetStatus } from 'src/app/ubs/ubs/order-status.enum';
@@ -18,7 +17,13 @@ describe('UbsAdminOrderStatusComponent', () => {
   let fixture: ComponentFixture<UbsAdminOrderStatusComponent>;
 
   const OrderServiceFake = {
-    getAvailableOrderStatuses: jasmine.createSpy('getAvailableOrderStatuses')
+    getAvailableOrderStatuses: jasmine.createSpy('getAvailableOrderStatuses'),
+    getNotTakenOutReason: jasmine.createSpy('getNotTakenOutReason').and.returnValue(
+      of({
+        description: 'desc',
+        images: ['src']
+      })
+    )
   };
 
   const matDialogMock = jasmine.createSpyObj('matDialog', ['open']);
