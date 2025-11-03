feba3b661e495e6a57573a441bab828d757dd057
witolDark <witalikspelina@gmail.com>
2025-11-03 16:38:04 +0200

    feat: added chat id column to customers table

--- src/app/ubs/ubs-admin/components/ubs-admin-customers/columnsParams.mock.ts
+++ src/app/ubs/ubs-admin/components/ubs-admin-customers/columnsParams.mock.ts
@@ -65,11 +65,19 @@ export const columnsParams: ColumnParam[] = [
   },
   {
     title: {
-      key: 'chatId',
+      key: 'toChat',
       uk: 'Чат з клієнтом',
       en: 'Client chat'
     },
     width: 90
+  },
+  {
+    title: {
+      key: 'chatId',
+      uk: 'ID чату',
+      en: 'Chat ID'
+    },
+    width: 35
   },
   {
     title: {
