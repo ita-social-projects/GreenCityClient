a7a22092f2a27374061772d1c7f7ec6511a05625
witolDark <witalikspelina@gmail.com>
2025-11-05 19:15:29 +0200

    #9178

--- src/app/greencity/shared/pipes/date-localisation-pipe/date-localisation.pipe.ts
+++ src/app/greencity/shared/pipes/date-localisation-pipe/date-localisation.pipe.ts
@@ -1,4 +1,4 @@
-import { OnDestroy, Pipe, PipeTransform } from '@angular/core';
+import { ChangeDetectorRef, NgZone, OnDestroy, Pipe, PipeTransform } from '@angular/core';
 import { DatePipe } from '@angular/common';
 import { takeUntil } from 'rxjs/operators';
 import { Subject } from 'rxjs';
@@ -6,6 +6,7 @@ import { TranslateService } from '@ngx-translate/core';
 
 @Pipe({
   name: 'dateLocalisation',
+  standalone: true,
   pure: false
 })
 export class DateLocalisationPipe implements PipeTransform, OnDestroy {
@@ -14,13 +15,18 @@ export class DateLocalisationPipe implements PipeTransform, OnDestroy {
 
   constructor(
     private readonly translate: TranslateService,
-    private readonly datePipe: DatePipe
+    private readonly datePipe: DatePipe,
+    private readonly cdr: ChangeDetectorRef
   ) {
     this.locale = this.translate.getDefaultLang() || 'en';
-    this.translate.onDefaultLangChange.pipe(takeUntil(this.destroy$)).subscribe((langObj) => (this.locale = langObj.lang));
+    this.translate.onDefaultLangChange.pipe(takeUntil(this.destroy$)).subscribe((langObj) => {
+      this.locale = langObj.lang;
+      this.cdr.markForCheck();
+    });
   }
 
   transform(value: any, format = 'mediumDate'): string {
+    console.log(this.datePipe.transform(value, format, undefined, this.locale));
     return this.datePipe.transform(value, format, undefined, this.locale);
   }
 
