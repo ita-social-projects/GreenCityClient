dd6ac3d79349786ffc77c72928c994a9f7bb7bc9
witoldDark <witalikspelina@gmail.com>
2025-12-23 20:42:32 +0200

    pick up service popup tests

--- src/app/ubs/ubs/components/ubs-pick-up-service-pop-up/ubs-pick-up-service-pop-up.component.spec.ts
+++ src/app/ubs/ubs/components/ubs-pick-up-service-pop-up/ubs-pick-up-service-pop-up.component.spec.ts
@@ -7,6 +7,9 @@ import { Store } from '@ngrx/store';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { of } from 'rxjs';
 import { Component } from '@angular/core';
+import { GetOrderDetails } from '../../../../store/actions/order.actions';
+import { OrderDetails } from '@ubs/ubs/models/ubs.interface';
+import { Language } from '../../../../shared/i18n/Language';
 
 @Component({
   selector: 'app-spinner',
@@ -17,30 +20,58 @@ export class MockSpinnerComponent {}
 describe('UbsPickUpServicePopUpComponent', () => {
   let component: UbsPickUpServicePopUpComponent;
   let fixture: ComponentFixture<UbsPickUpServicePopUpComponent>;
+  let storeSpy: jasmine.SpyObj<Store>;
+  let orderServiceSpy: jasmine.SpyObj<OrderService>;
+  let localStorageServiceSpy: jasmine.SpyObj<LocalStorageService>;
 
-  const mockOrderService = {
-    getAllActiveCouriers: jasmine.createSpy().and.returnValue(of([])),
-    getLocations: jasmine.createSpy().and.returnValue(of({ allActiveLocationsDtos: [] })),
-    getLocationName: jasmine.createSpy().and.callFake((city, region) => `${region.regionName} - ${city.name}`)
-  };
-
-  const mockLocalStorageService = {
-    getCurrentLanguage: jasmine.createSpy().and.returnValue('en')
-  };
-
-  const mockStore = {
-    dispatch: jasmine.createSpy(),
-    select: jasmine.createSpy()
-  };
+  const mockCouriers = [
+    {
+      courierId: 1,
+      courierStatus: 'ACTIVE',
+      nameUk: 'UBS',
+      nameEn: 'УБС',
+      createDate: '2025-12-23T12:00:00Z',
+      createdBy: 'admin'
+    },
+    {
+      courierId: 2,
+      courierStatus: 'INACTIVE',
+      nameUk: 'Кур’єр Львів',
+      nameEn: 'Lviv Courier',
+      createDate: '2025-12-22T09:30:00Z',
+      createdBy: 'admin'
+    }
+  ];
+  const mockTariffs = [
+    { id: 10, tariffNameEn: 'Tariff1', tariffNameUk: 'Тариф1' },
+    { id: 11, tariffNameEn: 'Tariff2', tariffNameUk: 'Тариф2' }
+  ];
+  const mockOrderDetails: OrderDetails = { bags: [{ id: 1, nameEn: 'Bag 1', quantity: 1 }], points: 100 } as OrderDetails;
 
   beforeEach(waitForAsync(() => {
+    orderServiceSpy = jasmine.createSpyObj('OrderService', [
+      'getAllActiveCouriers',
+      'getActiveTariffsInfo',
+      'getLocationName',
+      'getTariffName'
+    ]);
+    localStorageServiceSpy = jasmine.createSpyObj('LocalStorageService', ['getCurrentLanguage', 'getTariffId']);
+    storeSpy = jasmine.createSpyObj('Store', ['dispatch', 'select']);
+
+    orderServiceSpy.getAllActiveCouriers.and.returnValue(of(mockCouriers));
+    orderServiceSpy.getActiveTariffsInfo.and.returnValue(of(mockTariffs));
+    orderServiceSpy.getLocationName.and.callFake((city, region) => `${region} - ${city}`);
+    localStorageServiceSpy.getCurrentLanguage.and.returnValue(Language.EN);
+    localStorageServiceSpy.getTariffId.and.returnValue(mockTariffs[0].id);
+    storeSpy.select.and.returnValue(of(mockOrderDetails));
+
     TestBed.configureTestingModule({
       imports: [TranslateModule.forRoot(), ReactiveFormsModule],
       declarations: [UbsPickUpServicePopUpComponent, MockSpinnerComponent],
       providers: [
-        { provide: OrderService, useValue: mockOrderService },
-        { provide: LocalStorageService, useValue: mockLocalStorageService },
-        { provide: Store, useValue: mockStore }
+        { provide: OrderService, useValue: orderServiceSpy },
+        { provide: LocalStorageService, useValue: localStorageServiceSpy },
+        { provide: Store, useValue: storeSpy }
       ]
     }).compileComponents();
   }));
@@ -52,7 +83,39 @@ describe('UbsPickUpServicePopUpComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create component', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should set currentLanguage on init', () => {
+    component.ngOnInit();
+    expect(localStorageServiceSpy.getCurrentLanguage).toHaveBeenCalled();
+    expect(component.currentLanguage).toBe('en');
+  });
+
+  it('should call getActiveTariffs and set myControl value', fakeAsync(() => {
+    component.ngOnInit();
+    tick();
+    expect(orderServiceSpy.getAllActiveCouriers).toHaveBeenCalled();
+    expect(orderServiceSpy.getActiveTariffsInfo).toHaveBeenCalled();
+    expect(component.tariffs).toEqual(mockTariffs);
+    expect(component.myControl.value).toEqual(mockTariffs[0]);
+  }));
+
+  it('should dispatch GetOrderDetails on myControl value change', fakeAsync(() => {
+    component.ngOnInit();
+    tick();
+    const tariff = mockTariffs[1];
+    component.myControl.setValue(tariff);
+    tick();
+    expect(storeSpy.dispatch).toHaveBeenCalledWith(GetOrderDetails({ tariffId: tariff.id }));
+  }));
+
+  it('should set bags from orderDetailsSelector', fakeAsync(() => {
+    component.ngOnInit();
+    tick();
+    component.myControl.setValue(mockTariffs[0]);
+    tick();
+    expect(component.bags).toEqual(mockOrderDetails.bags);
+  }));
 });
