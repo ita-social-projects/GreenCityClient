5a1e1719cb7af975b0438ba9251d49f2024789bd
witoldDark <witalikspelina@gmail.com>
2026-03-10 18:21:53 +0200

    #9449 fixed phone pattern

--- src/assets/patterns/patterns.ts
+++ src/assets/patterns/patterns.ts
@@ -56,7 +56,7 @@ export const Masks = {
   certificateMask: '0000-0000',
   ecoStoreMask: '00000000',
   servicesMask: '000',
-  phoneMask: '{+380} (00) 000 00 00'
+  phoneMask: '{+38\\0} (00) 000 00 00'
 };
 
 export const phonePrefix = '+380';
